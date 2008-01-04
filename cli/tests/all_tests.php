29b9ad7ea3f65fed85da7da84a7701b45e174c8d
Mark Donohoe <mark.donohoe@hp.com>
2008-01-04 20:50:57 +0000

    minor fixes and new tests
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@49 a5643f74-2041-0410-a2b9-94ffef304a07

--- cli/tests/all_tests.php
+++ cli/tests/all_tests.php
@@ -3,13 +3,18 @@
 
 /**
  * Wrapper to run all tests
+ * 
+ * @todo figure out where the test archives should be stored and 
+ * installed.
 */
 // Standard requires for any test. 
 require_once('/usr/local/simpletest/unit_tester.php');
 require_once('/usr/local/simpletest/reporter.php');
 
-$test = &new TestSuite('All CLI tests');
-$test->addTestFile('1stest.php');
+$test = &new TestSuite('All tests for cp2foss');
+// parameters tests valid and invalid inputs
+$test->addTestFile('parameters.php');
+$test->addTestFile('desc.php');
 $test->run(new TextReporter());
 
 ?>
