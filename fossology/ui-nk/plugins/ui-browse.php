9c5f50ac54f2c4970b0467a24652537d3eb3b566
Neal Krawetz <krawetz.neal@hp.com>
2008-02-04 23:32:07 +0000

    Added "detail" view to browse in new ui.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@119 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/ui-nk/plugins/ui-browse.php
+++ fossology/ui-nk/plugins/ui-browse.php
@@ -35,7 +35,7 @@ class ui_browse extends Plugin
    ShowItem(): Given a upload_pk, list every item in it.
    If it is an individual file, then list the file contents.
    ***********************************************************/
-  function ShowItem($Upload,$Item)
+  function ShowItem($Upload,$Item,$Show)
     {
     global $Plugins;
     $V="";
@@ -46,17 +46,19 @@ class ui_browse extends Plugin
 
     /* Grab the directory */
     $Results = DirGetList($Upload,$Item);
+    $ShowSomething=0;
 
-    $V .= "<table width='100%' border=0 class='text'>\n";
+    $V .= "<table class='text' border=0 cellpadding=0>\n";
     foreach($Results as $Row)
       {
       if (empty($Row['uploadtree_pk'])) { continue; }
+      $ShowSomething=1;
       $View = NULL;
       $Download = NULL;
       $Link = NULL;
       $Meta = NULL;
       $Name = $Row['ufile_name'];
-      $V .= "<tr><td>";
+      $V .= "<tr>";
 
       /* Check for children */
       $Children = DirGetList($Upload,$Row['uploadtree_pk']);
@@ -69,9 +71,11 @@ class ui_browse extends Plugin
 
       /* Scan for meta data */
       $HasRealChildren=0;
+      $CountChildren=0;
       foreach($Children as $C)
         {
 	if (empty($C['ufile_name'])) { continue; }
+        $CountChildren++;
 	if (Isartifact($C['ufile_mode']))
 	  {
 	  if (!Isdir($C['ufile_mode']))
@@ -83,19 +87,35 @@ class ui_browse extends Plugin
 	} /* foreach Children */
 
       /* Set the traversal link */
-      if (count($Children) > 0) // if is directory
+      if ($CountChildren > 0) // if is directory
 	{
 	if ($HasRealChildren)
 	  {
-	  $Link = $Uri . "&upload=$Upload&item=" . $Row['uploadtree_pk'];
+	  $Link = $Uri . "&show=$Show&upload=$Upload&item=" . $Row['uploadtree_pk'];
 	  }
 	else
 	  {
-	  $Link = $Uri . "&upload=$Upload&item=" . DirGetNonArtifact($Row['uploadtree_pk']);
+	  $Link = $Uri . "&show=$Show&upload=$Upload&item=" . DirGetNonArtifact($Row['uploadtree_pk']);
 	  }
 	}
 
-      /* Display it */
+      /* Show details children */
+      if ($Show == 'detail')
+        {
+	$V .= "<td>" . DirMode2String($Row['ufile_mode']) . "</td>";
+	$V .= "<td>&nbsp;&nbsp;" . substr($Row['ufile_mtime'],0,19) . "</td>";
+	if (!empty($Row['pfile_size']))
+	  {
+	  $V .= "<td align=right>&nbsp;&nbsp;" . number_format($Row['pfile_size'], 0, "", ",") . "&nbsp;&nbsp;</td>";
+	  }
+	else
+	  {
+	  $V .= "<td align=right>&nbsp;&nbsp;&nbsp;&nbsp;</td>";
+	  }
+	}
+
+      /* Display item */
+      $V .= "<td>";
       if (!empty($Link)) /* if it's a directory */
 	{
 	$V .= "<a href='$Link'><b>$Name/</b></a>\n";
@@ -112,14 +132,16 @@ class ui_browse extends Plugin
       if (!empty($ModDownload) && !empty($Meta)) { $V .= "[<a href='$Meta'>Meta</a>] "; }
       $V .= "</td></tr>\n";
       } /* foreach($Results as $Row) */
+
     $V .= "</table>\n";
+    if (!$ShowSomething) { $V .= "<b>No files</b>\n"; }
     return($V);
     } // ShowItem()
 
   /***********************************************************
    ShowFolder(): Given a Folder_pk, list every upload in the folder.
    ***********************************************************/
-  function ShowFolder($Folder)
+  function ShowFolder($Folder,$Show)
     {
     global $Plugins;
     $V="";
@@ -159,14 +181,16 @@ class ui_browse extends Plugin
     $Folder = GetParm("folder",PARM_INTEGER);
     $Upload = GetParm("upload",PARM_INTEGER);
     $Item = GetParm("item",PARM_INTEGER);
-    switch(GetParm("show",PARM_INTEGER))
+    $Uri = Traceback_uri() . "?mod=" . $this->Name;
+
+    switch(GetParm("show",PARM_STRING))
 	{
 	case 'detail':
 		$Show='detail';
 		break;
-	case 'name':
+	case 'summary':
 	default:
-		$Show='name';
+		$Show='summary';
 		break;
 	}
 
@@ -177,20 +201,32 @@ class ui_browse extends Plugin
       case "HTML":
 	$V .= "<style type='text/css'>\n";
 	$V .= ".text { height:24px; font:normal 10pt verdana, arial, helvetica; }\n";
+	$V .= ".dir { height:24px; font:normal 10pt verdana, arial, helvetica; border: thin black; border-style: none none dotted none; }\n";
 	$V .= "a { text-decoration:none; }\n";
 	$V .= "div { padding:0; margin:0; }\n";
 	$V .= "</style>\n";
+
+	/* Create the micro-menu */
+        $V .= "<div align=right><small>";
+	$Opt = "";
+	if ($Folder) { $Opt .= "&folder=$Folder"; }
+	if ($Upload) { $Opt .= "&upload=$Upload"; }
+	if ($Item) { $Opt .= "&item=$Item"; }
+        if ($Show == 'detail') { $V .= "<a href='$Uri&show=summary$Opt'>Summary</a> | "; }
+        else { $V .= "<a href='$Uri&show=detail$Opt'>Detail</a> | "; }
+        $V .= "<a href='" . Traceback() . "'>Refresh</a>";
+        $V .= "</small></div>\n";
 
 	$V .= "<font class='text'>\n";
 
 	/* Get the folder description */
 	if (!empty($Folder))
 	  {
-	  $V .= $this->ShowFolder($Folder);
+	  $V .= $this->ShowFolder($Folder,$Show);
 	  }
 	if (!empty($Upload))
 	  {
-	  $V .= $this->ShowItem($Upload,$Item);
+	  $V .= $this->ShowItem($Upload,$Item,$Show);
 	  }
 
 	/* Display the browse */
