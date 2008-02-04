d8b5f6af86b5fd716751f2035fdc31a838a977f6
Neal Krawetz <krawetz.neal@hp.com>
2008-02-04 17:32:03 +0000

    Adding "Browse" plugin to new UI.  NOTE: it is not done yet.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@114 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/ui-nk/common/common-folders.php
@@ -0,0 +1,147 @@
+<?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************/
+
+/*************************************************
+ Restrict usage: Every PHP file should have this
+ at the very beginning.
+ This prevents hacking attempts.
+ *************************************************/
+global $GlobalReady;
+if (!isset($GlobalReady)) { exit; }
+
+/************************************************************
+ FolderGetTop(): Find the top-of-tree folder_pk for the current user.
+ TBD: "username" will be added in the future and it may change
+ how this function works.
+ ************************************************************/
+function FolderGetTop()
+{
+  global $Plugins;
+  $DB = &$Plugins[plugin_find_id("db")];
+  if (empty($DB)) { return; }
+  if (!empty($username)) { $Where = "WHERE user_name='$username'"; }
+  else { $Where = ""; }
+  /* Get the list of folders */
+  $Results = $DB->Action("SELECT root_folder_fk FROM users $Where ORDER BY user_pk ASC LIMIT 1;");
+  $Row = $Results[0];
+  return($Row['root_folder_fk']);
+} // FolderGetTop()
+
+/***********************************************************
+ FolderListOption(): Create the tree, using OPTION tags.
+ The caller must already have created the FORM and SELECT tags.
+ It returns the full HTML.
+ This is recursive!
+ NOTE: If there is a recursive loop in the folder table, then
+ this will loop INFINITELY.
+ ***********************************************************/
+function FolderListOption($ParentFolder,$Depth)
+  {
+  global $Plugins;
+  if ($ParentFolder == "-1") { $ParentFolder = FolderGetTop(); }
+  if (empty($ParentFolder)) { return; }
+  $DB = &$Plugins[plugin_find_id("db")];
+  if (empty($DB)) { return; }
+  $V="";
+  $V .= "<option value='$ParentFolder'>\n";
+  if ($Depth != 0) { $V .= "&nbsp;&nbsp;"; }
+  for($i=1; $i < $Depth; $i++) { $V .= "&nbsp;&nbsp;"; }
+
+  /* Load this folder's name */
+  $Results = $DB->Action("SELECT folder_name FROM folder WHERE folder_pk=$ParentFolder LIMIT 1;");
+  $Name = trim($Results[0]['folder_name']);
+  if ($Name == "") { $Name = "[default]"; }
+
+  /* Load any subfolders */
+  $Results = $DB->Action("SELECT folder_pk FROM leftnav WHERE parent=$ParentFolder AND folder_pk IS NOT NULL ORDER BY name;");
+  /* Now create the HTML */
+  $V .= htmlentities($Name);
+  $V .= "</option>\n";
+  if (isset($Results[0]['folder_pk']))
+    {
+    $Hide="";
+    if ($Depth > 0) { $Hide = "style='display:none;'"; }
+    $V .= "<div id='TreeDiv-$ParentFolder' $Hide>\n";
+    foreach($Results as $R)
+	{
+	$V .= FolderListOption($R['folder_pk'],$Depth+1);
+	}
+    $V .= "</div>\n";
+    }
+  return($V);
+} // FolderListOption()
+
+/***********************************************************
+ FolderListDiv(): Create the tree, using DIVs.
+ It returns the full HTML.
+ This is recursive!
+ NOTE: If there is a recursive loop in the folder table, then
+ this will loop INFINITELY.
+ ***********************************************************/
+function FolderListDiv($ParentFolder,$Depth)
+  {
+  global $Plugins;
+  if ($ParentFolder == "-1") { $ParentFolder = FolderGetTop(); }
+  if (empty($ParentFolder)) { return; }
+  $DB = &$Plugins[plugin_find_id("db")];
+  if (empty($DB)) { return; }
+  $Browse = &$Plugins[plugin_find_id("browse")];
+  $Uri = Traceback_uri();
+  $V="";
+
+  if ($Depth != 0) { $V .= "<font color='white'>+&nbsp;</font>"; }
+  for($i=1; $i < $Depth; $i++) { $V .= "<font class='item' color='white'>+&nbsp;</font>"; }
+
+  /* Load this folder's name */
+  $Results = $DB->Action("SELECT folder_name FROM folder WHERE folder_pk=$ParentFolder LIMIT 1;");
+  $Name = trim($Results[0]['folder_name']);
+  if ($Name == "") { $Name = "[default]"; }
+
+  /* Load any subfolders */
+  $Results = $DB->Action("SELECT folder_pk FROM leftnav WHERE parent=$ParentFolder AND folder_pk IS NOT NULL ORDER BY name;");
+  /* Now create the HTML */
+  if ($Depth > 0) { $V .= "<font class='item'>"; }
+  else { $V .= "<font>"; }
+  if (isset($Results[0]['folder_pk']))
+    {
+    $V .= '<a href="javascript:ShowHide(' . "'TreeDiv-$ParentFolder'" . ')"><font color="blue">+</font></a>';
+    }
+  else
+    {
+    $V .= "<font color='gray'>&ndash;</font>";
+    }
+  $V .= "&nbsp;</font>";
+  if (!empty($Browse)) { $V .= "<a target='basenav' href='$Uri?mod=browse&folder=$ParentFolder'>"; }
+  $V .= htmlentities($Name);
+  if (!empty($Browse)) { $V .= "</a>"; }
+  $V .= "<br>\n";
+  if (isset($Results[0]['folder_pk']))
+    {
+    $Hide="";
+    if ($Depth > 0) { $Hide = "style='display:none;'"; }
+    $V .= "<div id='TreeDiv-$ParentFolder' $Hide>\n";
+    foreach($Results as $R)
+      {
+      $V .= FolderListDiv($R['folder_pk'],$Depth+1);
+      }
+    $V .= "</div>\n";
+    }
+  return($V);
+} /* FolderListDiv() */
+
+?>
