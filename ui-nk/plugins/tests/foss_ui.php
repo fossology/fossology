630d755427cd7553e9451c71976a9e99979b4293
Mark Donohoe <mark.donohoe@hp.com>
2008-02-06 17:54:36 +0000

    added copyright
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@136 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui-nk/plugins/tests/foss_ui.php
+++ ui-nk/plugins/tests/foss_ui.php
@@ -1,6 +1,22 @@
 #!/usr/bin/php
 
 <?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************/
 /*
  *  first try at testing web interface...
  */
@@ -8,6 +24,7 @@
 class TestRepoLogin extends WebTestCase{
 
   function testLogin(){
+    $this->useProxy('http://web-proxy.fc.hp.com:8088', 'web-proxy', '');
     $this->assertTrue($this->get('http://repo.fossology.org/'));
     $this->assertAuthentication('Basic');
     $this->authenticate('fossology', 'OeyiW5go');
@@ -17,6 +34,7 @@ class TestRepoLogin extends WebTestCase{
 class TestAboutMenu extends WebTestCase {
   
   function testMenuAbout(){
+    $this->useProxy('http://web-proxy.fc.hp.com:8088', 'web-proxy', '');
     $this->assertTrue($this->get('http://repo.fossology.org/'));
     $this->assertAuthentication('Basic');
     $this->authenticate('fossology', 'OeyiW5go');
@@ -33,9 +51,9 @@ class TestAboutMenu extends WebTestCase {
 class TestToolsMenu extends WebTestCase {
   
   function testMenuTools(){
-    $this->assertTrue($this->get('http://osrb-1.fc.hp.com:3128/~markd/ui-md/'));
-    $this->assertAuthentication('Basic');
-    $this->authenticate('fossology', 'OeyiW5go');
+    $this->assertTrue($this->get('http://osrb-1.fc.hp.com/~markd/ui-md/'));
+    //$this->assertAuthentication('Basic');
+    //$this->authenticate('fossology', 'OeyiW5go');
     $this->assertText('Software Repository');
     
     $this->click('Tools');
@@ -46,9 +64,9 @@ class TestToolsMenu extends WebTestCase {
 class TestAdminFolder extends WebTestCase {
   
   function testFolderCreate(){
-    $this->assertTrue($this->get('http://osrb-1.fc.hp.com:3128/~markd/ui-md/'));
-    $this->assertAuthentication('Basic');
-    $this->authenticate('fossology', 'OeyiW5go');
+    $this->assertTrue($this->get('http://osrb-1.fc.hp.com/~markd/ui-md/'));
+    //$this->assertAuthentication('Basic');
+    //$this->authenticate('fossology', 'OeyiW5go');
     $this->assertText('Software Repository');
     
     $this->click('Admin');
