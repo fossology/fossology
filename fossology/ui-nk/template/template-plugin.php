d74ba5527c3d890f90247a33b2fc62c35fecccc1
Neal Krawetz <krawetz.neal@hp.com>
2008-02-12 23:06:08 +0000

    Changed location for DTD in the template.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@168 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/ui-nk/template/template-plugin.php
+++ fossology/ui-nk/template/template-plugin.php
@@ -255,7 +255,7 @@ class Plugin
 	$V = "<html>\n";
 	$V .= "<head>\n";
 	/* DOCTYPE is required for IE to use styles! (else: css menu breaks) */
-	$V .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">' . "\n";
+	$V .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "xhtml1-frameset.dtd">' . "\n";
 	// $V .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' . "\n";
 	// $V .= '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Loose//EN" "http://www.w3.org/TR/html4/loose.dtd">' . "\n";
 	if (!empty($Title)) { $V .= "<title>" . htmlentities($Title) . "</title>\n"; }
