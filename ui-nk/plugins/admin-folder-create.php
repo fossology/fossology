6fae9e186efdbeb4a9f42f365f04cc92caddb20f
Mark Donohoe <mark.donohoe@hp.com>
2008-02-13 04:17:23 +0000

    code reviewed, fixed dup folder defect
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@169 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui-nk/plugins/admin-folder-create.php
+++ ui-nk/plugins/admin-folder-create.php
@@ -14,10 +14,10 @@
  You should have received a copy of the GNU General Public License along
  with this program; if not, write to the Free Software Foundation, Inc.,
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-***********************************************************/
+ ***********************************************************/
 
 /**
- * @version "$Id$" 
+ * @version "$Id$"
  */
 
 /*************************************************
@@ -29,25 +29,30 @@ global $GlobalReady;
 if (!isset($GlobalReady)) { exit; }
 
 class folder_create extends Plugin
-  {
-  var $Type=PLUGIN_UI;
-  var $Name="folder_create";
-  public $Title = "Create a new Fossology folder";
-  var $Version="1.0";
-  var $MenuList="Organize::Folders::Create";
-  var $Dependency=array("db");
+{
+  public $Type       = PLUGIN_UI;
+  public $Name       = "folder_create";
+  public $Title      = "Create a new Fossology folder";
+  public $Version    = "1.0";
+  public $MenuList   = "Organize::Folders::Create";
+  public $Dependency = array("db");
 
-  /*********************************************
-   Create(): Given a parent folder ID, a name and description,
-   create the named folder under the parent.
-   Includes idiot checking since the input comes from stdin.
-   Returns: 1 if created, 0 if failed.
-   *********************************************/
+  /**
+   * Create(): Given a parent folder ID, a name and description,
+   * create the named folder under the parent.
+   *
+   * Includes idiot checking since the input comes from stdin.
+   * 
+   * @param int    $ParentId
+   * @param string $NewFolder
+   * @param string $Desc
+   * @return 1 if created, 0 if failed
+   */
   function Create($ParentId, $NewFolder, $Desc)
-    {
+  {
     global $Plugins;
     global $DB;
-    
+
     /* Check the name */
     $NewFolder = trim($NewFolder);
     if (empty($NewFolder)) { return(0); }
@@ -58,13 +63,13 @@ class folder_create extends Plugin
     if ($Row['folder_pk'] != $ParentId) { return(0); }
 
     // folder name exists under the parent?
-    $Sql = "SELECT * FROM leftnav WHERE parent = '$ParentId' AND foldercontents_mode = '1';";
-    $Results = $DB->Action("SELECT * FROM folder WHERE folder_pk = '$ParentId';");
-    $Row = $Results[0];
-    if ($Row['name'] == $NewFolder) { return(0); }
+    $Sql = "SELECT * FROM leftnav WHERE name = '$NewFolder' AND 
+    			parent = '$ParentId' AND foldercontents_mode = '1';";
+    $Results = $DB->Action($Sql);
+    if ($Results[0]['name'] == $NewFolder) { return(0); } 
 
-    /* Create the folder 
-     ** Block SQL injection by protecting single quotes 
+    /* Create the folder
+     * Block SQL injection by protecting single quotes
      *
      * Protect the folder name with htmlentities.
      */
@@ -77,63 +82,63 @@ class folder_create extends Plugin
 
     $DB->Action("INSERT INTO foldercontents (parent_fk,foldercontents_mode,child_id) VALUES ('$ParentId','1','$FolderPk');");
     return(1);
-    } // Create()
+  } // Create()
 
   /*********************************************
    Output(): Generate the text for this plugin.
    *********************************************/
   function Output()
-    {
+  {
     if ($this->State != PLUGIN_STATE_READY) { return; }
     $V="";
     switch($this->OutputType)
-      {
+    {
       case "XML":
-	break;
+        break;
       case "HTML":
-	$V .= "<H1>$this->Title</H1>\n";
-	/* If this is a POST, then process the request. */
-	$ParentId = GetParm('parentid',PARM_INTEGER);
-	$NewFolder = GetParm('newname',PARM_TEXT);
-	$Desc = GetParm('description',PARM_TEXT);
-	if (!empty($ParentId) && !empty($NewFolder))
-	  {
-	  $rc = $this->Create($ParentId, $NewFolder, $Desc);
-	  if ($rc==1)
-	    {
-	    /* Need to refresh the screen */
-	    $V .= "<script language='javascript'>\n";
-	    $V .= "alert('Folder $NewFolder Created')\n";
-	    $Uri = Traceback_uri() . "?mod=refresh&remod=" . $this->Name;
-	    $V .= "window.open('$Uri','_top');\n";
-	    $V .= "</script>\n";
-	    }
-	  }
-	/* Display the form */
-	$V .= "<form method='post'>\n"; // no url = this url
-	$V .= "<ol>\n";
-	$V .= "<li>Select the parent folder:  \n";
-	$V .= "<select name='parentid'>\n";
-	$V .= FolderListOption(-1,0);
-	$V .= "</select><P />\n";
-	$V .= "<li>Enter the new folder name:  \n";
-	$V .= "<INPUT type='text' name='newname' size=40 />\n<br>";
-	$V .= "<br><li>Enter a meaningful description:  \n";
-	$V .= "<INPUT type='text' name='description' size=80 />\n";
-	$V .= "</ol>\n";
-	$V .= "<input type='submit' value='Create!'>\n";
-	$V .= "</form>\n";
-	break;
+        $V .= "<H1>$this->Title</H1>\n";
+        /* If this is a POST, then process the request. */
+        $ParentId = GetParm('parentid',PARM_INTEGER);
+        $NewFolder = GetParm('newname',PARM_TEXT);
+        $Desc = GetParm('description',PARM_TEXT);
+        if (!empty($ParentId) && !empty($NewFolder))
+        {
+          $rc = $this->Create($ParentId, $NewFolder, $Desc);
+          if ($rc==1)
+          {
+            /* Need to refresh the screen */
+            $V .= "<script language='javascript'>\n";
+            $V .= "alert('Folder $NewFolder Created')\n";
+            $Uri = Traceback_uri() . "?mod=refresh&remod=" . $this->Name;
+            $V .= "window.open('$Uri','_top');\n";
+            $V .= "</script>\n";
+          }
+        }
+        /* Display the form */
+        $V .= "<form method='post'>\n"; // no url = this url
+        $V .= "<ol>\n";
+        $V .= "<li>Select the parent folder:  \n";
+        $V .= "<select name='parentid'>\n";
+        $V .= FolderListOption(-1,0);
+        $V .= "</select><P />\n";
+        $V .= "<li>Enter the new folder name:  \n";
+        $V .= "<INPUT type='text' name='newname' size=40 />\n<br>";
+        $V .= "<br><li>Enter a meaningful description:  \n";
+        $V .= "<INPUT type='text' name='description' size=80 />\n";
+        $V .= "</ol>\n";
+        $V .= "<input type='submit' value='Create!'>\n";
+        $V .= "</form>\n";
+        break;
       case "Text":
-	break;
+        break;
       default:
-	break;
-      }
+        break;
+    }
     if (!$this->OutputToStdout) { return($V); }
     print("$V");
     return;
-    }
-  };
+  }
+};
 $NewPlugin = new folder_create;
 $NewPlugin->Initialize();
 ?>
