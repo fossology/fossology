718199e70a3807b4b885590be4dce210fd357089
Neal Krawetz <krawetz.neal@hp.com>
2008-03-03 19:24:32 +0000

    New UI: TBD plugin to show all of the current TBDs.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@242 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/ui-nk/plugins/TBD.php
@@ -0,0 +1,57 @@
+<?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+
+/*************************************************
+ Restrict usage: Every PHP file should have this
+ at the very beginning.
+ This prevents hacking attempts.
+ *************************************************/
+global $GlobalReady;
+if (!isset($GlobalReady)) { exit; }
+
+class tbd extends Plugin
+{
+  public $Name       = "TBD";
+  public $Version    = "1.0";
+  public $Dependency = array("db");
+  public $DBaccess   = PLUGIN_DB_WRITE;
+
+  function RegisterMenus()
+    {
+    menu_insert("Main::Search::Advanced (TBD)");
+    menu_insert("Main::Organize::Uploads::Edit Properties (TBD)");
+    menu_insert("Main::Organize::Uploads::Move Folder (TBD)");
+    menu_insert("Main::Tasks::Analyze::Code Compare (TBD)");
+    menu_insert("Main::Admin::Dashboard (TBD)");
+    menu_insert("Main::Admin::Scheduler::Stop (TBD)");
+    menu_insert("Main::Admin::Scheduler::Start (TBD)");
+    menu_insert("Main::Admin::Scheduler::Kill Job (TBD)");
+    menu_insert("Main::Admin::Scheduler::Update (TBD)");
+    menu_insert("Main::Admin::Scheduler::Log (TBD)");
+    menu_insert("Main::Admin::Jobs::Pause (TBD)");
+    menu_insert("Main::Admin::Jobs::Abort (TBD)");
+    menu_insert("Main::Admin::Jobs::Reset (TBD)");
+    menu_insert("Main::Admin::Jobs::Edit Properties (TBD)");
+    menu_insert("Main::Admin::Database::View Table (TBD)");
+    menu_insert("Main::Admin::Database::Stats (TBD)");
+    menu_insert("Main::Upload::From Mount (TBD)");
+    }
+};
+$NewPlugin = new tbd;
+$NewPlugin->Initialize();
+?>
