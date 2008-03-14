e33a537336f3a3203c2caedacddf25faba1af013
Neal Krawetz <krawetz.neal@hp.com>
2008-03-14 20:00:50 +0000

    New UI: Fixed login issue for disabling stuff.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@296 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/ui-nk/plugins/core-init.php
+++ fossology/ui-nk/plugins/core-init.php
@@ -31,7 +31,7 @@ class core_init extends Plugin
   var $Version    = "1.0";
   var $MenuList   = "Admin::Initialize";
   var $Dependency = array("db","auth","refresh","menus","Default");
-  var $DBaccess   = PLUGIN_DB_USERADMIN;
+  var $DBaccess   = PLUGIN_DB_NONE;
   var $PluginLevel= 100; /* make this run first! */
 
   /******************************************
@@ -61,7 +61,7 @@ class core_init extends Plugin
 	if ($P->State == PLUGIN_STATE_READY) { continue; }
 	if ($P->DBaccess == PLUGIN_DB_DEBUG) { continue; }
 	$Key = array_search($P->Name,$this->Dependency);
-	if (($Key === FALSE) && ($P->Name != $this->Name))
+	if (($Key === FALSE) && strcmp($P->Name,$this->Name))
 	  {
 	  // print "Disable " . $P->Name . " as $Key <br>\n";
 	  $P->Destroy();
@@ -73,7 +73,7 @@ class core_init extends Plugin
 	  }
 	}
     $this->State = PLUGIN_STATE_READY;
-    if (!empty($_SESSION['UserId']) && ($this->MenuList !== ""))
+    if (($_SESSION['UserLevel'] >= PLUGIN_DB_USERADMIN) && ($this->MenuList !== ""))
 	{
 	menu_insert("Main::" . $this->MenuList,$this->MenuOrder,$this->Name,$this->MenuTarget);
 	}
