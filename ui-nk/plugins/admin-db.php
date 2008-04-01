452aad831f96b86472b35fbc89146454d2795572
Neal Krawetz <krawetz.neal@hp.com>
2008-04-01 14:34:50 +0000

    New UI: Added "Details" to DB check.  Now I can see exactly what is unreferenced.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@386 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui-nk/plugins/admin-db.php
+++ ui-nk/plugins/admin-db.php
@@ -82,10 +82,12 @@ class admin_db_cleanup extends FO_Plugin
 	$Checks[$i]['tag']   = "bad_upload_pfile";
 	$Checks[$i]['label'] = "Uploads missing pfiles";
 	$Checks[$i]['sql']   = "FROM upload WHERE ufile_fk IN (SELECT ufile_pk FROM ufile WHERE pfile_fk IS NULL);";
+	$Checks[$i]['list']  = "SELECT ufile_name AS list FROM upload INNER JOIN ufile ON ufile_fk = ufile_pk WHERE ufile_fk IN (SELECT ufile_pk FROM ufile WHERE pfile_fk IS NULL);";
 	$i++;
 	$Checks[$i]['tag']   = "unreferenced_ufile";
 	$Checks[$i]['label'] = "Unreferenced ufiles";
 	$Checks[$i]['sql']   = "FROM ufile WHERE ufile_pk NOT IN (SELECT ufile_fk FROM upload) AND ufile_pk NOT IN (SELECT ufile_fk FROM uploadtree);";
+	$Checks[$i]['list']   = "SELECT ufile_name AS list FROM ufile WHERE ufile_pk NOT IN (SELECT ufile_fk FROM upload) AND ufile_pk NOT IN (SELECT ufile_fk FROM uploadtree);";
 	$i++;
 
 // bobg: not possible due to referential integrity constraint
@@ -117,6 +119,7 @@ class admin_db_cleanup extends FO_Plugin
 	$Checks[$i]['tag']   = "unreferenced_folder";
 	$Checks[$i]['label'] = "Unreferenced folders";
 	$Checks[$i]['sql']   = "FROM folder WHERE folder_pk NOT IN (SELECT child_id FROM foldercontents WHERE foldercontents_mode = 1) AND folder_pk != '1';";
+	$Checks[$i]['list']  = "SELECT folder_name AS list FROM folder WHERE folder_pk NOT IN (SELECT child_id FROM foldercontents WHERE foldercontents_mode = 1) AND folder_pk != '1';";
 
 	/* Check for anything to fix */
         $Args=0;
@@ -138,6 +141,25 @@ class admin_db_cleanup extends FO_Plugin
 	$V .= " or ufile entries that are not linked by any uploadtree records.";
 	$V .= " Inconsistencies usually arise due to failed unpacking, partial deletions, and testing.\n";
 
+	$V .= "<script language='javascript'>\n";
+	$V .= "<!--\n";
+	$V .= "function ShowHideDBDetails(name)\n";
+	$V .= "  {\n";
+	$V .= "  if (name.length < 1) { return; }\n";
+	$V .= "  var Element, State;\n";
+	$V .= "  if (document.getElementById) // standard\n";
+	$V .= "    { Element = document.getElementById(name); }\n";
+	$V .= "  else if (document.all) // IE 4, 5, beta 6\n";
+	$V .= "    { Element = document.all[name]; }\n";
+	$V .= "  else // if (document.layers) // Netscape 4 and older\n";
+	$V .= "    { Element = document.layers[name]; }\n";
+	$V .= "  State = Element.style;\n";
+	$V .= "  if (State.display == 'none') { State.display='block'; }\n";
+	$V .= "  else { State.display='none'; }\n";
+	$V .= "  }\n";
+	$V .= "// -->\n";
+	$V .= "</script>\n";
+
 	$Results = $DB->Action("SELECT COUNT(*) AS count FROM jobqueue WHERE (jq_type = 'unpack' OR jq_type = 'delagent') AND jq_starttime IS NOT NULL AND jq_endtime IS NULL;");
 	$Count = $Results[0]['count'];
 	if ($Count == 1) { $Verb = "is"; $String = "task"; }
@@ -153,8 +175,8 @@ class admin_db_cleanup extends FO_Plugin
 
 	$V .= "<P>The following inconsistencies have been identified:\n";
 	$V .= "<form method='POST'>";
-	$V .= "<table border=1>\n";
-	$V .= "<tr><th>Fix</th><th>Type of Inconsistency</th><th>Count</th></tr>\n";
+	$V .= "<table border=1 width='100%'>\n";
+	$V .= "<tr><th width='5%'>Fix</th><th width='80%'>Type of Inconsistency</th><th>Count</th></tr>\n";
 
 	$FixCount=0;
 	for($i=0; !empty($Checks[$i]['tag']); $i++)
@@ -164,12 +186,25 @@ class admin_db_cleanup extends FO_Plugin
 	  $V .= "<tr>";
 	  if ($Count > 0)
 	    {
-	    $V .= "<td><input type='checkbox' name='" . $Checks[$i]['tag'] . "' value='1'></td>";
+	    $V .= "<td valign='top'><input type='checkbox' name='" . $Checks[$i]['tag'] . "' value='1'></td>";
 	    $FixCount++;
 	    }
 	  else { $V .= "<td></td>"; }
-	  $V .= "<td>" . $Checks[$i]['label'] . "</td>";
-	  $V .= "<td align='right'>" . number_format($Count,0,"",",") . "</td></tr>\n";
+	  $V .= "<td valign='top'>" . $Checks[$i]['label'];
+	  if (!empty($Checks[$i]['list']) && ($Count > 0))
+	    {
+	    $V .= " (<a href=\"javascript:ShowHideDBDetails('Details_$i')\">Details</a>)<br>\n";
+	    $V .= "<div id='Details_$i' style='display:none;'>";
+	    $Results = $DB->Action($Checks[$i]['list']);
+	    for($j=0; !empty($Results[$j]['list']); $j++)
+	      {
+	      if ($j > 0) { $V .= "<br>\n"; }
+	      $V .= ($j+1) . ": " . htmlentities($Results[$j]['list']);
+	      }
+	    $V .= "</div>\n";
+	    }
+	  $V .= "</td>";
+	  $V .= "<td align='right' valign='top'>" . number_format($Count,0,"",",") . "</td></tr>\n";
 	  }
 
 	$V .= "</table>\n";
