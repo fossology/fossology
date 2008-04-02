01bddde87c14c7d9f2b5a04b5179110e16879385
Mark Donohoe <mark.donohoe@hp.com>
2008-04-02 23:43:01 +0000

    Updated tests to be more flexible and match the install process.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@427 a5643f74-2041-0410-a2b9-94ffef304a07

--- cli/tests/parameters.php
+++ cli/tests/parameters.php
@@ -4,48 +4,59 @@
  * Parameters, test -h, -p, -n, -a input parameters to cp2foss.
  * 
  * NOTE: test version of cp2foss should be installed to /usr/local/bin as
- * cp2foss.test.
+ * test.cp2foss
  * 
  * @version "$Id: $"
 */
 
+
 class TestCLInputCP2foss extends UnitTestCase {
+
+  public $command = '/usr/local/bin/test.cp2foss';
   
   function Testhnpa(){
-    $help = exec('/usr/local/bin/cp2foss.test -h', $output, $retval);
+    
+    global $command;
+    $help = exec('$command -h', $output, $retval);
     //print_r($output);
     $this->assertPattern('/Usage: cp2foss/', $output[1]);
     $output = array();
-    $error = exec('/usr/local/bin/cp2foss.test -n foo -a /bar/baz -d "a comment"', $output, $retval);
+    $error = exec('$command -n foo -a /bar/baz -d "a comment"', $output, $retval);
     //print_r($output);
     $this->assertPattern('/ERROR, -p /', $output[1]);
     $output = array();
-    $error = exec('/usr/local/bin/cp2foss.test -p foo -a /bar/baz -d "a comment"', $output, $retval);
+    $error = exec('$command -p foo -a /bar/baz -d "a comment"', $output, $retval);
     //print_r($output);
     $this->assertPattern('/ERROR, -n /', $output[1]);
     $output = array();
-    $error = exec('/usr/local/bin/cp2foss.test -p baz -n foo -d "a comment"', $output, $retval);
+    $error = exec('$command -p baz -n foo -d "a comment"', $output, $retval);
     //print_r($output);
     $this->assertPattern('/ERROR, -a /', $output[1]);
   }
   
   function MissingDashP(){
+    
+    global $command;
     // Note you must have a valid archive 
-    $error = exec('/usr/local/bin/cp2foss.test -n foo -a /bar/baz -d "a comment"', $output, $retval);
+    $error = exec('$command -n foo -a /tmp/zlib.tar.bz2 -d "a comment"', $output, $retval);
     print_r($output);
     $this->assertPattern('/ERROR, -p /', $output[1]);
   }
   
   function MissingDashN(){
+    
+    global $command;
     // Note you must have a valid archive 
-    $error = exec('/usr/local/bin/cp2foss.test -p foo -a /bar/baz -d "a comment"', $output, $retval);
+    $error = exec('$command -p foo -a /tmp/zlib.tar.bz2 -d "a comment"', $output, $retval);
     print_r($output);
     $this->assertPattern('/ERROR, -n /', $output[1]);
   }
   
   function MissingDasha(){
+    
+    global $command;
     // Note you must have a valid archive 
-    $error = exec('/usr/local/bin/cp2foss.test -p baz -n foo -d "a comment"', $output, $retval);
+    $error = exec('$command -p baz -n foo -d "a comment"', $output, $retval);
     print_r($output);
     $this->assertPattern('/ERROR, -a /', $output[1]);
   }
