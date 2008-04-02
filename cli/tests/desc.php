15dbfe425b74895e0151acaa3afd529ce9774622
Mark Donohoe <mark.donohoe@hp.com>
2008-04-02 23:42:42 +0000

    Updated tests to be more flexible and match the install process.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@426 a5643f74-2041-0410-a2b9-94ffef304a07

--- cli/tests/desc.php
+++ cli/tests/desc.php
@@ -5,17 +5,21 @@
  */
 class TestCP2foss extends UnitTestCase {
   
+  public $command = '/usr/local/bin/test.cp2foss';
+  
   function TestDesc(){
+    
+    global $command;
+    
     $output = array();
-    $error = exec('/usr/local/bin/cp2foss -p baz -n foo -a ~/fedora/Fedora8-pkgs/zsh.tar.bz2 -d "a comment"', $output, $retval);
+    $error = exec("$command -p baz -n foo -a /tmp/zip.tar.bz2 -d \"a comment\"", $output, $retval);
     //print_r($output);
     $this->assertPattern('/Working on /', $output[1]);
     $output = array();
     // No description specified
-    $error = exec('/usr/local/bin/cp2foss -p foo -n Bar -a ~/fedora/Fedora8-pkgs/zsh.tar.bz2', $output, $retval);
+    $error = exec("$command -p foo -n Bar -a /tmp/zip.tar.bz2", $output, $retval);
     //print_r($output);
     $this->assertPattern('/ERROR, -d /', $output[1]);
   }
 }
-
 ?>
