bebc7d094bd63d35012132f2501a471d248a2c75
Mark Donohoe <mark.donohoe@hp.com>
2008-04-03 15:58:56 +0000

    Added copyright.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@435 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/cli/tests/parameters.php
+++ fossology/cli/tests/parameters.php
@@ -1,5 +1,20 @@
 <?php
+/***********************************************************
+ Copyright (C) 2007 Hewlett-Packard Development Company, L.P.
 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
 /**
  * Parameters, test -h, -p, -n, -a input parameters to cp2foss.
  *
@@ -59,6 +74,4 @@ class TestCLInputCP2foss extends UnitTestCase {
     $this->assertPattern('/ERROR, -a /', $output[0]);
   }
 }
-
-
 ?>
