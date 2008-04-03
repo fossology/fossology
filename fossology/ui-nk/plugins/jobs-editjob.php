d040ff37190deb577f60d5683928224fc03ea4d6
Neal Krawetz <krawetz.neal@hp.com>
2008-04-03 20:25:09 +0000

    New UI: Adding jobs-editjob.  (Seems I never added this.)
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@442 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/ui-nk/plugins/jobs-editjob.php
@@ -0,0 +1,69 @@
+<?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************/
+
+/*************************************************
+ Restrict usage: Every PHP file should have this
+ at the very beginning.
+ This prevents hacking attempts.
+ *************************************************/
+global $GlobalReady;
+if (!isset($GlobalReady)) { exit; }
+
+class job_reset extends FO_Plugin
+  {
+  var $Name       = "job_reset";
+  var $Title      = "Reset Job";
+  var $Version    = "1.0";
+  // var $MenuList   = "Organize::Jobs::Edit"; // No menu
+  var $DBaccess   = PLUGIN_DB_ANALYZE;
+
+  /*****************************************************
+   SelectJob(): Create a list of jobs to use.
+   *****************************************************/
+  function SelectJob()
+    {
+    } // SelectJob()
+
+  /*****************************************************
+   Output(): Generate output for this plugin.
+   *****************************************************/
+  function Output()
+    {
+    if ($this->State != PLUGIN_STATE_READY) { return; }
+    $V="";
+    switch($this->OutputType)
+      {
+      case "XML":
+	break;
+      case "HTML":
+	$V .= $this->SelectJob();
+	break;
+      case "Text":
+	break;
+      default:
+	break;
+      }
+    if (!$this->OutputToStdout) { return($V); }
+    print($V);
+    return;
+    } // Output()
+
+  };
+$NewPlugin = new job_reset;
+$NewPlugin->Initialize();
+?>
