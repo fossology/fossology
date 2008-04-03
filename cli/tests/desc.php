273123748a33a3527a49496b6150ff610bdf6d74
Mark Donohoe <mark.donohoe@hp.com>
2008-04-03 17:18:43 +0000

    now use attribute for the command to run
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@439 a5643f74-2041-0410-a2b9-94ffef304a07

--- cli/tests/desc.php
+++ cli/tests/desc.php
@@ -21,18 +21,17 @@
  */
 class TestCP2foss extends UnitTestCase {
   
+  public $command = '/usr/local/bin/test.cp2foss';
 
   function TestDesc(){
     
-    $command = '/usr/local/bin/test.cp2foss';
-    
     $output = array();
-    $error = exec("$command -p baz -n foo -a /tmp/zlib.tar.bz2 -d \"a comment\"", $output, $retval);
+    $error = exec("$this->command -p baz -n foo -a /tmp/zlib.tar.bz2 -d \"a comment\"", $output, $retval);
     //print_r($output);
     $this->assertPattern('/Working on /', $output[0]);
     $output = array();
     // No description specified
-    $error = exec("$command -p foo -n Bar -a /tmp/zlib.tar.bz2", $output, $retval);
+    $error = exec("$this->command -p foo -n Bar -a /tmp/zlib.tar.bz2", $output, $retval);
     //print_r($output);
     $this->assertPattern('/ERROR, -d /', $output[0]);
   }
