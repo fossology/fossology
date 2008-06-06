0b53960363186498682ab48c6c226ec9b2256f29
Neal Krawetz <krawetz.neal@hp.com>
2008-06-06 16:40:25 +0000

    3 fixes: upload-url now places the URL in the name area.  browse now shows the uploaded URL location (oops).  agent-license-once now uses licinspect.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@685 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui/plugins/ui-browse.php
+++ ui/plugins/ui-browse.php
@@ -232,8 +232,8 @@ class ui_browse extends FO_Plugin
           $V .= "<a href='$Uri&upload=$UploadPk&folder=$Folder&show=$Show'>";
           $V .= $Name . "/";
           $V .= "</a>";
-	  if ($Row['upload_mode'] & 1<<2) { $V .= "<br>Added by file upload: " . htmlentities($Row['upload_filename']); }
-	  if ($Row['upload_mode'] & 1<<3) { $V .= "<br>Added by URL: " . htmlentities($Row['upload_filename']); }
+	  if ($Row['upload_mode'] & 1<<2) { $V .= "<br>Added by URL: " . htmlentities($Row['upload_filename']); }
+	  if ($Row['upload_mode'] & 1<<3) { $V .= "<br>Added by file upload: " . htmlentities($Row['upload_filename']); }
 	  if ($Row['upload_mode'] & 1<<4) { $V .= "<br>Added from filesystem: " . htmlentities($Row['upload_filename']); }
 	  $V .= "<br>" . $Desc;
           $V .= "<br>Contains $ItemCount ";
