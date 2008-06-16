a616a8fb1ebc33114bcb3941892516bd6752d342
Mark Donohoe <mark.donohoe@hp.com>
2008-06-16 17:27:30 +0000

    removed proxys
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@743 a5643f74-2041-0410-a2b9-94ffef304a07

--- utils/fedora/get-fsrc.php
+++ utils/fedora/get-fsrc.php
@@ -1,4 +1,5 @@
 #!/usr/bin/php
+<?php
 /***********************************************************
  get-fsrc.php
  Copyright (C) 2007 Hewlett-Packard Development Company, L.P.
@@ -16,8 +17,7 @@
  with this program; if not, write to the Free Software Foundation, Inc.,
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  ***********************************************************/
- 
-<?php
+
 /**
  * Get the fedora sources with cvs.  Then use make prep to really get all
  * the sources.  As the initial co in cvs only gets patches.
@@ -26,85 +26,87 @@
  *       placed MUST be big.
  *
  * @param string $out-path output path where the sources will be checked out.
- * @param string $path-dead-pkg-list fully qualified path to file with list
- * of dead packages to skip when processing.
- * 
+ * @param string $path-skip-pkg-list fully qualified path to file with list
+ * of skipped packages to skip when processing further.
+ *
  * @version "$Id$"
  *
  */
 
-# Steps:
-#
-# 1. establish cvs root and set proxy in the environment
-#     -> Make sure that set -o noclobber is set in environment.
-# 2. cd to destination
-# 3. cvs co
-# 4. filter for dead packages or packages with no .spec files
-#    - make a file of each for use in next step or just keep in memory?
-#    - if no dead package file exists, process all.
-# 5. Using fileter output, make prep for each package.
+/*
+ * Steps:
+ * 1. establish cvs root in the environment
+ *     -> Make sure that set -o noclobber is set in environment.
+ * 2. cd to destination
+ * 3. cvs co
+ * 4. filter for dead packages or packages with no .spec files
+ *    - skip processing them, and use a file in the current directory to store them.
+ * 5. make prep for each package.
+ *
+ */
 
-# Paramters: path where sources will be placed
-#            filepath where list of dead packages is
-#
-#            e.g. -o path -d path2dead
-# $1 should be the path to packages?
-# for now we just hardcode it:
+$path = '/usr/local/fossology/agents';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
-require_once("./pathinclude.h.php");
-require_once("$LIBDIR/libcp2foss.h.php");
+require_once("pathinclude.h.php");
+global $WEBDIR;
+require_once("$WEBDIR/common/common-cli.php");
 
-// Sad, but this just doesn't work!
-//$_ENV['http_proxy'] = 'http://web-proxy.fc.hp.com:8088';
-//$_ENV['CVSROOT'] = ':pserver:anonymous@cvs.fedoraproject.org:/cvs/pkgs';
+$usage = "get-fsrc [-h] -s <skip-path> -o <output-path>\n";
 
-# sirius path....
-//$fedora='/home/fossy/repository/fawkes.rags/Fedora-8/devel';
-# fawkes path....
-$fedora='/home/repository/fawkes.rags/FT/devel';
-
-$usage = <<< USAGE
-
-get-fsrc [-h] -d <dead-path> -o <output-path>
+/*
+ *
+ <<< USAGE
 
 Where:
 -h standard help, usage message.
--d dead-path path were list of dead packages will be stored.
--o output-path path where sources will be checked out.
 
-NOTE: parameters are NOT processed at this time!
+-o output-path path where sources will be checked out, the packages will
+   be checked out into a directory call devel.
+-s skip-path path were list of dead packages and other skipped packages
+   will be stored.
+   Note is it ALWAYS a good idea to run this script using screen and to
+   capture stdout and stderr into a log file.
 
 USAGE;
+*/
 
-for ($i = 1; $i < $argc; $i++) {
-  switch ($argv[$i]) {
-    case '-d':
-      $i++;
-      if (isset($argv[$i])) {
-        $dead_path = $argv[$i];
-      }
-      else {
-        $dead_path = NULL;
-      }
-      break;
-    case '-h':
-      $i++;
-      echo "$usage\n";
-      exit(0);
-    case '-o':
-      $i++;
-      if (isset($argv[$i])) {
-        $fedora = $argv[$i];
-      }
-      else {
-        die("ERROR: Must supply a valid path to a file after -o");
-      }
-      break;
-    default:
-      die("ERROR: Unknown argument: $argv[$i]\n$usage");
-      break;
+$options = getopt("ho:s:");
+//print_r($options);
+if (empty($options))
+  {
+  echo $usage;
+  exit(1);
   }
-}
+if (array_key_exists("h",$options))
+  {
+  echo $usage;
+  exit(0);
+  }
+if (array_key_exists("o",$options))
+  {
+  $fedora = $options['o'];
+  if (empty($fedora))
+  {
+    echo $usage;
+    exit(1);
+  }
+  }
+  if (array_key_exists("s",$options))
+  {
+  $spath = $options['s'];
+  if (empty($spath))
+  {
+   $spath = '/tmp/skipped.fedora9.pkgs';
+   print "NOTE: setting the path for skipped file to /tmp/skipped.fedora9.pkgs\n";
+  }
+  else
+  {
+   // defect here, should check to see if there is a trailing /....
+   $spath .= '/skipped.fedora9.pkgs';
+  }
+  }
+
 
 chdir($fedora) or die("Can't chdir to $fedora, $php_errormsg\n");
 
@@ -112,41 +114,85 @@ $date=`date`;
 echo "Starting at: $date\n";
 
 // checkout and filter
-cvs_co($fedora);
-chdir($fedora) or die("Can't chdir to $fedora, $php_errormsg\n");
+$checked_out = cvs_co($fedora);
+if(!empty($checked_out))
+{
+	print($checked_out);
+}
+// need to check for trailing / and then do the right thing....add
+// later
+$devel = "$fedora" . '/devel';
+chdir($devel) or die("Can't chdir to $fedora, $php_errormsg\n");
 
-//$list=`ls`;
-// For testing....
-$list = array(abiword, amanda, bugzilla, 'configure-thinkpad', common, eclipse);
+$list = array();
 
+$last = exec('ls', $list, $rtn);
+if ($rtn != 0)
+{
+	print "Error, cannot get list of packages with ls\n";
+	exit(1);
+}
+
+// Filter and make
 foreach($list as $pkg){
   // wrinkle.... common package does not have a devel, need to special
   // case it.
   rtrim($pkg);
-  pdbg("\$pkg is:$pkg");
-  if(!(chdir("$pkg/devel"))){
+  //cli_PrintDebugMessage("\$pkg is:$pkg");
+  $dir = `pwd`;
+  print "Now at:$dir";
+  if(!(chdir("$pkg"))){
     echo "ERROR: Can't chdir to $pkg, skipping: $php_errormsg\n";
     continue;
   }
   $plist=`ls`;
+
   if (preg_match('/dead.package/', $plist)){
-    echo "$pkg is a dead.package\n";
-    chdir('../..') or die("Can't chdir to ../.., $php_errormsg\n");
+    echo "$pkg is a dead.package, skipping\n";
+    $saved = save_skipped($spath, "$pkg is a dead package\n");
+    if(!empty($saved))
+    {
+    	print "Warning! ";
+    	print($saved);
+    	chdir('..') or die("Can't chdir to .., $php_errormsg\n");
+    	continue;
+    }
+    chdir('..') or die("Can't chdir to .., $php_errormsg\n");
+  }
+  elseif (!(preg_match('/.spec/', $plist)))
+  {
+    echo "$pkg has no spec file, skipping\n";
+    $saved = save_skipped($spath, "$pkg has no spec file\n");
+    if(!empty($saved))
+    {
+    	print "Warning! ";
+    	print($saved);
+    	chdir('..') or die("Can't chdir to .., $php_errormsg\n");
+    	continue;
+    }
+    chdir('..') or die("Can't chdir to .., $php_errormsg\n");
   }
   else{
     $dir = `pwd`;
     $date = `date`;
     print "Now at:$dir";
     print "on $date";
-    echo "Makeing $pkg\n";
-    $mpcmd = 'export http_proxy=http://web-proxy.fc.hp.com:8088; ' .
-             "alias rm='rm -f'; make > make.out 2>&1";
+    echo "Making $pkg\n";
+    $mpcmd = "alias rm='rm -f'; make prep > make-prep.out 2>&1";
     $last = exec("$mpcmd", $mpout, $rtn);
     if($rtn != 0) {
-      print "ERROR: make prep did not exit zero: return was: $rtn\n\n";
+      print "ERROR: make prep for $pkg did not exit zero: return was: $rtn\n\n";
+      $saved = save_skipped($spath, "$pkg failed make prep, return code was: $rtn\n");
+      if(!empty($saved))
+      {
+         print "Warning! ";
+         print($saved);
+      }
     }
-    chdir('../..') or die("Can't chdir to ../.., $php_errormsg\n");
+    // put the removeal of the make.out here....as else clause...
+    chdir('..') or die("Can't chdir to .., $php_errormsg\n");
   }
+  // look for and remove the compressed file... look at older scripts.
   echo "-----\n\n";
 }
 
@@ -166,15 +212,30 @@ print "Ending at: $date";
 function cvs_co($fedora){
 
   // make sure cvs root is set
-  $cmd = 'export http_proxy=http://web-proxy.fc.hp.com:8088; ' .
-         'export CVSROOT=:pserver:anonymous@cvs.fedoraproject.org:/cvs/pkgs; ' .
-         'cvs co abiword amanda bugzilla configure-thinkpad common eclipse';
+  $cmd = 'export CVSROOT=:pserver:anonymous@cvs.fedoraproject.org:/cvs/pkgs; '
+          . 'cvs co -r HEAD devel';
+
   chdir($fedora) or die("Can't chdir to $fedora, $php_errormsg\n");
   $last = exec("$cmd", $cvs_co_out, $retval);
   if ($retval != 0){
-    echo "ERROR: cvs co did not return zero status: $retval\n";
-    return(FALSE);
+    return("ERROR: cvs co did not return zero status: $retval\n");
   }
-  return TRUE;
+  return NULL;
 }
+
+function save_skipped ($path, $message)
+{
+	global $WEBDIR;
+	require_once("$WEBDIR/common/common-cli.php");
+	// save the message containing the package name that failed in a file at $path
+
+	$logged = cli_logger($path, $message);
+	if(!empty($logged))
+	{
+		return($logged);
+	}
+	return(NULL);
+}
+
+
 ?>
