73dfaf01909acab1454a6a955e776502dcf53941
Mark Donohoe <mark.donohoe@hp.com>
2008-06-21 05:36:19 +0000

    fixes for change in location of classes
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@820 a5643f74-2041-0410-a2b9-94ffef304a07

--- utils/freshmeat/tests/GpTestSuite.php
+++ utils/freshmeat/tests/GpTestSuite.php
@@ -25,8 +25,14 @@
  * Created on Jun 20, 2008
  */
 // Standard requires for any test.
-require_once('/usr/local/simpletest/unit_tester.php');
-require_once('/usr/local/simpletest/reporter.php');
+if (!defined('SIMPLE_TEST'))
+  define('SIMPLE_TEST', '/usr/local/simpletest/');
+
+/* simpletest includes */
+require_once SIMPLE_TEST . 'unit_tester.php';
+require_once SIMPLE_TEST . 'reporter.php';
+//require_once SIMPLE_TEST . 'mock_objects.php';
+//require_once SIMPLE_TEST . 'web_tester.php';
 
 class GpClassTestSuite extends TestSuite
 {
