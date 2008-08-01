b2581efbc6b453095f3a47ee3f66ead4e0935708
Mark Donohoe <mark.donohoe@hp.com>
2008-08-01 00:41:08 +0000

    more test infrastructure
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@1007 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/ui/plugins/tests/PageTests/runPageTests.php
@@ -0,0 +1,56 @@
+#!/usr/bin/php
+<?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************/
+/*
+ * Runner script to run Page level tests
+ */
+// set the path for where simpletest is
+$path = '/usr/share/php' . PATH_SEPARATOR;
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+/* simpletest includes */
+require_once '/usr/local/simpletest/unit_tester.php';
+require_once '/usr/local/simpletest/web_tester.php';
+require_once '/usr/local/simpletest/reporter.php';
+
+require_once ('../../../../tests/fossologyWebTestCase.php');
+
+$usage = "$argv[0] Url User Password path-to-suite";
+
+// process parameters
+if(count($argc) >= 4)
+{
+  print $usage;
+  exit(1);
+}
+
+list($me, $url, $user, $password, $Testpath) = $argv;
+
+fossologyWebTestCase.php::setTestEnv($url, $user, $password);
+
+$test = &new TestSuite('Fossology Repo Site UI tests');
+$test->addTestFile('PassTest.php');
+
+if (TextReporter::inCli())
+{
+  exit ($test->run(new TextReporter()) ? 0 : 1);
+}
+$test->run(new HtmlReporter());
+
+
+?>
