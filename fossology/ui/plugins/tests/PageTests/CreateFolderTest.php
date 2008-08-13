43a12658487a8a0f70ace36c2a1d3637f6145376
Mark Donohoe <mark.donohoe@hp.com>
2008-08-13 03:04:45 +0000

    two new functional tests create and delete folder
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@1057 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/ui/plugins/tests/PageTests/CreateFolderTest.php
@@ -0,0 +1,86 @@
+<?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+
+/**
+ * Template to use for a simpletest test
+ *
+ * @param
+ *
+ * @return
+ *
+ * @version "$Id: $"
+ *
+ * Created on Aug 1, 2008
+ */
+
+/* every test must use these includes, adjust the paths based on where the
+ * tests are in the source tree.
+ */
+require_once ('../../../../tests/fossologyWebTestCase.php');
+require_once ('../../../../tests/TestEnvironment.php');
+
+/* every test must use these globals, at least $URL */
+global $URL;
+
+/* The class name should end in Test */
+
+class CreateFolderTest extends fossologyWebTestCase
+{
+
+  function testCreateFolder()
+  {
+    global $URL;
+
+    print "starting CreateFoldertest\n";
+    $browser = & new SimpleBrowser();
+    $page = $browser->get($URL);
+    $this->assertTrue($page);
+    $this->assertTrue(is_object($browser));
+    $cookie = $this->repoLogin($browser);
+    $host = $this->getHost($URL);
+    $browser->setCookie('Login', $cookie, $host);
+
+    $loggedIn = $browser->get($URL);
+    $this->assertTrue($this->assertText($loggedIn, '/Organize/'),
+                      "FAIL! Could not find Organize menu\n");
+    $this->assertTrue($this->assertText($loggedIn, '/Folders /'));
+    $this->assertTrue($this->assertText($loggedIn, '/Create/'));
+    /* ok, this proves the text is on the page, let's see if we can
+     * go to the page and create a folder
+     */
+    $page = $browser->get("$URL?mod=folder_create");
+    $this->assertTrue($this->assertText($page, '/Create a new Fossology folder/'));
+    /* select the folder to create this folder under */
+    $FolderId = $this->getFolderId('Testing', $page);
+    $this->assertTrue($browser->setField('parentid', $FolderId));
+    /* create unique name and insert into form */
+    $id = getmypid();
+    $folder_name = 'TestCreateFolder-' . "$id";
+    $this->assertTrue($browser->setField('newname', $folder_name));
+    $desc = 'Folder created by CreateFolderTest as subfolder of Testing';
+    $this->assertTrue($browser->setField('description', "$desc"));
+    $page = $browser->clickSubmit('Create!');
+    $this->assertTrue(page);
+    $this->assertTrue($this->assertText($page, "/Folder $folder_name Created/"),
+                      "FAIL! Folder $folder_name Created not found\n");
+
+    //print "************ page after Folder Create! *************\n$page\n";
+  }
+}
+
+?>
