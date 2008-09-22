198adbc9865b63cb36848a0fc215996b39439dd4
Mark Donohoe <mark.donohoe@hp.com>
2008-09-22 21:04:49 +0000

    moved test up 1 level to be under ui
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@1342 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/ui/tests/VerifyTests/runVerifyTests.php
@@ -0,0 +1,51 @@
+#!/usr/bin/php
+<?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************/
+/*
+ * Runner script to run Verify tests
+ */
+// set the path for where simpletest is
+$path = '/usr/share/php' . PATH_SEPARATOR;
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+/* simpletest includes */
+require_once '/usr/local/simpletest/unit_tester.php';
+require_once '/usr/local/simpletest/web_tester.php';
+require_once '/usr/local/simpletest/reporter.php';
+
+require_once ('../../../../tests/fossologyWebTestCase.php');
+require_once ('../../../../tests/TestEnvironment.php');
+require_once('../../../../tests/testClasses/timer.php');
+
+$start = new timer();
+print "Startnig Verify Tests at: " . date('r') . "\n";
+$test = &new TestSuite('Fossology Repo UI Verification Functional tests');
+$test->addTestFile('verifyFossTestAr.php');
+
+if (TextReporter::inCli())
+{
+  $results = $test->run(new TextReporter()) ? 0 : 1;
+  print "Ending Verify Tests at: " . date('r') . "\n";
+  $elapseTime = $start->TimeAgo($start->getStartTime());
+  print "The Verify Tests took {$elapseTime}to run\n";
+  exit($results);
+}
+$test->run(new HtmlReporter());
+$elapseTime = $start->TimeAgo($start->getStartTime());
+print "The Verify Tests took {$elapseTime}to run\n";
+?>
