c0b6c6a5e5a18de83957eb564c09e75ff9de08f8
Mark Donohoe <mark.donohoe@hp.com>
2008-09-24 17:47:37 +0000

    adjusted require paths and renamed runPage to runBasic
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@1361 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui/tests/BasicTests/DupUploadTest.php
+++ ui/tests/BasicTests/DupUploadTest.php
@@ -28,8 +28,8 @@
  * Created on Aug 28, 2008
  */
 
-require_once ('../../../../tests/fossologyTestCase.php');
-require_once ('../../../../tests/TestEnvironment.php');
+require_once ('../../../tests/fossologyTestCase.php');
+require_once ('../../../tests/TestEnvironment.php');
 
 global $URL;
 
--- ui/tests/BasicTests/UploadFileTest.php
+++ ui/tests/BasicTests/UploadFileTest.php
@@ -27,8 +27,8 @@
  * Created on Aug 1, 2008
  */
 
-require_once ('../../../../tests/fossologyTestCase.php');
-require_once ('../../../../tests/TestEnvironment.php');
+require_once ('../../../tests/fossologyTestCase.php');
+require_once ('../../../tests/TestEnvironment.php');
 
 global $URL;
 
