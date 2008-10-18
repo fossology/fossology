e3e32b483fbc29ac292357205dce028e6f4b3ff4
Mark Donohoe <mark.donohoe@hp.com>
2008-10-18 03:12:01 +0000

    new common library so all classes can use it, not just test classes
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@1545 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ tests/commonTestFuncs.php
@@ -0,0 +1,114 @@
+<?php
+
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+/**
+ * public function getHost
+ *
+ * returns the host (if present) from a URL
+ *
+ * @param string $URL a url in the form of http://somehost.xx.com/repo/
+ *
+ * @return string $host the somehost.xx.com part is returned or NULL,
+ * if there is no host in the uri
+ *
+ */
+
+function getHost($URL)
+{
+  if (empty ($URL))
+  {
+    return (NULL);
+  }
+  return (parse_url($URL, PHP_URL_HOST)); // can return NULL
+}
+/**
+ * escapeDots($string)
+ *
+ * Escape '.' in a string by replacing '.' with '\.'
+ * @param string $string the input string to escape.
+ * @return string $estring the escaped string or False.
+ */
+function escapeDots($string)
+{
+  if (empty ($string))
+  {
+    return (FALSE);
+  }
+  $estring = preg_replace('/\./', '\\.', $string);
+  //print  "ED: string is:$string, estring is:$estring\n";
+  if ($estring === NULL)
+  {
+    return (FALSE);
+  }
+  return ($estring);
+}
+
+/**
+ * getBrowserUri($name, $page)
+ *
+ * Get the url fragment to display the upload from the xhtml page.
+ *
+ * @param string $name the name of a folder or upload
+ * @param string $page the xhtml page to search
+ *
+ * TODO: finish or scrap this method
+ *
+ * @return $string the matching uri or null.
+ *
+ */
+function getBrowseUri($name, $page)
+{
+  //print "DB: GBURI: page is:\n$page\n";
+  //$found = preg_match("/href='(.*?)'>($uploadName)<\/a>/", $page, $matches);
+  // doesn't work: '$found = preg_match("/href='(.*?)'>$name/", $page, $matches);
+  $found = preg_match("/href='((.*?)&show=detail).*?/", $page, $matches);
+  //$found = preg_match("/ class=.*?href='(.*?)'>$name/", $page, $matches);
+  print "DB: GBURI: found matches is:$found\n";
+  print "DB: GBURI: matches is:\n";
+  var_dump($matches) . "\n";
+  if ($found)
+  {
+    return ($matches[1]);
+  } else
+  {
+    return (NULL);
+  }
+}
+/**
+ * makeUrl($host,$query)
+ *
+ * Make a url from the host and query strings.
+ *
+ * @param $string $host the host (e.g. somehost.com, host.privatenet)
+ * @param $string $query the query to append to the host.
+ *
+ * @return the http string or NULL on error
+ */
+function makeUrl($host, $query)
+{
+  if (empty ($host))
+  {
+    return (NULL);
+  }
+  if (empty ($query))
+  {
+    return (NULL);
+  }
+  return ("http://$host$query");
+}
+?>
