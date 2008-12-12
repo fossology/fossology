2da9ad194c5c86bc162eb0bc78028affc10dff28
Neal Krawetz <krawetz.neal@hp.com>
2008-12-12 17:52:38 +0000

    UI: still tweaking the unable-to-create-first-folder bug.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@1764 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui/plugins/core-schema.php
+++ ui/plugins/core-schema.php
@@ -790,6 +790,7 @@ if (0)
 	$Results = $DB->Action("SELECT max($Column) AS max FROM $Table LIMIT 1;");
 	$Max = intval($Results[0]['max']);
 	if (empty($Max) || ($Max <= 0)) { $Max = 1; }
+	else { $Max++; }
 	// print "Setting table($Table) column($Column) sequence($Seq) to $Max\n";
 	$DB->Action("SELECT setval('$Seq',$Max);");
 	}
