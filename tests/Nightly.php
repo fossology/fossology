1286a190414e024548cd2839c88aae0a2e57cc7a
Mark Donohoe <mark.donohoe@hp.com>
2008-12-19 01:15:57 +0000

    start of nighly regression test automation
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@1784 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ tests/Nightly.php
@@ -0,0 +1,61 @@
+#!/usr/bin/php
+<?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+
+/**
+ * Nightly test runs of Top of Trunk
+ *
+ * @param
+ *
+ * @return
+ *
+ * @version "$Id: $"
+ *
+ * Created on Dec 18, 2008
+ */
+
+require_once('TestRun.php');
+
+$tonight = new TestRun();
+print "Updating svn sources\n";
+if($tonight->svnUpdate() !== TRUE)
+{
+  print "Error, could not svn Update the sources, aborting test\n";
+  exit(1);
+}
+print "Making sources\n";
+if($tonight->makeSrcs() !== TRUE)
+{
+  print "There were Errors in the make of the sources examing make.out\n";
+  exit(1);
+}
+print "Installing fossology\n";
+if($tonight->makeInstall() !== TRUE)
+{
+  print "There were Errors in the Installation examine make-install.out\n";
+  exit(1);
+}
+/*
+ * next steps:
+ * run fo-postinstall
+ * check scheduler
+ * can you check if you can login as fossy?
+ *
+ */
+
+?>
