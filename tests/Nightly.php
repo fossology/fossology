1717cdf938ecc4eeb88a2c124f6614aa3e421659
Mark Donohoe <mark.donohoe@hp.com>
2009-01-10 01:21:47 +0000

    setting execute bit
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@1789 a5643f74-2041-0410-a2b9-94ffef304a07

--- tests/Nightly.php
+++ tests/Nightly.php
@@ -31,46 +31,81 @@
 
 require_once('TestRun.php');
 
+// Using the standard source path /home/fosstester/Src/fossology
 $tonight = new TestRun();
-/*
-print "Updating svn sources\n";
+
+// Step 1 update sources
+print "Updating sources with svn update\n";
 if($tonight->svnUpdate() !== TRUE)
 {
   print "Error, could not svn Update the sources, aborting test\n";
   exit(1);
 }
+
+// Step 2 make clean and make sources
 print "Making sources\n";
 if($tonight->makeSrcs() !== TRUE)
 {
-  print "There were Errors in the make of the sources examing make.out\n";
+  print "There were Errors in the make of the sources examine make.out\n";
   exit(1);
 }
-//need to add in stopping the scheduler...
+//try to stop the scheduler before the make install step.
+if($tonight->stopScheduler() !== TRUE)
+{
+  print "Could not stop fossology-scheduler, maybe it wasn't running?\n";
+}
+
+// Step 4 install fossology
 print "Installing fossology\n";
 if($tonight->makeInstall() !== TRUE)
 {
   print "There were Errors in the Installation examine make-install.out\n";
   exit(1);
 }
-*/
+
+// Step 5 run the post install process
+print "Running fo-postinstall\n";
+if($tonight->foPostinstall() !== TRUE)
+{
+  print "There were errors in the postinstall process check fop.out\n";
+  exit(1);
+}
+
+// Step 6 run the scheduler test to make sure everything is clean
+print "Starting Scheduler Test\n";
+if($tonight->schedulerTest() !== TRUE)
+{
+  print "Error! in scheduler test examine ST.out\n";
+  exit(1);
+}
+
 print "Starting Scheduler\n";
 if($tonight->startScheduler() !== TRUE)
 {
   print "Error! Could not start fossology-scheduler\n";
   exit(1);
 }
+
+print "Running tests\n";
+$testPath = "$tonight->srcPath" . "/tests";
+print "testpath is:$testPath\n";
+if(!chdir($testPath))
+     {
+       print "Error can't cd to $testPath\n";
+       exit(1);
+     }
+
+$TestLast = exec('./testFOSSology.php -a', $results, $rtn);
+print "after running tests the output is\n";
+print_r($results) . "\n";
+
+/*
 print "Stoping Scheduler\n";
 if($tonight->stopScheduler() !== TRUE)
 {
   print "Error! Could not stop fossology-scheduler\n";
   exit(1);
 }
-/*
- * next steps:
- * run fo-postinstall
- * check scheduler
- * can you check if you can login as fossy?
- *
- */
+*/
 
 ?>
