61f1edd3ff765f3fbeb9c226093b78f0ea58ae6f
Mark Donohoe <mark.donohoe@hp.com>
2009-03-05 01:53:49 +0000

    checkpointing before changing code...this version does not work....has changes in UploadOne
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/emailNotification-1824@1853 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/cli/cp2foss.php
+++ fossology/cli/cp2foss.php
@@ -1,28 +1,28 @@
 #!/usr/bin/php
 <?php
 /***********************************************************
-Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-version 2 as published by the Free Software Foundation.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-***********************************************************/
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
 /**************************************************************
-cp2foss
+ cp2foss
 
-Import a file (or directory) into FOSSology for processing.
+ Import a file (or directory) into FOSSology for processing.
 
-@return 0 for success, 1 for failure.
-*************************************************************/
+ @return 0 for success, 1 for failure.
+ *************************************************************/
 /* Have to set this or else plugins will not load. */
 $GlobalReady = 1;
 /* Load all code */
@@ -91,9 +91,9 @@ $Test = 0;
 /************************************************************************/
 /************************************************************************/
 /****************************************************
-GetBucketFolder(): Given an upload name and the number
-of letters per bucket, return the bucket folder name.
-****************************************************/
+ GetBucketFolder(): Given an upload name and the number
+ of letters per bucket, return the bucket folder name.
+ ****************************************************/
 function GetBucketFolder($UploadName, $BucketGroupSize) {
   $Letters = "abcdefghijklmnopqrstuvwxyz";
   $Numbers = "0123456789";
@@ -124,11 +124,11 @@ function GetBucketFolder($UploadName, $BucketGroupSize) {
   return ("Other");
 } /* GetBucketFolder() */
 /****************************************************
-GetFolder(): Given a folder path, return the folder_pk.
-NOTE: If any part of the folder path does not exist, thenscp cp2foss will
-create it.
-NOTE: This is recursive!
-****************************************************/
+ GetFolder(): Given a folder path, return the folder_pk.
+ NOTE: If any part of the folder path does not exist, thenscp cp2foss will
+ create it.
+ NOTE: This is recursive!
+ ****************************************************/
 function GetFolder($FolderPath, $Parent = NULL) {
   global $DB;
   global $Verbose;
@@ -178,9 +178,9 @@ function GetFolder($FolderPath, $Parent = NULL) {
   return (GetFolder($FolderPath, $Parent));
 } /* GetFolder() */
 /****************************************************
-UploadOne(): Given one object (file or URL), upload it.
-This is a function because it is can also be recursive!
-****************************************************/
+ UploadOne(): Given one object (file or URL), upload it.
+ This is a function because it is can also be recursive!
+ ****************************************************/
 function UploadOne($FolderPath, $UploadArchive, $UploadName, $UploadDescription, $TarSource = NULL) {
   global $Verbose;
   global $Test;
@@ -188,7 +188,8 @@ function UploadOne($FolderPath, $UploadArchive, $UploadName, $UploadDescription,
   /* $Mode determines where it came from */
   if (preg_match("@^[a-zA-Z0-9_]+://@", $UploadArchive)) {
     $Mode = 1 << 2; /* Looks like a URL */
-  } else if (is_dir($UploadArchive)) {
+  }
+  else if (is_dir($UploadArchive)) {
     /* It's a directory, tar it! */
     global $VARDATADIR;
     global $TarExcludeList;
@@ -209,9 +210,11 @@ function UploadOne($FolderPath, $UploadArchive, $UploadName, $UploadDescription,
     UploadOne($FolderPath, $Filename, $UploadName, $UploadDescription, $UploadArchive);
     unlink($Filename);
     return;
-  } else if (file_exists($UploadArchive)) {
+  }
+  else if (file_exists($UploadArchive)) {
     $Mode = 1 << 4; /* Looks like a filesystem */
-  } else {
+  }
+  else {
     /* Don't know what it is... */
     print "FATAL: '$UploadArchive' does not exist.\n";
     exit(1);
@@ -228,7 +231,8 @@ function UploadOne($FolderPath, $UploadArchive, $UploadName, $UploadDescription,
   $FolderPk = GetFolder($FolderPath);
   if ($FolderPk == 1) {
     print "  Uploading to folder: Software Repository\n";
-  } else {
+  }
+  else {
     print "  Uploading to folder: '$FolderPath'\n";
   }
   print "  Uploading as '$UploadName'\n";
@@ -261,26 +265,50 @@ function UploadOne($FolderPath, $UploadArchive, $UploadName, $UploadDescription,
     print "CMD=$Cmd\n";
   }
   if (!$Test) {
+    //print "CP2: CMD=$Cmd\n";
     system($Cmd);
   }
   if (!empty($QueueList)) {
     switch ($QueueList) {
       case 'agent_unpack':
         $Cmd = "";
-      break; /* already scheduled */
+        break; /* already scheduled */
       case 'ALL':
       case 'all':
         $Cmd = "fossjobs -U '$UploadPk'";
-      break;
+        break;
       default:
         $Cmd = "fossjobs -U '$UploadPk' -A '$QueueList'";
-      break;
+        break;
     }
+    //print "  CP2:CMD=$Cmd\n";
     if ($Verbose) {
       print "CMD=$Cmd\n";
     }
     if (!$Test) {
       system($Cmd);
+    }
+    /* $_SESSION may not be valid, go get the enotification flag */
+    global $DB;
+    print "  CP2: Before 1st query UploadPk is:$UploadPk\n";
+    $Sql = "SELECT upload_pk, job_upload_fk,job_user_fk FROM upload,job WHERE " .
+           "job_upload_fk=$UploadPk order by upload_pk desc;";
+    $Users = $DB->Action($Sql);
+    print "  CP2: After 1st query\n";
+    $UserPk = $Users[0]['job_user_fk'];
+    print "  CP2: after 1st query to get user_pk, UserPk is:$UserPk\n";
+    $Sql = "SELECT user_pk, user_name, email_notify FROM users WHERE " .
+           "user_pk=$UserPk; ";
+    //"user_pk=$UserPk and user_name='fossy'; ";
+    $Fossy= $DB->Action($Sql);
+    $EmailNote = $Fossy[0]['email_notify'];
+    print "  CP2: after 2nd query to get EN, EmailNote is:$EmailNote\n";
+    if ($EmailNote == 'y') {
+      print "  Scheduling email notification\n";
+      $sched = scheduleEmailNotification($UploadPk);
+      if ($sched !== NULL) {
+        return($sched);
+      }
     }
   }
 } /* UploadOne() */
@@ -298,7 +326,7 @@ for ($i = 1;$i < $argc;$i++) {
   switch ($argv[$i]) {
     case '-A': /* use alphabet buckets */
       $OptionA = true;
-    break;
+      break;
     case '-AA': /* use alphabet buckets */
       $OptionA = true;
       $i++;
@@ -306,7 +334,7 @@ for ($i = 1;$i < $argc;$i++) {
       if ($bucket_size < 1) {
         $bucket_size = 1;
       }
-    break;
+      break;
     case '-f': /* folder path */
     case '-p': /* depricated 'path' to folder */
       $i++;
@@ -317,56 +345,56 @@ for ($i = 1;$i < $argc;$i++) {
       $FolderPath = preg_replace('@^/*@', "", $FolderPath);
       $FolderPath = preg_replace('@/*$@', "", $FolderPath);
       /* Note: the pattern below should probably be generalized to remove everything
-      * up to and including the 1st /, This pattern works in what I've
-      * tested: @^.*\/@ ( I had to escape the / so the comment works!)
-      */
+       * up to and including the 1st /, This pattern works in what I've
+       * tested: @^.*\/@ ( I had to escape the / so the comment works!)
+       */
       $FolderPath = preg_replace("@^S.*? Repository@", "", $FolderPath);
       $FolderPath = preg_replace('@//*@', "/", $FolderPath);
       $FolderPath = '/' . $FolderPath;
       //print "  AFTER Idiot Checks: '$FolderPath'\n";
-      
-    break;
+
+      break;
     case '-R': /* obsolete: recurse directories */
     case '-w': /* obsolete: URL switch to use wget */
-    break;
+      break;
     case '-d': /* specify upload description */
       $i++;
       $UploadDescription = $argv[$i];
-    break;
+      break;
     case '-n': /* specify upload name */
       $i++;
       $UploadName = $argv[$i];
-    break;
+      break;
     case '-Q':
       system("fossjobs -a");
       return (0);
     case '-q':
       $i++;
       $QueueList = $argv[$i];
-    break;
+      break;
     case '-T': /* Test mode */
       $Test = 1;
       if (!$Verbose) {
         $Verbose++;
       }
-    break;
+      break;
     case '-v':
       $Verbose++;
-    break;
+      break;
     case '-X':
       if (!empty($TarExcludeList)) {
         $TarExcludeList.= " ";
       }
       $i++;
       $TarExcludeList.= "--exclude '" . $argv[$i] . "'";
-    break;
+      break;
     case '-h':
     case '-?':
       print $Usage . "\n";
       return (0);
     case '-a': /* it's an archive! */
       /* ignore -a since the next name is a file. */
-    break;
+      break;
     case '-': /* it's an archive list from stdin! */
       $Fin = fopen("php://stdin", "r");
       while (!feof($Fin)) {
@@ -382,7 +410,7 @@ for ($i = 1;$i < $argc;$i++) {
         }
       }
       fclose($Fin);
-    break;
+      break;
     default:
       if (substr($argv[$i], 0, 1) == '-') {
         print "Unknown parameter: '" . $argv[$i] . "'\n";
@@ -399,7 +427,7 @@ for ($i = 1;$i < $argc;$i++) {
       UploadOne($FolderPath, $UploadArchive, $UploadName, $UploadDescription);
       /* prepare for next parameter */
       $UploadName = "";
-    break;
+      break;
   } /* switch */
 } /* for each parameter */
 return (0);
