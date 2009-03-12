e2625583c141ea3bc7006ce9a8a805fd8854c650
Mark Donohoe <mark.donohoe@hp.com>
2009-03-12 23:14:50 +0000

    removed debug statement, changes to support email notification
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/emailNotification-1824@1886 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/ui/plugins/agent-add.php
+++ fossology/ui/plugins/agent-add.php
@@ -38,41 +38,50 @@ class agent_add extends FO_Plugin
    *********************************************/
   function AgentsAdd	($uploadpk, $agentlist)
   {
-  $rc="";
+    $rc="";
+    $Alist = array();
 
-  /* Make sure the uploadpk is valid */
-  global $Plugins;
-  global $DB;
-  $Results = $DB->Action("SELECT upload_pk FROM upload WHERE upload_pk = '$upload_pk';");
-  if ($Results[0]['upload_pk'] != $upload_pk)
+    /* Make sure the uploadpk is valid */
+    global $Plugins;
+    global $DB;
+    $Results = $DB->Action("SELECT upload_pk FROM upload WHERE upload_pk = '$upload_pk';");
+    if ($Results[0]['upload_pk'] != $upload_pk)
     {
-    return("Upload not found.");
+      return("Upload not found.");
     }
 
-  /* Validate the agent list and add agents as needed. */
-  /** Don't worry about order or duplicates -- it will do the right thing. **/
-  $Depth=0;
-  $agent_list = menu_find("Agents", $depth);
-  for($al=0; !empty($agentlist[$al]); $al++)
+    /* Validate the agent list and add agents as needed. */
+    /** Don't worry about order or duplicates -- it will do the right thing. **/
+    $Depth=0;
+    $agent_list = menu_find("Agents", $depth);
+    for($al=0; !empty($agentlist[$al]); $al++)
     {
-    /* check if the agent exists in the list of viable agents */
-    $Found = -1;
-    for($ac=0; ($Found < 0) && !empty($agent_list[$ac]->URI); $ac++)
+      /* check if the agent exists in the list of viable agents */
+      $Found = -1;
+      for($ac=0; ($Found < 0) && !empty($agent_list[$ac]->URI); $ac++)
       {
-      if (!strcmp($agent_list[$ac]->URI,$agentlist[$al])) { $Found = $al; }
+        if (!strcmp($agent_list[$ac]->URI,$agentlist[$al])) { $Found = $al; }
       }
-    if ($Found >= 0)
+      if ($Found >= 0)
       {
-      // print "Adding to " . $agentlist[$Found] . "<br>\n";
-      $P = &$Plugins[plugin_find_id($agentlist[$Found])];
-      $P->AgentAdd($uploadpk);
+        //print "Adding to " . $agentlist[$Found] . "<br>\n";
+        $P = &$Plugins[plugin_find_id($agentlist[$Found])];
+        $P->AgentAdd($uploadpk);
+        $Alist[] = $agentlist[$Found];
       }
-    else
+      else
       {
-      $rc .= "Agent '" . htmlentities($agentlist[$al]) . "' not found.\n";
+        $rc .= "Agent '" . htmlentities($agentlist[$al]) . "' not found.\n";
       }
     }
-  return($rc);
+    if (CheckEnotification) {
+      /* Create list for dependency checking by enotification */
+      $sched = scheduleEmailNotification($uploadpk,NULL,NULL,NULL,$Alist);
+      if ($sched !== NULL) {
+        return($sched);
+      }
+    }
+    return($rc);
   } // AgentsAdd()
 
   /*********************************************
@@ -86,95 +95,95 @@ class agent_add extends FO_Plugin
     switch($this->OutputType)
     {
       case "XML":
-	break;
+        break;
       case "HTML":
-	/* If this is a POST, then process the request. */
-	$Folder = GetParm('folder',PARM_INTEGER);
-	if (empty($Folder)) { $Folder = FolderGetTop(); }
-	$uploadpk = GetParm('upload',PARM_INTEGER);
-	$agents = $_POST['agents'];
-	if (!empty($uploadpk) && !empty($agents) && is_array($agents))
-	  {
-	  $rc = $this->AgentsAdd($uploadpk,$agents);
-	  if (empty($rc))
-	    {
-	    /* Need to refresh the screen */
-	    $V .= PopupAlert('Analysis added to job queue');
-	    }
-	  else
-	    {
-	    $V .= PopupAlert('Scheduling failed: $rc');
-	    }
-	  }
+        /* If this is a POST, then process the request. */
+        $Folder = GetParm('folder',PARM_INTEGER);
+        if (empty($Folder)) { $Folder = FolderGetTop(); }
+        $uploadpk = GetParm('upload',PARM_INTEGER);
+        $agents = $_POST['agents'];
+        if (!empty($uploadpk) && !empty($agents) && is_array($agents))
+        {
+          $rc = $this->AgentsAdd($uploadpk,$agents);
+          if (empty($rc))
+          {
+            /* Need to refresh the screen */
+            $V .= PopupAlert('Analysis added to job queue');
+          }
+          else
+          {
+            $V .= PopupAlert("Scheduling failed: $rc");
+          }
+        }
 
         /*************************************************************/
-	/* Create the AJAX (Active HTTP) javascript for doing the reply
-	   and showing the response. */
-	$V .= ActiveHTTPscript("Uploads");
-	$V .= "<script language='javascript'>\n";
-	$V .= "function Uploads_Reply()\n";
-	$V .= "  {\n";
-	$V .= "  if ((Uploads.readyState==4) && (Uploads.status==200))\n";
-	$V .= "    {\n";
-	/* Remove all options */
-	$V .= "    document.formy.upload.innerHTML = Uploads.responseText;\n";
-	$V .= "    document.getElementById('agents').innerHTML = '';\n";
-	/* Add new options */
-	$V .= "    }\n";
-	$V .= "  }\n";
-	$V .= "</script>\n";
+        /* Create the AJAX (Active HTTP) javascript for doing the reply
+         and showing the response. */
+        $V .= ActiveHTTPscript("Uploads");
+        $V .= "<script language='javascript'>\n";
+        $V .= "function Uploads_Reply()\n";
+        $V .= "  {\n";
+        $V .= "  if ((Uploads.readyState==4) && (Uploads.status==200))\n";
+        $V .= "    {\n";
+        /* Remove all options */
+        $V .= "    document.formy.upload.innerHTML = Uploads.responseText;\n";
+        $V .= "    document.getElementById('agents').innerHTML = '';\n";
+        /* Add new options */
+        $V .= "    }\n";
+        $V .= "  }\n";
+        $V .= "</script>\n";
 
-	$V .= ActiveHTTPscript("Agents");
-	$V .= "<script language='javascript'>\n";
-	$V .= "function Agents_Reply()\n";
-	$V .= "  {\n";
-	$V .= "  if ((Agents.readyState==4) && (Agents.status==200))\n";
-	$V .= "    {\n";
-	/* Remove all options */
-	$V .= "    document.getElementById('agents').innerHTML = Agents.responseText;\n";
-	/* Add new options */
-	$V .= "    }\n";
-	$V .= "  }\n";
-	$V .= "</script>\n";
+        $V .= ActiveHTTPscript("Agents");
+        $V .= "<script language='javascript'>\n";
+        $V .= "function Agents_Reply()\n";
+        $V .= "  {\n";
+        $V .= "  if ((Agents.readyState==4) && (Agents.status==200))\n";
+        $V .= "    {\n";
+        /* Remove all options */
+        $V .= "    document.getElementById('agents').innerHTML = Agents.responseText;\n";
+        /* Add new options */
+        $V .= "    }\n";
+        $V .= "  }\n";
+        $V .= "</script>\n";
 
         /*************************************************************/
-	/* Display the form */
-	$V .= "<form name='formy' method='post'>\n"; // no url = this url
-	$V .= "Select an uploaded file for license analysis.\n";
-	$V .= "Only uploads that are not already scheduled will be scheduled.\n";
+        /* Display the form */
+        $V .= "<form name='formy' method='post'>\n"; // no url = this url
+        $V .= "Select an uploaded file for license analysis.\n";
+        $V .= "Only uploads that are not already scheduled will be scheduled.\n";
 
-	$V .= "<ol>\n";
-	$V .= "<li>Select the folder containing the upload you wish to analyze:<br>\n";
-	$V .= "<select name='folder'\n";
-	$V .= "onLoad='Uploads_Get((\"" . Traceback_uri() . "?mod=upload_options&folder=$Folder' ";
-	$V .= "onChange='Uploads_Get(\"" . Traceback_uri() . "?mod=upload_options&folder=\" + this.value)'>\n";
-	$V .= FolderListOption(-1,0,1,$Folder);
-	$V .= "</select><P />\n";
+        $V .= "<ol>\n";
+        $V .= "<li>Select the folder containing the upload you wish to analyze:<br>\n";
+        $V .= "<select name='folder'\n";
+        $V .= "onLoad='Uploads_Get((\"" . Traceback_uri() . "?mod=upload_options&folder=$Folder' ";
+        $V .= "onChange='Uploads_Get(\"" . Traceback_uri() . "?mod=upload_options&folder=\" + this.value)'>\n";
+        $V .= FolderListOption(-1,0,1,$Folder);
+        $V .= "</select><P />\n";
 
-	$V .= "<li>Select the upload to analyze:<br>";
-	$V .= "<select size='10' name='upload' onChange='Agents_Get(\"" . Traceback_uri() . "?mod=upload_agent_options&upload=\" + this.value)'>\n";
-	$List = FolderListUploads($Folder);
-	foreach($List as $L)
-	  {
-	  $V .= "<option value='" . $L['upload_pk'] . "'>";
-	  $V .= htmlentities($L['name']);
-	  if (!empty($L['upload_desc']))
-		{
-		$V .= " (" . htmlentities($L['upload_desc']) . ")";
-		}
-	  $V .= "</option>\n";
-	  }
-	$V .= "</select><P />\n";
-	$V .= "<li>Select one or more agents to schedule. Only available, unscheduled agents are listed.<br>\n";
-	$V .= "<select multiple size='10' id='agents' name='agents[]'></select>\n";
-	$V .= "</ol>\n";
-	$V .= "<input type='submit' value='Analyze!'>\n";
-	$V .= "</form>\n";
-	break;
+        $V .= "<li>Select the upload to analyze:<br>";
+        $V .= "<select size='10' name='upload' onChange='Agents_Get(\"" . Traceback_uri() . "?mod=upload_agent_options&upload=\" + this.value)'>\n";
+        $List = FolderListUploads($Folder);
+        foreach($List as $L)
+        {
+          $V .= "<option value='" . $L['upload_pk'] . "'>";
+          $V .= htmlentities($L['name']);
+          if (!empty($L['upload_desc']))
+          {
+            $V .= " (" . htmlentities($L['upload_desc']) . ")";
+          }
+          $V .= "</option>\n";
+        }
+        $V .= "</select><P />\n";
+        $V .= "<li>Select one or more agents to schedule. Only available, unscheduled agents are listed.<br>\n";
+        $V .= "<select multiple size='10' id='agents' name='agents[]'></select>\n";
+        $V .= "</ol>\n";
+        $V .= "<input type='submit' value='Analyze!'>\n";
+        $V .= "</form>\n";
+        break;
       case "Text":
-	break;
+        break;
       default:
-	break;
+        break;
     }
     if (!$this->OutputToStdout) { return($V); }
     print("$V");
