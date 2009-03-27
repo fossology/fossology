430554d3327ac0b92f83a6b8500a00fc7f0877ee
Mark Donohoe <mark.donohoe@hp.com>
2009-03-27 03:09:42 +0000

    new verifier/test checks that email sent to the test user says completed, if not fails, also have new function to parse subject lines out of a users email box in /var/mail
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@1929 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ tests/checkEmailMailTest.php
@@ -0,0 +1,78 @@
+<?php
+/***********************************************************
+ Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+
+/**
+ * Test checkMail function
+ *
+ *
+ * @version "$Id: templateTest.php 1262 2008-09-04 20:35:11Z rrando $"
+ *
+ * Created on March 26, 2009
+ */
+
+require_once ('fossologyTestCase.php');
+require_once ('TestEnvironment.php');
+
+/* Globals for test use, most tests need $URL, only login needs the others */
+global $URL;
+global $USER;
+global $PASSWORD;
+
+class someTest extends fossologyTestCase
+{
+  public $mybrowser;          // must have
+
+  /*
+   * Every Test needs to login so we use the setUp method for that.
+   * setUp is called before any other method by default.
+   *
+   * If other actions like creating a folder or something are needed,
+   * put them in the setUp method after login.
+   *
+   */
+  function setUp() {
+    global $URL;
+    $this->Login();
+  }
+
+  public function testCheckCompletedJobs() {
+    global $URL;
+
+    print "starting testckm\n";
+
+    $headers = getMailSubjects();
+    //print "Got back from checkMail:\n";print_r($headers) . "\n";
+    /* find any duplicates, count them */
+    $pattern = 'completed with no errors';
+    foreach($headers as $header) {
+      /* Make sure all say completed */
+      $match = preg_match("/$pattern/",$header,$matches);
+      if($match == 0) {
+        $failed[] = $header;
+      }
+    }
+    if(empty($failed)) {
+      $this->fail("the following jobs did not report as completed\n");
+      foreach($failed as $fail) {
+        print "$fail\n";
+      }
+    }
+  }
+}
+
+?>
