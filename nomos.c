536995c89b14ef97762aa577a4317cf13af7a0b6
Channing D.Benson <benson.channing@hp.com>
2009-05-07 00:08:25 +0000

    General code cleanup and "standardization".
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology/agents/nomos@2123 a5643f74-2041-0410-a2b9-94ffef304a07

--- nomos.c
+++ nomos.c
@@ -23,6 +23,8 @@
 #include "nomos.h"
 #include "util.h"
 #include "list.h"
+#include "licenses.h"
+#include "process.h"
 #include "nomos_regex.h"
 #include "_autodefs.h"
 
@@ -36,170 +38,144 @@ struct globals gl;
 struct curPkg cur;
 extern licText_t licText[];
 
-static char *progVersion = "NomosVersion: 2.25$";	/* NOT a CVS tag! */
-
 #define	_MAXFILESIZE	/* was 512000000-> 800000000 */	1600000000
 
 void Bail(int exitval)
 {
-	int ret;
 #ifdef	PROC_TRACE
-#ifdef	PROC_TRACE_SWITCH
-    if (gl.ptswitch)
-#endif	/* PROC_TRACE_SWITCH */
-	printf("== Bail(%d)\n", exitval);
+    traceFunc("== Bail(%d)\n", exitval);
 #endif	/* PROC_TRACE */
+
 #ifdef	DEBUG
-	printf("Bail(%d)\n", exitval);
-	if (exitval) {
-		printf("Bailing in dir \"%s\"\n", gl.cwd);
-		(void) mySystem("ls -lR");
-	}
+    printf("Bail(%d)\n", exitval);
+    if (exitval) {
+	printf("Bailing in dir \"%s\"\n", gl.cwd);
+	(void) mySystem("ls -lR");
+    }
 #endif	/* DEBUG */
-	(void) chdir(gl.initwd);
-	if (gl.mcookie != (magic_t) NULL) {
-		magic_close(gl.mcookie);
-	}
-	if (isDIR(gl.tmpdir)) {
-		removeDir(gl.tmpdir);
-	}
-#if	defined(MEMORY_TRACING) && defined(MEM_ACCT)
-	if (exitval) {
-		memCacheDump("Mem-cache @ Bail() time:");
-	}
+    (void) chdir(gl.initwd);
+    if (gl.mcookie != (magic_t) NULL) {
+	magic_close(gl.mcookie);
+    }
+    if (isDIR(gl.tmpdir)) {
+	removeDir(gl.tmpdir);
+    }
+#if defined(MEMORY_TRACING) && defined(MEM_ACCT)
+    if (exitval) {
+	memCacheDump("Mem-cache @ Bail() time:");
+    }
 #endif	/* MEMORY_TRACING && MEM_ACCT */
-/*
- * Question: do we want to wait for any processes that might still
- * be executing?
- */
-#if	0
-	(void) wait(&ret);	/* just in case... */
-#endif
-	if (isDIR(gl.mntpath)) {
-		(void) mySystem("%s %s > %s 2>&1", _REGEX(_UMNT_IMG),
-		    gl.mntpath, DEVNULL);
-		if (rmdir(gl.mntpath)) {
-			perror(gl.mntpath);
-		}
+    /*
+     * Question: do we want to wait for any processes that might still
+     * be executing?
+     */
+    if (isDIR(gl.mntpath)) {
+	(void) mySystem("%s %s > %s 2>&1", _REGEX(_UMNT_IMG),
+			gl.mntpath, DEVNULL);
+	if (rmdir(gl.mntpath)) {
+	    perror(gl.mntpath);
 	}
-	exit(exitval);
+    }
+    exit(exitval);
 }
 
 void alreadyDone(char *pathname)
 {
-	fprintf(stderr, "%s: %s already processed\n", gl.progName, pathname);
-	Bail(0);
+    fprintf(stderr, "%s: %s already processed\n", gl.progName, pathname);
+    Bail(0);
 }
 
 static void setOption(int val)
 {
 #ifdef	PROC_TRACE
-#ifdef	PROC_TRACE_SWITCH
-    if (gl.ptswitch)
-#endif	/* PROC_TRACE_SWITCH */
-	printf("== setOption(%x)\n", val);
+    traceFunc("== setOption(%x)\n", val);
 #endif	/* PROC_TRACE */
-	/*if (optionIsNotSet(val)) {*/
-		gl.progOpts |= val;
-	/*}*/
-	return;
+    gl.progOpts |= val;
+    return;
 }
 
 static void unsetOption(int val)
 {
 #ifdef	PROC_TRACE
-#ifdef	PROC_TRACE_SWITCH
-    if (gl.ptswitch)
-#endif	/* PROC_TRACE_SWITCH */
-	printf("== unsetOption(%x)\n", val);
-#endif	/* PROC_TRACE */
-	/*if (optionIsSet(val)) {*/
-		gl.progOpts &= ~val;
-	/*}*/
-	return;
+    traceFunc("== unsetOption(%x)\n", val);
+#endif /* PROC_TRACE */
+    gl.progOpts &= ~val;
+    return;
 }
 
 int optionIsSet(int val)
 {
 #ifdef	PROC_TRACE
-#ifdef	PROC_TRACE_SWITCH
-    if (gl.ptswitch)
-#endif	/* PROC_TRACE_SWITCH */
-	printf("== optionIsSet(%x)\n", val);
+    traceFunc("== optionIsSet(%x)\n", val);
 #endif	/* PROC_TRACE */
-	return(gl.progOpts & val);
-}
 
-int optionIsNotSet(int val)
-{
-#ifdef	PROC_TRACE
-#ifdef	PROC_TRACE_SWITCH
-    if (gl.ptswitch)
-#endif	/* PROC_TRACE_SWITCH */
-	printf("== optionIsNotSet(%x)\n", val);
-#endif	/* PROC_TRACE */
-	return(!optionIsSet(val));
+    return(gl.progOpts & val);
 }
-
 
 
 static void parseOpts(int argc, char **argv)
 {
-	register int optc, i;
-	register char *cp;
+    char *cp;
 
 #ifdef  PROC_TRACE
-#ifdef  PROC_TRACE_SWITCH
-    if (gl.ptswitch)
-#endif  /* PROC_TRACE_SWITCH */
-        printf("== parseOpts(%d, **argv)\n", argc);
+    traceFunc("== parseOpts(%d, **argv)\n", argc);
 #endif  /* PROC_TRACE */
 
-    /* CDB 	setOption(OPTS_NOREPORT|OPTS_SRCONLY); */
-	argc--; /* CDB, Lame, but will fix in transition */
-	*argv++; /* CDB, Ditto */
+    argc--; /* CDB, Lame, but will fix in transition */
+    *argv++; /* CDB, Ditto */
 	
-	if (argc != 1) {
-	    Fatal("Usage: %s <file>", gl.progName);
-	}
+    if (argc != 1) {
+	Fatal("Usage: %s <file>", gl.progName);
+    }
 
-	if (isDIR(NOMOS_TEMP)) {
-	    if (isFILE(NOMOS_TLOCK)) {
-		Fatal("Another %s instance is running!",
-		      gl.progName);
-	    }
-	    Fatal("Temp directory %s already exists!", NOMOS_TEMP);
+    if (isDIR(NOMOS_TEMP)) {
+	if (isFILE(NOMOS_TLOCK)) {
+	    Fatal("Another %s instance is running!",
+		  gl.progName);
 	}
+	Fatal("Temp directory %s already exists!", NOMOS_TEMP);
+    }
 
-	if (!isFILE(*argv)) {
-	    Fatal("\"%s\" is not a plain file", *argv);
-	}
-	if ((cp = strrchr(*argv, '/')) == NULL_STR) {
-	    cp = *argv;
-	} else {
-	    cp++;
-	}
-	if (mkdir(NOMOS_TEMP, 0755)) {
-	    perror(NOMOS_TEMP);
-	    Fatal("%s: cannot make temp directory %s", gl.progName);
-	}
-	if (mySystem("cp '%s' %s", *argv, NOMOS_TEMP)) {
-	    Fatal("Cannot copy %s to temp-directory", *argv);
-	}
-	strcpy(gl.target, NOMOS_TEMP);
-	strcpy(gl.targetFile, NOMOS_TEMP);
-	strcat(gl.targetFile, "/");
-	strcat(gl.targetFile, basename(*argv));
+    if (!isFILE(*argv)) {
+	Fatal("\"%s\" is not a plain file", *argv);
+    }
+    if ((cp = strrchr(*argv, '/')) == NULL_STR) {
+	cp = *argv;
+    } else {
+	cp++;
+    }
+    /*
+      CDB - Need to change this so that we use a unique temp
+      directory for each scan. Means we have to figure out how
+      to keep track of them.
+    */
+    if (mkdir(NOMOS_TEMP, 0755)) {
+	perror(NOMOS_TEMP);
+	Fatal("%s: cannot make temp directory %s", gl.progName);
+    }
+    if (mySystem("cp '%s' %s", *argv, NOMOS_TEMP)) {
+	Fatal("Cannot copy %s to temp-directory", *argv);
+    }
+    strcpy(gl.target, NOMOS_TEMP);
+    strcpy(gl.targetFile, NOMOS_TEMP);
+    strcat(gl.targetFile, "/");
+    strcat(gl.targetFile, basename(*argv));
 
-	gl.targetLen = strlen(gl.target);
-	return;
+    gl.targetLen = strlen(gl.target);
+    return;
 }
 
 static int fileDirScan(char *p, struct stat *st, int flag, struct FTW *s)
 {
-    register char *cp, *textp, *md, *what;
-    register item_t *ip, *xp;
-    register int saveBasename = 1, isPackage = 0, *bufp, notPkgFmt = 0;
+    char *cp;
+    char *textp;
+    char *md;
+    char *what;
+    item_t *ip;
+    item_t *xp;
+    int saveBasename = 1;
+    int isPackage = 0;
+    int notPkgFmt = 0;
     list_t *l;
 
     /*
@@ -227,31 +203,26 @@ static int fileDirScan(char *p, struct stat *st, int flag, struct FTW *s)
 		if (endsIn(cp, "src.rpm")) {
 		    l = &gl.srcpList;
 		    isPackage = 1;
-		}
-		else {
+		} else {
 		    l = &gl.instpList;
 		    isPackage = 1;
 		}
 		gl.nRpm++;
-	    }
-	    else {
+	    } else {
 		notPkgFmt++;
 		what = "RPM-format package";
 	    }
-	}
-	else if (endsIn(cp, ".deb") || endsIn(cp, ".udeb")) {
+	} else if (endsIn(cp, ".deb") || endsIn(cp, ".udeb")) {
 	    if (idxGrep(_FTYP_DEB, md, REG_ICASE)) {
 		l = &gl.instpList;
 		isPackage = 1;
 		gl.nDeb++;
-	    }
-	    else {
+	    } else {
 		notPkgFmt++;
 		what = "Debian binary package";
 	    }
-	}
-	else if (endsIn(cp, ".dsc")) {
-	    register char *textp;
+	} else if (endsIn(cp, ".dsc")) {
+	    char *textp;
 	    textp = mmapFile(p);
 	    notPkgFmt++;
 	    what = "Debian-source spec";
@@ -259,18 +230,17 @@ static int fileDirScan(char *p, struct stat *st, int flag, struct FTW *s)
 	} else {
 	    l = &gl.sarchList;
 	}
+
 	if (notPkgFmt) {
 	    Note("%s: NOT %s", p, what);
 	    l = &gl.regfList;
 	    saveBasename--;
 	}
-    }
-    else {			/* ASSUMPTION: we won't find a package here. */
+    } else {			/* ASSUMPTION: we won't find a package here. */
 	textp = mmapFile(p);
 	if (fileIsShar(textp, md)) {
 	    l = &gl.sarchList;
-	}
-	else {		/* regular: DON'T save the basename */
+	} else {		/* regular: DON'T save the basename */
 	    l = &gl.regfList;
 	    saveBasename--;
 	}
@@ -299,168 +269,154 @@ static int fileDirScan(char *p, struct stat *st, int flag, struct FTW *s)
 
 static void printListToFile(list_t *l, char *filename, char *mode)
 {
-	FILE *fp;
-	register int i;
-	register item_t *ip;
+    FILE *fp;
+    item_t *ip;
 
-	fp = fopenFile(filename, mode);
-	while ((ip = listIterate(l)) != NULL_ITEM) {
-		fprintf(fp, "%s\n", ip->str);
-	}
-	(void) fclose(fp);
-	return;
+    fp = fopenFile(filename, mode);
+    while ((ip = listIterate(l)) != NULL_ITEM) {
+	fprintf(fp, "%s\n", ip->str);
+    }
+    (void) fclose(fp);
+    return;
 }
 
 
 static void getFileLists(char *dirpath)
 {
-	register item_t *p;
-/*
- * Construct lists of source packages, installable-packages, plain files
- * and source archives, and classify all files in the distribution.
- */
+    /*
+     * Construct lists of source packages, installable-packages, plain files
+     * and source archives, and classify all files in the distribution.
+     */
 #ifdef	PROC_TRACE
-#ifdef	PROC_TRACE_SWITCH
-    if (gl.ptswitch)
-#endif	/* PROC_TRACE_SWITCH */
-	printf("== getFileLists(%s)\n", dirpath);
+    traceFunc("== getFileLists(%s)\n", dirpath);
 #endif	/* PROC_TRACE */
-	listInit(&gl.sarchList, 0, "source-archives list & md5sum map");
-	listInit(&gl.regfList, 0, "regular-files list");
-	listInit(&gl.corrList, 0, "corrupt-sources list");
-	listInit(&gl.hugeOkList, 0, "huge-files-OK list");
-	listInit(&gl.allLicList, 0, "all-licenses list");
+
+    listInit(&gl.sarchList, 0, "source-archives list & md5sum map");
+    listInit(&gl.regfList, 0, "regular-files list");
+    listInit(&gl.corrList, 0, "corrupt-sources list");
+    listInit(&gl.hugeOkList, 0, "huge-files-OK list");
+    listInit(&gl.allLicList, 0, "all-licenses list");
 #ifdef	SHOW_LOCATION
-	listInit(&gl.offList, 0, "buffer-offset list");
+    listInit(&gl.offList, 0, "buffer-offset list");
 #endif	/* SHOW_LOCATION */
 #ifdef	FLAG_NO_COPYRIGHT
-	listInit(&gl.nocpyrtList, 0, "no-copyright list");
+    listInit(&gl.nocpyrtList, 0, "no-copyright list");
 #endif	/* FLAG_NO_COPYRIGHT */
 #ifdef	SEARCH_CRYPTO
-	listInit(&gl.cryptoList, 0, "cryptography list");
+    listInit(&gl.cryptoList, 0, "cryptography list");
 #endif	/* SEARCH_CRYPTO */
 
-	listGetItem(&gl.regfList, gl.targetFile);
-	return;
+    listGetItem(&gl.regfList, gl.targetFile);
+    return;
 }
 
 
 static void setupEnviron(char *dirpath)
 {
-	char buf[myBUFSIZ];
-	register char *cp, *textp, *x;
-	register item_t *p;
-	int i;
-/*
- * default setup, expect to be a raw source archive (not a distro).
- */
+    char *cp;
+    
+    /*
+     * default setup, expect to be a raw source archive (not a distro).
+     */
 #ifdef	PROC_TRACE
-#ifdef	PROC_TRACE_SWITCH
-    if (gl.ptswitch)
-#endif	/* PROC_TRACE_SWITCH */
-	printf("== setupEnviron(%s)\n", dirpath);
+	traceFunc("== setupEnviron(%s)\n", dirpath);
 #endif	/* PROC_TRACE */
 
-	*gl.refVendor = NULL_CHAR;			/* default */
-	if ((cp = strrchr(dirpath, '/')) == NULL) {
-		Fatal("%s: directory has no slashes", gl.progName);
-	}
-	cp++;
-	strcpy(gl.arch, "");
-	(void) strcpy(gl.vendor, "source archive for");	/* default */
-	(void) strcpy(gl.prod, cp);
-	(void) strcpy(gl.prodName, cp);
-	(void) strcpy(gl.dist, dirpath);
-	return;
+    *gl.refVendor = NULL_CHAR;			/* default */
+    if ((cp = strrchr(dirpath, '/')) == NULL) {
+	Fatal("%s: directory has no slashes", gl.progName);
+    }
+    cp++;
+    strcpy(gl.arch, "");
+    (void) strcpy(gl.vendor, "source archive for");	/* default */
+    (void) strcpy(gl.prod, cp);
+    (void) strcpy(gl.prodName, cp);
+    (void) strcpy(gl.dist, dirpath);
+    return;
 }
 
 
 
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	char pathname[myBUFSIZ], *cp;
-	register int i, dbMtime, dbIsNewer = 0;
-	register list_t *l;
-	register item_t *ip;
-	double fTime, fFoo;
-	item_t *p;
-	struct tm *thyme;
+    char *cp;
+    int i;
 
 #ifdef	PROC_TRACE
-	printf("== main(%d, %p)\n", argc, argv);
+    traceFunc("== main(%d, %p)\n", argc, argv);
 #endif	/* PROC_TRACE */
 
 #ifdef	MEMORY_TRACING
-	mcheck(0);
+    mcheck(0);
 #endif	/* MEMORY_TRACING */
 #ifdef	GLOBAL_DEBUG
-	gl.DEEBUG = gl.MEM_DEEBUG = 0;
+    gl.DEEBUG = gl.MEM_DEEBUG = 0;
 #endif	/* GLOBAL_DEBUG */
 #ifdef	STOPWATCH
-	START_TIMER;
+    START_TIMER;
 #endif	/* STOPWATCH */
-/*
- * Record the progname name
- */
-	if ((cp = strrchr(*argv, '/')) == NULL_STR) {
-		(void) strcpy(gl.progName, *argv);
-	} else {
-		while (*cp == '.' || *cp == '/') {
-			cp++;
-		}
-		(void) strcpy(gl.progName, cp);
+    /*
+     * Record the progname name
+     */
+    if ((cp = strrchr(*argv, '/')) == NULL_STR) {
+	(void) strcpy(gl.progName, *argv);
+    } else {
+	while (*cp == '.' || *cp == '/') {
+	    cp++;
 	}
+	(void) strcpy(gl.progName, cp);
+    }
 
-	if (putenv("LANG=C") < 0) {
-		perror("putenv");
-		Fatal("Cannot set LANG=C in environment");
-	}
-	unbufferFile(stdout);
-	(void) umask(022);
-/*
- * Grab miscellaneous things from the environent
- */
-	if (getcwd(gl.initwd, sizeof(gl.initwd)) == NULL_STR) {
-		perror("getcwd");
-		Fatal("Cannot obtain starting directory");
-	}
-	(void) strcpy(gl.cwd, gl.initwd);
-	cp = getenv("SHELL");
-	(void) strcpy(gl.shell, cp ? cp : DEF_SHELL);
-	parseOpts(argc, argv);
-/*
- * chdir to target, call getcwd() to get real pathname; then, chdir back
- */
-	licenseInit();
-	gl.fSearch = gl.fSave = gl.eSave = gl.flags = 0;
-	gl.totBytes = 0.0;
-	gl.blkUpperLimit = _MAXFILESIZE/512;
+    if (putenv("LANG=C") < 0) {
+	perror("putenv");
+	Fatal("Cannot set LANG=C in environment");
+    }
+    unbufferFile(stdout);
+    (void) umask(022);
+    /*
+     * Grab miscellaneous things from the environent
+     */
+    if (getcwd(gl.initwd, sizeof(gl.initwd)) == NULL_STR) {
+	perror("getcwd");
+	Fatal("Cannot obtain starting directory");
+    }
+    (void) strcpy(gl.cwd, gl.initwd);
+    cp = getenv("SHELL");
+    (void) strcpy(gl.shell, cp ? cp : DEF_SHELL);
+    parseOpts(argc, argv);
+    /*
+     * chdir to target, call getcwd() to get real pathname; then, chdir back
+     */
+    licenseInit();
+    gl.fSearch = gl.fSave = gl.eSave = gl.flags = 0;
+    gl.totBytes = 0.0;
+    gl.blkUpperLimit = _MAXFILESIZE/512;
 
-	i = 0; /* CDB - Added so we don't have a custom Magic file */
-	if ((gl.mcookie = magic_open(MAGIC_NONE)) == (magic_t) NULL) {
-		Fatal("magic_open() fails!");
-	}
-	if (magic_load(gl.mcookie, i ? gl.magicFile : NULL_STR)) {
-		Fatal("magic_load() fails!");
-	}
-/*
- * We've saved the specified directory in 'gl.target'; now, normalize
- * the pathname (in case we were passed a symlink to another dir).
- */
-	changeDir(gl.target);	/* see if we can chdir to the target */
-	getFileLists(gl.target);
-	changeDir(gl.initwd);
-	listInit(&gl.licHistList, 0, "found-licenses list");
-	listInit(&gl.fLicFoundMap, 0, "file-license-found map");
-	listInit(&gl.parseList, 0, "license-components list");
+    i = 0; /* CDB - Added so we don't have a custom Magic file */
+    if ((gl.mcookie = magic_open(MAGIC_NONE)) == (magic_t) NULL) {
+	Fatal("magic_open() fails!");
+    }
+    if (magic_load(gl.mcookie, i ? gl.magicFile : NULL_STR)) {
+	Fatal("magic_load() fails!");
+    }
+    /*
+     * We've saved the specified directory in 'gl.target'; now, normalize
+     * the pathname (in case we were passed a symlink to another dir).
+     */
+    changeDir(gl.target);	/* see if we can chdir to the target */
+    getFileLists(gl.target);
+    changeDir(gl.initwd);
+    listInit(&gl.licHistList, 0, "found-licenses list");
+    listInit(&gl.fLicFoundMap, 0, "file-license-found map");
+    listInit(&gl.parseList, 0, "license-components list");
 
 #ifdef	STOPWATCH
-	END_TIMER;
-	PRINT_TIMER("init", 0);
+    END_TIMER;
+    PRINT_TIMER("init", 0);
 #endif	/* STOPWATCH */
-	processRawSource();
-	changeDir("/tmp");
-	(void) mySystem("rm -rf %s", NOMOS_TEMP);
-	Bail(0);
+    processRawSource();
+    changeDir("/tmp");
+    (void) mySystem("rm -rf %s", NOMOS_TEMP);
+    Bail(0);
 }
 
