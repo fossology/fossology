a0e2acf1b1fca75f1e6210b43d40d970aceaf131
Robert Gobeille <bob.gobeille@hp.com>
2009-06-05 18:24:14 +0000

    use postgresql.conf statement_timeout instead of changing is for fossology
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@2230 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/devel/libfossdb/libfossdb.c
+++ fossology/devel/libfossdb/libfossdb.c
@@ -162,7 +162,8 @@ void *	DBopen	()
   /* Got an open handle.  Set a delay of 2 minutes per command. */
   /* In milliseconds: 2*60*1000 = 120000 */
   /* Two minutes is too short for sqlagent, changing to 10 min 6/6/2008 bobg */
-  DBaccess(DB,"SET statement_timeout = 600000;");
+ // Use statement timeout in postgresql.conf 6/5/2009
+ // DBaccess(DB,"SET statement_timeout = 600000;");
   return((void *)DB);
 } /* DBopen() */
 
