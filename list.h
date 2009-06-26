a34e5863f7524ee62510d5bda6f654eb26ff2f6b
Channing D.Benson <benson.channing@hp.com>
2009-06-26 01:56:56 +0000

    Very basic implementation of handling multiple files (passed in on command line).
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology/agents/nomos@2259 a5643f74-2041-0410-a2b9-94ffef304a07

--- list.h
+++ list.h
@@ -22,15 +22,20 @@ void listInit(list_t *l, int size, char *label);
 void listClear(list_t *l, int deallocFlag);
 item_t *listGetItem(list_t *l, char *s);
 item_t *listAppend(list_t *l, char *s);
-#ifdef DEAD_CODE
+
+#ifdef notdef
 item_t *listLookupName(list_t *l, char *s);
 item_t *listLookupAlias(list_t *l, char *s);
-#endif /* DEAD_CODE */
+#endif /* notdef */
+
 item_t *listIterate(list_t *l);
 void listIterationReset(list_t *l);
 int listDelete(list_t *l, item_t *p);
 void listSort(list_t *l, int sortType);
 int listCount(list_t *l);
 void listDump(list_t *l, int verbose);
+
+#define DEALLOC_LIST	1
+#define NOTOUCH_LIST	0
 
 #endif /* _LIST_H */
