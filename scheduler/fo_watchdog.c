338e88bbf84f2dc0582a40245bfbbac77d8f3743
Robert Gobeille <bob.gobeille@hp.com>
2009-07-09 02:45:57 +0000

    sync to tot v 2325
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology-1.1.0@2327 a5643f74-2041-0410-a2b9-94ffef304a07

--- scheduler/fo_watchdog.c
+++ scheduler/fo_watchdog.c
@@ -38,33 +38,61 @@ int Verbose=0;
 
 #include "logging.h"
 #include "scheduler.h"
+#include "sched_utils.h"
 #include "lockfs.h"
 #include <libfossdb.h>
 
+void  Usage (char *Name)
+{
+  fprintf(stderr,"Usage: %s [options]\n",Name);
+  fprintf(stderr,"  -k :: Kill the watchdog\n");
+}
 
-/************************************************************************/
-/************************************************************************/
-/** Program *************************************************************/
-/************************************************************************/
-/************************************************************************/
 
 int	main	(int argc, char *argv[])
 {
-  int rv;
-  pid_t mypid = -1;
+  int rv, c;
+  pid_t mypid = -1, oldpid;
   char *ProcessName="fo_watchdog";
+  PGresult *Res;
+  PGconn   *Conn;
+
+  /* set user/group */
+  SetPuserPgrp(ProcessName);
+
+  /* check args */
+  while((c = getopt(argc,argv,"k")) != -1)
+  {
+    switch(c)
+    {
+      case 'k':
+        StopWatchdog();
+        exit(0);
+      default:
+        Usage(argv[0]);
+        exit(0);
+    }
+  }
+
+  /* Is another watchdog running? */
+  oldpid = LockGetPID(ProcessName);
+  if (oldpid)
+  {
+    if (Verbose) 
+    {
+      LogPrint("*** %s (pid %d) already running.  No need to start another.  ***\n", ProcessName, oldpid);
+    }
+    exit(0);
+  }
 
   /* start as daemon */
   if (daemon(0,(LogFile!=NULL)) != 0)
   {
     LogPrint("*** %s exiting due to failure to start as a daemon. %s ***\n", ProcessName, strerror(errno));
-    rv = UnlockName(ProcessName);
-    if (rv) LogPrint("*** Unlocking %s failed, %s ***\n", ProcessName, strerror(errno));
+    exit(-1);
   }
 
-  /* store lock for this process.
-   * If another fo_watchdog is running, just exit
-   */
+  /* store lock for this process.  */
   rv = LockName(ProcessName);
   if (rv < 0)
   {
@@ -72,18 +100,13 @@ int	main	(int argc, char *argv[])
     exit(-1);
   }
 
+  /* rv == 0  Got a new lock */
   if (rv == 0)
   {
     if (Verbose) 
       LogPrint("*** New %s successfully locked ***\n", ProcessName);
   }
-  else
-  {
-    if (Verbose) 
-      LogPrint("*** %s (pid %d) already running.  No need to start another.  ***\n", ProcessName, rv);
-    exit(0);
-  }
-  
+
   mypid = getpid();
   LogPrint("*** %s daemon started. PID %d ***\n", ProcessName, mypid);
 
@@ -93,20 +116,29 @@ int	main	(int argc, char *argv[])
     LogPrint("FATAL: %s unable to connect to database.  Terminating.\n", ProcessName);
     exit(-1);
   }
-  
+  Conn = DBgetconn(DB);
+
   while(1)
   {
     /* Check every 5 minutes to see if the scheduler is updating the scheduler_status table */
     sleep(5*60);
 
-    DBaccess(DB, "SELECT record_update from scheduler_status where agent_number='-1' and (now()-record_update) > '4 minutes' ");
-    if (DBdatasize(DB) > 0)
+    Res = PQexec(Conn, "SELECT record_update from scheduler_status where agent_number='-1' and (now()-record_update) < '4 minutes' ");
+
+    if (PQresultStatus(Res) != PGRES_TUPLES_OK)
+    {
+      LogPrint("*** %s FATAL error: %s ***\n", ProcessName, PQerrorMessage(Conn));
+      LogPrint("%s exiting\n", ProcessName);
+      exit(1);
+    } 
+
+    if (PQntuples(Res) == 0)
     {
       /* scheduler is dead. Log restart */
       LogPrint("*** Scheduler not responding: killing and restarting ***\n");
       
       /* kill the scheduler and clean up locks, db */
-      StopScheduler(1,0);
+      StopScheduler(1);
 
       // Restart scheduler as daemon, reset job queue
       rv = system( LIBEXECDIR "/fossology-scheduler -dR");
@@ -120,5 +152,6 @@ int	main	(int argc, char *argv[])
         LogPrint("*** Scheduler restarted successfully by %s ***\n", ProcessName);
       }
     }
+    PQclear(Res);
   }    /* while(1) */
 } /* main() */
