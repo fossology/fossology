6c44c24c4e469e04141be3a4f48d6bbc63c2c800
Robert Gobeille <bob.gobeille@hp.com>
2009-07-09 02:37:31 +0000

    seperate fo_watchdog from scheduler code. fo_watchdog will start with /etc/init.d/fossology start
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@2325 a5643f74-2041-0410-a2b9-94ffef304a07

--- scheduler/lockfs.c
+++ scheduler/lockfs.c
@@ -27,6 +27,7 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 
+#include "lockfs.h"
 #include "debug.h"
 #include "scheduler.h"
 #include "logging.h"
@@ -34,11 +35,13 @@
 /********************************************
  LockGetPID() returns PID of process that 
  owns a lock (or zero if there is no lock).
+ Stale lock files are removed.
  ********************************************/
 pid_t	LockGetPID	(char *ProcessName)
 {
   pid_t Pid = 0;
   int Handle;
+  int rc;
   char S[10];
 
   Handle = shm_open(ProcessName,O_RDONLY,0444);
@@ -55,10 +58,35 @@ pid_t	LockGetPID	(char *ProcessName)
   Pid = atoi(S);
   if (Pid < 2)
   {
-    /* bogus pid, remove lockfile */
+    /* bogus pid can't be less than 2 digits, remove lockfile */
     if (shm_unlink(ProcessName) == -1)
       LogPrint("*** failed to remove invalid lock file for %s (see LockGetPID). %s\n", ProcessName, strerror(errno));
     return(0);
+  }
+
+  /* make sure pid in lock file exists */
+  rc = kill(Pid,0); /* check if pid exists */
+  if (0 == rc)
+  {
+    /* pid is good, lock is good, return the pid */
+    return(Pid);
+  }
+  else
+  {
+    /* PID in lock is stale.
+     * Remove the lock 
+     */
+    if (Verbose) LogPrint("*** PID[%d] (%s) is stale. Attempt to unlock. \n",Pid, ProcessName);
+    if (UnlockName(ProcessName))
+    {
+      /* Unlock failed */
+      LogPrint("*** %s PID[%d] is stale.  Unlock failed %s\n",ProcessName, Pid, strerror(errno));
+    }
+    else
+      if (Verbose) LogPrint("*** %s  Unlock successful ***\n",ProcessName);
+
+   /* File is unlocked */
+   return(0);
   }
 
   if (Verbose) LogPrint("DEBUG: Successfully found PID[%s] in lock for %s.\n",S, ProcessName); 
@@ -82,86 +110,34 @@ int	UnlockName	(char *ProcessName)
  ********************************************/
 pid_t	LockName	(char *ProcessName)
 {
-  static int RecurseCheck=0;
   int Handle;
-  int rc, rv;
+  int rv;
   pid_t Pid = 0;
   char S[10];
 
-  /* Create the lock file */
+  /* return if there is already a lock */
+  Pid = LockGetPID(ProcessName);
+  if (Pid) return (Pid);
+
+  /* No lock, so create the lock file */
   Handle = shm_open(ProcessName,O_RDWR|O_CREAT|O_EXCL,0744);
   if (-1 == Handle)
   {
     /* create failed */
-    if (errno == EEXIST)  /* create failed because file exists */
-    {
-      /* At this point, we have an existing lock file.
-       * Make sure the pid in the lock file still exists.
-       * If it doesn't, remove the file and try to lock again.
-       */
-      Pid = LockGetPID(ProcessName);
-      if (!Pid) return (-1);
-
-      rc = kill(Pid,0); /* check if pid exists */
-      if (0 == rc)
-      {
-        /* pid is good, lock is good, return the pid */
-        return(Pid);
-      }
-      else
-      {
-        /* PID in lock is stale.
-         * Remove the lock and try again (1 time).
-         */
-        LogPrint("*** PID[%d] (%s) may be stale. Attempt to unlock. \n",Pid, ProcessName);
-        if (UnlockName(ProcessName))
-        {
-          /* Unlock failed */
-          LogPrint("*** %s PID[%d] is stale.  Unlock failed %s\n",ProcessName, Pid, strerror(errno));
-          return (-1);
-        }
-        else
-          LogPrint("*** %s  Unlock successful ***\n",ProcessName);
-
-        /* File is unlocked, try, one time,  to lock again */
-        if (!RecurseCheck)
-        {
-          RecurseCheck = 1;
-          return (LockName(ProcessName));
-        }
-        else
-        {
-          LogPrint("*** %s Unlock failed (max recursion depth = 1)\n",ProcessName);
-          return (-1);
-        }
-      }
-    }
-    else
-    {
-      /* create failed for reasons besides the file already existing */
-      LogPrint("*** %s failed on shm_open. %s\n", ProcessName, strerror(errno));
-      return (-1);
-    }
+    LogPrint("*** %s failed on shm_open. %s\n", ProcessName, strerror(errno));
+    return (-1);
   }
-  else
+  
+  /* Store the PID and return.  */
+  snprintf(S,sizeof(S),"%d          ",getpid());
+  if (Verbose) fprintf(stderr,"DEBUG: Storing PID[%s] in lock for %s.\n",S, ProcessName); 
+  rv = write(Handle,S,10);
+  if (rv < 1)
   {
-    /* New lock file created.
-       Store the PID and return.
-     */
-    snprintf(S,sizeof(S),"%d          ",getpid());
-    if (Verbose) fprintf(stderr,"DEBUG: Storing PID[%s] in lock for %s.\n",S, ProcessName); 
-    rv = write(Handle,S,10);
-    if (rv < 1)
-    {
-      LogPrint("*** %s failed to write pid to lock file.  %s\n", ProcessName, strerror(errno));
-      return(-1);
-    }
-    return(0);  // Successful new lock file
+    LogPrint("*** %s failed to write pid to lock file.  %s\n", ProcessName, strerror(errno));
+    return(-1);
   }
-
-  /* No execution path should get here  */
-  LogPrint("FATAL: LockName(%s) should't not be able to reach this code.\n", ProcessName);
-  return (-1);
+  return(0);  // Successful new lock file
 
 } /* LockName() */
 
