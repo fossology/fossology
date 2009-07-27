e53bd516eb35f109b403b6c9cf7ad4e8f610b2d7
Adam Bates <bates.adam@hp.com>
2009-07-27 19:37:49 +0000

    Added a usage statement to train_sent_model.cpp. Also added options for a file to read training files from, and an option to name the output model file.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@2390 a5643f74-2041-0410-a2b9-94ffef304a07

--- project/adam/sentence_based_classification/c/train_sent_model.cpp
+++ project/adam/sentence_based_classification/c/train_sent_model.cpp
@@ -1,23 +1,29 @@
 /*********************************************************************
-Copyright (C) 2009 Hewlett-Packard Development Company, L.P.
+  train_sent_model.cpp: Takes a collection of files with labeled sentence
+    boundaries and trains a MaxEnt model.
+  
+  Copyright (C) 2009 Hewlett-Packard Development Company, L.P.
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-version 2 as published by the Free Software Foundation.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2 as published by the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 *********************************************************************/
 
+extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <unistd.h>
+}
 #include "tokenizer.h"
 #include "list.h"
 #include "re.h"
@@ -27,31 +33,82 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "maxent_utils.h"
 #include "file_utils.h"
 
-int main(int argc, char **argv) {
+void print_usage(char *name) {
+    fprintf(stderr, "Usage: %s [options]\n",name);
+    fprintf(stderr, "   Creates a MaxEnt model for locating sentence breaks.\n");
+    fprintf(stderr, "   -f path ::  Read the paths of the training files from a file.\n");
+    fprintf(stderr, "   -o path ::  Save MaxEnt model at the specified path.\n");
+    fprintf(stderr, "               Defaults to './maxent.model'.\n");
+}
+
+int main(int argc, char *argv[]) {
     char *buffer;
-    int i,j;
+    int i,j,c;
     default_list *sentence_list = NULL;
     default_list *feature_type_list = NULL;
     default_list *label_list = NULL;
     int left_window = 3;
     int right_window = 3;
+    char *training_files = NULL;
+    char *model_file = NULL;
+
+    opterr = 0;
+    while ((c = getopt(argc, argv, "o:f:")) != -1) {
+        switch (c) {
+            case 'f':
+                training_files = optarg;
+
+                FILE *file;
+                file = fopen(training_files, "rb");
+                if (file==NULL) {
+                    fprintf(stderr, "File provided to -f parameter does not exists.\n");
+                    exit(1);
+                }
+
+                break;
+            case 'o':
+                model_file = optarg;
+                break;
+            case '?':
+                if (optopt == 'f' || optopt == 'o') {
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                } else if (isprint(optopt)) {
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                } else {
+                    fprintf(stderr, "Unknown option character `\\x%x'.\n",optopt);
+                }
+                exit(-1);
+            default:
+                print_usage(argv[0]);
+                exit(-1);
+        }
+    }
+
+    FILE *pFile;
+    pFile = fopen(training_files, "rb");
+    if (pFile==NULL) {
+        fputs("File error.\n", stderr);
+        exit(1);
+    }
 
     // Create a maxent model object and tell it that we will start to add
     // training data.
     MaxentModel m;
     m.begin_add_event();
-    for (i = 1; i<argc; i++) {
-        printf("Starting on %s...\n", argv[i]);
+    char *filename = NULL;
+    while (readline(pFile,&filename)!=EOF) {
+        printf("Starting on %s...\n", filename);
         buffer = NULL;
         sentence_list = NULL;
         feature_type_list = NULL;
         label_list = NULL;
-        openfile(argv[i],&buffer);
+        openfile(filename,&buffer);
         create_sentence_list(buffer,&sentence_list);
         create_features_from_sentences(&sentence_list,&feature_type_list, &label_list);
         // This function adds the training data to the model.
         create_model(m, &feature_type_list, &label_list, left_window, right_window);
         free(buffer);
+        free(filename);
         default_list_free(&sentence_list,&token_free);
         default_list_free(&feature_type_list,&feature_type_free);
         default_list_free(&label_list,&token_free);
@@ -60,7 +117,7 @@ int main(int argc, char **argv) {
 
     printf("Training MaxEnt model...\n");
     m.train(1000, "lbfgs");
-    m.save("SentenceModel.dat");
+    m.save(model_file);
 
     return(0);
 }
