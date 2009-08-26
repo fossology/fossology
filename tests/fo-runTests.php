b157c46d51008cba797a200ee20dc8f3738c916e
Mark Donohoe <mark.donohoe@hp.com>
2009-08-26 04:26:55 +0000

    new and improved fo-runTests, conforms to output standandard for parsing test output
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@2480 a5643f74-2041-0410-a2b9-94ffef304a07

--- tests/fo-runTests.php
+++ tests/fo-runTests.php
@@ -28,13 +28,23 @@
  *
  * @version "$Id$"
  *
+ * @todo make -l optional, should be able to just say fo-runTests foo.php and
+ * have it run foo.php
+ *
+ * @todo add a -n suite-name parameter, optional, if none given and we have
+ * an argument, then use the argument as the name, nothing?  Then use the
+ * string 'Generic FOSSology Test Suite'.
+ *
+ * @todo always print the starting message with the date and time, bonus is
+ * svn rev. (pass that in?) so the results parse correctly.
+ *
  * Created on March 18, 2009
  */
 
 $path = '/usr/local/simpletest' . PATH_SEPARATOR;
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 if (!defined('SIMPLE_TEST'))
-  define('SIMPLE_TEST', '/usr/local/simpletest/');
+define('SIMPLE_TEST', '/usr/local/simpletest/');
 
 /* simpletest includes */
 require_once SIMPLE_TEST . 'unit_tester.php';
@@ -42,20 +52,78 @@ require_once SIMPLE_TEST . 'reporter.php';
 require_once SIMPLE_TEST . 'web_tester.php';
 
 require_once ('TestEnvironment.php');
+require_once(TESTROOT . '/testClasses/timer.php');
 
-$Usage = "$argv[0] -l 'list of tests space seperated'\n or\n" .
-         "$argv[0] -l \"`ls`\" to run everything in the directory\n".
-         "\n$argv[0] -t 'Title' to supply an optional title\n";
 
-$options = getopt("l:t:");
+$Usage =  "Usage: $argv[0] options...
+           Options:
+             [ {<test-file.php> || a single test or
+             [-l 'list of tests'}] a list of tests, space seperated
+             [ -n <suite-name>]    optional test suite name
+             [ -t 'A Title']       optional title\n
+             To run everything in a directory $argv[0] -l \"`ls`\"\n" ;
+
+/*
+ * NOTE on parameters, can't guess a suite name from a list, so
+ * only get a suite name if -n is used or no -a and a single test to run.
+ */
+
+/*
+ "$argv[0] -l 'list of tests space seperated'\n or\n" .
+ "$argv[0] -l \"`ls`\" to run everything in the directory\n".
+ "\n$argv[0] -t 'Title' to supply an optional title\n";
+ */
+
+$options = getopt("l:n:t:");
+
+/*
+ Must have at least 1 argument (the test file to run)
+ */
+$RunList = array();
+$aTest   = NULL;
+$suite   = NULL;
+
+//print "argc,argv is:$argc\n";print_r($argv) . "\n";
+//print "argc is:$argc\n";
 if (empty($options)) {
-  print $Usage;
-  exit(1);
+  if ($argc < 2){
+    print $Usage;
+    exit(1);
+  }
+}
+if($argc >= 2){
+  /*
+   If first argument does not start with a '-' then it must be a test to run
+   */
+  $len = strspn($argv[1],'-');
+  if(strspn($argv[1],'-') == 0) {
+    $aTest = $argv[1];
+  }
 }
 if (array_key_exists("l",$options)) {
   /* split on spaces AND newlines so you can do a -l "`ls`" */
   $RunList = preg_split('/\s|\n/',$options['l']);
   //print "runx: runlist is:\n"; print_r($RunList) . "\n";
+}
+if (array_key_exists("n",$options)) {
+  $suite = $options['n'];
+  //print "DB: suite is:$suite\n";
+}
+// no suite specified
+if (!is_null($aTest)) {
+  if(file_exists($aTest)) {
+    $suite = $aTest;
+    $RunList = array($aTest);
+    //print "DB: runlist after assignment of aTest is:\n";print_r($RunList) . "\n";
+  }
+  else {
+    print "Error! File $aTest does not exist!\n";
+    exit(1);
+  }
+}
+// default name
+if(is_null($suite)) {
+  $suite = 'Generic FOSSology Test Suite';
 }
 $Title = NULL;
 if (array_key_exists("t",$options)) {
@@ -63,12 +131,19 @@ if (array_key_exists("t",$options)) {
   //print "DB: Title is:$Title\n";
 }
 
+//$Svn = `svnversion`;
+$start = new timer();
+$date = date('Y-m-d');
+$time = date('h:i:s-a');
+print "Starting $suite on: " . $date . " at " . $time . "\n";
+//print "Using Svn Version:$Svn\n";
 $Runtest = & new TestSuite("Fossology tests $Title");
 /*
  * tests will run serially...
  *
  * allow filenames without .php or with it
  */
+//print "DB: runlist is:\n";print_r($RunList) . "\n";
 foreach($RunList as $ptest) {
   if(preg_match('/^.*?\.php/',$ptest)) {
     $test = $ptest;
@@ -83,9 +158,17 @@ foreach($RunList as $ptest) {
  * leave the code below alone, it allows the tests to be run either by
  * the cli or in a web browser
  */
-if (TextReporter :: inCli())
-{
-  exit ($Runtest->run(new TextReporter()) ? 0 : 1);
+
+if (TextReporter::inCli()) {
+  $results = $Runtest->run(new TextReporter()) ? 0 : 1;
+  print "Ending $suite at: " . date('r') . "\n";
+  $elapseTime = $start->TimeAgo($start->getStartTime());
+  print "The suite $suite took {$elapseTime}to run\n\n";
+  exit($results);
 }
+
 $Runtest->run(new HtmlReporter());
+print "<pre>Ending $suite at: " . date('r') . "</pre>\n";
+$elapseTime = $start->TimeAgo($start->getStartTime());
+print "<pre>The suite $suite took {$elapseTime}to run</pre>\n";
 ?>
