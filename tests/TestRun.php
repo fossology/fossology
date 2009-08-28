3927d5c8829e30cadc9c4515120d7bb7980e731b
Mark Donohoe <mark.donohoe@hp.com>
2009-08-28 23:26:44 +0000

    reformatted code
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@2497 a5643f74-2041-0410-a2b9-94ffef304a07

--- tests/TestRun.php
+++ tests/TestRun.php
@@ -1,21 +1,20 @@
 <?php
 /***********************************************************
- Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License along
- with this program; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- ***********************************************************/
-
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************/
 /**
  * Test Run class for nightly regression tests
  *
@@ -30,14 +29,10 @@
  *
  * Created on Dec 18, 2008
  */
-
-class TestRun
-{
+class TestRun {
   public $srcPath;
   private $NotRunning = FALSE;
-  private $Running    = TRUE;
-
-
+  private $Running = TRUE;
   /**
    *  constructor
    *
@@ -52,42 +47,34 @@ class TestRun
    * @return resource object reference
    *
    */
-  public function __construct($srcPath=NULL)
-  {
-    if(empty($srcPath))
-    {
+  public function __construct($srcPath = NULL) {
+    if (empty($srcPath)) {
       // default
       $this->srcPath = '/home/fosstester/fossology';
     }
-    else
-    {
+    else {
       $scrPath = rtrim($scrPath, '/');
       $this->srcPath = $srcPath . "/fossology";
     }
     return;
   }
-
   public function checkOutTot() {
-
     $Tot = 'svn co https://fossology.svn.sourceforge.net/svnroot/fossology/trunk/fossology';
-
     $home = rtrim($this->srcPath, '/fossology');
-    if(chdir($home)){
+    if (chdir($home)) {
       $last = exec($Tot, $output, $rtn);
       //print "checkout results are, last and output:$last\n";
       //print_r($output) . "\n";
       if ($rtn != 0) {
         print "ERROR! Could not check out FOSSology sources at\n$Tot\n";
-        return(FALSE);
+        return (FALSE);
       }
     }
     else {
       print "ERROR! could not cd to $home\n";
-      return(FALSE);
+      return (FALSE);
     }
-
   }
-
   /**
    * checkScheduler
    *
@@ -95,99 +82,90 @@ class TestRun
    * 1. stop it with the standard /etc/initd./fossology stop
    * 2. If it is still running find the pid and kill -9
    */
-  private function checkScheduler()
-  {
+  private function checkScheduler() {
     $pLast = NULL;
     $pLast = exec('ps -ef | grep scheduler | grep -v grep', $results, $rtn);
     //print "DB: CKS: pLast is:$pLast\n";
-    if(empty($pLast)) { return($this->NotRunning); }
-    else { return($this->Running); }
+    if (empty($pLast)) {
+      return ($this->NotRunning);
+    }
+    else {
+      return ($this->Running);
+    }
   }
-
-  private function getSchedPid()
-  {
+  private function getSchedPid() {
     $psLast = NULL;
     $cmd = 'ps -ef | grep fossology-scheduler | grep -v grep';
-    $psLast = exec($cmd, $results, $rtn );
+    $psLast = exec($cmd, $results, $rtn);
     //print "DB: psLast is:$psLast\nresults are:\n"; print_r($results) . "\n";
     $parts = split(' ', $psLast);
     //print "parts is:\n"; print_r($parts) . "\n";
-    return($parts[5]);
+    return ($parts[5]);
   }
-
-  public function foPostinstall()
-  {
-    if(!chdir($this->srcPath))
-    {
+  public function foPostinstall() {
+    if (!chdir($this->srcPath)) {
       print "Error can't cd to $this->srcPath\n";
     }
     $foLast = exec('sudo /usr/local/lib/fossology/fo-postinstall > fop.out 2>&1', $results, $rtn);
-    if($rtn == 0)
-    {
-      return(TRUE);
+    if ($rtn == 0) {
+      return (TRUE);
     }
-    else { return(FALSE); }
+    else {
+      return (FALSE);
+    }
   }
-
-  public function makeInstall()
-  {
-    if(!chdir($this->srcPath))
-    {
+  public function makeInstall() {
+    if (!chdir($this->srcPath)) {
       print "Error can't cd to $this->srcPath\n";
     }
     $miLast = exec('sudo make install > make-install.out 2>&1', $results, $rtn);
-    if($rtn == 0)
-    {
-      if(array_search('Error', $results))
-      {
+    if ($rtn == 0) {
+      if (array_search('Error', $results)) {
         // TODO: write results out to: make-install.out
         //print "Found Error string in the Make output\n";
-        return(FALSE);
+        return (FALSE);
       }
-      else { return(TRUE); }
+      else {
+        return (TRUE);
+      }
     }
-    else { return(FALSE); }
+    else {
+      return (FALSE);
+    }
   }
-  public function makeSrcs()
-  {
-    if(!chdir($this->srcPath))
-    {
+  public function makeSrcs() {
+    if (!chdir($this->srcPath)) {
       print "Error can't cd to $this->srcPath\n";
     }
     $mcLast = exec('make clean > make-clean.out 2>&1', $results, $rtn);
     $makeLast = exec('make > make.out 2>&1', $results, $rtn);
-    if($rtn == 0 )
-    {
+    if ($rtn == 0) {
       //print "results of the make are:\n"; print_r($results) . "\n";
-      if(array_search('Error', $results))
-      {
+      if (array_search('Error', $results)) {
         //print "Found Error string in the Make output\n";
         // TODO: write results out to: make.out
-        return(FALSE);
+        return (FALSE);
       }
-      else { return(TRUE); }
+      else {
+        return (TRUE);
+      }
     }
-    else { return(FALSE); }
+    else {
+      return (FALSE);
+    }
   }
-
-  public function schedulerTest()
-  {
+  public function schedulerTest() {
     $StLast = exec('sudo /usr/local/lib/fossology/fossology-scheduler -t -L stdout > ST.out 2>&1', $results, $rtn);
-    if($rtn != 0 )
-    {
-      if(array_search('FATAL', $results))
-      {
-        return(FALSE);
+    if ($rtn != 0) {
+      if (array_search('FATAL', $results)) {
+        return (FALSE);
       }
-      return(FALSE);
+      return (FALSE);
     }
-    return(TRUE);
+    return (TRUE);
   }
-  public function setSrcPath($path)
-  {
-
+  public function setSrcPath($path) {
   }
-
   /**
    * startScheduler
    *
@@ -195,23 +173,21 @@ class TestRun
    * 1. stop it with the standard /etc/initd./fossology stop
    * 2. If it is still running find the pid and kill -9
    */
-  public function startScheduler()
-  {
-    if($this->checkScheduler() === $this->Running) { return($this->Running); }
-    else
-    {
+  public function startScheduler() {
+    if ($this->checkScheduler() === $this->Running) {
+      return ($this->Running);
+    }
+    else {
       $stdStart = exec("sudo /etc/init.d/fossology start > /dev/null 2>&1 &", $results, $rtn);
       sleep(5);
-      if($this->checkScheduler() === $this->Running)
-      {
-        return($this->Running);
+      if ($this->checkScheduler() === $this->Running) {
+        return ($this->Running);
       }
       else {
-        return($this->NotRunning);
+        return ($this->NotRunning);
       }
     }
   }
-
   /**
    * stopScheduler
    *
@@ -219,22 +195,20 @@ class TestRun
    * 1. stop it with the standard /etc/initd./fossology stop
    * 2. If it is still running find the pid and kill -9
    */
-  public function stopScheduler()
-  {
-    if($this->checkScheduler() === $this->NotRunning) { return(TRUE); }
-    else
-    {
+  public function stopScheduler() {
+    if ($this->checkScheduler() === $this->NotRunning) {
+      return (TRUE);
+    }
+    else {
       $stdStop = exec('sudo /etc/init.d/fossology stop 2>&1', $results, $rtn);
     }
     // still running, kill with -9
-    if($this->checkScheduler() === $this->Running)
-    {
+    if ($this->checkScheduler() === $this->Running) {
       $this->schedulerPid = $this->getSchedPid();
       $killLast = exec("sudo kill -9 $this->schedulerPid 2>&1", $results, $rtn);
     }
-    return(TRUE);
+    return (TRUE);
   }
-
   /**
    * svnUpdate
    *
@@ -242,15 +216,17 @@ class TestRun
    *
    * return boolean
    */
-  public function svnUpdate()
-  {
-    if(!chdir($this->srcPath))
-    {
+  public function svnUpdate() {
+    if (!chdir($this->srcPath)) {
       print "Error can't cd to $this->srcPath\n";
     }
     $svnLast = exec('svn update', $results, $rtn);
-    if($rtn != 0 ) { return(FALSE); }
-    else { return(TRUE); }
+    if ($rtn != 0) {
+      return (FALSE);
+    }
+    else {
+      return (TRUE);
+    }
   }
 }
 ?>
