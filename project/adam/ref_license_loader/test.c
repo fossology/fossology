8c277322e8ef62894fb584cb481a0f8b08502afd
Adam Bates <bates.adam@hp.com>
2009-09-08 19:47:57 +0000

    Everything works just need to write the sql to populate the database.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@2506 a5643f74-2041-0410-a2b9-94ffef304a07

--- project/adam/ref_license_loader/test.c
+++ project/adam/ref_license_loader/test.c
@@ -7,6 +7,85 @@
 #define MAX_PATHNAME 512
 #define MAX_FILENAME 256
 #define MAX_FILES 1000
+#define MAX_LINE 1000
+#define MAX_FIELD 32
+
+typedef struct meta_data {
+    char date[MAX_LINE+1];
+    char URL[MAX_LINE+1];
+    char shortname[MAX_LINE+1];
+    char fullname[MAX_LINE+1];
+    char OSIapproved[MAX_LINE+1];
+    char FSFfree[MAX_LINE+1];
+    char GPLv2compatible[MAX_LINE+1];
+    char GPLv3compatible[MAX_LINE+1];
+    char copyleft[MAX_LINE+1];
+    char Fedora[MAX_LINE+1];
+    char notes[MAX_LINE+1];
+} meta_data;
+
+typedef struct field {
+    char key[MAX_FIELD+1];
+    char value[MAX_LINE+1];
+} field;
+
+// reads a field and it value from a file
+// a field should look like this:
+// key: value\n
+field* read_field(FILE *fptr) {
+    field *f = malloc(sizeof(field));
+    int n = 0;
+    int c;
+
+    if (f==NULL) {
+        free(f);
+        return NULL;
+    }
+
+    n = 0;
+    while ((c = getc(fptr)) != EOF && n<MAX_FIELD) {
+        f->key[n] = c;
+        if (c == ':') {
+            f->key[n] = '\0';
+            break;
+        }
+        n++;
+    }
+    if (n >= MAX_FIELD) {
+        f->key[MAX_FIELD] = '\0';
+        fprintf(stderr, "READ ERROR: %d character without reaching a ':'. Stopping read process. Got '%s'.\n", MAX_FIELD, f->key);
+        free(f);
+        return NULL;
+    }
+    if (c == EOF) {
+        free(f);
+        return NULL;
+    }
+
+    n = 0;
+    while ((c = getc(fptr)) != EOF && n<MAX_LINE) {
+        if (c == ' ') {
+            continue;
+        }
+        f->value[n] = c;
+        if (c == '\n') {
+            f->value[n] = '\0';
+            break;
+        }
+        n++;
+    }
+    if (n >= MAX_LINE) {
+        f->value[MAX_LINE] = '\0';
+        fprintf(stderr, "READ ERROR: %d character without reaching a '\n'. Stopping read process. Got '%s'.\n", MAX_LINE, f->value);
+        free(f);
+        return NULL;
+    }
+    if (c == EOF) {
+        f->value[n] = '\0';
+    }
+
+    return f;
+}
 
 int main (int argc, char **argv) {
     DIR *dp;
@@ -47,41 +126,59 @@ int main (int argc, char **argv) {
     for (i = 0; i < file_count; i++) {
         char license_file[MAX_PATHNAME];
         char license_meta[MAX_PATHNAME];
+        meta_data data;
         FILE *meta_fptr;
         FILE *file_fptr;
-        char *meta_buffer;
         char *file_buffer;
         int len = strlen(file_list[i]);
+        field *f = NULL;
+
         if (strcmp(file_list[i]+(len-5),".meta") != 0) {
             continue;
         }
+
         strcpy(license_meta, path);
         strcat(license_meta, file_list[i]);
-        strcpy(license_file, path);
-        strncat(license_file, file_list[i], (len-5));
-        license_file[(len-5)] = '\0';
-        printf("%s -> %s\n", license_meta, license_file);
+        
         meta_fptr = fopen(license_meta,"rb");
         if (meta_fptr == NULL) {
             fprintf(stderr, "ERROR: Could not open %s for reading.\n", license_meta);
             continue;
         }
-        fseek(meta_fptr, 0, SEEK_END);
-        len = ftell(meta_fptr);
-        rewind(meta_fptr);
-        meta_buffer = malloc(len+1);
-        if (meta_buffer == NULL) {
-            fprintf(stderr, "Memory error.\n");
-            continue;
-        }
-        j = fread(meta_buffer, 1, len, meta_fptr);
-        if (j != len) {
-            fprintf(stderr, "Reading error");
-            continue;
-        }
-        meta_buffer[len] = '\0';
         
-        free(meta_buffer);
+        while (1) {
+            f = read_field(meta_fptr);
+            if (f == NULL) {
+                break;
+            } else {
+                printf("%s: %s\n", f->key, f->value);
+                if (strcmp(f->key,"Date") == 0) {
+                    strcpy(data.date,f->value);
+                } else if (strcmp(f->key,"URL") == 0) {
+                    strcpy(data.URL,f->value);
+                } else if (strcmp(f->key,"shortname") == 0) {
+                    strcpy(data.shortname,f->value);
+                } else if (strcmp(f->key,"fullname") == 0) {
+                    strcpy(data.fullname,f->value);
+                } else if (strcmp(f->key,"OSIapproved") == 0) {
+                    strcpy(data.OSIapproved,f->value);
+                } else if (strcmp(f->key,"FSFfree") == 0) {
+                    strcpy(data.FSFfree,f->value);
+                } else if (strcmp(f->key,"GPLv2compatible") == 0) {
+                    strcpy(data.GPLv2compatible,f->value);
+                } else if (strcmp(f->key,"GPLv3compatible") == 0) {
+                    strcpy(data.GPLv3compatible,f->value);
+                } else if (strcmp(f->key,"copyleft") == 0) {
+                    strcpy(data.copyleft,f->value);
+                } else if (strcmp(f->key,"Fedora") == 0) {
+                    strcpy(data.Fedora,f->value);
+                } else if (strcmp(f->key,"notes") == 0) {
+                    strcpy(data.notes,f->value);
+                } else {
+                    fprintf(stderr, "Unknown META field in %s\n\t%s\n", license_meta, f->key);
+                }
+            }
+        }
     }
 
     return 0;
