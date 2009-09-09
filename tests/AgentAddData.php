891cee5c99c7f026fdf91c1e1a4805f7ac735d1e
Mark Donohoe <mark.donohoe@hp.com>
2009-09-09 21:01:51 +0000

    changed url to work around vm networking issues
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@2509 a5643f74-2041-0410-a2b9-94ffef304a07

--- tests/AgentAddData.php
+++ tests/AgentAddData.php
@@ -67,7 +67,7 @@ class uploadAgentDataTest extends fossologyTestCase {
     $urlList = array('http://downloads.sourceforge.net/simpletest/simpletest_1.0.1.tar.gz',
                          'http://www.gnu.org/licenses/gpl-3.0.txt',
                          'http://www.gnu.org/licenses/agpl-3.0.txt',
-                         'http://snape.west/~fosstester/fossDirsOnly.tar.bz2');
+                         'http://osrb-1.fc.hp.com/~fosstester/fossDirsOnly.tar.bz2');
     /* upload the archives using the upload from file menu */
     //$desciption = "File $upload uploaded by Upload Data Test";
     print "Starting file uploads\n";
