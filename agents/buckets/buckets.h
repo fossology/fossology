53f1b1802e4ff8f1917e482af9346e5765c99032
Robert Gobeille <bob.gobeille@hp.com>
2010-01-27 23:30:36 +0000

    This is the first phase of reimplementing buckets. This version walks an uploadfile tree in the order needed (depth first) for bucket processing.  The actual bucket processing functions are just stubs.
    This replaces completely replaces the previous version of buckets.c which
    was just a copy of some of glen's code.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@2733 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ agents/buckets/buckets.h
@@ -0,0 +1,65 @@
+/***************************************************************
+ Copyright (C) 2010 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+***************************************************************/
+#ifndef _BUCKETS_H
+#define _BUCKETS_H 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
+#include <regex.h>
+#include <libgen.h>
+#include <getopt.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "libfossdb.h"
+#include "libfossagent.h"
+#include "libfossrepo.h"
+
+#define FUNCTION
+
+#ifdef SVN_REV
+char BuildVersion[]="Build version: " SVN_REV ".\n";
+#endif /* SVN_REV */
+
+/* Bucket definition */
+struct bucketdef_struct 
+{
+  int      bucket_type;
+  regex_t  compRegex;     /* compiled regex if type=3 */
+  char    *execFilename;  /* name of file to exec if type=4.  Files are in DATADIR */
+  int     *match_only;    /* array of rf_pk's if type=2 */
+  int    **match_every;   /* array of arrays of rf_pk's if type=1 */
+};
+typedef struct bucketdef_struct *pbucketdef_t;
+
+int walkTree(PGconn *pgConn, pbucketdef_t *bdeflist, int agent_pk, long uploadtree_pk);
+int processLeaf(PGconn *pgConn, pbucketdef_t *bdeflist, long pfile_pk, int agent_pk);
+long *getLeafBuckets(PGconn *pgConn, pbucketdef_t *bdeflist, long pfile_pk);
+long *getContainerBuckets(PGconn *pgConn, pbucketdef_t *bdeflist, long pfile_pk);
+int writeBuckets(PGconn *pgConn, long pfile_pk, long *bucketList, int agent_pk);
+int checkPQresult(PGresult *result, char *sql, char *FcnName, int LineNumb);
+int processed(PGconn *pgConn, int agent_pk, long pfile_pk);
+int processRootNode(PGconn *pgConn, pbucketdef_t *bucketDefList, int agent_pk, long uploadtree_pk);
+
+
+#endif /* _BUCKETS_H */
