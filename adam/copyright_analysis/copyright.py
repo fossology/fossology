ad9912ab8e70673c17efb3478cc9db2f05ccd3a4
Adam Bates <bates.adam@hp.com>
2010-02-02 14:49:37 +0000

    Fixed the testing interface so that it doesn't print output to stdout. Also fixed the one-shot agent to match the new testing interface. Added the database code to write the copyright_test table. Added code to write copyright_test entries. Created a symlink to the main Makefile.conf so we can install the agent and ui stuff to the correct locations.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/project@2756 a5643f74-2041-0410-a2b9-94ffef304a07

--- adam/copyright_analysis/copyright.py
+++ adam/copyright_analysis/copyright.py
@@ -22,6 +22,7 @@ import sys
 import copyright_library as library
 import cPickle as pickle
 from optparse import OptionParser
+import libfosspython
 
 import psyco
 psyco.full()
@@ -60,8 +61,15 @@ def main():
             help="Path to the files to analyze.")
     optparser.add_option("-c", "--analyze-from-command-line", action="store_true",
             help="File to be analyzed will be passed over the command line.")
+    optparser.add_option("--setup-database", action="store_true",
+            help="Creates the tables for copyright analysis that the fossology database needs.")
+    optparser.add_option("--agent", action="store_true",
+            help="Starts up in agent mode. Files will be read from stdin.")
 
     (options, args) = optparser.parse_args()
+
+    if options.setup_database:
+        return(setup_database())
 
     if not options.model:
         print >> sys.stderr, 'You must specify a model file for all phases of the algorithm.\n\n'
@@ -85,11 +93,123 @@ def main():
         files = [line.rstrip() for line in open(options.analyze_from_file).readlines()]
         for file in files:
             results = library.label_file(file,model)
+            print "%s :: " % (file)
+            if len(results) == 0:
+                print "No copyrights"
+            for i in range(len(results)):
+                print "\t[%d:%d]" % (results[i][0], results[i][1])
 
     if options.analyze_from_command_line:
         files = args
         for file in files:
             results = library.label_file(file,model)
+            print "%s :: " % (file)
+            if len(results) == 0:
+                print "No copyrights"
+            for i in range(len(results)):
+                print "\t[%d:%d]" % (results[i][0], results[i][1])
+
+    if options.agent:
+         return(agent())   
+
+def agent():
+    db = None
+    try:
+        db = libfosspython.FossDB()
+    except:
+        print >> sys.stderr, 'ERROR: Something is broken. Could not connect to database.'
+        return 1
+
+    line = sys.stdin.readline().strip()
+    while line:
+        if re.match("[0-9]+ [0-9a-f]+\.[0-9a-f]+\.[0-9]+", line):
+            (pfile, file) = re.findall("([0-9]+) ([0-9a-f]+\.[0-9a-f]+\.[0-9]+)", line)[0]
+            path = libfosspython.repMkPath('files', file)
+            offsets = library.label_file(file,model)
+            if len(offsets) == 0:
+                result = db.access("INSERT INTO copyright_test (agent_fk, pfile_fk, copy_startbyte, copy_endbyte)"
+                    "VALUES (1, '%s', NULL, NULL);" % (pfile))
+            else:
+                for i in range(len(offsets)):
+                    result = db.access("INSERT INTO copyright_test (agent_fk, pfile_fk, copy_startbyte, copy_endbyte)"
+                        "VALUES (1, '%s', %d, %d);" % (pfile, offsets[i][0], offsets[i][1]))
+        elif re.match("quit", line):
+            print "BYE."
+            break
+        else:
+            print >> sys.stderr, "ERROR: unrecognized command."
+
+        line = sys.stdin.readline().strip()
+
+    return 0
+
+def setup_database():
+    db = None
+    try:
+        db = libfosspython.FossDB()
+    except:
+        print >> sys.stderr, 'ERROR: Something is broken. Could not connect to database.'
+        sys.exit(1)
+
+    result = db.access("CREATE SEQUENCE copyright_test_agent_fk_seq "
+        "START WITH 1 "
+        "INCREMENT BY 1 "
+        "NO MAXVALUE "
+        "NO MINVALUE "
+        "CACHE 1;")
+    if result != 0:
+        print >> sys.stderr, "ERROR: Couldn't create copyright_test_agent_fk_seq."
+        sys.exit(1)
+
+    result = db.access("ALTER TABLE public.copyright_test_agent_fk_seq OWNER TO fossy;")
+    if result != 0:
+        print >> sys.stderr, "ERROR: Couldn't alter copyright_test_agent_fk_seq."
+        sys.exit(1)
+
+    result = db.access("CREATE SEQUENCE copyright_test_ct_pk_seq "
+        "START WITH 1 "
+        "INCREMENT BY 1 "
+        "NO MAXVALUE "
+        "NO MINVALUE "
+        "CACHE 1;")
+    if result != 0:
+        print >> sys.stderr, "ERROR: Couldn't create copyright_test_ct_pk_seq."
+        sys.exit(1)
+
+    result = db.access("ALTER TABLE public.copyright_test_ct_pk_seq OWNER TO fossy;")
+    if result != 0:
+        print >> sys.stderr, "ERROR: Couldn't alter copyright_test_ct_pk_seq."
+        sys.exit(1)
+
+    result = db.access("CREATE SEQUENCE copyright_test_pfile_fk_seq "
+        "START WITH 1 "
+        "INCREMENT BY 1 "
+        "NO MAXVALUE "
+        "NO MINVALUE "
+        "CACHE 1;")
+    if result != 0:
+        print >> sys.stderr, "ERROR: Couldn't create copyright_test_pfile_fk_seq."
+        sys.exit(1)
+
+    result = db.access("ALTER TABLE public.copyright_test_pfile_fk_seq OWNER TO fossy;")
+    if result != 0:
+        print >> sys.stderr, "ERROR: Couldn't alter copyright_test_pfile_fk_seq."
+        sys.exit(1)
+
+    result = db.access("CREATE TABLE copyright_test ( "
+        "ct_pk bigint DEFAULT nextval('copyright_test_ct_pk_seq'::regclass) NOT NULL, "
+        "agent_fk bigint DEFAULT nextval('copyright_test_agent_fk_seq'::regclass) NOT NULL, "
+        "pfile_fk bigint DEFAULT nextval('copyright_test_pfile_fk_seq'::regclass) NOT NULL, "
+        "copy_startbyte integer, "
+        "copy_endbyte integer);")
+    if result != 0:
+        print >> sys.stderr, "ERROR: Couldn't create license_test table."
+        sys.exit(1)
+
+    result = db.access("ALTER TABLE public.copyright_test OWNER TO fossy;")
+    if result != 0:
+        print >> sys.stderr, "ERROR: Couldn't alter license_test table."
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
