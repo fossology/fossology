903d16909c6d53bd9e589a2336460bbfa98fecbf
Adam Bates <bates.adam@hp.com>
2010-03-01 21:39:45 +0000

    Changed Makefile to use standard dependencies for the fossology libraries. Also modified setup.py to used the .a files from libfoss* instead of the .o's
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@2827 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/devel/libfosspython/setup.py
+++ fossology/devel/libfosspython/setup.py
@@ -4,7 +4,7 @@ from Pyrex.Distutils import build_ext
 setup(
   name = "PyrexDevelWrapper",
   ext_modules=[ 
-    Extension("libfosspython", ["libfosspython.pyx"], extra_objects = ["../libfossagent/libfossagent.o", "../libfossdb/libfossdb.o", "../libfossrepo/libfossrepo.o"], libraries = ["pq"], include_dirs = ["/usr/include/postgresql"])
+    Extension("libfosspython", ["libfosspython.pyx"], extra_objects = ["../libfossagent/libfossagent.a", "../libfossdb/libfossdb.a", "../libfossrepo/libfossrepo.a"], libraries = ["pq"], include_dirs = ["/usr/include/postgresql"])
     ],
   cmdclass = {'build_ext': build_ext}
 )
