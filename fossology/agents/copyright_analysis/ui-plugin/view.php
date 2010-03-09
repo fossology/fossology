5b147e9dcd33a768ff84c52f50302a3ff8014c89
Adam Bates <bates.adam@hp.com>
2010-03-09 22:27:25 +0000

    Corrected copyright dates.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@2861 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/agents/copyright_analysis/ui-plugin/view.php
+++ fossology/agents/copyright_analysis/ui-plugin/view.php
@@ -1,6 +1,6 @@
 <?php
 /***********************************************************
- Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2010 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
