0b49a0e84ccad32ee86ce96d0297fed862d44973
Mark Donohoe <mark.donohoe@hp.com>
2010-03-19 04:05:35 +0000

    start of copyright regression tests
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@2902 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/tests/copyright/verifySimpletest.php
+++ fossology/tests/copyright/verifySimpletest.php
@@ -38,7 +38,7 @@ global $URL;
  * This test verifies that the archive 3files.tar.bz2 contains the correct set
  * of copyrights, emails and urls.
  */
-class verify3filesCopyright extends fossologyTestCase
+class verifySimpletest extends fossologyTestCase
 {
 	public $mybrowser;
 	public $host;
@@ -53,7 +53,7 @@ class verify3filesCopyright extends fossologyTestCase
 		global $name;
 		global $safeName;
 
-		$name = '3files.tar.bz2';
+		$name = 'simpletest_1.0.1.tar.gz';
 		$safeName = escapeDots($name);
 		$this->host = getHost($URL);
 		$this->Login();
@@ -62,44 +62,56 @@ class verify3filesCopyright extends fossologyTestCase
 		$page = $this->mybrowser->get($URL);
 		$page = $this->mybrowser->clickLink('Browse');
 		$this->assertTrue($this->myassertText($page, '/Browse/'),
-     "verify3files FAILED! Could not find Browse menu\n");
-		$page = $this->mybrowser->clickLink('copyright');
-		$this->assertTrue($this->myassertText($page, '/Copyright/'),
-     "verify3files FAILED! Could not find copyright folder\n");
+     "verifySimpletest FAILED! Could not find Browse menu\n");
+		$this->assertTrue($this->myassertText($page, "/$safename/"),
+     "verifySimpleTest FAILED! Could not find simpletest_1.0.1.tar.gz upload\n");
 		$result = $this->myassertText($page, "/$name/");
 		if(!($result)) { exit(FALSE); }
 	}
 
-	function test3filesCopyright()
+	function testSimpletest()
 	{
 		global $URL;
 		global $name;
 		global $safeName;
+		
+		$copyRights = array('1' =>'(C) 1991, 1999 Free Software Foundation, Inc. 51');
+		$emailStd   = array('4' => 'secret@www.lastcraft.com',
+												'2' => 'marcus@lastcraft.com',
+												'2' => 'name@example.com',
+												'2' => 'password@www.somewhere.com',
+												'2' => 'secret@host.com',
+												'2' => 'secret@this.com',
+												'2' => 'secret@www.here.com',
+												'2' => 'simpletest-support@lists.sourceforge.net',
+												'1' => 'development@domain51.com',
+												'1' => 'password@somewhere.com',
+												'1' => 'secret@this.host',
+												'1' => 'username@www.somewhere.com'
+												);
 
-		print "starting Verify3filesCopyright test\n";
+		print "starting VerifySimpletest test\n";
 		$page = $this->mybrowser->clickLink('Browse');
 		$this->assertTrue($this->myassertText($page, '/Browse/'),
-             "verify3files FAILED! Could not find Browse menu\n");
-		$page = $this->mybrowser->clickLink('copyright');
-		//print "************ Page after upload link *************\n$page\n";
+             "verifySimpleTest FAILED! Could not find Browse menu\n");
 		$this->assertTrue($this->myassertText($page, "/Browse/"),
-       "verify3files FAILED! Browse Title not found\n");
+       "verifySimpleTest FAILED! Browse Title not found\n");
 		$this->assertTrue($this->myassertText($page, "/$safeName/"),
-       "verify3files FAILED! did not find $name\n");
+       "verifySimpleTest FAILED! did not find $name\n");
 		$this->assertTrue($this->myassertText($page, "/>View</"),
-       "verify3files FAILED! >View< not found\n");
+       "verifySimpletest FAILED! >View< not found\n");
 		$this->assertTrue($this->myassertText($page, "/>Meta</"),
-       "verify3files FAILED! >Meta< not found\n");
+       "verifySimpletest FAILED! >Meta< not found\n");
 		$this->assertTrue($this->myassertText($page, "/>Download</"),
-       "verify3files FAILED! >Download< not found\n");
+       "verifySimpletest FAILED! >Download< not found\n");
 
 		/* Select archive */
 		$page = $this->mybrowser->clickLink($name);
 		//print "************ Page after select foss archive *************\n$page\n";
-		$this->assertTrue($this->myassertText($page, "/Affero-v1\.0/"),
-      "verify3files FAILED! '3files/' not found\n");
-		$this->assertTrue($this->myassertText($page, "/3 items/"),
-      "verify3files FAILED! '1 item' not found\n");
+		$this->assertTrue($this->myassertText($page, "/simpletest\//"),
+      "verifySimpletest FAILED! 'simpletest/' not found\n");
+		$this->assertTrue($this->myassertText($page, "/1 item/"),
+      "verifySimpletest FAILED! '1 item' not found\n");
 
 
 		/* Select the License link to View License Historgram */
@@ -107,58 +119,61 @@ class verify3filesCopyright extends fossologyTestCase
 		$mini = new parseMiniMenu($page);
 		$miniMenu = $mini->parseMiniMenu();
 		$url = makeUrl($this->host, $miniMenu['Copyright/Email/Url']);
-		if($url === NULL) { 
-			$this->fail("verify3files Failed, host is not set or url cannot be made"); 
-		}
+		if($url === NULL) { $this->fail("verifySimpletest Failed, host is not set"); }
 
 		$page = $this->mybrowser->get($url);
 		//print "page after get of $url is:\n$page\n";
 		$this->assertTrue($this->myassertText($page, '/Copyright\/Email\/Url Browser/'),
-          "verify3files FAILED! Copyright/Email/Url Browser Title not found\n");
-		$this->assertTrue($this->myassertText($page, '/Total Copyrights: 3/'),
-        "verify3files FAILED! Total copyrights does not equal 3\n");
+          "verifySimpleTest FAILED! Copyright/Email/Url Browser Title not found\n");
+		$this->assertTrue($this->myassertText($page, '/Total Copyrights: 1/'),
+        "verifySimpleTest FAILED! Total copyrights does not equal 1\n");
 
-		// get the count, show links and text or link
-		$copyR = new domParseLicenseTbl($page, 'copyright');
-		$copyR->parseLicenseTbl();
-		if(empty($copyR->hList)) {
+		// get the 'Show' links and License color links
+		$ct = new domParseLicenseTbl($page, 'copyright');
+		$ct->parseLicenseTbl();
+		// empty table? check no copyrights.
+		if($ct->noRows) {
+			$this->assertTrue($this->myassertText($page, '/Total Copyrights: 0/'),
+        "verifySimpleTest FAILED! Total copyrights does not equal 0\n");
+			$this->assertTrue($this->myassertText($page, '/Unique Copyrights: 0/'),
+        "verifySimpleTest FAILED! Total copyrights does not equal 0\n");
+		}
+		else if(empty($ct->hList)) {
 			$this->fail("FATAL! table with id=copyright was not found on" .
 									"the page, nothing to process, stopping test\n");
 			exit(1);
 		}
+		else {
+			//print "copyright list is:\n"; print_r($ct->hList) . "\n";
+			foreach($ct->hList as $list) {
+				//print "list is:$list\n";print_r($list) . "\n";
+				foreach($copyRights as $ccount => $text){
+					// make sure there are no imbeded new lines
+					$cleanText = str_replace("\n", '', $list['textOrLink']);
+					$trimct = trim($cleanText);
+					$trimText = trim($text);
+					if($trimct == $trimText) {
+						$this->pass();
+					}
+					else {
+						$this->fail("verifySimpleTest FAILED! Should be $trimText " .
+    	 			"based on the Standard got:$trimct");
+					}
+					$this->assertEqual($ccount, $list['count'],
+    			"verifySimpleTest FAILED! Should be $list[count] files
+    	 		based on Standard $ccount got:$list[count]\n");
+				}
+			}
+		}
 
 		$email = new domParseLicenseTbl($page, 'copyrightemail');
 		$email->parseLicenseTbl();
-		// empty table?, verify counts are zero
-		if($email->noRows) {
-			$this->assertTrue($this->myassertText($page, '/Total Emails: 0/'),
-        "verify3files FAILED! Total Emails does not equal 0\n");
-			$this->assertTrue($this->myassertText($page, '/Unique Emails: 0/'),
-        "verify3files FAILED! Unique Emails does not equal 0\n");
-		}
-		// noRows found, if list is empty, could not find the table on the page.
-		// This is a fatal error.
-		else if(empty($email->hList)) {
+		if(empty($email->hList)) {
 			$this->fail("FATAL! table with id=copyrightemail was not found on" .
 									"the page, nothing to process, stopping test\n");
 			exit(1);
 		}
-		$urls = new domParseLicenseTbl($page, 'copyrighturl');
-		$urls->parseLicenseTbl();
-		// empty table?, verify counts are zero
-		if($urls->noRows) {
-			$this->assertTrue($this->myassertText($page, '/Total URLs: 0/'),
-        "verify3files FAILED! Total URL's does not equal 0\n");
-			$this->assertTrue($this->myassertText($page, '/Unique URLs: 0/'),
-        "verify3files FAILED! Unique URL's does not equal 0\n");
-		}
-		// noRows found, if list is empty, could not find the table on the page.
-		// This is a fatal error.
-		else if(empty($urls->hList)) {
-			$this->fail("FATAL! table with id=copyrighturl was not found on" .
-									"the page, nothing to process, stopping test\n");
-			exit(1);
-		}
+		//print "email list is:\n"; print_r($email->hList) . "\n";
 	}
 }
 ?>
