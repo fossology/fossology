8a5c5e7e8dfff82922393b952f52c36d9a0fe993
Robert Gobeille <bob.gobeille@hp.com>
2010-06-18 14:38:17 +0000

    update copyright dates
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@3267 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui/plugins/ui-about.php
+++ ui/plugins/ui-about.php
@@ -1,6 +1,6 @@
 <?php
 /***********************************************************
- Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2008-2010 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -34,7 +34,7 @@ class ui_about extends FO_Plugin
   var $LoginFlag  = 0;
 
   var $_Project="FOSSology";
-  var $_Copyright="Copyright (C) 2007-2008 Hewlett-Packard Development Company, L.P.";
+  var $_Copyright="Copyright (C) 2007-2010 Hewlett-Packard Development Company, L.P.";
   var $_Text="This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2 as published by the Free Software Foundation.\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.";
 
   function Output()
