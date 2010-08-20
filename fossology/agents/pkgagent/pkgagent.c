16e497d72578a55895fa037247ed57ab17be3e73
Larry Shi <yao-bin.shi@hp.com>
2010-08-20 08:51:25 +0000

    in cunit framework, unlock the dependency between coverage and test
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@3404 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/agents/pkgagent/pkgagent.c
+++ fossology/agents/pkgagent/pkgagent.c
@@ -1,5 +1,5 @@
 /***************************************************************
- Copyright (C) 2009 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2010 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -26,79 +26,11 @@
  *
  * Pkgagent get Debian source package info from .dsc file.
  */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <signal.h>
-#include <libgen.h>
-#include <time.h>
-
-#include <sys/wait.h>
-
-#include "libfossrepo.h"
-#include "libfossdb.h"
-#include "libfossagent.h"
-#include "rpmlib.h"
-#include "rpmts.h"
-#include "rpmlog.h"
-#include "rpmmacro.h"
+#include "pkgagent.h"
 
 #ifdef SVN_REV
 char BuildVersion[]="Build version: " SVN_REV ".\n";
 #endif /* SVN_REV */
-
-#define MAXCMD 5000
-
-#define NO      0
-#define YES     1
-#define TRUE YES
-#define FALSE NO
-
-struct rpmpkginfo {
-  char pkgName[256];
-  char pkgAlias[256];
-  char pkgArch[64];
-  char version[64];
-  char rpmFilename[256];
-  char license[512];
-  char group[128];
-  char packager[1024];
-  char release[64];
-  char buildDate[128];
-  char vendor[128];
-  char url[256];
-  char sourceRPM[256];
-  char summary[MAXCMD];
-  char description[MAXCMD];
-  long pFileFk;
-  char pFile[MAXCMD];
-  char **requires;
-  int req_size;
-};
-struct rpmpkginfo *glb_rpmpi;
-
-struct debpkginfo {
-  char pkgName[MAXCMD];
-  char source[MAXCMD];
-  char version[MAXCMD];
-  char section[MAXCMD];
-  char priority[MAXCMD];
-  char pkgArch[MAXCMD];
-  int installedSize;
-  char maintainer[MAXCMD];
-  char homepage[MAXCMD];
-  char summary[MAXCMD];
-  char description[MAXCMD];
-  long pFileFk;
-  char pFile[MAXCMD];
-  char **depends;
-  int dep_size;
-  char uploaders[MAXCMD];
-  char format[MAXCMD];
-  char standardsVersion[MAXCMD];
-};
-struct debpkginfo *glb_debpi;
 
 int tag[15] = {RPMTAG_NAME,
 		RPMTAG_EPOCH,
@@ -116,15 +48,6 @@ int tag[15] = {RPMTAG_NAME,
 		RPMTAG_DESCRIPTION,
 		RPMTAG_REQUIRENAME
         	};
-
-
-void *DB=NULL;
-int Agent_pk;
-int PKG_RPM = 0; /**< Non-zero when it's RPM package */
-int PKG_DEB = 0; /**< Non-zero when it's DEBINE package */
-int PKG_DEB_SRC = 0; /**< Non-zero when it's DEBINE source package */
-int Verbose = 0;
-
 
 /* ***********************************************
  *  Trimming whitespace
@@ -884,113 +807,3 @@ void	Usage	(char *Name)
   printf("  no file :: process data from the scheduler.\n");
 } /* Usage() */
 
-/***********************************************/
-int	main	(int argc, char *argv[])
-{
-  char Parm[MAXCMD];
-  int c;
-  char *agent_desc = "Pulls metadata out of RPM or DEBIAN packages";
-  glb_rpmpi = (struct rpmpkginfo *)malloc(sizeof(struct rpmpkginfo));
-  glb_debpi = (struct debpkginfo *)malloc(sizeof(struct debpkginfo));
-
-  //extern long HBItemsProcessed;
-
-  DB = DBopen();
-  if (!DB)
-  {
-    printf("FATAL: Unable to connect to database\n");
-    fflush(stdout);
-    exit(-1);
-  }
-
-  Agent_pk = GetAgentKey(DB, basename(argv[0]), 0, SVN_REV, agent_desc);
-  /* Process command-line */
-  while((c = getopt(argc,argv,"iv")) != -1)
-  {
-    switch(c)
-	{
-	case 'i':
-                DBclose(DB);  /* DB was opened above, now close it and exit */
-                exit(0);
-        case 'v':
-                Verbose++;
-                break;
-	default:
-		Usage(argv[0]);
-		DBclose(DB);
-		exit(-1);
-	}
-  }
-
-  /* If no args, run from scheduler! */
-  if (argc == 1)
-  {
-    char *repFile;
-    signal(SIGALRM,ShowHeartbeat);
-    alarm(60);
-
-    printf("OK\n"); /* inform scheduler that we are ready */
-    fflush(stdout);
-
-    while(ReadLine(stdin,Parm,MAXCMD) >= 0)
-    {
-      if (Verbose) { printf("PKG: pkgagent read %s\n", Parm);}
-      fflush(stdout);
-
-      if (Parm[0] != '\0') 
-      {
-	memset(glb_rpmpi,0,sizeof(struct rpmpkginfo));
-	memset(glb_debpi,0,sizeof(struct debpkginfo));
-	ParseSchedInput(Parm,glb_rpmpi,glb_debpi);
-        if (PKG_RPM) {
-          repFile = RepMkPath("files", glb_rpmpi->pFile);
-	  if (!repFile) {
-	    printf("FATAL: pfile %ld PkgAgent unable to open file %s\n",
-                            glb_rpmpi->pFileFk, glb_rpmpi->pFile);
-            fflush(stdout);
-            DBclose(DB);
-            exit(-1);
-	  }
-          if (GetMetadata(repFile,glb_rpmpi)){
-            RecordMetadataRPM(glb_rpmpi);
-	  }
-        } else if (PKG_DEB) {
-	  if (GetMetadataDebBinary(glb_debpi)){
-            RecordMetadataDEB(glb_debpi);
-	  }	
-	} else if (PKG_DEB_SRC) {
-	  repFile = RepMkPath("files", glb_debpi->pFile);
-	  if (!repFile) {
-	    printf("FATAL: pfile %ld PkgAgent unable to open file %s\n",
-                            glb_debpi->pFileFk, glb_debpi->pFile);
-            fflush(stdout);
-            DBclose(DB);
-            exit(-1);
-          }
-	  if (GetMetadataDebSource(repFile,glb_debpi)){
-	    RecordMetadataDEB(glb_debpi);
-	  }
-        } else {
-	  /* Deal with the other package*/
-	}
-
-	//Heartbeat(++HBItemsProcessed);
-
-        printf("OK\n");
-        fflush(stdout);
-      }
-    }
-  }
-  else
-  {
-    /* printf("DEBUG: running in cli mode, processing file(s)\n"); */
-    for (; optind < argc; optind++)
-    {
-       PKG_RPM=1;
-       GetMetadata(argv[optind],glb_rpmpi);
-    }
-  }
-
-  DBclose(DB);
-  return(0);
-} /* main() */
