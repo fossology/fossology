d7a77d74183f6cc13bd9620dbd9187ccd56cd317
Mark Donohoe <mark.donohoe@hp.com>
2010-09-24 18:30:46 +0000

    enhanced ckzend to report the expected vs found, fixed notice in fo-runTests
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@3491 a5643f74-2041-0410-a2b9-94ffef304a07

--- tests/nomos/ckzend.php
+++ tests/nomos/ckzend.php
@@ -29,7 +29,7 @@
  */
 
 require_once ('../fossologyTestCase.php');
-require_once ('../TestEnvironment.php');
+//require_once ('../TestEnvironment.php');
 require_once('../testClasses/parseMiniMenu.php');
 require_once('../testClasses/parseBrowseMenu.php');
 require_once('../commonTestFuncs.php');
@@ -76,6 +76,8 @@ class zendTest extends fossologyTestCase
        "ckzend FAILED! did not find $name\n");
 
 		/* Select archive */
+    //print "CKZDB: page before call parseBMenu:\n$page\n";
+    
 		$browse = new parseBrowseMenu($page, 'browsetbl', 1);
 		$browse->parseBrowseMenuFiles();
 		// get the View link for zend-license
@@ -91,9 +93,15 @@ class zendTest extends fossologyTestCase
           "ckzend FAILED! View File Title not found\n");
 		$page = $this->mybrowser->get($url);
 		// Check License 
-		$this->assertTrue($this->myassertText($page,
-		  '/The <b>Nomos<\/b> license detector found: <b>Zend_v2\.0<\/b>/'),
-          "ckzend FAILED! Nomos license string does not match\n");
+		// Get the displayed result
+		$matched = preg_match("/<hr>\nThe(.*?)<div class='text'>--/", $page, $matches);
+		//print "DBCKZ: we found:\n";print_r($matches) . "\n";
+		$found = $matches[1];
+		$stringToMatch = 'The <b>Nomos<\/b> license detector found: <b>Zend_v2\.0<\/b>';
+		$this->assertTrue($this->myassertText($page,"/$stringToMatch/"),
+          "ckzend FAILED! Nomos license string does not match\n" .
+		      "Expected: $stringToMatch\n" . 
+		      "Got: $found\n");
 		$this->assertTrue($this->myassertText($page, '/View License/'),
           "ckzend FAILED! View License Title not found\n");
 		// Check One-shot Analysis
@@ -103,7 +111,7 @@ class zendTest extends fossologyTestCase
 		$this->assertTrue($this->myassertText($page, '/One-Shot License Analysis/'),
           "ckzend FAILED! One-Shot License Analysis Title not found\n");
 		$osLicText = '<strong>Zend_v2\.0';
-		$this->assertTrue($this->myassertText($page, "/$t/"),
+		$this->assertTrue($this->myassertText($page, "/$osLicText/"),
           "ckzend FAILED! the text:\n$osLicText\n was not found\n");
 	}
 }
