49916fb173459ecf42c1d85672d4255e7f2a6735
Larry Shi <yao-bin.shi@hp.com>
2010-10-26 10:20:59 +0000

    changed  makefile organization, testing automatically
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@3609 a5643f74-2041-0410-a2b9-94ffef304a07

--- agents/ununpack/tests/utility.h
+++ agents/ununpack/tests/utility.h
@@ -15,7 +15,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 *********************************************************************/
 #include <magic.h>
-
+#include "ununpack.h"
 int file_dir_existed(char *path_name);
 
 void deleteTmpFiles(char *dir);
