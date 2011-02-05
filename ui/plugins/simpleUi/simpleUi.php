b8a0543f9738efc38ef335f2087cae9bd7b17624
Mark Donohoe <mark.donohoe@hp.com>
2011-02-05 02:53:58 +0000

    start of simple UI framework.  Operational, but still has a number of defects in menu display.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/ui-config@3813 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ ui/plugins/simpleUi/simpleUi.php
@@ -0,0 +1,275 @@
+<?php
+/*
+ Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+global $GlobalReady;
+if (!isset($GlobalReady))
+{
+  exit;
+}
+
+global $WEBDIR;
+
+require($WEBDIR . '/plugins/admin-bucket-pool.php');
+require($WEBDIR . '/plugins/admin-check-template.php');
+require($WEBDIR . '/plugins/admin-config.php');
+require($WEBDIR . '/plugins/admin-db.php');
+require($WEBDIR . '/plugins/admin-folder-create.php');
+require($WEBDIR . '/plugins/admin-folder-delete.php');
+require($WEBDIR . '/plugins/admin-folder-edit.php');
+require($WEBDIR . '/plugins/admin-folder-move.php');
+require($WEBDIR . '/plugins/admin-tag-ns-perm.php');
+require($WEBDIR . '/plugins/admin-tag-ns.php');
+require($WEBDIR . '/plugins/admin-upload-edit.php');
+require($WEBDIR . '/plugins/admin-upload-move.php');
+require($WEBDIR . '/plugins/agent-add.php');
+require($WEBDIR . '/plugins/agent-bucket.php');
+require($WEBDIR . '/plugins/agent-license.php');
+require($WEBDIR . '/plugins/agent-mimetype.php');
+require($WEBDIR . '/plugins/agent-nomos-once.php');
+require($WEBDIR . '/plugins/agent-nomos.php');
+require($WEBDIR . '/plugins/agent-pkgagent.php');
+require($WEBDIR . '/plugins/agent-unpack.php');
+require($WEBDIR . '/plugins/ajax-filebucket.php');
+require($WEBDIR . '/plugins/ajax-filelic.php');
+require($WEBDIR . '/plugins/ajax-perms.php');
+require($WEBDIR . '/plugins/ajax-tags.php');
+require($WEBDIR . '/plugins/ajax-upload-agents.php');
+require($WEBDIR . '/plugins/ajax-uploads.php');
+require($WEBDIR . '/plugins/copyright/agent.php');
+require($WEBDIR . '/plugins/copyright/hist.php');
+require($WEBDIR . '/plugins/copyright/library.php');
+require($WEBDIR . '/plugins/copyright/list.php');
+require($WEBDIR . '/plugins/copyright/oneshot.php');
+require($WEBDIR . '/plugins/copyright/view.php');
+require($WEBDIR . '/plugins/core-auth.php');
+require($WEBDIR . '/plugins/core-db.php');
+require($WEBDIR . '/plugins/core-debug-fileloc.php');
+require($WEBDIR . '/plugins/core-debug-flush-cache.php');
+require($WEBDIR . '/plugins/core-debug-menus.php');
+require($WEBDIR . '/plugins/core-debug-plugins.php');
+require($WEBDIR . '/plugins/core-debug-user.php');
+require($WEBDIR . '/plugins/core-init.php');
+require($WEBDIR . '/plugins/core-schema.dat');
+require($WEBDIR . '/plugins/core-schema.php');
+require($WEBDIR . '/plugins/group-manage.php');
+require($WEBDIR . '/plugins/group-manage-self.php');
+require($WEBDIR . '/plugins/jobs-showjobs.php');
+require($WEBDIR . '/plugins/jobs-showjobs-upload.php');
+require($WEBDIR . '/plugins/myjobs.php');
+require($WEBDIR . '/plugins/search-file-advance.php');
+require($WEBDIR . '/plugins/search-file-by-license.php');
+require($WEBDIR . '/plugins/search.php');
+require($WEBDIR . '/plugins/search-repo.php');
+require($WEBDIR . '/plugins/ui-about.php');
+require($WEBDIR . '/plugins/ui-browse.php');
+require($WEBDIR . '/plugins/ui-buckets.php');
+require($WEBDIR . '/plugins/ui-default.php');
+require($WEBDIR . '/plugins/ui-download.php');
+require($WEBDIR . '/plugins/ui-folders.php');
+require($WEBDIR . '/plugins/ui-license.php');
+require($WEBDIR . '/plugins/ui-list-bucket-files.php');
+require($WEBDIR . '/plugins/ui-menus.php');
+require($WEBDIR . '/plugins/ui-nomos-license.php');
+require($WEBDIR . '/plugins/ui-refresh.php');
+require($WEBDIR . '/plugins/ui-reunpack.php');
+require($WEBDIR . '/plugins/ui-tags.php');
+require($WEBDIR . '/plugins/ui-topnav.php');
+require($WEBDIR . '/plugins/ui-treenav.php');
+require($WEBDIR . '/plugins/ui-view-info.php');
+require($WEBDIR . '/plugins/ui-view-license.php');
+require($WEBDIR . '/plugins/ui-view.php');
+require($WEBDIR . '/plugins/ui-welcome.php');
+require($WEBDIR . '/plugins/upload-file.php');
+require($WEBDIR . '/plugins/upload-instructions.php');
+require($WEBDIR . '/plugins/upload-url.php');
+require($WEBDIR . '/plugins/upload-srv-files.php');
+require($WEBDIR . '/plugins/user-add.php');
+require($WEBDIR . '/plugins/user-del.php');
+require($WEBDIR . '/plugins/user-edit-self.php');
+
+
+define("TITLE_SimpleUi", _("Simplified UI"));
+
+class simpleUi extends FO_Plugin
+{
+  public $Name = "simple UI";
+  public $Title = TITLE_SimpleUi;
+  public $Version = "1.0";
+  public $MenuList = "";
+  public $Dependency = array();
+  public $DBaccess = PLUGIN_DB_READ;
+  public $PluginLevel= 20; /* make this run after level 10 plugins */
+
+
+  /**
+   * \brief adjust the menu structure $MenuList
+   *
+   * Replace or adjust the contents of selected menu items to so they display
+   * as needed in the simplified UI.
+   */
+  function adjustMenus()
+  {
+    //global $MenuList;
+
+    // change the user-edit-self menu
+    $userEditSelf = plugin_find_any(user_edit_self);  // can be null
+    if(!empty($userEditSelf))
+    {
+      $userEditSelf->MenuList = "My Account";
+      $md = menu_insert("My Account",$userEditSelf->MenuOrder,
+      $userEditSelf->Name,$userEditSelf->MenuTarget);
+    }
+    /*
+     $maxdepth = null;
+     $admin = array();
+     $admin = menu_find("Main::Admin", & $maxdepth);
+     echo "<pre>admin menu found is:\n";
+     print_r($admin) . "\n";
+     echo "</pre>";
+     */
+    //return(TRUE);
+  }
+
+  /**
+   * \brief adjust the dependencies of selected plugins so they work with the
+   * simplified UI.
+   *
+   * @return void
+   */
+  function adjustPlugins()
+  {
+    /*
+     * List of plugins that need dependencies adjusted
+     */
+    $newDependencies = array(
+      'upload_instructions' => array("upload_file", "upload_url"),
+      'upload_file' => array("db", "agent_unpack"),
+    );
+
+    foreach($newDependencies as $plugin => $depends)
+    {
+      $pluginRef = plugin_find_any($plugin);  // can be null
+      if(!empty($pluginRef))
+      {
+        $pluginRef->Dependency = $depends;
+        if($this->setState($pluginRef))
+        {
+          // insert into menu if state = READY
+          $md = menu_insert("Main::Upload::From File",$pluginRef->MenuOrder,
+          $pluginRef->Name,$pluginRef->MenuTarget);
+        }
+      }
+    }
+  } // adjustPlugins
+
+  /**
+   * \brief disable plugins not needed for simple UI, when users with perms
+   * > 5 login, these disabled plugins should get enabled.
+   */
+  function disablePlugins()
+  {
+    $disableList = array(
+        'admin_folder_delete',
+        'agent_license',
+        'agent_nomos_once',
+        'runningjobs',
+        'showjobs',
+        'jobs_showjobs_upload',
+        'upload_srv_files',
+        'user_del',
+        'user_add',
+        'user_edit_any',
+    );
+    foreach($disableList as $plugin)
+    {
+      $pluginRef = plugin_find_any($plugin);  // can be null
+      if(!empty($pluginRef))
+      {
+        $pluginRef->Destroy();    // kill thyself
+      }
+    }
+  }
+
+  /**
+   * \brief check and set the state of the plugin
+   *
+   * @param $pluginRef object reference to the plugin
+   * @return boolean
+   */
+  function setState($pluginRef)
+  {
+    if(!is_object($pluginRef)) { return(FALSE); }
+
+    if($pluginRef->State == PLUGIN_STATE_INVALID)
+    {
+      echo "<pre>";
+      echo "Plugin state is $pluginRef->State for $pluginRef->Name\n";
+      $pluginRef->State = PLUGIN_STATE_VALID;
+      echo "<pre>State after setting is:$pluginRef->State\n";
+      $pluginRef->PostInitialize();
+      echo "<pre>State after PostInit is:$pluginRef->State\n";
+      if ($pluginRef->State == PLUGIN_STATE_READY) { $pluginRef->RegisterMenus(); }
+    }
+    echo "</pre>";
+    return(TRUE);
+  } // setState($pluginRef)
+
+  function PostInitialize()
+  {
+    global $Plugins;
+    //echo "<pre>TEMP: State is:$this->State for $this->Name\n</pre>";
+    if ($this->State != PLUGIN_STATE_VALID) {
+      return(0);
+    } // don't run
+    if (empty($_SESSION['User']) && $this->LoginFlag) {
+      //echo "<pre>Didn't pass session/LoginFlag check\n</pre>";
+      return(0);
+    }
+    // Make sure dependencies are met
+    foreach($this->Dependency as $key => $val) {
+      $id = plugin_find_id($val);
+      if ($id < 0) {
+        $this->Destroy();
+        return(0);
+      }
+    }
+    // this makes it so anybody above user level 5 gets the full UI
+    if($_SESSION['UserLevel'] <= 5)
+    {
+    $this->adjustPlugins();
+    $this->adjustMenus();
+    $this->disablePlugins();
+    }
+
+    // It worked, so mark this plugin as ready.
+    $this->State = PLUGIN_STATE_READY;
+    return($this->State == PLUGIN_STATE_READY);
+  } // PostInitialize()
+
+  /**
+   * \brief Remove a menu item and it's children from the menu list.  Optionally
+   * keep the children. (do they become orphans and not visible?).
+   */
+  function removeMenuItem($item)
+  {
+    return(TRUE);
+  }
+
+};
+$NewPlugin = new simpleUi();
+?>
