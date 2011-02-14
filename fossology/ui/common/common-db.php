acf3fb0b089c116b0da1d9c057ffd3c48a564397
Robert Gobeille <bob.gobeille@hp.com>
2011-02-14 18:29:58 +0000

    used by picker/diff
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@3834 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/ui/common/common-db.php
@@ -0,0 +1,56 @@
+<?php
+/***********************************************************
+ Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************/
+
+/**********************************************************
+ *  This file contains common core database functions
+ **********************************************************/
+
+/*************************************************
+ Restrict usage: Every PHP file should have this
+ at the very beginning.
+ This prevents hacking attempts.
+ *************************************************/
+global $GlobalReady;
+if (!isset($GlobalReady)) { exit; }
+
+/*****************************************
+ GetSingleRec
+   Get a single record
+
+ Params:
+   $Table
+   $Where   SQL where clause 
+            e.g. "where uploadtree_pk=2"
+
+ Returns:
+   Associative array for this record.  
+   May be empty.
+ *****************************************/
+function GetSingleRec($Table, $Where="")
+{
+  global $PG_CONN;
+
+  $sql = "SELECT * from $Table $Where";
+  $result = pg_query($PG_CONN, $sql);
+  DBCheckResult($result, $sql, __FILE__, __LINE__);
+
+  $row = pg_fetch_assoc($result);
+  return $row;
+}
+
+?>
