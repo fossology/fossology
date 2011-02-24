200246f022409ac2d13a0bf405d9739669ba6679
Mark Donohoe <mark.donohoe@hp.com>
2011-02-24 03:16:13 +0000

    changed the plugin_load call to load simpleUi instead of plugins
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/ui-config@3883 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui/index.php
+++ ui/index.php
@@ -33,17 +33,18 @@ require_once("common/common.php");
 /****************************************************
  This is the main guts of the UI: Find the plugin and run it.
  ****************************************************/
-plugin_load("plugins");
+plugin_load("plugins/simpleUi");
 
 $Mod = GetParm("mod",PARM_STRING);
 if (!isset($Mod)) { $Mod = "Default"; }
 $PluginId = plugin_find_id($Mod);
 if ($PluginId >= 0)
   {
-  /* Initialize global system configuration variables $SysConfig[] */ 
+  /* Initialize global system configuration variables $SysConfig[] */
   InitSysConfig();
 
   /* Found a plugin, so call it! */
+  //echo "<pre>Calling plugin {$Plugins[$PluginId]->Name}\n</pre>";
   $Plugins[$PluginId]->OutputOpen("HTML",1);
   // error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
   $Plugins[$PluginId]->Output();
