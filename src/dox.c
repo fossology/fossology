11057686a49a086fa98e3b8739ba7005bdcb2ee7
Robert Gobeille <bob.gobeille@hp.com>
2011-05-17 18:39:28 +0000

    doxygen main page
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@4208 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ src/dox.c
@@ -0,0 +1,28 @@
+/* This file defines the doxygen home page.  Currently doxygen is only used on
+ * source.
+ */
+
+/*!
+ * \mainpage Overview of $PROJECTDIR/src
+ * Main source directory
+ *
+ * More developer information can be found at http://fossology.org/developer_documentation
+ *
+ * \section libsection FOSSology C library
+ * lib/ contains fossology.a and all the component files that go into this
+ common core C library.  
+ *
+ * \section schedsection scheduler 
+ * You can find all the scheduler code in scheduler/.  But we might want to move it
+ * under modules/ since there is both a backend and UI components.
+ *
+ * \section modsection modules 
+ * The module directory structure can be found at  http://fossology.org/2.0:modulestruct
+ *
+ * \subsection subui ui/
+ * \subsection subuitests ui_tests/
+ * \subsection subagent agent/
+ * \subsection subagenttests agent_tests/
+ * \subsection submodconf module.conf
+ * \subsection subscripts scripts/
+ */
