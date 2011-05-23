757bb9d85c3566e3263744eac53fa52ed8de71d3
Mark Donohoe <mark.donohoe@hp.com>
2011-05-23 22:32:12 +0000

    Updating requires for jenkins.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@4256 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui/tests/BasicTests/BasicSetup.php
+++ ui/tests/BasicTests/BasicSetup.php
@@ -1,6 +1,6 @@
 <?php
 /***********************************************************
- Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -24,9 +24,14 @@
  * Created on Oct. 15, 2008
  */
 
-require_once ('../../../tests/fossologyTest.php');
-require_once ('../../../tests/fossologyTestCase.php');
-require_once ('../../../tests/TestEnvironment.php');
+// old requires when run by testFOSSology.php
+//require_once('../../../tests/fossologyTestCase.php');
+//require_once ('../../../tests/TestEnvironment.php');
+
+require_once('../../tests/fossologyTestCase.php');
+require_once ('../../tests/TestEnvironment.php');
+
+//require_once ('../../../tests/fossologyTest.php');
 
 /* every test must use these globals, at least $URL */
 global $URL;
