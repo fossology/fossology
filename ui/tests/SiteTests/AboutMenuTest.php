00aa5f958020d655338dd25b557945a126dd0994
Mark Donohoe <mark.donohoe@hp.com>
2011-05-23 21:49:36 +0000

    Adjusted requires so they work when run by jenkins inside jenkins.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@4248 a5643f74-2041-0410-a2b9-94ffef304a07

--- ui/tests/SiteTests/AboutMenuTest.php
+++ ui/tests/SiteTests/AboutMenuTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /***********************************************************
- Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -25,8 +25,12 @@
  * Created on Jul 21, 2008
  */
 
-require_once('../../../tests/fossologyTestCase.php');
-require_once ('../../../tests/TestEnvironment.php');
+// old requires when run by testFOSSology.php
+//require_once('../../../tests/fossologyTestCase.php');
+//require_once ('../../../tests/TestEnvironment.php');
+
+require_once('../../tests/fossologyTestCase.php');
+require_once ('../../tests/TestEnvironment.php');
 
 global $URL;
 global $USER;
