dee701dc3d5e7b36cbdc0b5e524f656488d69b40
Mark Donohoe <mark.donohoe@hp.com>
2011-05-24 00:32:22 +0000

    Modified based on a better example.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@4259 a5643f74-2041-0410-a2b9-94ffef304a07

--- tests/Reports/hudson/xml2Junit.php
+++ tests/Reports/hudson/xml2Junit.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 /*
- Copyright (C) 2010 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -20,7 +20,7 @@
 /**
  * xml2Junit
  * \brief transform an xml output into junit style xml that is
- * compatible with hudson
+ * compatible with Jenkins
  *
  * @version "$Id$"
  *
@@ -43,8 +43,8 @@ $usage = "{$argv[0]} [-h] -f <file> [-o <file>] -x <file>\n" .
          "Options:\n" .
          "-h: usage\n" .
          "-f <file>: the xml input file\n" .
-         "-o <file>: optional output filepath, overwritten if exists\n" .
-         "-x <file>: the xsl file to use in the transformation\n";
+         "-o <file>: optional output filepath, overwritten if exists. StdOut Default\n" .
+         "-x <file>: the xsl style sheet file to use in the transformation\n";
 
 // process options
 $options = getopt('hf:o:x:');
@@ -52,6 +52,7 @@ $options = getopt('hf:o:x:');
 if(array_key_exists('h',$options))
 {
   echo "$usage\n";
+  exit(0);
 }
 
 if(array_key_exists('f',$options))
@@ -64,6 +65,7 @@ if(array_key_exists('f',$options))
   else
   {
     echo "FATAL: xml file {$options['f']} does not exist or cannot be read\n";
+    exit(1);
   }
 }
 
@@ -82,32 +84,32 @@ if(array_key_exists('x',$options))
   else
   {
     echo "FATAL: xsl file {$options['x']} does not exist or cannot be read\n";
+    exit(1);
   }
 }
 //$xslFile = 'simpletest_to_junit.xsl';
 //$ckz = '../../nomos/ckzend.xml';
 
-$xslDoc = new DOMDocument();
-$xslDoc->load($xslFile);
+$xsl = new XSLTProcessor();
+$xsldoc = new DOMDocument();
+$xsldoc->load($xslFile);
+$xsl->importStyleSheet($xsldoc);
 
-$xmlDoc = new DOMDocument();
-$xmlDoc->load($xmlFile);
-
-$proc = new XSLTProcessor();
-$proc->importStylesheet($xslDoc);
-$transformed = $proc->transformToXML($xmlDoc);
+$xmldoc = new DOMDocument();
+$xmldoc->load($xmlFile);
+//echo $xsl->transformToXML($xmldoc);
 
 if($outputFile)
 {
   // open file and write output
   $OF = fopen($outputFile, 'w') or
     die("Fatal cannot open output file $outputFile\n");
-  $wrote = fwrite($OF, $transformed);
+  $wrote = fwrite($OF, $xsl->transformToXML($xmldoc));
   fclose($OF);
 }
 else
 {
   // no output file, echo to stdout
-  echo "$transformed\n";
+  echo $xsl->transformToXML($xmldoc);
 }
 ?>
