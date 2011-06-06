b2112b49fdcd803200c244c0cf47ba8afe049de9
Mark Donohoe <mark.donohoe@hp.com>
2011-06-06 18:13:50 +0000

    Removed PHP notice and added logic for use of TESTROOT.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@4335 a5643f74-2041-0410-a2b9-94ffef304a07

--- tests/fo-runTests.php
+++ tests/fo-runTests.php
@@ -52,8 +52,16 @@ require_once SIMPLE_TEST . 'reporter.php';
 require_once SIMPLE_TEST . 'web_tester.php';
 
 require_once ('TestEnvironment.php');
-require_once($tr . '/testClasses/timer.php');
-
+if(defined('TESTROOT'))
+{
+  echo TESTROOT . "\n";
+  require_once(TESTROOT . '/testClasses/timer.php');
+}
+else
+{
+  echo "ERROR! cannot load /testClasses/timer.php, is TESTROOT defined?\n";
+  exit(1);
+}
 
 $Usage =  "Usage: $argv[0] options...
            Options:
