c66bcaad484fee4d163f26782079fd36925b966b
Mark Donohoe <mark.donohoe@hp.com>
2011-06-13 22:52:25 +0000

    checking in changes before removal
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@4389 a5643f74-2041-0410-a2b9-94ffef304a07

--- tests/runUnit.sh
+++ tests/runUnit.sh
@@ -20,14 +20,24 @@
 
 # run and process the unit test results
 #
-# assumes in ...fossology/tests
-echo "workspace is:$WORKSPACE"
+
+cd fossology/tests
+echo "we are at:";pwd
+
+./checkTestData.php
+if [[ $? -ne 0 ]]
+then
+  echo "FATAL! $0 could not check or downlown load test data, stopping tests"
+  exit 1
+fi
+
+//echo "workspace is:$WORKSPACE"
 cd "$WORKSPACE/fossology/agents"
 echo -n  "Where are at:"
 pwd
 
 echo "starting runUnit"
-
+xml2html=$WORKSPACE"/fossology/tests/Reports/hudson/xml2Junit.php"
 unitList='copyright ununpack'
 
 for unit in $unitList
@@ -46,7 +56,7 @@ do
     echo -n  "we are at:"
     pwd
     
-    #remove previous results
+    #remove previous results ?? really? doesn't this remove current results?
     rm $unit"_Tests-Results.xml"
     rm $unit"_Tests-Listing.xml"
     
@@ -54,7 +64,15 @@ do
     sed '1,$s/href\="/href\="http:\/\/fonightly.usa.hp.com\/~fosstester\/dtds\//' *-Listing.xml > uLTmp.xml
 
     sed '1,$s/CUnit-Run.dtd/http:\/\/fonightly.usa.hp.com\/~fosstester\/dtds\/CUnit-Run.dtd/' uRTmp.xml > uRBoth.xml
-    sed '1,$s/CUnit-Run.dtd/http:\/\/fonightly.usa.hp.com\/~fosstester\/dtds\/CUnit-Run.dtd/' uLTmp.xml > uLBoth.xml
+    sed '1,$s/CUnit-List.dtd/http:\/\/fonightly.usa.hp.com\/~fosstester\/dtds\/CUnit-List.dtd/' uLTmp.xml > uLBoth.xml
+    
+    
+    ## experiments that sorta or didn't work.
+    #sed '1,$s/href\="/href\="\//' *-Results.xml > uRTmp.xml
+    #sed '1,$s/href\="/href\="\//' *-Listing.xml > uLTmp.xml
+
+    #sed '1,$s/CUnit-Run.dtd/http:\/\/fonightly.usa.hp.com\/home\/fosstester\/public_html\/dtds\/CUnit-Run.dtd/' uRTmp.xml > uRBoth.xml
+    #sed '1,$s/CUnit-List.dtd/http:\/\/fonightly.usa.hp.com\/home\/fosstester\/public_html\/dtds\/CUnit-List.dtd/' uLTmp.xml > uLBoth.xml
     
     echo -n "we are at:"
     pwd
@@ -62,6 +80,37 @@ do
     mv uRBoth.xml $unit"_Tests-Results.xml"
     mv uLBoth.xml $unit"_Tests-Listing.xml"
     rm uRTmp.xml uLTmp.xml
+    
+    $failCount=0
+# check the xml files for test failures    
+ 
+# process Run results into html 
+    inFile=$unit"_Tests-Results.xml"
+    outFile=$WORKSPACE"/fossology/tests/Reports/"$unit"_Tests-Results.html"
+    echo "outfile is:$outFile"
+    xslFile=$WORKSPACE"/fossology/tests/Reports/CUnit-Run.xsl"
+    $xml2html -f $inFile -o $outFile -x $xslFile > xmlhtml.out 2>&1
+    if [[ $? -ne 0 ]]
+    then
+      echo "Error when processing Results file $inFile\n"
+      echo "Error was:"
+      cat xmlhtml.out
+      rm xmlhtml.out
+    fi
+    
+# process listings into html
+    inFile=$unit"_Tests-Listing.xml"
+    outFile=$WORKSPACE"/fossology/tests/Reports/"$unit"_Tests-Listing.html"
+    echo "outfile is:$outFile"
+    xslFile=$WORKSPACE"/fossology/tests/Reports/CUnit-List.xsl"
+    $xml2html -f $inFile -o $outFile -x $xslFile > xmlhtml.out 2>&1
+    if [[ $? -ne 0 ]]
+    then
+      echo "Error when processing Results file $inFile\n"
+      echo "Error was:\n"
+      cat xmlhtml.out
+      rm xmlhtml.out
+    fi
     
     today=`date +'%b.%d.%Y'`
     mkdir /home/jenkins/public_html/UnitTests/Results-$today
