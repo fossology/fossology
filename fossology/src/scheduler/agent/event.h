74e0a716cba90fede242a2d7974c45f7a959f9c5
Alex Norton <norton.alex@hp.com>
2011-07-14 20:09:48 +0000

    added debugging facilities to the event queue, turning on the 4th bit of verbose (8 -> 15) will not cause the name of all events to be printed
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@4487 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/scheduler/agent/event.h
+++ fossology/src/scheduler/agent/event.h
@@ -52,14 +52,16 @@ typedef void(*event_function)(void*);
 /* **** Constructor Destructor ********************************************** */
 /* ************************************************************************** */
 
-event event_init(void(*func)(void*), void* arg);
+event event_init(void(*func)(void*), void* arg, char* name);
 void event_destroy(event e);
 
 /* ************************************************************************** */
 /* **** EventLoop Functions ************************************************* */
 /* ************************************************************************** */
 
-void event_signal(void* func, void* args);
+#define event_signal(func, args) event_signal_ext(func, args, #func)
+
+void event_signal_ext(void* func, void* args, char* name);
 void event_loop_terminate();
 int  event_loop_enter(void(*)(void));
 void event_loop_destroy();
