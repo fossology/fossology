4c88763909a683cc0da35b94b57f35c9330b0876
Vincent Ma <dong.ma@hp.com>
2011-07-28 09:05:28 +0000

    pkgagent unit test refact to work with v2.0 src structure, add 1 new testcase.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@4510 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/src/pkgagent/agent_tests/testGetMetadata.c
@@ -0,0 +1,67 @@
+/*********************************************************************
+Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*********************************************************************/
+#include "pkgagent.h"
+
+#include <stdio.h>
+#include "CUnit/CUnit.h"
+#define MAXCMD 8192
+
+/*
+struct rpmpkginfo {
+  char pkgName[256];
+  char pkgAlias[256];
+  char pkgArch[64];
+  char version[64];
+  char rpmFilename[256];
+  char license[512];
+  char group[128];
+  char packager[1024];
+  char release[64];
+  char buildDate[128];
+  char vendor[128];
+  char url[256];
+  char sourceRPM[256];
+  char summary[MAXCMD];
+  char description[MAXCMD];
+  long pFileFk;
+  char pFile[MAXCMD];
+  char **requires;
+  int req_size;
+};*/
+
+extern int GetMetadata(char *pkg, struct rpmpkginfo *pi);
+
+void test_GetMetadata_normal()
+{
+    char *pkg = "./testdata/fossology-1.2.0-1.el5.i386.rpm";
+    struct rpmpkginfo *pi;
+    pi = (struct rpmpkginfo *)malloc(sizeof(struct rpmpkginfo));
+    int predictValue = 0;
+    rpmReadConfigFiles(NULL, NULL);
+    db_conn = fo_dbconnect();
+    int Result = GetMetadata(pkg, pi);
+    printf("GetMetadata Result is:%d\n", Result);
+    PQfinish(db_conn);
+    rpmFreeMacros(NULL);
+    CU_ASSERT_EQUAL(Result, predictValue);
+}
+
+CU_TestInfo testcases_GetMetadata[] = {
+	{"Testing the function GetMetadata, paramters are  normal", test_GetMetadata_normal}, 
+        CU_TEST_INFO_NULL
+};
+
---
+++ fossology/src/pkgagent/agent_tests/testRun.c
@@ -0,0 +1,74 @@
+/*********************************************************************
+Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "CUnit/CUnit.h"
+#include "CUnit/Automated.h"
+
+extern CU_TestInfo testcases_GetFieldValue[];
+extern CU_TestInfo testcases_GetMetadata[];
+extern CU_TestInfo testcases_Trim[];
+//extern CU_TestInfo testcases_ProcessUpload[];
+
+CU_SuiteInfo suites[] = {
+        {"Testing the function trim:", NULL, NULL, testcases_Trim},
+        {"Testing the function GetFieldValue:", NULL, NULL, testcases_GetFieldValue},
+        {"Testing the function GetMetadata:", NULL, NULL, testcases_GetMetadata},
+//        {"Testing the function ProcessUpload:", NULL, NULL, testcases_ProcessUpload},
+        CU_SUITE_INFO_NULL
+};
+
+void AddTests(void)
+{
+        assert(NULL != CU_get_registry());
+        assert(!CU_is_test_running());
+
+
+        if(CUE_SUCCESS != CU_register_suites(suites)){
+                fprintf(stderr, "Register suites failed - %s ", CU_get_error_msg());
+                exit(EXIT_FAILURE);
+        }
+}
+
+int main( int argc, char *argv[] )
+{
+       printf("test start\n");
+       if(CU_initialize_registry()){
+
+                fprintf(stderr, "\nInitialization of Test Registry failed.\n");
+                exit(EXIT_FAILURE);
+        }else{
+                AddTests();
+
+                CU_set_output_filename("Pkgagent Test");
+                CU_list_tests_to_file();
+                CU_automated_run_tests();
+                //CU_cleanup_registry();
+        }
+        printf("end\n");
+printf("Results:\n");
+    printf("  Number of suites run: %d\n", CU_get_number_of_suites_run());
+    printf("  Number of tests run: %d\n", CU_get_number_of_tests_run());
+    printf("  Number of tests failed: %d\n", CU_get_number_of_tests_failed());
+    printf("  Number of asserts: %d\n", CU_get_number_of_asserts());
+    printf("  Number of successes: %d\n", CU_get_number_of_successes());
+    printf("  Number of failures: %d\n", CU_get_number_of_failures());
+CU_cleanup_registry();
+        return 0;
+}
