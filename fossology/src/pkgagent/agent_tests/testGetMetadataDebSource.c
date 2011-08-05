a8f6257691e0041994697ab0f33833c253809dc4
Vincent Ma <dong.ma@hp.com>
2011-08-05 10:22:04 +0000

    Lost one testfile for pkagent UT.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@4541 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ fossology/src/pkgagent/agent_tests/testGetMetadataDebSource.c
@@ -0,0 +1,57 @@
+/*********************************************************************
+Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *********************************************************************/
+#include "pkgagent.h"
+
+#include <stdio.h>
+#include "CUnit/CUnit.h"
+
+extern int GetMetadataDebSource(char *repFile, struct debpkginfo *pi);
+
+/**
+ * \brief test_GetMetadataDebSource()
+ *
+ * Test pkgagent.c Function GetMetadataDebSource()
+ * get debian source package info from .dsc file
+ *
+ */
+void test_GetMetadataDebSource()
+{
+  char *repFile = "./testdata/fossology_1.4.1.dsc";
+  struct debpkginfo *pi;
+  pi = (struct debpkginfo *)malloc(sizeof(struct debpkginfo));
+  int predictValue = 0;
+  db_conn = fo_dbconnect();
+  int Result = GetMetadataDebSource(repFile, pi);
+  printf("GetMetadataDebSource Result is:%d\n", Result);
+
+  //printf("GetMetadataDebSource Result is:%s\n", pi->homepage);
+  CU_ASSERT_STRING_EQUAL(pi->pkgName, "fossology");
+  CU_ASSERT_STRING_EQUAL(pi->pkgArch, "any");
+  CU_ASSERT_STRING_EQUAL(pi->version, "1.4.1");
+  CU_ASSERT_STRING_EQUAL(pi->maintainer, "Matt Taggart <taggart@debian.org>");
+  CU_ASSERT_STRING_EQUAL(pi->homepage, "http://fossology.org");
+  CU_ASSERT_EQUAL(pi->dep_size, 13);
+
+  PQfinish(db_conn);
+  CU_ASSERT_EQUAL(Result, predictValue);
+}
+
+CU_TestInfo testcases_GetMetadataDebSource[] = {
+    {"Testing the function GetMetadataDebSource", test_GetMetadataDebSource},
+    CU_TEST_INFO_NULL
+};
+
