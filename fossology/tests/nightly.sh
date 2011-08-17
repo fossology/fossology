2da0434a387174ad8e04448d51820f5ff25351a0
Mark Donohoe <mark.donohoe@hp.com>
2011-08-17 05:07:52 +0000

    Removing moved files as part of 2.0 reorg.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@4612 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/tests/nightly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# cron wrapper to launch the nightly script as fosstester
-#
-cd fossology/tests
-today=`date +'%b.%d.%Y'`
-export  PATH=$PATH:/usr/local/bin
-./Nightly.php > nightly-$today
