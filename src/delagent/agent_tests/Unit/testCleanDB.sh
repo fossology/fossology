1b7ccfdb49da76a8614778c1451cc75d84064e7c
Vincent Ma <dong.ma@hp.com>
2011-08-18 11:48:32 +0000

    delagent code changes and unit test. 1. Makefile and 2.0api changes 2.Part of Unit test 3.doxgen header
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@4625 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ src/delagent/agent_tests/Unit/testCleanDB.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#/*********************************************************************
+#Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
+#
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#version 2 as published by the Free Software Foundation.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along
+#with this program; if not, write to the Free Software Foundation, Inc.,
+#51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#*********************************************************************/
+
+VARS=../../../../Makefile.conf
+
+UID=`id -un`
+if [ "$UID" != "fossy" ];then
+  echo "Must be fossy to run this script."
+  exit 1
+fi
+
+echo $UID
+
+dropdb fossologytest
+if [ $? -ne 0 ];
+then
+  echo "Delete Test database Error!"
+  exit 1
+fi
+
+PREFIX=`cat $VARS|grep -i '^PREFIX'|awk -F = '{print $2}'`
+cp $PREFIX/etc/fossology/Db.conf ~/
+sed -i 's/fossologytest/fossology/' ~/Db.conf
+cp ~/Db.conf $PREFIX/etc/fossology/Db.conf
+
+echo "Create Test database success!"
