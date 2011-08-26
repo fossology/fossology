0d244b9e38feec4a6c5b8b9b10ecf71d687730bb
Robert Gobeille <bob.gobeille@hp.com>
2011-08-26 16:39:50 +0000

    rearrange tests - in progress
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@4667 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/ununpack/agent_tests/test_ununpack-disk.c
+++ src/ununpack/agent_tests/test_ununpack-disk.c
@@ -114,3 +114,72 @@ CU_TestInfo ExtractDisk_testcases[] =
 //    {"Testing function ExtractDisk for nfts image:", testExtractDisk4Ntfs},
     CU_TEST_INFO_NULL
 };
+
+
+/* locals */
+static char *Name = NULL;
+
+/* test functions */
+void FatDiskName(char *name);
+
+/**
+ * @brief initialize
+ */
+int  FatDiskNameInit()
+{
+  Name = (char *)malloc(100);
+  return 0;
+}
+
+/**
+ * @brief clean env and others
+ */
+int FatDiskNameClean()
+{
+  free(Name);
+  return 0;
+}
+
+/**
+ * @brief Convert to lowercase.
+ */
+void testFatDiskName1()
+{
+  strcpy(Name, "Fossology\0");
+  FatDiskName(Name);
+  CU_ASSERT_EQUAL(strcmp(Name, "fossology"), 0); 
+}
+ 
+void testFatDiskName2()
+{
+  strcpy(Name, "Fosso\0");
+  FatDiskName(Name);
+  CU_ASSERT_EQUAL(strcmp(Name, "fosso"), 0);
+
+  strcpy(Name, "FOSSOLOGY HELLO\0");
+  FatDiskName(Name);
+  CU_ASSERT_EQUAL(strcmp(Name, "fossology hello"), 0);
+}
+
+void testFatDiskName3()
+{
+  strcpy(Name, "Fosso (hello)");
+  FatDiskName(Name);
+  CU_ASSERT_EQUAL(strcmp(Name, "fosso"), 0);
+}
+
+void testFatDiskNameNameEmpty()
+{
+  strcpy(Name, "");
+  FatDiskName(Name);
+  CU_ASSERT_EQUAL(strcmp(Name, ""), 0);
+}
+
+CU_TestInfo FatDiskName_testcases[] =
+{
+    {"Testing function FatDiskName, 1:", testFatDiskName1},
+    {"Testing function FatDiskName, 2:", testFatDiskName2},
+    {"Testing function FatDiskName, 3:", testFatDiskName3},
+    {"Testing function FatDiskName, the parameter is empty:", testFatDiskNameNameEmpty},
+    CU_TEST_INFO_NULL
+};
