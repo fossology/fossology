88ce44c078c1b23fa9a936ebba0b84431e191a59
Robert Gobeille <bob.gobeille@hp.com>
2011-09-07 21:36:43 +0000

    update with FO_wrappers around CU_ macros Change number of parameters in cunit_main()
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@4710 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/testing/lib/c/libcunit.c
+++ src/testing/lib/c/libcunit.c
@@ -17,61 +17,61 @@
 
 #include "libcunit.h"
 
-/**
- * \file libcunit.c
- */
-
-extern CU_SuiteInfo suites[];
-
-void AddTests(void)
-{
-  assert(NULL != CU_get_registry());
-  assert(!CU_is_test_running());
-
-
-  if (CUE_SUCCESS != CU_register_suites(suites))
-  {
-    fprintf(stderr, "Register suites failed - %s ", CU_get_error_msg());
-    exit(EXIT_FAILURE);
-  }
-}
 
 /**
  * \brief cunit main test function
+ *        This function standardizes how CUnit tests are reported.
  *
- * \param char *test_name - the test name, who invoke this cunit_main, if no failure,
- * will get test report for this test, the test report are test_name-Result.xml
- * and test_name-Listing.xml
+ *  There are three parts to the test output.
+ *  - {test_name}-Listing.xml
+ *  - {test_name}-Result.xml
+ *  - stdout, to which is printed a run summary and any specific error details 
+ (    (the actual and expected results).
+ *
+ *  The caller also needs to use the FO_ macros in libcunit.h instead of the CU_ macros.
+ *
+ * \param test_name - the test name
+ * \param suites - the test suite array
  *
  * \return 0 on sucess, not 1 on failure
  */
-int cunit_main(char *test_name)
+int cunit_main(char *test_name, CU_SuiteInfo *suites)
 {
   /** test name is empty? */
   if (!test_name)
   {
-    printf("error, the test name is empty\n");
+    fprintf(stderr, "FATAL: empty test name.\n");
     exit(1);
   }
+
   if (CU_initialize_registry())
   {
-    fprintf(stderr, "\nInitialization of Test Registry failed.\n");
-    exit(EXIT_FAILURE);
-  } else
+    fprintf(stderr, "FATAL: Initialization of Test Registry failed.\n");
+    exit(1);
+  } 
+
+  assert(CU_get_registry());
+  assert(!CU_is_test_running());
+
+  if(CU_register_suites(suites) != CUE_SUCCESS)
   {
-    AddTests();
-    CU_set_output_filename(test_name);
-    CU_list_tests_to_file();
-    CU_automated_run_tests();
-    printf(" #%s# cunit test results:\n", test_name);
-    printf("  Number of suites run: %d\n", CU_get_number_of_suites_run());
-    printf("  Number of tests run: %d\n", CU_get_number_of_tests_run());
-    printf("  Number of tests failed: %d\n", CU_get_number_of_tests_failed());
-    printf("  Number of asserts: %d\n", CU_get_number_of_asserts());
-    printf("  Number of successes: %d\n", CU_get_number_of_successes());
-    printf("  Number of failures: %d\n", CU_get_number_of_failures());
-    CU_cleanup_registry();
+    fprintf(stderr, "FATAL: Register suites failed - %s\n", CU_get_error_msg());
+    exit(1);
   }
+
+  CU_set_output_filename(test_name);
+  CU_list_tests_to_file();
+  CU_automated_run_tests();
+
+  printf(" #%s# cunit test results:\n", test_name);
+  printf("  Number of suites run: %d\n", CU_get_number_of_suites_run());
+  printf("  Number of tests run: %d\n", CU_get_number_of_tests_run());
+  printf("  Number of tests failed: %d\n", CU_get_number_of_tests_failed());
+  printf("  Number of asserts: %d\n", CU_get_number_of_asserts());
+  printf("  Number of successes: %d\n", CU_get_number_of_successes());
+  printf("  Number of failures: %d\n", CU_get_number_of_failures());
+  CU_cleanup_registry();
+
   return 0;
 }
 
