ae226b344dc3feee08a2c3726dd988e8d0134a4d
Robert Gobeille <bob.gobeille@hp.com>
2011-09-07 21:36:43 +0000

    update with FO_wrappers around CU_ macros Change number of parameters in cunit_main()
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@4710 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/testing/lib/c/libcunit.h
+++ fossology/src/testing/lib/c/libcunit.h
@@ -20,21 +20,294 @@
 #include "CUnit/CUnit.h"
 #include "CUnit/Automated.h"
 
-/**
- * \brief cunit main test function
+int cunit_main(char *test_name, CU_SuiteInfo *suites);
+
+/** 
+ * @file libcunit.h
+ * @brief FOSSology wrappers for CUnit macros.
+ * These wrappers have the same name as their CUnit
+ * counterparts defined in CUnit.h but substitute FO_ for CU_
+ * For example, CU_ASSERT_EQUAL becomes FO_ASSERT_EQUAL
  *
- * \param char *test_name - the test name, who invoke this cunit_main, if no failure,
- * will get test report for this test, the test report are test_name-Result.xml
- * and test_name-Listing.xml.
- * also can get a brief command line report, if test_name is mimetype, the brief report is like:
- * #mimetype# cunit test results:
- *  Number of suites run: 6
- *  Number of tests run: 6
- *  Number of tests failed: 0
- *  Number of asserts: 9
- *  Number of successes: 9
- *  Number of failures: 0
- *
- * \return 0 on sucess, not 1 on failure
+ * For convienence all CU_* macros are redefined as FO_* but
+ * only macros that have an expected value have a different behavior.
+ * These macros will print both the actual and expected value if an 
+ * assertion fails.
  */
-int cunit_main(char *TestName);
+
+/** Record a pass condition without performing a logical test. */
+#define FO_PASS(msg) CU_PASS(msg)
+
+/** Simple assertion.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT(value) CU_ASSERT(value)
+
+/** Simple assertion.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_FATAL(value)  CU_ASSERT_FATAL(value) 
+
+/** Simple assertion.
+ *  Reports failure with no other action.
+ */
+#define FO_TEST(value) CU_TEST(value)
+
+/** Simple assertion.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_TEST_FATAL(value) CU_TEST_FATAL(value)
+
+/** Record a failure without performing a logical test. */
+#define FO_FAIL(msg) CU_FAIL(msg)
+
+/** Record a failure without performing a logical test, and abort test. */
+#define FO_FAIL_FATAL(msg) CU_FAIL_FATAL(msg)
+
+/** Asserts that value is CU_TRUE.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_TRUE(value) CU_ASSERT_TRUE(value)
+
+/** Asserts that value is CU_TRUE.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_TRUE_FATAL(value) CU_ASSERT_TRUE_FATAL(value)
+
+/** Asserts that value is CU_FALSE.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_FALSE(value) CU_ASSERT_FALSE(value)
+
+/** Asserts that value is CU_FALSE.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_FALSE_FATAL(value) CU_ASSERT_FALSE_FATAL(value)
+
+/** Asserts that actual == expected.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_EQUAL(actual, expected) \
+  { \
+    if (actual != expected) printf("%s(%d) Expected %d, got %d\n",\
+                 __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_EQUAL(actual, expected)\
+  }
+
+/** Asserts that actual == expected.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_EQUAL_FATAL(actual, expected) \
+  { \
+    if (actual != expected) printf("FATAL: %s(%d) Expected %d, got %d\n",\
+                 __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_EQUAL_FATAL(actual, expected)\
+  }
+
+/** Asserts that actual != expected.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_NOT_EQUAL(actual, expected) \
+  { \
+    if (actual != expected) printf("%s(%d) Expected != %d got %d\n",\
+                 __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_NOT_EQUAL(actual, expected)\
+  }
+
+/** Asserts that actual != expected.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_NOT_EQUAL_FATAL(actual, expected) \
+  { \
+    if (actual != expected) printf("FATAL: %s(%d) Expected != %d, got %d\n",\
+                 __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_NOT_EQUAL_FATAL(actual, expected)\
+  }
+
+/** Asserts that pointers actual == expected.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_PTR_EQUAL(actual, expected) \
+  { \
+    if (actual != expected) printf("%s(%d) Expected %p, got %p\n",\
+                 __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_PTR_EQUAL(actual, expected)\
+  }
+
+/** Asserts that pointers actual == expected.
+ * Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_PTR_EQUAL_FATAL(actual, expected) \
+  { \
+    if (actual != expected) printf("FATAL: %s(%d) Expected %p, got %p\n",\
+                 __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_PTR_EQUAL_FATAL(actual, expected)\
+  }
+
+/** Asserts that pointers actual != expected.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_PTR_NOT_EQUAL(actual, expected) \
+  { \
+    if (actual != expected) printf("%s(%d) Expected != %p got %p\n",\
+                 __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_PTR_NOT_EQUAL(actual, expected)\
+  }
+
+/** Asserts that pointers actual != expected.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_PTR_NOT_EQUAL_FATAL(actual, expected) \
+  { \
+    if (actual != expected) printf("FATAL: %s(%d) Expected != %p, got %p\n",\
+                 __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_PTR_NOT_EQUAL_FATAL(actual, expected)\
+  }
+
+/** Asserts that pointer value is NULL.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_PTR_NULL(value) CU_ASSERT_PTR_NULL(value)
+
+/** Asserts that pointer value is NULL.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_PTR_NULL_FATAL(value) CU_ASSERT_PTR_NULL_FATAL(value)
+
+/** Asserts that pointer value is not NULL.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_PTR_NOT_NULL(value) CU_ASSERT_PTR_NOT_NULL(value)
+
+/** Asserts that pointer value is not NULL.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_PTR_NOT_NULL_FATAL(value) CU_ASSERT_PTR_NOT_NULL_FATAL(value)
+
+/** Asserts that string actual == expected.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_STRING_EQUAL(actual, expected) \
+  { \
+    if (!(strcmp((const char*)(actual), (const char*)(expected)))) \
+      printf("%s(%d) Expected (%s), got (%s)\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_STRING_EQUAL(actual, expected)\
+  }
+
+/** Asserts that string actual == expected.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_STRING_EQUAL_FATAL(actual, expected) \
+  { \
+    if (!(strcmp((const char*)(actual), (const char*)(expected)))) \
+       printf("FATAL: %s(%d) Expected (%s), got (%s)\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_STRING_EQUAL_FATAL(actual, expected)\
+  }
+
+/** Asserts that string actual != expected.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_STRING_NOT_EQUAL(actual, expected) \
+  { \
+    if (strcmp((const char*)(actual), (const char*)(expected))) \
+       printf("%s(%d) Expected != (%s), got (%s)\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_STRING_NOT_EQUAL(actual, expected)\
+  }
+
+/** Asserts that string actual != expected.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_STRING_NOT_EQUAL_FATAL(actual, expected) \
+  { \
+    if (strcmp((const char*)(actual), (const char*)(expected))) \
+       printf("FATAL: %s(%d) Expected != (%s), got (%s)\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_STRING_NOT_EQUAL_FATAL(actual, expected)\
+  }
+
+/** Asserts that string actual == expected with length specified.
+ *  The comparison is limited to count characters.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_NSTRING_EQUAL(actual, expected, count) \
+  { \
+    if (!strcmp((const char*)(actual), (const char*)(expected), (size_t)(count))) \
+      printf("%s(%d) Expected (%s), got (%s)\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_NSTRING_EQUAL(actual, expected, count)\
+  }
+
+/** Asserts that string actual == expected with length specified.
+ *  The comparison is limited to count characters.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_NSTRING_EQUAL_FATAL(actual, expected, count) \
+  { \
+    if (!strcmp((const char*)(actual), (const char*)(expected), (size_t)(count))) \
+      printf("FATAL: %s(%d) Expected (%s), got (%s)\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_NSTRING_EQUAL_FATAL(actual, expected, count)\
+  }
+
+/** Asserts that string actual != expected with length specified.
+ *  The comparison is limited to count characters.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_NSTRING_NOT_EQUAL(actual, expected, count) \
+  { \
+    if (strcmp((const char*)(actual), (const char*)(expected), (size_t)(count))) \
+      printf("%s(%d) Expected (%s), got (%s)\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_NSTRING_NOT_EQUAL(actual, expected, count)\
+  }
+
+/** Asserts that string actual != expected with length specified.
+ *  The comparison is limited to count characters.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_NSTRING_NOT_EQUAL_FATAL(actual, expected, count) \
+  { \
+    if (strcmp((const char*)(actual), (const char*)(expected), (size_t)(count))) \
+      printf("FATAL: %s(%d) Expected (%s), got (%s)\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_NSTRING_NOT_EQUAL_FATAL(actual, expected, count)\
+  }
+
+/** Asserts that double actual == expected within the specified tolerance.
+ *  If actual is within granularity of expected, the assertion passes.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_DOUBLE_EQUAL(actual, expected, granularity) \
+  { \
+    if (fabs((double)(actual) - (expected)) <= fabs((double)(granularity))) \
+      printf("%s(%d) Expected %d, got %d\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_DOUBLE_EQUAL(actual, expected, granularity)\
+  }
+
+/** Asserts that double actual == expected within the specified tolerance.
+ *  If actual is within granularity of expected, the assertion passes.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_DOUBLE_EQUAL_FATAL(actual, expected, granularity) \
+  { \
+    if (fabs((double)(actual) - (expected)) <= fabs((double)(granularity))) \
+      printf("FATAL: %s(%d) Expected %d, got %d\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_DOUBLE_EQUAL_FATAL(actual, expected, granularity)\
+  }
+
+/** Asserts that double actual != expected within the specified tolerance.
+ *  If actual is within granularity of expected, the assertion fails.
+ *  Reports failure with no other action.
+ */
+#define FO_ASSERT_DOUBLE_NOT_EQUAL(actual, expected, granularity) \
+  { \
+    if (fabs((double)(actual) - (expected)) > fabs((double)(granularity)))\
+      printf("%s(%d) Expected %d, got %d\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_DOUBLE_NOT_EQUAL(actual, expected, granularity)\
+  }
+
+/** Asserts that double actual != expected within the specified tolerance.
+ *  If actual is within granularity of expected, the assertion fails.
+ *  Reports failure and causes test to abort.
+ */
+#define FO_ASSERT_DOUBLE_NOT_EQUAL_FATAL(actual, expected, granularity) \
+  { \
+    if (fabs((double)(actual) - (expected)) > fabs((double)(granularity))) \
+      printf("FATAL: %s(%d) Expected %d, got %d\n", __FILE__,__LINE__, expected, actual);\
+    CU_ASSERT_DOUBLE_NOT_EQUAL_FATAL(actual, expected, granularity)\
+  }
