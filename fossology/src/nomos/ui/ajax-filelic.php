6c7df19b0dc127f5adbf9de9b93c73fc0509920b
Larry Shi <yao-bin.shi@hp.com>
2011-10-18 10:18:03 +0000

    1. Format the nomos php files; 2. fix several bugs mainly are lacking of pg_free_result(); 3. Some variables are changed, so revise accordingly, for example: ; 4. remove some unused code; 5. Replace with new DB api
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@4872 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/nomos/ui/ajax-filelic.php
+++ fossology/src/nomos/ui/ajax-filelic.php
@@ -1,6 +1,6 @@
 <?php
 /***********************************************************
- Copyright (C) 2009 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2009-2011 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -16,21 +16,18 @@
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ***********************************************************/
 
-/*************************************************
- This plugin finds all the uploadtree_pk's in the first directory
- level under a parent, that contain a given license.
-
- GET args: napk, lic, item, (optional debug)
-
- item   is the parent uploadtree_pk
- napk   is the nomosagent_pk whos results you are looking for
- lic    is the shortname of the license
-
- ajax usage:
-   http://...?mod=ajax_filelic&napk=123&item=123456&lic=FSF
- 
- Returns the rf_shortname, and comma delimited string of uploadtree_pks: "FSF,123,456"
- *************************************************/
+/**
+ * \file ajax-filelic.php
+ * \brief This plugin finds all the uploadtree_pk's in the first directory
+ * level under a parent, that contain a given license.
+ * GET args: napk, lic, item, (optional debug)
+ * item   is the parent uploadtree_pk
+ * napk   is the nomosagent_pk whos results you are looking for
+ * lic    is the shortname of the license
+ * ajax usage:
+ * http://...?mod=ajax_filelic&napk=123&item=123456&lic=FSF
+ * \return the rf_shortname, and comma delimited string of uploadtree_pks: "FSF,123,456"
+ */
 
 define("TITLE_ajax_filelic", _("ajax find items by license"));
 
@@ -44,55 +41,57 @@ class ajax_filelic extends FO_Plugin
   var $NoHTML     = 1; /* This plugin needs no HTML content help */
   var $LoginFlag = 0;
 
-  /***********************************************************
-   Output(): Display the loaded menu and plugins.
-   ***********************************************************/
+  /**
+   * \brief Display the loaded menu and plugins.
+   */
   function Output()
-  {  
-    global $DB, $PG_CONN;
+  {
     global $Plugins;
 
-    if ($this->State != PLUGIN_STATE_READY) { return; }
+    if ($this->State != PLUGIN_STATE_READY) {
+      return;
+    }
     //$uTime = microtime(true);
 
-    // make sure there is a db connection since I've pierced the core-db abstraction
-    if (!$PG_CONN) { $dbok = $DB->db_init(); if (!$dbok) echo "NO DB connection"; }
+    // make sure there is a db connection
 
     switch($this->OutputType)
     {
       case "XML":
-	    break;
+        break;
       case "HTML":
-	    $nomosagent_pk = GetParm("napk",PARM_INTEGER);
-    	$rf_shortname = GetParm("lic",PARM_RAW);
-    	$uploadtree_pk = GetParm("item",PARM_INTEGER);
-    	$debug = array_key_exists("debug", $_GET) ? true : false;
+        $nomosagent_pk = GetParm("napk",PARM_INTEGER);
+        $rf_shortname = GetParm("lic",PARM_RAW);
+        $uploadtree_pk = GetParm("item",PARM_INTEGER);
+        $debug = array_key_exists("debug", $_GET) ? true : false;
 
         $Files = Level1WithLicense($nomosagent_pk, $rf_shortname, $uploadtree_pk);
         $V = "";
         if (count($Files) == 0)
-          $V .= "";  // no matches found
+        $V .= "";  // no matches found
         else
         {
           $V .= rawurlencode($rf_shortname);
-          foreach ($Files as $uppk => $fname) $V .= ",$uppk"; 
+          foreach ($Files as $uppk => $fname) $V .= ",$uppk";
         }
-	    break;
+        break;
       case "Text":
-	    break;
+        break;
       default:
     }
 
-/*
-    if ($debug) 
+    /*
+     if ($debug)
     {
-      $Time = microtime(true) - $uTime;  // convert usecs to secs
-$text = _("Elapsed time: %.2f seconds");
-      printf( "<small>$text</small>", $Time);
+    $Time = microtime(true) - $uTime;  // convert usecs to secs
+    $text = _("Elapsed time: %.2f seconds");
+    printf( "<small>$text</small>", $Time);
     }
-*/
+    */
 
-    if (!$this->OutputToStdout) { return($V); }
+    if (!$this->OutputToStdout) {
+      return($V);
+    }
     print("$V");
     return;
   } // Output()
