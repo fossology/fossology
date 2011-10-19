3fa55bd295c86d4486aa620f063d6f7ce41b82e0
Mark Donohoe <mark.donohoe@hp.com>
2011-10-19 21:25:31 +0000

    Reformatted to meet fossology code standard.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@4879 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/cli/fossinit.php
+++ src/cli/fossinit.php
@@ -15,15 +15,15 @@
  You should have received a copy of the GNU General Public License along
  with this program; if not, write to the Free Software Foundation, Inc.,
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-***********************************************************/
+ ***********************************************************/
 
 /**************************************************************
  fossinit
- 
+
  Initialize the FOSSology system (no UI required).
  This should be used immediately after an install, and before
  starting up the scheduler.
- 
+
  @return 0 for success, 1 for failure.
  *************************************************************/
 
@@ -35,10 +35,10 @@ $GID = posix_getgrnam("fossy");
 posix_setgid($GID['gid']);
 $Group = `groups`;
 if (!preg_match("/\sfossy\s/",$Group) && (posix_getgid() != $GID['gid']))
-  {
+{
   print "ERROR: Unable to run as group 'fossy'\n";
   exit(1);
-  }
+}
 
 /* Load all code */
 require_once(dirname(__FILE__) . '/../share/fossology/php/pathinclude.php');
@@ -65,68 +65,68 @@ $usage = "Usage: " . basename($argv[0]) . " [options]
 /*** -v  = verbose ***/
 $Options = getopt('vh');
 if (array_key_exists('h',$Options))
-  {
+{
   print "$usage\n";
   exit(0);
-  }
+}
 $Verbose = array_key_exists("v",$Options);
 if ($Verbose == "") { $Verbose=0; }
 
 global $DB;
 if (array_key_exists('d',$Options))
-  {
+{
   $DB->Debug=1;
-  }
+}
 if (array_key_exists('D',$Options))
-  {
+{
   $DB->Debug=2;
-  }
+}
 
 /* Initialize the system! */
 $Schema = &$Plugins[plugin_find_any_id("schema")];
 if (empty($Schema))
-  {
+{
   print "FAILED: Unable to find the schema plugin.\n";
   exit(1);
-  }
+}
 
 global $WEBDIR;
 $Filename = "$WEBDIR/plugins/core-schema.dat";
 if (!file_exists($Filename))
-  {
+{
   print "FAILED: Schema data file ($Filename) not found.\n";
   exit(1);
-  }
-  
+}
+
 $PGCONN = dbConnect(NULL);
 $Debug=0;
 $FailFlag = ApplySchema($Filename,$Debug,$Verbose);
 
 /* Remove the "Need to initialize" flag */
 if (!$FailFlag)
-  {
+{
   $Filename = "$WEBDIR/init.ui";
   $State = 1;
   if (file_exists($Filename))
-    {
+  {
     if ($Verbose) { print "Removing flag '$Filename'\n"; }
     if (is_writable($WEBDIR)) { $State = unlink($Filename); }
     else { $State = 0; }
-    }
+  }
   if (!$State)
-    {
+  {
     print "Failed to remove $Filename\n";
     print "Remove this file to complete the initialization.\n";
-    }
-  else
-    {
-    print "Initialization completed successfully.\n";
-    }
   }
-else
+  else
   {
+    print "Initialization completed successfully.\n";
+  }
+}
+else
+{
   print "Initialization had errors.\n";
   exit(1);
-  }
+}
 exit(0);
 ?>
