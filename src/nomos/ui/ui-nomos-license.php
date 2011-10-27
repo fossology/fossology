3df72ae8cdf5f19428990b525898c1b1a6b9611f
Larry Shi <yao-bin.shi@hp.com>
2011-10-27 05:58:38 +0000

    replace '<br>' with '\n' in doxygen header.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@4933 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/nomos/ui/ui-nomos-license.php
+++ src/nomos/ui/ui-nomos-license.php
@@ -131,8 +131,8 @@ class ui_nomos_license extends FO_Plugin
 
 
   /**
-   * \brief Given an $Uploadtree_pk, display: <br>
-   * (1) The histogram for the directory BY LICENSE. <br>
+   * \brief Given an $Uploadtree_pk, display: \n 
+   * (1) The histogram for the directory BY LICENSE. \n
    * (2) The file listing for the directory.
    */
   function ShowUploadHist($Uploadtree_pk,$Uri, $tag_pk)
