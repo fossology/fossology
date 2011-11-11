5ba0d877e0ddcc77824ca7d740e9505ac33d1379
Larry Shi <yao-bin.shi@hp.com>
2011-11-11 08:05:47 +0000

    DB/repo/sysconfig api C version, welcome your comments.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@5022 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ src/testing/db/c/libfodbreposysconf.c
@@ -0,0 +1,172 @@
+/* **************************************************************
+ Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+************************************************************** */
+/**
+ * \file libfodbreposysconf.c
+ * \brief api for db, sysconfig, repo. 
+ *        you can create/drop a DB/sysconfig/repo
+ */
+
+#include "libfodbreposysconf.h"
+
+static char *Sysconf = NULL;
+static char DBName[ARRAY_LENGTH];
+static char DBConf[ARRAY_LENGTH];
+
+/**
+ * \brief get command output
+ * 
+ * \param char *command - the command will be executed
+ */
+static void command_output(char *command)
+{
+  FILE *stream;
+  char tmp[ARRAY_LENGTH];
+  int i=0;
+
+  stream = popen(command, "r");
+  memset(tmp, '\0', sizeof(tmp));
+  if (fgets(tmp, ARRAY_LENGTH, stream) != NULL)
+  {
+    while((tmp[i] != '\n') && (tmp[i] != ' ') && (tmp[i] != EOF))
+      i++;
+    Sysconf = malloc(i);
+    memcpy(Sysconf, tmp, i);
+    Sysconf[i] = '\0';
+  }
+  pclose(stream);
+  return;
+}
+
+/** 
+ * \biref create DB, the db name looks linke fosstestxxxxx
+ *
+ * \param int type - 0 on create db, sysconf dir and repository, the db is empty
+ *                   1 on create db, sysconf dir and repository, the db is initialized
+ *
+ * \return 0 on sucess, other on failure
+ */
+int create_db_repo_sysconf(int type)
+{
+#if 0
+  char *sysconfdir;
+  /** get sysconfig dir from ENV */
+  sysconfdir = getenv ("SYSCONFDIR");
+  if (sysconfdir == NULL)
+  {
+    printf ("The SYSCONFDIR enviroment variable is not existed.\n");
+    return 1;
+  }
+#endif
+  char CMD[] = "../../../testing/db/createTestDB.php";
+  command_output(CMD);
+  int argc = 3;
+  char* argv[] = {"./unit_test", "-c", Sysconf};
+  fo_scheduler_connect(&argc, argv);
+
+#ifdef TEST
+  printf("create_db_repo_sysconf sucessfully\n");
+#endif
+  return 0;
+}
+
+/**
+ * \brief drop db, sysconfig dir and repo
+ * 
+ * \param char *DBName - the db name, looks like fosstestxxxx
+ */
+void drop_db_repo_sysconf(char *DBName)
+{
+  char CMD[ARRAY_LENGTH];
+  memset(CMD, '\0', sizeof(CMD));
+  sprintf(CMD, "../../../testing/db/createTestDB.php -d %s", DBName);
+  command_output(CMD);
+#ifdef TEST
+  printf("remove DBName is:%s\n", DBName);
+#endif
+  fo_scheduler_disconnect(0);
+  free(Sysconf);
+  Sysconf = NULL;
+#ifdef TEST
+  printf("drop_db_repo_sysconf sucessfully\n");
+#endif
+}
+
+/**
+ * \brief get the test name just created by  create_db_repo_sysconf()
+ *
+ * \return the test name
+ */
+char *get_test_name()
+{
+  char *TestName = strstr(Sysconf, "Conf") + 4;
+#ifdef TEST
+  printf("TestName is:%s\n", TestName);
+#endif
+  return TestName;
+}
+
+/**
+ * \brief get the DB name just created by  create_db_repo_sysconf()
+ *
+ * \return the DB name
+ */
+char *get_db_name()
+{
+  memset(DBName, '\0', sizeof(DBName));
+  char *TestName = get_test_name();
+  sprintf(DBName, "fosstest%s", TestName);
+#ifdef TEST
+  printf("DBName is:%s\n", DBName);
+#endif
+  return DBName;
+}
+
+/**
+ * \brief get sysconfig dir path just created by  create_db_repo_sysconf()
+ *
+ * \return the sysconfig dir path 
+ */
+char *get_sysconfdir()
+{
+#ifdef TEST
+  printf("Sysconf is:%s\n", Sysconf);
+#endif
+  return Sysconf;
+}
+
+/**
+ * \brief get Db.conf path just created by  create_db_repo_sysconf()
+ *
+ * \return Db.conf path
+ */
+char *get_dbconf()
+{
+  memset(DBConf, '\0', sizeof(DBConf));
+  sprintf(DBConf, "%s/Db.conf", Sysconf);
+  return DBConf;
+}
+
+#if 0
+int main()
+{
+  create_db_repo_sysconf(1);
+  get_test_name();
+  get_sysconfdir();
+  get_db_name();
+  drop_db_repo_sysconf(DBName);
+}
+#endif
