127e60645d55692fc9831cbf6fff03ccea303eb6
Larry Shi <yao-bin.shi@hp.com>
2011-11-11 09:21:45 +0000

    refactor cunit tests with new test DB/sysconf/repo API, thanks for your review. before make test, you should set the ENV variable SYSCONFDIR, then 'sudo -E make test'. why 'sudo -E ' is because 1) we should get the value of SYSCONFDIR from ENV; 2) some php script want to mkdir under /srv/fossology/, have to have the sudo privilege.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@5023 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/pkgagent/agent_tests/Unit/testRun.c
+++ src/pkgagent/agent_tests/Unit/testRun.c
@@ -22,6 +22,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "CUnit/Automated.h"
 
 #include "libfossology.h"
+#include "libfocunit.h"
+#include "libfodbreposysconf.h"
 
 /* test case sets */
 extern CU_TestInfo testcases_GetFieldValue[];
@@ -34,53 +36,14 @@ extern CU_TestInfo testcases_GetMetadataDebSource[];
 extern CU_TestInfo testcases_GetMetadataDebBinary[];
 
 char *DBConfFile = NULL;
-char *TestSysconf = NULL;
-char *TestName = NULL;
 
-/**
- * brief get command output
- */
-void command_output(char *command)
-{
-  FILE *stream;
-  char tmp[256];
-  int i=0;
- 
-  stream = popen(command, "r");
-  memset(tmp, '\0', sizeof(tmp));
-  if (fgets(tmp, 256, stream) != NULL)
-  {
-    while((tmp[i] != '\n') && (tmp[i] != ' ') && (tmp[i] != EOF))
-      i++; 
-    TestSysconf = malloc(i);
-    memcpy(TestSysconf, tmp, i);
-    TestSysconf[i] = '\0';
-  }
-  pclose(stream);
-  return;
-}
 /**
  * \brief initialize db
  */
 int PkgagentDBInit()
 {
-  char CMD[256];
-
-  memset(CMD, '\0', sizeof(CMD));
-  sprintf(CMD, "../../../testing/db/createTestDB.php -c /usr/local/etc/fossology");
-  command_output(CMD);
-  TestName = strstr(TestSysconf, "Conf") + 4;
-
-  if ( TestSysconf != NULL)
-  {
-    DBConfFile = malloc(256);
-    sprintf(DBConfFile, "%s/Db.conf", TestSysconf);
-    //printf("EE:%s:FF\n", DBConfFile);
-  }
-
-  int argc = 3;
-  char* argv[] = {"./test_pkgagent", "-c", TestSysconf};
-  fo_scheduler_connect(&argc, argv);
+  create_db_repo_sysconf(1);
+  DBConfFile = get_dbconf();
   return 0;
 }
 /**
@@ -88,42 +51,7 @@ int PkgagentDBInit()
  */
 int PkgagentDBClean()
 {
-  char CMD[256];
-  int rc;
-
-  // remove test database 
-  memset(CMD, '\0', sizeof(CMD));
-  sprintf(CMD, "../../../testing/db/createTestDB.php -d fosstest%s", TestName);
-  rc = system(CMD);
-  if (rc != 0)
-  {
-    printf("Test Database clean ERROR!\n");
-    return -1;
-  }
-  // remove test config files
-  memset(CMD, '\0', sizeof(CMD));
-  sprintf(CMD, "rm -rf /srv/fossology/testDbConf%s", TestName);
-  rc = system(CMD);
-  if (rc != 0)
-  {
-    printf("Test Database Conf files clean ERROR!\n");
-    return -1;
-  }
-  // remove test repo
-  memset(CMD, '\0', sizeof(CMD));
-  sprintf(CMD, "rm -rf /srv/fossology/testDbRepo%s", TestName);
-  rc = system(CMD);
-  if (rc != 0)
-  {
-    printf("Test Repo clean ERROR!\n");
-    return -1;
-  }
-  free(TestSysconf);
-  TestName = NULL;
-  TestSysconf = NULL;
-  DBConfFile = NULL;
-
-  fo_scheduler_disconnect(0);
+  drop_db_repo_sysconf(get_db_name());
   return 0;
 }
 
@@ -140,40 +68,7 @@ CU_SuiteInfo suites[] = {
     CU_SUITE_INFO_NULL
 };
 
-void AddTests(void)
-{
-  assert(NULL != CU_get_registry());
-  assert(!CU_is_test_running());
-
-
-  if(CUE_SUCCESS != CU_register_suites(suites)){
-    fprintf(stderr, "Register suites failed - %s ", CU_get_error_msg());
-    exit(EXIT_FAILURE);
-  }
-}
-
 int main( int argc, char *argv[] )
 {
-  printf("Test Start\n");
-  if(CU_initialize_registry()){
-
-    fprintf(stderr, "\nInitialization of Test Registry failed.\n");
-    exit(EXIT_FAILURE);
-  }else{
-    AddTests();
-    CU_set_output_filename("pkgagent_Test");
-    CU_list_tests_to_file();
-    CU_automated_run_tests();
-    //CU_cleanup_registry();
-  }
-  printf("Test End\n");
-  printf("Results:\n");
-  printf("  Number of suites run: %d\n", CU_get_number_of_suites_run());
-  printf("  Number of tests run: %d\n", CU_get_number_of_tests_run());
-  printf("  Number of tests failed: %d\n", CU_get_number_of_tests_failed());
-  printf("  Number of asserts: %d\n", CU_get_number_of_asserts());
-  printf("  Number of successes: %d\n", CU_get_number_of_successes());
-  printf("  Number of failures: %d\n", CU_get_number_of_failures());
-  CU_cleanup_registry();
-  return 0;
+  return focunit_main(argc, argv, "mimetype_Tests", suites) ;
 }
