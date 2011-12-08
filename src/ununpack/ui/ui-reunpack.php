9ac41a22d5e8f1ad497c28fa6dcc670b7607deb0
Larry Shi <yao-bin.shi@hp.com>
2011-12-08 04:16:09 +0000

    formating the code, DB api replacement, and doxygen header
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@5198 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/ununpack/ui/ui-reunpack.php
+++ src/ununpack/ui/ui-reunpack.php
@@ -17,7 +17,8 @@
  ***********************************************************/
 
 /**
- * \note this file is deprecated
+ * \file ui-reunpack.php
+ * \brief reunpack the archive
  */
 
 define("TITLE_ui_reunpack", _("Schedule an Reunpack"));
@@ -31,90 +32,89 @@ class ui_reunpack extends FO_Plugin
   public $Dependency = array("db");
   public $DBaccess   = PLUGIN_DB_UPLOAD;
 
-  /*********************************************
-   Output(): Generate the text for this plugin.
-   *********************************************/
+  /**
+   * \brief Generate the text for this plugin.
+   */
   function Output()
   {
     if ($this->State != PLUGIN_STATE_READY) { return; }
-    global $DB;
     global $Plugins;
     $V="";
     switch($this->OutputType)
     {
       case "XML":
-	     break;
+        break;
       case "HTML":
-	     /* If this is a POST, then process the request. */
-	     $uploadpk = GetParm('uploadunpack',PARM_INTEGER);
-	     if (!empty($uploadpk))
-	     {
-	       $P = &$Plugins[plugin_find_id("agent_unpack")];
-	       $rc = $P->AgentAdd($uploadpk);
-	       if (empty($rc))
-	       {
-	       /* Need to refresh the screen */
-$text = _("Unpack added to job queue");
-	         $V .= displayMessage($text);
-	       }
-	       else
-	       {
-$text = _("Unpack of Upload failed");
-	         $V .= displayMessage("$text: $rc");
-	       }
-	     }
+        /* If this is a POST, then process the request. */
+        $uploadpk = GetParm('uploadunpack',PARM_INTEGER);
+      if (!empty($uploadpk))
+      {
+        $P = &$Plugins[plugin_find_id("agent_unpack")];
+        $rc = $P->AgentAdd($uploadpk);
+        if (empty($rc))
+        {
+          /* Need to refresh the screen */
+          $text = _("Unpack added to job queue");
+          $V .= displayMessage($text);
+        }
+        else
+        {
+          $text = _("Unpack of Upload failed");
+          $V .= displayMessage("$text: $rc");
+        }
+      }
 
-	     /* Set default values */
-	     if (empty($GetURL)) { $GetURL='http://'; }
+      /* Set default values */
+      if (empty($GetURL)) { $GetURL='http://'; }
 
-	     //$V .= $this->ShowReunpackView($uploadtree_pk);
-	
-	     break;
+      //$V .= $this->ShowReunpackView($uploadtree_pk);
+
+      break;
       case "Text":
-	     break;
+        break;
       default:
-	     break;
+      break;
     }
     if (!$this->OutputToStdout) { return($V); }
     print("$V");
     return;
   }
-  
-   /*********************************************
-   CheckStatus(): Given an uploadpk and job_name
-   to check if an reunpack/rewget job is running.
-   Returns 0 no reunpack/rewget job running;
-           1 reunpack/rewget job failed
-           2 reunpack/rewget job completed
-           3 reunpack/rewget job running
-           4 reunpack/rewget job pending
-   *********************************************/ 
+
+  /**
+   * \brief Given an uploadpk and job_name
+   * to check if an reunpack/rewget job is running.
+   * \return 0 no reunpack/rewget job running;
+   * - 1 reunpack/rewget job failed
+   * - 2 reunpack/rewget job completed
+   * - 3 reunpack/rewget job running
+   * - 4 reunpack/rewget job pending
+   */
   function CheckStatus ($uploadpk, $job_name, $jobqueue_type)
   {
-    global $DB;
-    if (empty($DB)) {return;}
-    
+    global $PG_CONN; 
     $SQLcheck = "SELECT jq_pk,jq_starttime,jq_endtime,jq_end_bits FROM jobqueue
-    LEFT OUTER JOIN job ON jobqueue.jq_job_fk = job.job_pk
-  	WHERE job.job_upload_fk = '$uploadpk'
-  	AND job.job_name = '$job_name'
-  	AND jobqueue.jq_type = '$jobqueue_type';";
-    
-    $Results = $DB->Action($SQLcheck);
-    if(empty($Results)) {
+      LEFT OUTER JOIN job ON jobqueue.jq_job_fk = job.job_pk
+      WHERE job.job_upload_fk = '$uploadpk'
+      AND job.job_name = '$job_name'
+      AND jobqueue.jq_type = '$jobqueue_type';";
+    $result = pg_query($PG_CONN, $SQLcheck);
+    DBCheckResult($result, $SQLcheck, __FILE__, __LINE__);
+    pg_free_result($result);
+    $row_count = pg_num_rows($result);
+    if($row_count) {
       print $SQLcheck;
       return(0);
     }
     else {
       $i = 0;
       $State = 0;
-      while (!empty($Results[$i]['jq_pk'])) {
-        if ($Results[$i]['jq_end_bits'] == 2) {
+      while ($Row = pg_fetch_assoc($result)) {
+        if ($Row['jq_end_bits'] == 2) {
           $State = 1;
           break;
         }
-        if (!empty($Results[$i]['jq_starttime'])) {
-          if (!empty($Results[$i]['jq_endtime'])) {
+        if (!empty($Row['jq_starttime'])) {
+          if (!empty($Row['jq_endtime'])) {
             $State = 2;
           } else {
             $State = 3;
@@ -129,52 +129,60 @@ $text = _("Unpack of Upload failed");
       return ($State);
     }
   }
-    /*********************************************
-   AgentAdd(): Given an uploadpk, add a job.
-   $Depends is for specifying other dependencies.
-   $Depends can be a jq_pk, or an array of jq_pks, or NULL.
-   Returns NULL on success, string on failure.
-   *********************************************/
+
+  /**
+   * \brief Given an uploadpk, add a job.
+   * \param $Depends - specifying other dependencies.
+   * $Depends can be a jq_pk, or an array of jq_pks, or NULL.
+   *
+   * \return NULL on success, string on failure.
+   */
   function AgentAdd ($uploadpk,$Depends=NULL,$priority=0)
   {
-    global $DB;
-    if (empty($DB)) {
-      return;
-    }
-    
+    global $PG_CONN; 
     $Job_name = str_replace("'", "''", "unpack");
 
     if (empty($uploadpk)) {
       $SQLInsert = "INSERT INTO job
-    	 (job_queued,job_priority,job_name) VALUES
-    	 (now(),'$priority','$Job_name');";
+        (job_queued,job_priority,job_name) VALUES
+        (now(),'$priority','$Job_name');";
     }
     else {
       $SQLInsert = "INSERT INTO job
-    	 (job_queued,job_priority,job_name,job_upload_fk) VALUES
-     	  (now(),'$priority','$Job_name','$uploadpk');";
+        (job_queued,job_priority,job_name,job_upload_fk) VALUES
+        (now(),'$priority','$Job_name','$uploadpk');";
     }
 
     $SQLcheck = "SELECT job_pk FROM job WHERE job_upload_fk = '$uploadpk' AND job_name = '$Job_name' AND job_user_fk is NULL;";
-    $Results = $DB->Action($SQLcheck);
-    if (!empty($Results)){
-      $jobpk = $Results[0]['job_pk'];
+    $result = pg_query($PG_CONN, $SQLcheck);
+    DBCheckResult($result, $SQLcheck, __FILE__, __LINE__);
+    $row = pg_fetch_assoc($result);
+    pg_free_result($result);
+
+    if (!empty($row)){
+      $jobpk = $row['job_pk'];
     } else {
-      $DB->Action($SQLInsert);
+      $result = pg_query($PG_CONN, $SQLInsert);
+      DBCheckResult($result, $SQLInsert, __FILE__, __LINE__);
+      $row = pg_fetch_assoc($result);
+      pg_free_result($result);
       $SQLcheck = "SELECT job_pk FROM job WHERE job_upload_fk = '$uploadpk' AND job_name = '$Job_name' AND job_user_fk is NULL;";
-      $Results = $DB->Action($SQLcheck);
-      $jobpk = $Results[0]['job_pk']; 
+      $result = pg_query($PG_CONN, $SQLcheck);
+      DBCheckResult($result, $SQLcheck, __FILE__, __LINE__);
+      $row = pg_fetch_assoc($result);
+      pg_free_result($result);
+      $jobpk = $row['job_pk']; 
     }      
-  
+
     if (empty($jobpk) || ($jobpk < 0)) { return("Failed to insert job record! $SQLInsert"); }
     if (!empty($Depends) && !is_array($Depends)) { $Depends = array($Depends); }
 
     /* job "unpack" has jobqueue item "unpack" */
     $jqargs = "SELECT pfile.pfile_sha1 || '.' || pfile.pfile_md5 || '.' || pfile.pfile_size AS pfile,
-	    upload_pk, pfile_fk
-	    FROM upload
-	    INNER JOIN pfile ON upload.pfile_fk = pfile.pfile_pk
-	    WHERE upload.upload_pk = '$uploadpk';";
+      upload_pk, pfile_fk
+        FROM upload
+        INNER JOIN pfile ON upload.pfile_fk = pfile.pfile_pk
+        WHERE upload.upload_pk = '$uploadpk';";
     $jobqueuepk = JobQueueAdd($jobpk,"unpack",$jqargs,"no","pfile",$Depends,1);
     if (empty($jobqueuepk)) { return("Failed to insert item into job queue"); }
 
@@ -189,54 +197,56 @@ $text = _("Unpack of Upload failed");
     //if (empty($jobqueuepk)) { return("Failed to insert delete sqlagent into job queue"); }
     return(NULL);
   } // AgentAdd()
-  /*********************************************
-   ShowReunpackView(): Generate the reunpack view
-   page. Give the unploadtree_pk, return page view
-   output.
-   *********************************************/
+
+  /**
+   * \brief Generate the reunpack view
+   * page. Give the unploadtree_pk, return page view output.
+   */
   function ShowReunpackView($Item, $Reunpack=0)
   {
-    global $DB;
+    global $PG_CONN;        
     $V = "";
-   
-    if (empty($DB)) {return; }
-   
-    $Sql = "SELECT upload_fk FROM uploadtree WHERE uploadtree_pk = $Item;";
-    $Result = $DB->Action($Sql);
-    $Row = $Result[0];
-    if (empty($Row['upload_fk'])) { return; }
-    $Upload_pk = $Row['upload_fk'];
-    $Sql = "SELECT pfile_fk,ufile_name from uploadtree where upload_fk=$Upload_pk and parent is NULL;";
-    $Result = $DB->Action($Sql);
-    $Row = $Result[0];
-    if (empty($Row['pfile_fk'])) { return; }
-    $Pfile_fk = $Row['pfile_fk'];
-    $Ufile_name = $Row['ufile_name'];
-      
+
+    $sql = "SELECT upload_fk FROM uploadtree WHERE uploadtree_pk = $Item;";
+    $result = pg_query($PG_CONN, $sql);
+    DBCheckResult($result, $sql, __FILE__, __LINE__);
+    $row = pg_fetch_assoc($result);
+    pg_free_result($result);
+    if (empty($row['upload_fk'])) { return; }
+    $Upload_pk = $row['upload_fk'];
+    $sql = "SELECT pfile_fk,ufile_name from uploadtree where upload_fk=$Upload_pk and parent is NULL;";
+    $result = pg_query($PG_CONN, $sql);
+    DBCheckResult($result, $sql, __FILE__, __LINE__);
+    $row = pg_fetch_assoc($result);
+    pg_free_result($result);
+    if (empty($row['pfile_fk'])) { return; }
+    $Pfile_fk = $row['pfile_fk'];
+    $Ufile_name = $row['ufile_name'];
+
     $Fin_gold = @fopen( RepPath($Pfile_fk,"gold") ,"rb");
     if (empty($Fin_gold))
     {
-$text = _("The File's Gold file are not available in the repository.");
-        $V = "<p/>$text\n";
-        return $V;
+      $text = _("The File's Gold file are not available in the repository.");
+      $V = "<p/>$text\n";
+      return $V;
     }
-      
+
     $V = "<p/>";
-$text = _("This file is unpacked from");
+    $text = _("This file is unpacked from");
     $V.= "$text <font color='blue'>[".$Ufile_name."]</font>\n";
 
-      	  /* Display the form */
-	  $V .= "<form method='post'>\n"; // no url = this url
+    /* Display the form */
+    $V .= "<form method='post'>\n"; // no url = this url
 
-$text = _("Reunpack");
-	  $V .= "<p />\n$text: " . $Ufile_name . "<input name='uploadunpack' type='hidden' value='$Upload_pk'/>\n";
-	  $V .= "<input type='submit' value='$text!' ";
-	  if ($Reunpack) {$V .= "disabled";}
-	  $V .= " >\n";
-	  $V .= "</form>\n";
-	  
-      return $V;
-}
+    $text = _("Reunpack");
+    $V .= "<p />\n$text: " . $Ufile_name . "<input name='uploadunpack' type='hidden' value='$Upload_pk'/>\n";
+    $V .= "<input type='submit' value='$text!' ";
+    if ($Reunpack) {$V .= "disabled";}
+    $V .= " >\n";
+    $V .= "</form>\n";
+
+    return $V;
+  }
 }
 $NewPlugin = new ui_reunpack;    
 
