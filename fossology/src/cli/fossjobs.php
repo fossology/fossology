bcdcd548954dd8dbaf475c4e313a4889aa525c90
Larry Shi <yao-bin.shi@hp.com>
2011-12-19 13:55:50 +0000

    check  ~/.fossology.rc existing or not.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@5268 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/cli/fossjobs.php
+++ fossology/src/cli/fossjobs.php
@@ -110,13 +110,16 @@ if (array_key_exists("password", $options)) {
   $passwd = $options["password"];
 }
 
+/** get username/passwd from ~/.fossology.rc */
 $user_passwd_file = getenv("HOME") . "/.fossology.rc";
-$user_passwd_array = file($user_passwd_file, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
+if (file_exists($user_passwd_file)) {
+  $user_passwd_array = file($user_passwd_file, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
 
-if($user_passwd_array && $user_passwd_array[0])
-  $user = $user_passwd_array[0];
-if($user_passwd_array && $user_passwd_array[1])
-  $passwd = $user_passwd_array[1];
+  if($user_passwd_array && $user_passwd_array[0])
+    $user = $user_passwd_array[0];
+  if($user_passwd_array && $user_passwd_array[1])
+    $passwd = $user_passwd_array[1];
+}
 /* check if the user name/passwd is valid */
 if (empty($user)) {
   $uid_arr = posix_getpwuid(posix_getuid());
