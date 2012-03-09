0b6eb9f76d28f4e99375bf024e15d67eadf849da
Mark Donohoe <mark.donohoe@hp.com>
2012-03-09 02:24:27 +0000

    Trying to get doxygen stuff to work.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@5590 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/nomos/agent_tests/Functional/OneShot-empty.php
+++ src/nomos/agent_tests/Functional/OneShot-empty.php
@@ -15,6 +15,7 @@
  with this program; if not, write to the Free Software Foundation, Inc.,
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  ***********************************************************/
+
 /**
  * \file OneShot-empty.php
  * \brief Perform a one-shot license analysis on an empty file
