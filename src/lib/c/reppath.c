9d7789034b6c2170ab7952d8b55ed1cfbf969b73
Robert Gobeille <bob.gobeille@hp.com>
2012-03-19 22:04:47 +0000

    Fix for http://www.fossology.org/issues/1593 Use fo_config_load to initialize the system configuration (instead of fo_scheduler_connect)
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@5645 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/lib/c/reppath.c
+++ src/lib/c/reppath.c
@@ -29,8 +29,28 @@ int	main	(int argc, char *argv[])
 {
   char *Path;
   int i;
+  char  fname[FILENAME_MAX + 1];
+  char* sysconfigdir;
+  GError* error = NULL;
 
-  fo_scheduler_connect(&argc, argv);
+  sysconfigdir = DEFAULT_SETUP;  /* defined in Makefile */
+  if(sysconfigdir) 
+  {
+    snprintf(fname, FILENAME_MAX, "%s/%s", sysconfigdir, "fossology.conf");
+    sysconfig = fo_config_load(fname, &error);
+    if(error)
+    {
+      fprintf(stderr, "FATAL %s.%d: unable to open system configuration: %s\n",
+              __FILE__, __LINE__, error->message);
+      exit(-1);
+    }
+  }
+  else
+  {
+    fprintf(stderr, "FATAL %s.%d: Build error, unspecified system configuration location.\n",
+            __FILE__, __LINE__);
+    exit(-1);
+  }
 
   if ((argc%2) != 1)
   {
