9cd801f113c2126db90ae3b3e326e830f1f337ca
Larry Shi <yao-bin.shi@hp.com>
2012-03-26 03:11:06 +0000

    add copyright/nomos license download list script for 2.0.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@5670 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/lib/php/common-license-file.php
+++ fossology/src/lib/php/common-license-file.php
@@ -1,6 +1,6 @@
 <?php
 /***********************************************************
- Copyright (C) 2009-2011 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2009-2012 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -81,6 +81,89 @@ function GetFileLicenses($agent_pk, $pfile_pk, $uploadtree_pk)
   }
   pg_free_result($result);
   return $LicArray;
+}
+
+/**
+ * \brief get all the copyright for a single file or uploadtree
+ * 
+ * \param $agent_pk - agent id
+ * \param $pfile_pk - pfile id, (if empty, $uploadtree_pk must be given)
+ * \param $uploadtree_pk - (used only if $pfile_pk is empty)
+ * 
+ * \return Array of file copyright CopyrightArray[ct_pk] = copyright.content
+ * FATAL if neither pfile_pk or uploadtree_pk were passed in
+ */
+function GetFileCopyrights($agent_pk, $pfile_pk, $uploadtree_pk)
+{
+  global $PG_CONN;
+
+  if (empty($agent_pk)) Fatal("Missing parameter: agent_pk", __FILE__, __LINE__);
+
+  // if $pfile_pk, then return the copyright for that one file
+  if ($pfile_pk)
+  {
+    $sql = "SELECT ct_pk, content 
+              from copyright
+              where pfile_fk='$pfile_pk' and agent_fk=$agent_pk;";
+    $result = pg_query($PG_CONN, $sql);
+    DBCheckResult($result, $sql, __FILE__, __LINE__);
+  }
+  else if ($uploadtree_pk)
+  {
+    /* Find lft and rgt bounds for this $uploadtree_pk  */
+    $sql = "SELECT lft, rgt, upload_fk FROM uploadtree
+                   WHERE uploadtree_pk = $uploadtree_pk";
+    $result = pg_query($PG_CONN, $sql);
+    DBCheckResult($result, $sql, __FILE__, __LINE__);
+    $row = pg_fetch_assoc($result);
+    $lft = $row["lft"];
+    $rgt = $row["rgt"];
+    $upload_pk = $row["upload_fk"];
+    pg_free_result($result);
+
+    /*  Get the copyright under this $uploadtree_pk*/
+    $sql = "SELECT ct_pk, content from copyright ,
+                  (SELECT distinct(pfile_fk) as PF from uploadtree
+                     where upload_fk=$upload_pk
+                       and uploadtree.lft BETWEEN $lft and $rgt) as SS
+              where PF=pfile_fk and agent_fk=$agent_pk;";
+    $result = pg_query($PG_CONN, $sql);
+    DBCheckResult($result, $sql, __FILE__, __LINE__);
+  }
+  else Fatal("Missing function inputs", __FILE__, __LINE__);
+
+  $CopyrightArray = array();
+  while ($row = pg_fetch_assoc($result))
+  {
+    $CopyrightArray[$row['ct_pk']] = $row["content"];
+  }
+  pg_free_result($result);
+  return $CopyrightArray;
+}
+
+/**
+ * \brief  returns copyright list as a single string
+ * \param $agent_pk - agent id
+ * \param $pfile_pk - pfile id, (if empty, $uploadtree_pk must be given)
+ * \param $uploadtree_pk - (used only if $pfile_pk is empty)
+ *
+ * \return copyright string for specified file
+ */
+function GetFileCopyright_string($agent_pk, $pfile_pk, $uploadtree_pk)
+{
+  $CopyrightStr = "";
+  $CopyrightArray = GetFileCopyrights($agent_pk, $pfile_pk, $uploadtree_pk);
+  $first = true;
+  foreach($CopyrightArray as $ct)
+  {
+    if ($first)
+    $first = false;
+    else
+    $CopyrightStr .= " ,";
+    $CopyrightStr .= $ct;
+  }
+
+  return $CopyrightStr;
 }
 
 /**
