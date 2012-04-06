93c3798babf3497d20e62832468abc38d0429200
Mark Donohoe <mark.donohoe@hp.com>
2012-04-06 19:07:15 +0000

    Update date in copyright.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@5739 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/testing/lib/common-vm.php
+++ fossology/src/testing/lib/common-vm.php
@@ -1,6 +1,6 @@
 <?php
 /*
- Copyright (C) 2011 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
