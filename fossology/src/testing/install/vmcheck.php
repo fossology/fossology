0bdaea9cf02af9c905ff98cce0f1de2769543ef4
Mark Donohoe <mark.donohoe@hp.com>
2012-04-07 00:18:33 +0000

    Added creation of vm.ini file for use by vmrevert.php.  This version should be ready for production.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@5740 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/testing/install/vmcheck.php
+++ fossology/src/testing/install/vmcheck.php
@@ -24,6 +24,8 @@
  * Created on March 15, 2012 by Mark Donohoe
  */
 
+require_once('../lib/common-vm.php');
+
 $vmServers = array(
     'foss-vmhost1.usa.hp.com',
     'foss-vmhost2.usa.hp.com',
@@ -78,94 +80,60 @@ foreach($vmServers as $host)
   $vmList = array();
   $listOut = array();
 } // foreach
-//echo "results are:\n";
-// print_r($hosts) . "\n";
 
 /*
- * For each machine, turn it on, make sure there is a snapshot, revert
+ * For each machine, turn it on, make sure there is a snapshot record machines
+ * that are ready to test in an array.  Write the array to a vm.ini file.  This
+ * file will be used by vmrevert to revert the vms to the current snapshot.
+ *
  */
+$machinesReady = array();
 foreach($hosts as $host => $vms)
 {
-  //echo "DB: vms is:\n"; print_r($vms) . "\n";
   if(empty($vms))
   {
-    echo "DB: no vm's for host $host\n";
+    echo "Note: no vm's for host $host\n";
     continue;
   }
   foreach($vms as $vm)
   {
-    //echo "DB: vm to operate on:\n$vm\n";
     if(!vmOps($host, $vm, 'start'))
     {
-      echo "DB: start: would adjust list of vms\n";
+      echo "Warning: $vm would not start, not not in this run.\n";
+      continue;
     }
     if(!vmOps($host, $vm, 'hassnapshot'))
     {
-      echo "DB: hassnapshot: would adjust list of vms\n";
+      echo "DB: hassnapshot: would adjusting list of vms\n";
+
+    }
+    $machinesReady[$host][] = $vm;
+  }
+}
+//echo "DB: the machines that will be used to test are:\n";
+//print_r($machinesReady) . "\n";
+
+// create ini file, use vmname=vm for entries.
+
+$dataFile = 'vm.ini';
+$VM = fopen($dataFile, 'w') or die("FATAL! Cannot open $dataFile\n");
+foreach ($machinesReady as $host => $vms)
+{
+  if(!fwrite($VM, '[' . $host . "]\n"))
+  {
+    echo "FATAL! could not write to $dataFile\n";
+    exit(1);
+  }
+  foreach ($vms as $vm)
+  {
+    $vmParts = explode('/', $vm);
+    if(!fwrite($VM, $vmParts[4] . '=' . "$vm\n"))
+    {
+      echo "FATAL! could not write to $dataFile\n";
+      exit(1);
     }
   }
+
 }
 exit(0);
-
-/**
- * \brief execute a vmware cmd one or more vm machines
- *
- * @param string $host, the host to operate on.
- * @param mixed $vm either a string or an array.  The array should be a list of
- * VM's to power on in the format returned by vmware cmd:
- *
- * e.g. /vmfs/volumes/4cbde042-037897c8-e60c-d8d385d9cf55/fed15-64/fed15-64.vmx
- *
- * @param string $command the vmware cmd to execute
- *
- * @return boolean
- */
-function vmOps($host,$vm, $command)
-{
-  $inout = array();
-  $inrtn = -1;
-  $turnOnVm = NULL;
-  $errors = 0;
-
-  if(empty($host))
-  {
-    return(FALSE);   // void
-  }
-  if(empty($command))
-  {
-    return(FALSE);
-  }
-  if(is_array($vm))
-  {
-    foreach($vm as $machine)
-    {
-      $turnOnVm = "ssh $host 'vmware-cmd " . "\"" . $vm . "\"" .  " $command' 2>&1";
-      $laston = exec($turnOnVm, $inout, $inrtn);
-      if($inrtn != 0)
-      {
-        echo "Error: could not $command on $vm on $host\n";
-        $errors++;
-      }
-      $inout = array();
-    }
-  }
-  else
-  {
-    $turnOnVm = "ssh $host 'vmware-cmd " . "\"" . $vm . "\"" .  " start' 2>&1";
-    $laston = exec($turnOnVm, $inout, $inrtn);
-    if($inrtn != 0)
-    {
-      echo "Error: could not $command on $vm on $host\n";
-      $errors++;
-    }
-  }
-  if($errors)
-  {
-    return(FALSE);
-  }
-  else
-  {
-    return(TRUE);
-  }
-}
 ?>
