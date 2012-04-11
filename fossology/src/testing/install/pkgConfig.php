7b19ebbb817526086dcc3b1e83494e2bb637085c
Mark Donohoe <mark.donohoe@hp.com>
2012-04-11 23:36:59 +0000

    Added scanning fossology install output for PHP stack traces, the word FATAL and the string Could not connect to FOSSology database.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0@5773 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/testing/install/pkgConfig.php
+++ fossology/src/testing/install/pkgConfig.php
@@ -421,6 +421,18 @@ function installFossology($objRef)
         echo implode("\n",$iOut) . "\n";
         return(FALSE);
       }
+      // check for php or other errors that don't make apt return 1
+      $installLog = implode("\n",$iOut);
+      $stack = '/PHP Stack trace:/';
+      $fatal = '/FATAL/';
+      $noConnect = '/Could not connect to FOSSology database/';
+      $traces = preg_match_all($stack, $installLog, $stackMatches);
+      $fates = preg_match_all($fatal, $installLog, $fatalMatches);
+      $connects =  preg_match_all($noConnect, $installLog, $noconMatches);
+      print "DB: number of PHP stack traces:$traces\n";
+      print "DB: number of FATAL's found:$fates\n";
+      print "DB: number of cannot connect found:$connects\n";
+      // if any of the above are non zero, return false
       break;
     case 'Fedora':
     case 'RedHat':
