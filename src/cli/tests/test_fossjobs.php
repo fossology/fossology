11ab21515ab157fb71b89f667945f6ce33891cdc
Larry Shi <yao-bin.shi@hp.com>
2012-05-31 06:41:02 +0000

    revised and added test cases for cp2foss and fossjobs, those test cases use individual DB/sysconf/repo.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/branches/fossology2.0/fossology@5894 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/cli/tests/test_fossjobs.php
+++ src/cli/tests/test_fossjobs.php
@@ -16,124 +16,150 @@
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+require_once("./test_common.php");
+
 /**
  * \brief test cli fossjobs
  */
 
 class test_fossjobs extends PHPUnit_Framework_TestCase {
 
-  public $SYSCONF_DIR = "/usr/local/etc/fossology/";
-  public $DB_NAME = "";
+  public $SYSCONF_DIR;
+  public $DB_NAME;
+  public $PG_CONN;
+  public $DB_COMMAND;
+  public $REPO_NAME;
 
-  /**
-   * \brief create DB 
-   */
-  function create_db() {
+  /* initialization */
+  protected function setUp() {
     global $SYSCONF_DIR;
+    global $DB_COMMAND;
     global $DB_NAME;
 
-    $DB_COMMAND  = "../../testing/db/createTestDB.php";
-    exec($DB_COMMAND, $dbout, $rc);
-    $this->assertEquals(0, $rc);
-    preg_match("/(\d+)/", $dbout[0], $matches);
-    $test_name = $matches[1];
-    $DB_NAME = "fosstest".$test_name;
-    $SYSCONF_DIR = $dbout[0];
-    //print "DB_NAME is:$DB_NAME, $SYSCONF_DIR\n";
-  }
-
-  /**
-   * \brief get upload id
-   *
-   * \param $upload_info - The string to search in.
-   *
-   * \return upload Id, false on failure.
-   */
-  function get_upload_id($upload_info) {
-    $upload_id = 0;
-    preg_match("/UploadPk is: '(\d+)'/", $upload_info, $matches);
-    $upload_id = $matches[1];
-    if (!$upload_id) return false;
-    else return $upload_id;
+    $SYSCONF_DIR = "/usr/local/etc/fossology/";
+    $DB_NAME = "fossology";
+    $DB_COMMAND = "../../testing/db/createTestDB.php";
+    print "Starting functional test for fossjobs \n";
+    create_db();
+    add_user();
+    replace_repo();
+    scheduler_operation();
   }
 
   /** 
-   * \brief reschedule all rest of the agent which are not sheduled
+   * \brief schedule agents
    */
-  function test_reschedule_all(){
+  function test_reschedule_agents(){
     global $SYSCONF_DIR;
-    //$this->create_db();
-    print "Starting functional test for fossjobs \n";
+    global $PG_CONN;
+
     $out = "";
-    /** upload one dir, not any agents except wget/unpack/adj2nest */
-    $auth = "--user fossy --password fossy";
+    /** 1. upload one dir, no any agents except wget/unpack/adj2nest */
+    $auth = "--user fossy --password fossy -c $SYSCONF_DIR";
     $cp2foss_command = "cp2foss $auth ./ -f fossjobs -d 'fossjobs testing'";
+    // print "cp2foss_command is:$cp2foss_command\n";
     $last = exec("$cp2foss_command 2>&1", $out, $rtn);
-    //print_r($out);
+    // print_r($out);
     $upload_id = 0;
     /** get upload id that you just upload for testing */
     if ($out && $out[4]) {
-      $upload_id = $this->get_upload_id($out[4]);
+      $upload_id = get_upload_id($out[4]);
     } else $this->assertFalse(TRUE);
-    $out = "";
+
+    sleep(5); //wait for the agents complete
+
+    $agent_status = 0;
+    $agent_status = check_agent_status("ununpack", $upload_id);
+    $this->assertEquals(1, $agent_status);
+
     /** reschedule all rest of agent */
     $command = "fossjobs $auth -U $upload_id -v";
     $last = exec("$command 2>&1", $out, $rtn);
     //print_r($out);
+    sleep(10); //wait for the agents complete
+    $agent_status = 0;
+    $agent_status = check_agent_status("nomos", $upload_id);
+    $this->assertEquals(1, $agent_status);
+    $agent_status = 0;
+    $agent_status = check_agent_status("copyright", $upload_id);
+    $this->assertEquals(1, $agent_status);
+    $agent_status = 0;
+    $agent_status = check_agent_status("pkgagent", $upload_id);
+    $this->assertEquals(1, $agent_status);
+    $agent_status = 0;
+    $agent_status = check_agent_status("mimetype", $upload_id);
+    $this->assertEquals(1, $agent_status);
+    $agent_status = 0;
+
     $out = "";
-    /** upload one file, schedule copyright except wget/unpack/adj2nest */
-    $auth = "--user fossy --password fossy";
+    /** 2. upload one file, schedule copyright except wget/unpack/adj2nest */
     $cp2foss_command = "cp2foss $auth ./test_fossjobs.php -f fossjobs -d 'fossjobs testing copyright' -q agent_copyright";
     $last = exec("$cp2foss_command 2>&1", $out, $rtn);
     //print_r($out);
     $upload_id = 0;
     /** get upload id that you just upload for testing */
     if ($out && $out[4]) {
-      $upload_id = $this->get_upload_id($out[4]);
+      $upload_id = get_upload_id($out[4]);
     } else $this->assertFalse(TRUE);
-    $out = "";
+
+    sleep(5); //wait for the agents complete
+    $agent_status = 0;
+    $agent_status = check_agent_status("ununpack", $upload_id);
+    $this->assertEquals(1, $agent_status);
+    $agent_status = 0;
+    $agent_status = check_agent_status("copyright", $upload_id);
+    $this->assertEquals(1, $agent_status);
+
     /** reschedule just nomos */
     $command = "fossjobs $auth -U $upload_id -v -A agent_nomos";
     $last = exec("$command 2>&1", $out, $rtn);
     //print_r($out);
+    sleep(5); //wait for the agents complete
 
+    $agent_status = 0;
+    $agent_status = check_agent_status("nomos", $upload_id);
+    $this->assertEquals(1, $agent_status);
   }
 
   /**
    * \brief list agents, list uploads, help msg
    */
   function test_list_agent_and_others(){
+    $this->test_reschedule_agents(); // using the uloads in test case test_reschedule_agents()
+    global $SYSCONF_DIR;
     /** help */
     $command = "fossjobs --help";
     $last = exec("$command 2>&1", $out, $rtn);
     $output_msg_count = count($out);
     //print_r($out);
     $this->assertEquals(15, $output_msg_count);
-    $auth = "--user fossy --password fossy";
+    $auth = "--user fossy --password fossy -c $SYSCONF_DIR";
     /** list agents */
     $out = "";
     $pos = 0;
     $command = "fossjobs $auth -a ";
     $last = exec("$command 2>&1", $out, $rtn);
     //print_r($out);
+    $output_msg_count = count($out);
+    $this->assertEquals(8, $output_msg_count);
+
     /** list uploads */
     $out = "";
     $pos = 0;
-    $command = "fossjobs $auth -a ";
+    $command = "fossjobs $auth -u ";
     $last = exec("$command 2>&1", $out, $rtn);
     //print_r($out);
-    print "End up functional test for fossjobs \n";
+    $output_msg_count = count($out);
+    $this->assertEquals(3, $output_msg_count); // have 2 = (3 -1_ upploads
   }
 
   /**
-   * \brief drop db
+   * \brief clean the env
    */
-  protected function drop_db() {
-    global $DB_NAME;
-    $DB_COMMAND  = "../../testing/db/createTestDB.php -d $DB_NAME";
-    print "DB_COMMAND is:$DB_COMMAND\n";
-    exec($DB_COMMAND, $dbout, $rc);
-    $this->assertEquals(0, $rc);
+  protected function tearDown() {
+    rollback_repo(); // rollback the repo dir in ununpack.conf and wget_agent.conf to the default
+    drop_db();
+    print "End up functional test for fossjobs \n";
   }
 }
 
