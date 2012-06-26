f6838e4ca08632f50fa167954d92825e0770fcd0
Alex Norton <norton.alex@hp.com>
2012-06-26 19:34:21 +0000

    fixing the copyright for the python script
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@5987 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/scheduler/agent_tests/Functional/Functional.py
+++ fossology/src/scheduler/agent_tests/Functional/Functional.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python -u
 
 ## 
-## Copyright (C) 2010 Hewlett-Packard Development Company, L.P.
+## Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
 ## 
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License
