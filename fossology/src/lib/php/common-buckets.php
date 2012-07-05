d941a5cc517ede390fda07392321c1fdd093324c
Larry Shi <yao-bin.shi@hp.com>
2012-07-05 08:39:47 +0000

    fixed for bug http://www.fossology.org/issues/3009, for src/lib/php/* files, change GPL to LGPL.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk@6005 a5643f74-2041-0410-a2b9-94ffef304a07

--- fossology/src/lib/php/common-buckets.php
+++ fossology/src/lib/php/common-buckets.php
@@ -1,19 +1,19 @@
 <?php
 /***********************************************************
- Copyright (C) 2010-2011 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2010-2012 Hewlett-Packard Development Company, L.P.
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License version 2.1 as published by the Free Software Foundation.
 
- This program is distributed in the hope that it will be useful,
+ This library is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
- You should have received a copy of the GNU General Public License along
- with this program; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.0
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  ***********************************************************/
 
 /************************************************************
