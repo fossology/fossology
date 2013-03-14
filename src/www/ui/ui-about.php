fa4ba2164af5e7572d029ddebe8d0908a8772695
Larry Shi <yao-bin.shi@hp.com>
2013-03-14 05:50:18 +0000

    change the copright date interval.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6484 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/www/ui/ui-about.php
+++ src/www/ui/ui-about.php
@@ -1,6 +1,6 @@
 <?php
 /***********************************************************
- Copyright (C) 2008-2012 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2008-2013 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -18,7 +18,7 @@
 
 define("TITLE_ui_about", _("About FOSSology"));
 define("_PROJECT", _("FOSSology"));
-define("_COPYRIGHT", _("Copyright (C) 2007-2012 Hewlett-Packard Development Company, L.P."));
+define("_COPYRIGHT", _("Copyright (C) 2007-2013 Hewlett-Packard Development Company, L.P."));
 define("_TEXT", _("This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2 as published by the Free Software Foundation.\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA."));
 
 /**
