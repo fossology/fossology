ae2872fc7e01651f1c0934df91d649a6fdfc102c
Vincent Ma <dong.ma@hp.com>
2013-06-18 01:45:31 +0000

    update the subversion url on packaging script.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6784 a5643f74-2041-0410-a2b9-94ffef304a07

--- utils/runBuild_v2.0.php
+++ utils/runBuild_v2.0.php
@@ -38,22 +38,26 @@ global $Trunk;
 $VMS = array(
              'rhel-6-i386',
              'rhel-6-x86_64',
-             'fedora-15-i386',
-             'fedora-15-x86_64',
+#             'fedora-15-i386',
+#             'fedora-15-x86_64',
              'debian-6.0-i386',
              'debian-6.0-x86_64',   
-             'ubuntu-11.10-i386',
-             'ubuntu-11.10-x86_64',
-             'ubuntu-11.04-i386',
-             'ubuntu-11.04-x86_64',
-             'ubuntu-10.04-i386',
-             'ubuntu-10.04-x86_64',
+             'debian-7.0-i386',
+             'debian-7.0-x86_64',
+#             'ubuntu-11.10-i386',
+#             'ubuntu-11.10-x86_64',
+#             'ubuntu-11.04-i386',
+#             'ubuntu-11.04-x86_64',
+#             'ubuntu-10.04-i386',
+#             'ubuntu-10.04-x86_64',
              'fedora-17-i386',
              'fedora-17-x86_64',
              'ubuntu-12.04-i386',
              'ubuntu-12.04-x86_64',
              'ubuntu-12.10-i386',
-             'ubuntu-12.10-x86_64'
+             'ubuntu-12.10-x86_64',
+             'fedora-18-i386',
+             'fedora-18-x86_64'
 );
 //$VMS = NULL;
 $Usage = "Usage: " . basename($argv[0]) . " [options]
@@ -92,7 +96,7 @@ for ($i = 1;$i < $argc;$i++) {
   } /* for each parameter */
 
   if (empty($Version)) {
-    $Version = "2.0.1";
+    $Version = "2.2.0";
   }
 
   /* Create new version of fossology project */
@@ -111,9 +115,9 @@ for ($i = 1;$i < $argc;$i++) {
   system("rm -rf /home/build/pb/projects/fossology/pbconf/$Version/.svn");
   system("cd /home/build/pb/projects/fossology/pbconf/$Version/");
   if ($Trunk) {
-    $Cmd = "svn co http://fossology.svn.sourceforge.net/svnroot/fossology/trunk/fossology/packaging/ /home/build/pb/projects/fossology/pbconf/$Version/";
+    $Cmd = "svn co http://svn.code.sf.net/p/fossology/code/trunk/fossology/packaging/ /home/build/pb/projects/fossology/pbconf/$Version/";
   } else {
-    $Cmd = "svn co http://fossology.svn.sourceforge.net/svnroot/fossology/tags/$Version/packaging/ /home/build/pb/projects/fossology/pbconf/$Version/";
+    $Cmd = "svn co http://svn.code.sf.net/p/fossology/code/tags/2.2.0/packaging/ /home/build/pb/projects/fossology/pbconf/$Version/";
   }
   if ($Verbose) {
     print "CMD=$Cmd\n";
@@ -140,7 +144,7 @@ for ($i = 1;$i < $argc;$i++) {
   if ($Trunk){
     //$showtime = date("Ymd");
     system("perl -pi -e 's/\/var\/ftp\/pub\/fossology/\/var\/ftp\/pub\/fossology\/$Version\/testing\/$showtime/' /home/build/pb/projects/fossology/pbconf/$Version/fossology.pb");
-    system("perl -pi -e 's/svnroot\/fossology\/trunk\/fossology\//svnroot\/fossology\/trunk\/fossology/' /home/build/pb/projects/fossology/pbconf/$Version/fossology.pb");
+    system("perl -pi -e 's/code\/trunk\/fossology\//code\/trunk\/fossology/' /home/build/pb/projects/fossology/pbconf/$Version/fossology.pb");
     system("perl -pi -e 's/projver fossology = trunk/projver fossology = $Version/' /home/build/pb/projects/fossology/pbconf/$Version/fossology.pb");
     system("perl -pi -e 's/trunk/$Version~$showtime/' /home/build/pb/projects/fossology/pbconf/$Version/fossology/deb/changelog");
   } else {
