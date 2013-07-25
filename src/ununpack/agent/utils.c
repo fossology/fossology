021bf74fbb5ac6b193476e1cc068b863292bee18
Vincent Ma <dong.ma@hp.com>
2013-07-25 11:14:13 +0000

    Fixed the bug5313, the folder name is incorrect when package name is ending with version number.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6849 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/ununpack/agent/utils.c
+++ src/ununpack/agent/utils.c
@@ -86,6 +86,8 @@ void RemovePostfix(char *Name)
   if (NULL == Name) return; // exception
   // keep the part before the last dot
   char *LastDot = strrchr(Name, '.');
+  // if the part after the last dot is number, do not get rid of the postfix
+  if ((LastDot[1]>='0')&&(LastDot[1]<='9')) return;
   if (LastDot) *LastDot = 0;
 }
 
