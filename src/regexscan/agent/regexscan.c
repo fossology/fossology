125685f122539e63f7c33765089c93718541340a
Paul Guttmann <pguttmann@bloomberg.net>
2013-08-07 19:28:05 +0000

    Second regexscan agent commit. Bare bones with make support. Addresses Tutorial 2 as per - http://www.fossology.org/boards/9/topics/63 No UI or Tests at this stage.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6878 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/regexscan/agent/regexscan.c
+++ src/regexscan/agent/regexscan.c
@@ -34,6 +34,9 @@
 #include <signal.h>
 #include <libgen.h>
 
+#include <regex.h>
+#include <stdbool.h>
+
 #include "libfossology.h"
 
 #define MAXCMD 4096
@@ -59,12 +62,30 @@ void    Usage   (char *Name)
   printf("  -c SYSCONFDIR :: FOSSology configuration directory.\n");
   printf("  -h        :: show available command line options.\n");
   printf("  -v        :: increase agent logging verbosity.\n");
+  printf("  -r        :: regex expression to load from command line.\n");
+  printf("  filename  :: filename to process with regex.\n");
 } /* Usage() */
 
 /*********************************************************/
 int	main	(int argc, char *argv[])
 {
-  int c;
+#ifdef REGEX_DEBUG
+  int index, nonoptargs = 0;
+#endif
+
+  int  c, retCode;
+  bool match = false;   /* regex match found indicator */ 
+
+  regex_t regex;
+
+  char msgBuff[250];
+  char regexStr[1024];  /* string storage for the regex expression */
+  char textBuff[2000];  /* line buffer for regex match processing */
+
+  regmatch_t  rm[1];
+  FILE *scanFilePtr;
+  char fileName[1000];
+  int  lineCount = 0;
 
   char *SVN_REV;
   char *VERSION;
@@ -79,11 +100,12 @@ int	main	(int argc, char *argv[])
   SVN_REV = fo_sysconfig("regexscan", "SVN_REV");
   VERSION = fo_sysconfig("regexscan", "VERSION");
   sprintf(agent_rev, "%s.%s", VERSION, SVN_REV);
-
+#ifdef REGEX_DEBUG
   fprintf(stdout, "regexscan reports version info as '%s.%s'.\n", VERSION, SVN_REV);
+#endif
 
   /* Process command-line */
-  while((c = getopt(argc,argv,"civ")) != -1)
+  while((c = getopt(argc,argv,"chir:v")) != -1)
   {
     switch(c)
     {
@@ -92,6 +114,9 @@ int	main	(int argc, char *argv[])
     case 'i':
       PQfinish(pgConn);
       return(0);
+    case 'r':
+      sprintf(regexStr, "%s", optarg);
+      break;
     case 'v':
       agent_verbose++;
       break;
@@ -103,6 +128,71 @@ int	main	(int argc, char *argv[])
       exit(-1);
     }
   }
+
+#ifdef REGEX_DEBUG
+  /* process filename after switches */
+  for (index = optind; index < argc; index++)
+  {
+    printf("Non-option argument %s\n", argv[index]);  /* diag display */
+    nonoptargs++;
+  }
+#endif
+
+  /* Compile the regex for improved performance */
+  retCode = regcomp(&regex, regexStr, REG_ICASE+REG_EXTENDED);
+  if (retCode)
+  {
+    fprintf(stderr, "regex %s failed to compile\n", regexStr);
+    return 1;
+  }
+
+  /* File access initialization */
+  sprintf(fileName, "%s", argv[optind]);      /* Grab first non-switch argument as filename */
+  scanFilePtr = fopen(fileName, "r");
+  if (!scanFilePtr)
+  {
+    fprintf(stderr, "failed to open text inout file %s\n", fileName);
+    regfree(&regex);
+    return 2;
+  }
+
+  /* Now scan the file for regex line by line */
+  while (fgets(textBuff, 1024, scanFilePtr) != NULL)
+  {
+    lineCount++;    /* Another line read */
+    retCode = regexec(&regex, textBuff, 1, rm, 0);  /* nmatch = 1, matchptr = rm */
+    if (!retCode)
+    {
+      sprintf(msgBuff, "%s: regex found at line %d at position %d. -> %.*s \n", fileName, lineCount, rm[0].rm_so+1, rm[0].rm_eo-rm[0].rm_so, textBuff + rm[0].rm_so);
+      puts(msgBuff);
+      if (!match)
+      {
+        match = true;   /* Indicate we've had at least one match */
+      }
+    }
+    else if (retCode == REG_NOMATCH)
+    {
+      /* Skip the "no match" retCode */
+    }
+    else
+    {
+      regerror(retCode, &regex, msgBuff, sizeof(msgBuff));
+      fprintf(stderr, "Out of memory? - regex match failure: %s\n", msgBuff);
+      fclose(scanFilePtr);
+      return 3;
+    }
+  }
+
+  /* Report if no matches found */
+  if (!match)
+  {
+    sprintf(msgBuff, "%s: %s not found\n", fileName, regexStr);
+    puts(msgBuff);
+  }
+
+  /* clean up and exit */
+  regfree(&regex);
+  fclose(scanFilePtr);
 
   PQfinish(pgConn);
   fo_scheduler_disconnect(0);
