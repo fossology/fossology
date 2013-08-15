bca72477662c45174397619c44b4fa6b4b5f3284
Paul Guttmann <pguttmann@bloomberg.net>
2013-08-15 12:56:56 +0000

    Adjust Copyright string.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6892 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/regexscan/agent/regexscan.c
+++ src/regexscan/agent/regexscan.c
@@ -1,7 +1,7 @@
 /***************************************************************
  regexscan: Scan file(s) for regular expression(s)
 
- Copyright (C) 2007-2013 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2013 Hewlett-Packard Development Company, L.P.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
