7aaffe3cb76899dabe63d85cd91f44f597c57aee
Robert Gobeille <bob.gobeille@hp.com>
2013-09-25 19:56:32 +0000

    No functional change.  Just split code into multiple functions for maintainability (readability). Next I'll actually fix the code (http://www.fossology.org/issues/4593)
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6929 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/www/ui/admin-dashboard.php
+++ src/www/ui/admin-dashboard.php
@@ -28,79 +28,156 @@ class dashboard extends FO_Plugin
   var $DBaccess   = PLUGIN_DB_ADMIN;
 
   /**
-   * \brief Determine amount of free disk space.
+   * \brief Database Contents metrics
+   * \returns html table containing metrics
    */
-  function DiskFree()
+  function DatabaseContents()
   {
-    global $SYSCONFDIR;
-    $Cmd = "df -Pk `cat '$SYSCONFDIR/fossology/RepPath.conf'`/*/* | sort -u | grep '%'";
-    $Fin = popen($Cmd,"r");
+    global $PG_CONN;
 
-    /* Read results */
-    $Buf = "";
-    while(!feof($Fin))
-    {
-      $Buf .= fread($Fin,8192);
-    }
-    pclose($Fin);
+        $V = "<table border=1>\n";
+        $text = _("Metric");
+        $text1 = _("Total");
+        $V .= "<tr><th>$text</th><th>$text1</th></tr>\n";
+        $sql = "SELECT count(*) AS val FROM upload;";
+        $result = pg_query($PG_CONN, $sql);
+        DBCheckResult($result, $sql, __FILE__, __LINE__);
+        $row = pg_fetch_assoc($result);
+        $item_count = '';
+        $item_count = $row['val'];
+        pg_free_result($result);
+        $text = _("Unique Uploads");
+        $V .= "<tr><td>$text</td>";
+        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
+        $sql = "SELECT count(*) AS val FROM pfile;";
+        $result = pg_query($PG_CONN, $sql);
+        DBCheckResult($result, $sql, __FILE__, __LINE__);
+        $row = pg_fetch_assoc($result);
+        $item_count = '';
+        $item_count = $row['val'];
+        pg_free_result($result);
+        $text = _("Unique Extracted Files");
+        $V .= "<tr><td>$text</td>";
+        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
+        $sql = "SELECT count(*) AS val FROM uploadtree;";
+        $result = pg_query($PG_CONN, $sql);
+        DBCheckResult($result, $sql, __FILE__, __LINE__);
+        $row = pg_fetch_assoc($result);
+        $item_count = '';
+        $item_count = $row['val'];
+        pg_free_result($result);
+        $text = _("Extracted Names");
+        $V .= "<tr><td>$text</td>";
+        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
+        $sql = "SELECT count(*) AS val FROM agent_lic_raw;";
+        $result = pg_query($PG_CONN, $sql);
+        DBCheckResult($result, $sql, __FILE__, __LINE__);
+        $row = pg_fetch_assoc($result);
+        $item_count = '';
+        $item_count = $row['val'];
+        pg_free_result($result);
+        $text = _("Known License Templates");
+        $V .= "<tr><td>$text</td>";
+        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
+        $sql = "SELECT count(*) AS val FROM agent_lic_meta;";
+        $result = pg_query($PG_CONN, $sql);
+        DBCheckResult($result, $sql, __FILE__, __LINE__);
+        $row = pg_fetch_assoc($result);
+        $item_count = '';
+        $item_count = $row['val'];
+        pg_free_result($result);
+        $text = _("Identified Licenses");
+        $V .= "<tr><td>$text</td>";
+        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
+        $V .= "</table>\n";
 
-    /* Separate lines */
-    $Lines = explode("\n",$Buf);
+    return $V;
+  }
 
-    /* Display results */
-    $V = "";
-    $V .= "<table border=1>\n";
-    $text = _("Filesystem");
-    $text1 = _("Used");
-    $text2 = _("Capacity");
-    $text3 = _("Percent Full");
-    $V .= "<tr><th>$text</th><th colspan=2>$text1</th><th colspan=2>$text2</th><th>$text3</th></tr>\n";
-    foreach($Lines as $L)
-    {
-      if (empty($L)) { continue; }
-      $L = trim($L);
-      $L = preg_replace("/[[:space:]][[:space:]]*/"," ",$L);
-      $List = explode(" ",$L);
-      $V .= "<tr><td>" . htmlentities($List[0]) . "</td>";
-      $Used = $List[2] * 1024;
-      $UsedH = Bytes2Human($Used);
-      $Capacity = $List[1] * 1024;
-      $CapacityH = Bytes2Human($Capacity);
-
-      $V .= "<td align='right' style='border-right:none'>$UsedH</td>";
-      $V .= "<td align='right' style='border-left:none'>(" . number_format($Used,0,"",",") . ")</td>";
-      $V .= "<td align='right' style='border-right:none'>$CapacityH</td>";
-      $V .= "<td align='right' style='border-left:none'>(" . number_format($Capacity,0,"",",") . ")</td>";
-      $V .= "<td align='right'>" . htmlentities($List[4]) . "</td></tr>\n";
-    }
-    $V .= "</table>\n";
-    return($V);
-  } // DiskFree()
 
   /**
-   * \brief Generate output.
+   * \brief Database metrics
+   * \returns html table containing metrics
    */
-  function Output() {
-
+  function DatabaseMetrics()
+  {
     global $PG_CONN;
-    global $Plugins;
 
-    if ($this->State != PLUGIN_STATE_READY) { return; }
-    $V="";
+        $V = "<table border=1>\n";
+        $text = _("Metric");
+        $text1 = _("Total");
+        $V .= "<tr><th>$text</th><th>$text1</th></tr>\n";
+        $sql = "SELECT pg_size_pretty(pg_database_size('fossology')) as val;";
+        $result = pg_query($PG_CONN, $sql);
+        DBCheckResult($result, $sql, __FILE__, __LINE__);
+        $row = pg_fetch_assoc($result);
+        $Size = $row['val'];
+        pg_free_result($result);
+        $text = _("FOSSology database size");
+        $V .= "<tr><td>$text</td>";
+        $V .= "<td align='right'>  $Size </td></tr>\n";;
 
-    switch($this->OutputType)
-    {
-      case "XML":
-        break;
-      case "HTML":
-        /**************************************************/
-        $V .= "<table border=0 width='100%'><tr>\n";
+        $sql = "SELECT count(*) AS val FROM pg_stat_activity;";
+        $result = pg_query($PG_CONN, $sql);
+        DBCheckResult($result, $sql, __FILE__, __LINE__);
+        $row = pg_fetch_assoc($result);
+        $item_count = '';
+        $item_count = $row['val'];
+        pg_free_result($result);
+        if (!empty($item_count))
+        {
+          $sql = "SELECT * from version();";
+          $result = pg_query($PG_CONN, $sql);
+          DBCheckResult($result, $sql, __FILE__, __LINE__);
+          DBCheckResult($result, $sql, __FILE__, __LINE__);
+          $row = pg_fetch_assoc($result);
+          pg_free_result($result);
+          $version = explode(' ', $row['version'], 3);
+          $current_sql = "current_query";
+          if (strcmp($version[1], "9.2") >= 0) // when greater than PostgreSQL 9.2 replace "current_query" with "state"
+          {
+            $current_sql = "state";
+          }
+          $text = _("Active database connections");
+          $V .= "<tr><td>$text</td>";
+          $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
+          $sql = "SELECT count(*) AS val FROM pg_stat_activity WHERE $current_sql != '<IDLE>' AND datname = 'fossology';";
+          $result = pg_query($PG_CONN, $sql);
+          DBCheckResult($result, $sql, __FILE__, __LINE__);
+          $row = pg_fetch_assoc($result);
+          $item_count = '';
+          $item_count = $row['val'];
+          pg_free_result($result);
+          $text = _("Active FOSSology queries");
+          $V .= "<tr><td>$text</td>";
+          $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
+          $sql = "SELECT datname,now()-query_start AS val FROM pg_stat_activity WHERE $current_sql != '<IDLE>' AND datname = 'fossology' ORDER BY val;"; 
+          $result = pg_query($PG_CONN, $sql);
+          DBCheckResult($result, $sql, __FILE__, __LINE__);
+          for ($i = 0; ($row = pg_fetch_assoc($result)) and !empty($row['datname']); $i++)
+          {
+            $text = _("Duration query #");
+            $text1 = _(" has been active");
+            $V .= "<tr><td>$text . $i . $text1</td>";
+            $V .= "<td align='right'>" . $row['val'] . "</td></tr>\n";
+          }
+          pg_free_result($result);
+        }
+        $V .= "</table>\n";
 
-        /**************************************************/
-        $V .= "<td valign='top'>\n";
-        $text = _("Job Queue");
-        $V .= "<H2>$text</H2>\n";
-        $V .= "<table border=1>\n";
+    return $V;
+  }
+
+
+  /**
+   * \brief Database metrics
+   * \returns html table containing information on the job queue
+   */
+  function JobQueueInfo()
+  {
+    global $PG_CONN;
+
+        $V = "<table border=1>\n";
         $text = _("Queue Information");
         $text1 = _("Total");
         $V .= "<tr><th>$text</th><th>$text1</th></tr>\n";
@@ -174,132 +251,100 @@ class dashboard extends FO_Plugin
         $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";
         $V .= "</table>\n";
         $V .= "</td>";
+    return $V;
+  }
+
+
+
+  /**
+   * \brief Determine amount of free disk space.
+   */
+  function DiskFree()
+  {
+    global $SYSCONFDIR;
+    $Cmd = "df -Pk `cat '$SYSCONFDIR/fossology/RepPath.conf'`/*/* | sort -u | grep '%'";
+    $Fin = popen($Cmd,"r");
+
+    /* Read results */
+    $Buf = "";
+    while(!feof($Fin))
+    {
+      $Buf .= fread($Fin,8192);
+    }
+    pclose($Fin);
+
+    /* Separate lines */
+    $Lines = explode("\n",$Buf);
+
+    /* Display results */
+    $V = "";
+    $V .= "<table border=1>\n";
+    $text = _("Filesystem");
+    $text1 = _("Used");
+    $text2 = _("Capacity");
+    $text3 = _("Percent Full");
+    $V .= "<tr><th>$text</th><th colspan=2>$text1</th><th colspan=2>$text2</th><th>$text3</th></tr>\n";
+    foreach($Lines as $L)
+    {
+      if (empty($L)) { continue; }
+      $L = trim($L);
+      $L = preg_replace("/[[:space:]][[:space:]]*/"," ",$L);
+      $List = explode(" ",$L);
+      $V .= "<tr><td>" . htmlentities($List[0]) . "</td>";
+      $Used = $List[2] * 1024;
+      $UsedH = Bytes2Human($Used);
+      $Capacity = $List[1] * 1024;
+      $CapacityH = Bytes2Human($Capacity);
+
+      $V .= "<td align='right' style='border-right:none'>$UsedH</td>";
+      $V .= "<td align='right' style='border-left:none'>(" . number_format($Used,0,"",",") . ")</td>";
+      $V .= "<td align='right' style='border-right:none'>$CapacityH</td>";
+      $V .= "<td align='right' style='border-left:none'>(" . number_format($Capacity,0,"",",") . ")</td>";
+      $V .= "<td align='right'>" . htmlentities($List[4]) . "</td></tr>\n";
+    }
+    $V .= "</table>\n";
+    return($V);
+  } // DiskFree()
+
+  /**
+   * \brief Generate output.
+   */
+  function Output() {
+
+    global $PG_CONN;
+    global $Plugins;
+
+    if ($this->State != PLUGIN_STATE_READY) { return; }
+    $V="";
+
+    switch($this->OutputType)
+    {
+      case "XML":
+        break;
+      case "HTML":
+        /**************************************************/
+        $V .= "<table border=0 width='100%'><tr>\n";
+
+        /**************************************************/
+        $V .= "<td valign='top'>\n";
+        $text = _("Job Queue");
+        $V .= "<H2>$text</H2>\n";
+        $V .= $this->JobQueueInfo();
+
 
         /**************************************************/
         $V .= "<td valign='top'>\n";
         $text = _("Database Contents");
         $V .= "<H2>$text</H2>\n";
-        $V .= "<table border=1>\n";
-        $text = _("Metric");
-        $text1 = _("Total");
-        $V .= "<tr><th>$text</th><th>$text1</th></tr>\n";
-        $sql = "SELECT count(*) AS val FROM upload;";
-        $result = pg_query($PG_CONN, $sql);
-        DBCheckResult($result, $sql, __FILE__, __LINE__);
-        $row = pg_fetch_assoc($result);
-        $item_count = '';
-        $item_count = $row['val'];
-        pg_free_result($result);
-        $text = _("Unique Uploads");
-        $V .= "<tr><td>$text</td>";
-        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
-        $sql = "SELECT count(*) AS val FROM pfile;";
-        $result = pg_query($PG_CONN, $sql);
-        DBCheckResult($result, $sql, __FILE__, __LINE__);
-        $row = pg_fetch_assoc($result);
-        $item_count = '';
-        $item_count = $row['val'];
-        pg_free_result($result);
-        $text = _("Unique Extracted Files");
-        $V .= "<tr><td>$text</td>";
-        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
-        $sql = "SELECT count(*) AS val FROM uploadtree;";
-        $result = pg_query($PG_CONN, $sql);
-        DBCheckResult($result, $sql, __FILE__, __LINE__);
-        $row = pg_fetch_assoc($result);
-        $item_count = '';
-        $item_count = $row['val'];
-        pg_free_result($result);
-        $text = _("Extracted Names");
-        $V .= "<tr><td>$text</td>";
-        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
-        $sql = "SELECT count(*) AS val FROM agent_lic_raw;";
-        $result = pg_query($PG_CONN, $sql);
-        DBCheckResult($result, $sql, __FILE__, __LINE__);
-        $row = pg_fetch_assoc($result);
-        $item_count = '';
-        $item_count = $row['val'];
-        pg_free_result($result);
-        $text = _("Known License Templates");
-        $V .= "<tr><td>$text</td>";
-        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
-        $sql = "SELECT count(*) AS val FROM agent_lic_meta;";
-        $result = pg_query($PG_CONN, $sql);
-        DBCheckResult($result, $sql, __FILE__, __LINE__);
-        $row = pg_fetch_assoc($result);
-        $item_count = '';
-        $item_count = $row['val'];
-        pg_free_result($result);
-        $text = _("Identified Licenses");
-        $V .= "<tr><td>$text</td>";
-        $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
-        $V .= "</table>\n";
+        $V .= $this->DatabaseContents();
+
 
         /**************************************************/
         $V .= "<td valign='top'>\n";
         $text = _("Database Metrics");
         $V .= "<H2>$text</H2>\n";
-        $V .= "<table border=1>\n";
-        $text = _("Metric");
-        $text1 = _("Total");
-        $V .= "<tr><th>$text</th><th>$text1</th></tr>\n";
-        $sql = "SELECT pg_size_pretty(pg_database_size('fossology')) as val;";
-        $result = pg_query($PG_CONN, $sql);
-        DBCheckResult($result, $sql, __FILE__, __LINE__);
-        $row = pg_fetch_assoc($result);
-        $Size = $row['val'];
-        pg_free_result($result);
-        $text = _("FOSSology database size");
-        $V .= "<tr><td>$text</td>";
-        $V .= "<td align='right'>  $Size </td></tr>\n";;
+        $V .= $this->DatabaseMetrics();
 
-        $sql = "SELECT count(*) AS val FROM pg_stat_activity;";
-        $result = pg_query($PG_CONN, $sql);
-        DBCheckResult($result, $sql, __FILE__, __LINE__);
-        $row = pg_fetch_assoc($result);
-        $item_count = '';
-        $item_count = $row['val'];
-        pg_free_result($result);
-        if (!empty($item_count))
-        {
-          $sql = "SELECT * from version();";
-          $result = pg_query($PG_CONN, $sql);
-          DBCheckResult($result, $sql, __FILE__, __LINE__);
-          DBCheckResult($result, $sql, __FILE__, __LINE__);
-          $row = pg_fetch_assoc($result);
-          pg_free_result($result);
-          $version = explode(' ', $row['version'], 3);
-          $current_sql = "current_query";
-          if (strcmp($version[1], "9.2") >= 0) // when greater than PostgreSQL 9.2 replace "current_query" with "state"
-          {
-            $current_sql = "state";
-          }
-          $text = _("Active database connections");
-          $V .= "<tr><td>$text</td>";
-          $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
-          $sql = "SELECT count(*) AS val FROM pg_stat_activity WHERE $current_sql != '<IDLE>' AND datname = 'fossology';";
-          $result = pg_query($PG_CONN, $sql);
-          DBCheckResult($result, $sql, __FILE__, __LINE__);
-          $row = pg_fetch_assoc($result);
-          $item_count = '';
-          $item_count = $row['val'];
-          pg_free_result($result);
-          $text = _("Active FOSSology queries");
-          $V .= "<tr><td>$text</td>";
-          $V .= "<td align='right'>" . number_format($item_count,0,"",",") . "</td></tr>\n";;
-          $sql = "SELECT datname,now()-query_start AS val FROM pg_stat_activity WHERE $current_sql != '<IDLE>' AND datname = 'fossology' ORDER BY val;"; 
-          $result = pg_query($PG_CONN, $sql);
-          DBCheckResult($result, $sql, __FILE__, __LINE__);
-          for ($i = 0; ($row = pg_fetch_assoc($result)) and !empty($row['datname']); $i++)
-          {
-            $text = _("Duration query #");
-            $text1 = _(" has been active");
-            $V .= "<tr><td>$text . $i . $text1</td>";
-            $V .= "<td align='right'>" . $row['val'] . "</td></tr>\n";
-          }
-          pg_free_result($result);
-        }
-        $V .= "</table>\n";
 
         /**************************************************/
         $V .= "</table>\n";
