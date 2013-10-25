ab6ca0a0003a0eef1bb54511b3962d535d4ab6d6
Larry Shi <yao-bin.shi@hp.com>
2013-10-25 09:31:10 +0000

    fixed for #5655 (permission is incorrect).
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6976 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/lib/php/common-job.php
+++ src/lib/php/common-job.php
@@ -324,16 +324,18 @@ function QueueUploadsOnAgents($upload_pk_list, $agent_list, $Verbose)
       if (empty($upload_pk))  continue;
 
       // Create a job for the upload
-      // Use the upload name for the job name
-      $where = "where job_upload_fk='$upload_pk'";
-      $UploadRec = GetSingleRec("job", $where);
+      $where = "where upload_pk ='$upload_pk'";
+      $UploadRec = GetSingleRec("upload", $where);
       if (empty($UploadRec))
       {
         echo "ERROR: unknown upload_pk: $upload_pk\n";
         continue;
       }
 
-      $job_pk = $UploadRec['job_pk'];
+      $ShortName = $UploadRec['upload_filename'];
+
+      /* Create Job */
+      $job_pk = JobAddJob($user_pk, $ShortName, $upload_pk);
 
       // don't exit on AgentAdd failure, or all the agents requested will
       // not get scheduled.
