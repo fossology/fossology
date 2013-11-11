678ef40620fae27d0c352040bb34f2d820fe59d1
Robert Gobeille <bob.gobeille@hp.com>
2013-11-11 22:22:39 +0000

    This is just to provide a way to schedule the maintenance agent before the real UI gets written. After the real UI is written, I'll get rid of this.
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@7000 a5643f74-2041-0410-a2b9-94ffef304a07

--- src/maintagent/ui/agent-maintagent.php
+++ src/maintagent/ui/agent-maintagent.php
@@ -18,7 +18,7 @@
 
 /**
  * \file agent-maintagent.php
- * \brief run the maintenance agent agent
+ * \brief run the maintenance agent
  */
 
 define("TITLE_agent_maintagent", _("Maintenance agent"));
@@ -44,16 +44,18 @@ class agent_maintagent extends FO_Plugin {
 
 
   /**
-   * \brief Queue the maintagent agent.
+   * \brief Queue an agent.  This is a simple version of AgentAdd() that can be
+   *  used by multiple plugins that only use upload_pk as jqargs.
    *  Before queuing, check if agent needs to be queued.  It doesn't need to be queued if:
    *  - It is already queued
    *  - It has already been run by the latest agent version
    *
    * \param $job_pk
-   * \param $upload_pk
+   * \param $upload_pk - not used
    * \param $ErrorMsg - error message on failure
-   * \param $Dependencies - array of plugin names representing dependencies.
-   *        This is for dependencies that this plugin cannot know about ahead of time.
+   * \param $Dependencies - array of named dependencies. Each array element is the plugin name.
+   *         For example,  array(agent_adj2nest, agent_pkgagent).  
+   *         Typically, this will just be array(agent_adj2nest).
    *
    * \returns
    * - jq_pk Successfully queued
@@ -62,9 +64,29 @@ class agent_maintagent extends FO_Plugin {
    **/
   function AgentAdd($job_pk, $upload_pk, &$ErrorMsg, $Dependencies)
   {
-    $Dependencies[] = "";
-    return CommonAgentAdd($this, $job_pk, $upload_pk, $ErrorMsg, $Dependencies);
+    global $PG_CONN;
+    global $Plugins;
+    $Deps = array();
+    $DependsEmpty = array();
+
+    /* if it is already scheduled, then return success */
+    if (($jq_pk = IsAlreadyScheduled($job_pk, $plugin->AgentName)) != 0 ) return $jq_pk;
+
+    /* schedule AgentName */
+    $jqargs = "";
+    $jq_pk = JobQueueAdd($job_pk, $this->AgentName, $jqargs, "", $Deps);
+    if (empty($jq_pk)){
+      $ErrorMsg = _("Failed to insert agent $plugin->AgentName into job queue. jqargs: $jqargs");
+      return (-1);
+    }
+
+    /* Tell the scheduler to check the queue. */
+    $success  = fo_communicate_with_scheduler("database", $output, $error_msg);
+    if (!$success) $ErrorMsg = $error_msg . "\n" . $output;
+
+    return ($jq_pk);
   } // AgentAdd()
+
 }
 
 $NewPlugin = new agent_maintagent;
