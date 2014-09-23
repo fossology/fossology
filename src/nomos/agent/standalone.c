fossology/fossology

commit dea9453e0d26a5cdd55885ec4838008dd467ef8d

Added some const specifiers

src/nomos/agent/standalone.c
@@ -10,14 +10,14 @@ char* fo_scheduler_current(){return(0);}
 int   fo_scheduler_userID(){return(0);}
 void  fo_scheduler_set_special(int option, int value){}
 int   fo_scheduler_get_special(int option){return(0);}
-char* fo_sysconfig(char* sectionname, char* variablename){return(0);}
+char* fo_sysconfig(const char* sectionname, const char* variablename){return(0);}
 int  fo_GetAgentKey   (PGconn *pgConn, char *agent_name, long unused, char *cpunused, char *agent_desc){return(0);}
 int fo_WriteARS(PGconn *pgConn, int ars_pk, int upload_pk, int agent_pk,
                          char *tableName, char *ars_status, int ars_success){return(0);}
 PGconn *fo_dbconnect(char *DBConfFile, char **ErrorBuf){return(0);}
 int     fo_checkPQcommand(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb){return(0);}
 int     fo_checkPQresult(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb){return(0);}
-int     fo_tableExists(PGconn *pgConn, char *tableName){return(0);}
+int     fo_tableExists(PGconn *pgConn, const char *tableName){return(0);}
 char * fo_RepMkPath (char *Type, char *Filename){return(0);}
 int GetUploadPerm(PGconn *pgConn, long UploadPk, int user_pk){return(10);}
 

src/nomos/agent/standalone.h
@@ -62,9 +62,9 @@ extern int   fo_scheduler_userID();
 extern void  fo_scheduler_set_special(int option, int value);
 extern int   fo_scheduler_get_special(int option);
 extern char* fo_sysconfig(char* sectionname, char* variablename);
-extern int  fo_GetAgentKey   (PGconn *pgConn, char *agent_name, long unused, char *cpunused, char *agent_desc);
+extern int  fo_GetAgentKey   (PGconn *pgConn,const char *agent_name, long unused, const char *cpunused, const char *agent_desc);
 extern int fo_WriteARS(PGconn *pgConn, int ars_pk, int upload_pk, int agent_pk,
-                         char *tableName, char *ars_status, int ars_success);
+                        const char *tableName, const char *ars_status, int ars_success);
 extern PGconn *fo_dbconnect(char *DBConfFile, char **ErrorBuf);
 extern int     fo_checkPQcommand(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb);
 extern int     fo_checkPQresult(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb);

src/lib/c/fossconfig.c
@@ -326,7 +326,7 @@ fo_conf* fo_config_load(char* rawname, GError** error) {
  * @param key c string that is the name of the key for the key/value pair
  * @return the c string representation of the value
  */
-char* fo_config_get(fo_conf* conf, char* group, char* key, GError** error)
+char* fo_config_get(fo_conf* conf, const char* group, const char* key, GError** error)
 {
   GTree* tree;
   char* ret = NULL;

src/lib/c/fossconfig.h
@@ -49,7 +49,7 @@ fo_conf* fo_config_load(char* fname, GError** error);
 void     fo_config_free(fo_conf* conf);
 void     fo_config_join(fo_conf* dst, fo_conf* src, GError** error);
 
-char* fo_config_get(fo_conf* conf, char* group, char* key, GError** error);
+char* fo_config_get(fo_conf* conf, const char* group, const char* key, GError** error);
 char* fo_config_get_list(fo_conf* conf, char* group, char* key, int idx, GError** error);
 int   fo_config_is_list(fo_conf* conf, char* group, char* key, GError** error);
 int   fo_config_list_length(fo_conf* conf, char* group, char* key, GError** error);

src/lib/c/libfossagent.c
@@ -42,7 +42,7 @@
  \todo This function is not checking if the agent is enabled.  And it is not setting 
        agent version when an agent record is inserted.
  */
-FUNCTION int fo_GetAgentKey(PGconn *pgConn, char * agent_name, long Upload_pk, char *rev, char *agent_desc)
+FUNCTION int fo_GetAgentKey(PGconn *pgConn, const char * agent_name, long Upload_pk, const char *rev, const char *agent_desc)
 {
   int Agent_pk=-1;    /* agent identifier */
   char sql[256];
@@ -91,7 +91,7 @@ FUNCTION int fo_GetAgentKey(PGconn *pgConn, char * agent_name, long Upload_pk, c
          On sql failure, return 0, and the error will be written to stdout.
  */
 FUNCTION int fo_WriteARS(PGconn *pgConn, int ars_pk, int upload_pk, int agent_pk,
-                         char *tableName, char *ars_status, int ars_success)
+                         const char *tableName, const char *ars_status, int ars_success)
 {
   char sql[1024];
   PGresult *result;
@@ -149,7 +149,7 @@ FUNCTION int fo_WriteARS(PGconn *pgConn, int ars_pk, int upload_pk, int agent_pk
 
  \return 0 on failure
  */
-FUNCTION int fo_CreateARSTable(PGconn *pgConn, char *tableName)
+FUNCTION int fo_CreateARSTable(PGconn *pgConn, const char *tableName)
 {
   char sql[1024];
   PGresult *result;

src/lib/c/libfossagent.h
@@ -21,10 +21,10 @@
 #include <stdio.h>
 #include <libpq-fe.h>
 
-int  fo_GetAgentKey   (PGconn *pgConn, char *agent_name, long unused, char *cpunused, char *agent_desc);
+int  fo_GetAgentKey   (PGconn *pgConn, const char *agent_name, long unused, const char *cpunused, const char *agent_desc);
 int fo_WriteARS       (PGconn *pgConn, int ars_pk, int upload_pk, int agent_pk,
-                         char *tableName, char *ars_status, int ars_success);
-int fo_CreateARSTable (PGconn *pgConn, char *table_name);
+                         const char *tableName, const char *ars_status, int ars_success);
+int fo_CreateARSTable (PGconn *pgConn, const char *table_name);
 int GetUploadPerm     (PGconn *pgConn, long UploadPk, int user_pk);
 char *GetUploadtreeTableName(PGconn *pgConn, int upload_pk);
 

src/lib/c/libfossdb.c
@@ -218,7 +218,7 @@ int fo_checkPQcommand(PGconn *pgConn, PGresult *result, char *sql, char *FileID,
  @todo REMOVE hardcoded catalog name "fossology"
  @return 1 if table exists, 0 on error (which is logged) or if table does not exist.
 ****************************************************/
-int fo_tableExists(PGconn *pgConn, char *tableName)
+int fo_tableExists(PGconn *pgConn, const char *tableName)
 {
   char sql[256];
   PGresult *result;

src/lib/c/libfossdb.h
@@ -28,6 +28,6 @@
 PGconn *fo_dbconnect(char *DBConfFile, char **ErrorBuf);
 int     fo_checkPQcommand(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb); 
 int     fo_checkPQresult(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb);
-int     fo_tableExists(PGconn *pgConn, char *tableName);
+int     fo_tableExists(PGconn *pgConn, const char *tableName);
 
 #endif  /* LIBFOSSDB_H */

commit 125a2e7a24ee1abe5aabc5e692b387f856fb7a30

Fix for issue http://www.fossology.org/issues/7761, nomossa compile warnings It looks like this problem was introduced in git dea9453 (added some const specifiers)

src/nomos/agent/standalone.c
@@ -11,9 +11,9 @@ int   fo_scheduler_userID(){return(0);}
 void  fo_scheduler_set_special(int option, int value){}
 int   fo_scheduler_get_special(int option){return(0);}
 char* fo_sysconfig(const char* sectionname, const char* variablename){return(0);}
-int  fo_GetAgentKey   (PGconn *pgConn, char *agent_name, long unused, char *cpunused, char *agent_desc){return(0);}
+int  fo_GetAgentKey   (PGconn *pgConn, const char *agent_name, long unused, const char *cpunused, const char *agent_desc){return(0);}
 int fo_WriteARS(PGconn *pgConn, int ars_pk, int upload_pk, int agent_pk,
-                         char *tableName, char *ars_status, int ars_success){return(0);}
+                         const char *tableName, const char *ars_status, int ars_success){return(0);}
 PGconn *fo_dbconnect(char *DBConfFile, char **ErrorBuf){return(0);}
 int     fo_checkPQcommand(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb){return(0);}
 int     fo_checkPQresult(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb){return(0);}

src/nomos/agent/standalone.h
@@ -61,14 +61,14 @@ extern char* fo_scheduler_current();
 extern int   fo_scheduler_userID();
 extern void  fo_scheduler_set_special(int option, int value);
 extern int   fo_scheduler_get_special(int option);
-extern char* fo_sysconfig(char* sectionname, char* variablename);
+extern char* fo_sysconfig(const char* sectionname, const char* variablename);
 extern int  fo_GetAgentKey   (PGconn *pgConn,const char *agent_name, long unused, const char *cpunused, const char *agent_desc);
 extern int fo_WriteARS(PGconn *pgConn, int ars_pk, int upload_pk, int agent_pk,
                         const char *tableName, const char *ars_status, int ars_success);
 extern PGconn *fo_dbconnect(char *DBConfFile, char **ErrorBuf);
 extern int     fo_checkPQcommand(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb);
 extern int     fo_checkPQresult(PGconn *pgConn, PGresult *result, char *sql, char *FileID, int LineNumb);
-extern int     fo_tableExists(PGconn *pgConn, char *tableName);
+extern int     fo_tableExists(PGconn *pgConn, const char *tableName);
 extern int GetUploadPerm(PGconn *pgConn, long UploadPk, int user_pk);
 extern char * fo_RepMkPath (char *Type, char *Filename);
 