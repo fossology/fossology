0ba7aacc2b6b78ba4f7220bab982946ea890a8e1
Daniele Fognini <daniele.fognini@tngtech.com>
2014-10-06 11:35:31 +0200

    extract common php Agent class

--- src/decider/agent/decider.php
+++ src/decider/agent/decider.php
@@ -10,10 +10,8 @@
  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-define("ALARM_SECS", 30);
 define("AGENT_NAME", "decider");
-define("AGENT_DESC", "decider agent");
-define("AGENT_ARS", AGENT_NAME . "_ars");
+define("VERSION", "trunk");
 
 /**
  * \file decider.php
@@ -23,153 +21,27 @@ define("AGENT_ARS", AGENT_NAME . "_ars");
  * include common-cli.php directly, common.php can not include common-cli.php
  * becuase common.php is included before UI_CLI is set
  */
-require_once("$MODDIR/lib/php/common-cli.php");
-cli_Init();
 
+use Fossology\Lib\Agent\Agent;
 
-function init()
+class DeciderAgent extends Agent
 {
-  $GLOBALS['processed'] = 0;
-  $GLOBALS['alive'] = false;
-
-  $args = getopt("scheduler_start", array("userID:"));
-
-  $userId = $args['userID'];
-  $GLOBALS['userId'] = $userId;
-
-  global $container;
-
-  $dbManager = $container->get('db.manager');
-  $GLOBALS['dbManager'] = $dbManager;
-
-  $GLOBALS['agentId'] = queryAgentId(AGENT_NAME, AGENT_DESC);
-
-  initArsTable(AGENT_ARS);
-}
-
-function heartbeat_handler($signo)
-{
-  global $processed;
-  global $alive;
-  echo "HEART: ".$processed." ".($alive ? 1 : 0)."\n";
-  $alive = false;
-  pcntl_alarm(ALARM_SECS);
-}
-
-function heartbeat($newProcessed)
-{
-  global $processed;
-  global $alive;
-
-  $processed += $newProcessed;
-  $alive = true;
-  pcntl_signal_dispatch();
-}
-
-pcntl_signal(SIGALRM, "heartbeat_handler");
-pcntl_alarm(ALARM_SECS);
-
-function bail($exitvalue)
-{
-  heartbeat_handler(SIGALRM);
-  echo "BYE $exitvalue\n";
-  exit($exitvalue);
-}
-
-function greet()
-{
-  global $VERSION;
-
-  echo "VERSION: $VERSION\n";
-  echo "OK\n";
-}
-
-function createArsTable($tableName)
-{
-  global $dbManager;
-
-  $dbManager->queryOnce("CREATE TABLE ".$tableName."() INHERITS(ars_master);
-  ALTER TABLE ONLY ".$tableName." ADD CONSTRAINT ".$tableName."_agent_fk_fkc FOREIGN KEY (agent_fk) REFERENCES agent(agent_pk);
-  ALTER TABLE ONLY ".$tableName." ADD CONSTRAINT ".$tableName."_upload_fk_fkc FOREIGN KEY (upload_fk) REFERENCES upload(upload_pk) ON DELETE CASCADE");
-}
-
-function initArsTable($tableName)
-{
-  if (!DB_TableExists($tableName)) {
-    createArsTable($tableName);
-  };
-}
-
-function writeArsRecord($arsTableName,$uploadId,$arsId=0,$success=false,$status="")
-{
-  global $dbManager;
-  global $agentId;
-
-  if ($arsId) {
-    $dbManager->queryOnce("UPDATE $arsTableName SET ars_success='".($success ? "t" : "f")."', ars_endtime=now() ".(
-      !empty($status) ? ", ars_status = $status" : ""
-      )." WHERE ars_pk = $arsId");
-  } else {
-    $row = $dbManager->getSingleRow("INSERT INTO $arsTableName(agent_fk,upload_fk) VALUES ($agentId,$uploadId) RETURNING ars_pk");
-    if ($row !== false)
-    {
-      return $row['ars_pk'];
-    }
-  }
-}
-
-function queryAgentId($agentName, $agentDesc)
-{
-  global $dbManager;
-
-  $row = $dbManager->getSingleRow("SELECT agent_pk FROM agent WHERE agent_name = $1 order by agent_ts desc limit 1", array($agentName), __METHOD__."select");
-
-  if ($row === false)
+  function __construct()
   {
-    $row = $dbManager->getSingleRow("INSERT INTO agent(agent_name,agent_desc) VALUES ($1,$2) RETURNING agent_pk", array($agentName, $agentDesc), __METHOD__."insert");
-    return $row['agent_pk'];
+    parent::__construct(AGENT_NAME, VERSION);
   }
 
-  return $row['agent_pk'];
-}
-
-function processUploadId($uploadId)
-{
-  global $userId;
-  global $userName;
-
-  global $dbManager;
-
-  $count = $dbManager->getSingleRow("SELECT count(*) AS count FROM uploadtree WHERE upload_fk = $1",
-  array($uploadId));
-
-  heartbeat($count['count']);
-
-  return true;
-}
-
-greet();
-init();
-
-while (false !== ($line = fgets(STDIN)))
-{
-  if ("CLOSE\n" === $line)
+  function processUploadId($uploadId)
   {
-    bail(0);
-  }
-  if ("END\n" === $line)
-  {
-    bail(0);
-  }
+    $count = $this->dbManager->getSingleRow("SELECT count(*) AS count FROM uploadtree WHERE upload_fk = $1",
+    array($uploadId));
 
-  $uploadId = intval($line);
+    $this->heartbeat($count['count']);
 
-  if ($uploadId > 0)
-  {
-    $arsId = writeArsRecord(AGENT_ARS, $uploadId);
-    $success = processUploadId($uploadId);
-    writeArsRecord(AGENT_ARS, $uploadId, $arsId, $success);
+    return true;
   }
 }
 
-bail(0);
+$agent = new DeciderAgent();
+$agent->run_schedueler_event_loop();
+$agent->bail(0);
---
+++ src/lib/php/Agent/Agent.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Fossology\Lib\Agent;
+
+use Fossology\Lib\Util\Object;
+use Fossology\Lib\Db\DbManager;
+
+require_once(dirname(dirname(__FILE__))."/common-cli.php");
+
+define("ALARM_SECS", 30);
+
+class Agent extends Object
+{
+  private $version;
+  private $agentName;
+  private $agentDesc;
+  private $agentArs;
+  private $agentId;
+
+  private $userId;
+
+  protected $dbManager;
+
+  private $isConnected;
+
+  function __construct($agentName, $version) {
+    $this->agentName = $agentName;
+    $this->version = $version;
+    $this->agentDesc = $agentName. " agent";
+    $this->agentArs = $agentName . "_ars";
+    $this->isConnected = false;
+
+    $GLOBALS['processed'] = 0;
+    $GLOBALS['alive'] = false;
+
+    cli_Init();
+
+    global $container;
+    $this->dbManager = $container->get('db.manager');
+
+    $this->agentId = $this->queryAgentId();
+
+    $this->scheduler_connect();
+  }
+
+  function scheduler_connect()
+  {
+    $args = getopt("scheduler_start", array("userID:"));
+
+    $this->userId = $args['userID'];
+
+    $this->initArsTable();
+
+    $this->schedueler_greet();
+
+    pcntl_signal(SIGALRM, function($signo) { Agent::heartbeat_handler($signo); });
+    pcntl_alarm(ALARM_SECS);
+
+    $this->isConnected = true;
+  }
+
+  static function heartbeat_handler($signo)
+  {
+    global $processed;
+    global $alive;
+
+    echo "HEART: ".$processed." ".($alive ? 1 : 0)."\n";
+    $alive = false;
+    pcntl_alarm(ALARM_SECS);
+  }
+
+  function heartbeat($newProcessed)
+  {
+    global $processed;
+    global $alive;
+
+    $processed += $newProcessed;
+
+    $alive = true;
+    pcntl_signal_dispatch();
+  }
+
+  function bail($exitvalue)
+  {
+    Agent::heartbeat_handler(SIGALRM);
+    echo "BYE $exitvalue\n";
+    exit($exitvalue);
+  }
+
+  function schedueler_greet()
+  {
+    echo "VERSION: ".$this->version."\n";
+    echo "OK\n";
+  }
+
+  function createArsTable()
+  {
+    $tableName = $this->agentArs;
+
+    $this->dbManager->queryOnce("CREATE TABLE ".$tableName."() INHERITS(ars_master);
+    ALTER TABLE ONLY ".$tableName." ADD CONSTRAINT ".$tableName."_agent_fk_fkc FOREIGN KEY (agent_fk) REFERENCES agent(agent_pk);
+    ALTER TABLE ONLY ".$tableName." ADD CONSTRAINT ".$tableName."_upload_fk_fkc FOREIGN KEY (upload_fk) REFERENCES upload(upload_pk) ON DELETE CASCADE");
+  }
+
+  function initArsTable()
+  {
+    if (!DB_TableExists($this->agentArs)) {
+      $this->createArsTable();
+    };
+  }
+
+  function writeArsRecord($uploadId,$arsId=0,$success=false,$status="")
+  {
+
+    $arsTableName = $this->agentArs;
+    if ($arsId) {
+      $this->dbManager->queryOnce("UPDATE $arsTableName SET ars_success='".($success ? "t" : "f")."', ars_endtime=now() ".(
+        !empty($status) ? ", ars_status = $status" : ""
+        )." WHERE ars_pk = $arsId");
+    } else {
+      $row = $this->dbManager->getSingleRow("INSERT INTO $arsTableName(agent_fk,upload_fk) VALUES (".$this->agentId.",$uploadId) RETURNING ars_pk");
+      if ($row !== false)
+      {
+        return $row['ars_pk'];
+      }
+    }
+  }
+
+  function queryAgentId()
+  {
+    $row = $this->dbManager->getSingleRow("SELECT agent_pk FROM agent WHERE agent_name = $1 order by agent_ts desc limit 1", array($this->agentName), __METHOD__."select");
+
+    if ($row === false)
+    {
+      $row = $this->dbManager->getSingleRow("INSERT INTO agent(agent_name,agent_desc) VALUES ($1,$2) RETURNING agent_pk", array($this->agentName, $this->agentDesc), __METHOD__."insert");
+      return $row['agent_pk'];
+    }
+
+    return $row['agent_pk'];
+  }
+
+  function processUploadId($uploadId){
+    return false;
+  }
+
+  function run_schedueler_event_loop(){
+    if (!$this->isConnected)
+      return false;
+
+    while (false !== ($line = fgets(STDIN)))
+    {
+      if ("CLOSE\n" === $line)
+      {
+        $this->bail(0);
+      }
+      if ("END\n" === $line)
+      {
+        $this->bail(0);
+      }
+
+      $uploadId = intval($line);
+
+      if ($uploadId > 0)
+      {
+        $arsId = $this->writeArsRecord($uploadId);
+        $success = $this->processUploadId($uploadId);
+        $this->writeArsRecord($uploadId, $arsId, $success);
+        if (!$success)
+          $this->bail(1);
+      }
+    }
+  }
+}
