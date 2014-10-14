f1934a249ad1c7de21bba59ed8a6d4b4f9b5fbaf
Daniele Fognini <daniele.fognini@tngtech.com>
2014-10-14 12:02:39 +0200

    call php cli modules to get json

--- src/reportgen/agent/getCleared.php
+++ src/reportgen/agent/getCleared.php
@@ -1,9 +1,12 @@
-#!/bin/sh
+<?php
 
-cat <<EOF
+require_once("$MODDIR/lib/php/common-cli.php");
+
+cli_Init();
+
+print '
 { "licenses" : [
                  { "name": "Apache-2.0", "text" : "licText", "files" : [ "/a.txt", "/b.txt" ]},
                  { "name": "Apache-1.0", "text" : "lic3Text", "files" : [ "/c.txt" ]},
                ]
-}
-EOF
+}';
--- src/reportgen/agent/getClearedCopy.php
+++ src/reportgen/agent/getClearedCopy.php
@@ -1,9 +1,12 @@
-#!/bin/sh
+<?php
 
-cat <<EOF
+require_once("$MODDIR/lib/php/common-cli.php");
+
+cli_Init();
+
+print '
 { "statements" : [
                  { "name": "Copyright Siemens", "text" : "we wrote this stuff", "files" : [ "/a.txt", "/b.txt" ]},
                  { "name": "Copyright 2001-2014", "text" : "they wrote this other", "files" : [ "/c.txt", "d/file.c" ]},
                ]
-}
-EOF
+}';
