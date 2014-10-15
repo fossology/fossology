64cfa0f854b25e1a49bd4ae10f20d6cfb4fccba7
Daniele Fognini <daniele.fognini@tngtech.com>
2014-10-15 14:39:34 +0200

    cleanup headers

--- src/reportgen/agent/main.c
+++ src/reportgen/agent/main.c
@@ -1,22 +1,20 @@
-/* **************************************************************
-		free(rfEcc1);
-Copyright (C) 2010-2013 Hewlett-Packard Development Company, L.P.
+/*
+ Copyright (C) 2014, Siemens AG
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-version 2 as published by the Free Software Foundation.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
-************************************************************** */
-/* std library includes */
 #define _GNU_SOURCE
 #include <string.h>
 
