3c21ae742f08a769c4d797b448e6af8399468fff
Steffen Weber <steffen.weber@tngtech.com>
2014-10-17 17:09:21 +0200

    tests

--- src/lib/php/Db/test/DbManagerTest.php
+++ src/lib/php/Db/test/DbManagerTest.php
@@ -20,22 +20,22 @@ namespace Fossology\Lib\Db;
 
 use Mockery as M;
 use Mockery\MockInterface;
-use Monolog\Logger;
 
 class DbManagerTest extends \PHPUnit_Framework_TestCase
 {
-
   /** @var Driver|MockInterface */
   private $driver;
-
+  /** @var Logger|MockInterface */
+  private $logger;
   /** @var DbManager */
   private $dbManager;
 
   function setUp()
   {
     $this->driver = M::mock('Fossology\\Lib\\Db\\Driver');
-    $logger = new Logger(__FILE__);
-    $this->dbManager = new DbManager($logger);
+    $this->logger = M::mock('Monolog\\Logger'); // new Logger(__FILE__);
+    $this->logger->shouldReceive('addDebug');
+    $this->dbManager = new DbManager($this->logger);
     $this->dbManager->setDriver($this->driver);
   }
 
@@ -44,16 +44,57 @@ class DbManagerTest extends \PHPUnit_Framework_TestCase
     M::close();
   }
 
-  function test_begin_transaction()
+  function testBeginTransaction()
   {
     $this->driver->shouldReceive("begin")->withNoArgs()->once();
     $this->dbManager->begin();
   }
 
-  function test_commit_transaction()
+  function testCommitTransaction()
   {
     $this->driver->shouldReceive("commit")->withNoArgs()->once();
     $this->dbManager->commit();
   }
-}
- 
+  
+  function testInsertTableRow()
+  {
+    $tableName = 'foo';
+    $assocParams = array('cola'=>1,'colb'=>2);
+    $sqlLog = 'bar';
+    $preSql = "INSERT INTO $tableName (cola,colb) VALUES ($1,$2)";
+    $this->driver->shouldReceive('prepare')->with($sqlLog,$preSql)->once();
+    $this->driver->shouldReceive('execute')->with($sqlLog,array_values($assocParams))->once();
+    $this->driver->shouldReceive('freeResult');
+    $this->dbManager->insertTableRow($tableName,$assocParams,$sqlLog);
+  }
+  
+  function testFlushStats()
+  {
+    $this->driver->shouldReceive('prepare');
+    $sqlStmt = 'foo';
+    $this->dbManager->prepare($sqlStmt,'SELECT elephant FROM africa');
+    $this->logger->shouldReceive('addDebug')->with("/executing '$sqlStmt' took /");
+    $this->dbManager->flushStats();
+  }
+  
+  function testCreateMap()
+  {
+    $keyColumn = 'yek';
+    $valueColumn = 'lav';
+    $tableName = 'foo';
+    $sqlLog = 'bar';
+    $preSql = "select $keyColumn,$valueColumn from $tableName";
+    $this->driver->shouldReceive('prepare')->with($sqlLog,$preSql)->once();
+    $this->driver->shouldReceive('execute')->andReturn('fakeRes');
+    $this->driver->shouldReceive('fetchArray')->andReturn(
+            array($keyColumn=>'k0',$valueColumn=>'v0'),
+            array($keyColumn=>'k1',$valueColumn=>'v1'),
+            false
+          );
+    $this->driver->shouldReceive('freeResult');
+    $map = $this->dbManager->createMap($tableName,$keyColumn,$valueColumn,$sqlLog);
+    assertThat($map,hasKey('k0'));
+    assertThat($map,EqualTo(array('k0'=>'v0','k1'=>'v1')));
+  }
+  
+} 
