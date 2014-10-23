c0d30d2b9ab34f5240ee510ece87fd9f20a451e0
Steffen Weber <steffen.weber@tngtech.com>
2014-10-23 11:23:52 +0200

    solidDbManager with tests

--- src/lib/php/Db/test/SolidDbManagerTest.php
+++ src/lib/php/Db/test/SolidDbManagerTest.php
@@ -21,7 +21,7 @@ namespace Fossology\Lib\Db;
 use Mockery as M;
 use Mockery\MockInterface;
 
-class DbManagerTest extends \PHPUnit_Framework_TestCase
+class SolidDbManagerTest extends \PHPUnit_Framework_TestCase
 {
   /** @var Driver|MockInterface */
   private $driver;
@@ -35,7 +35,7 @@ class DbManagerTest extends \PHPUnit_Framework_TestCase
     $this->driver = M::mock('Fossology\\Lib\\Db\\Driver');
     $this->logger = M::mock('Monolog\\Logger'); // new Logger(__FILE__);
     $this->logger->shouldReceive('addDebug');
-    $this->dbManager = new DbManager($this->logger);
+    $this->dbManager = new SolidDbManager($this->logger);
     $this->dbManager->setDriver($this->driver);
   }
 
@@ -55,19 +55,17 @@ class DbManagerTest extends \PHPUnit_Framework_TestCase
     $this->driver->shouldReceive("commit")->withNoArgs()->once();
     $this->dbManager->commit();
   }
-  
+
   function testInsertTableRow()
   {
     $tableName = 'foo';
     $assocParams = array('cola'=>1,'colb'=>2);
     $sqlLog = 'bar';
-    $preSql = "INSERT INTO $tableName (cola,colb) VALUES ($1,$2)";
-    $this->driver->shouldReceive('prepare')->with($sqlLog,$preSql)->once();
-    $this->driver->shouldReceive('execute')->with($sqlLog,array_values($assocParams))->once();
+    $this->driver->shouldReceive('query')->once();
     $this->driver->shouldReceive('freeResult');
     $this->dbManager->insertTableRow($tableName,$assocParams,$sqlLog);
   }
-  
+
   function testFlushStats()
   {
     $this->driver->shouldReceive('prepare');
@@ -84,8 +82,7 @@ class DbManagerTest extends \PHPUnit_Framework_TestCase
     $tableName = 'foo';
     $sqlLog = 'bar';
     $preSql = "/ $keyColumn, *$valueColumn /";
-    $this->driver->shouldReceive('prepare')->with($sqlLog,$preSql)->once();
-    $this->driver->shouldReceive('execute')->andReturn('fakeRes');
+    $this->driver->shouldReceive('query')->andReturn('fakeRes');
     $this->driver->shouldReceive('fetchArray')->andReturn(
             array($keyColumn=>'k0',$valueColumn=>'v0'),
             array($keyColumn=>'k1',$valueColumn=>'v1'),
@@ -95,6 +92,47 @@ class DbManagerTest extends \PHPUnit_Framework_TestCase
     $map = $this->dbManager->createMap($tableName,$keyColumn,$valueColumn,$sqlLog);
     assertThat($map,hasKey('k0'));
     assertThat($map,EqualTo(array('k0'=>'v0','k1'=>'v1')));
+  }
+  
+  function testEvaluateStatement()
+  {
+    $this->driver->shouldReceive('booleanToDb')->with(true)->andReturn('t');
+    $this->driver->shouldReceive('booleanToDb')->with(false)->andReturn('f');
+    $this->driver->shouldReceive('query');
+    $sqlStmt = 'SELECT pet FROM africa WHERE cervical=$1 AND class=$2 AND $3';
+    $this->dbManager->prepare($stmt='statement',$sqlStmt);
+    
+    $reflection = new \ReflectionClass($this->dbManager->classname() );
+    $method = $reflection->getMethod('evaluateStatement');
+    $method->setAccessible(true);
+
+    $params = array(7,'Mammalia',true);
+    $sql = $method->invoke($this->dbManager,$stmt,$params);
+    assertThat($sql, is('SELECT pet FROM africa WHERE cervical=7 AND class=\'Mammalia\' AND t') );
+    
+    $params = array(7,'Mammalia\'; SELECT * FROM passwords WHERE user like \'%',true);
+    $sql = $method->invoke($this->dbManager,$stmt,$params);
+    assertThat($sql, is('SELECT pet FROM africa WHERE cervical=7 AND class=\'Mammalia\'\'; SELECT * FROM passwords WHERE user like \'\'%\' AND t') );
+  }  
+    
+  function testEvaluateStatement_exception()
+  {
+    $sqlStmt = 'SELECT pet FROM africa WHERE cervical=$1 AND class=$2';
+    $this->dbManager->prepare($stmt='statement',$sqlStmt);
+    
+    $reflection = new \ReflectionClass($this->dbManager->classname() );
+    $method = $reflection->getMethod('evaluateStatement');
+    $method->setAccessible(true);
+    
+    $exceptionMsg = false;
+    $params = array(7,'Mammalia','non-used parameter');
+    try {
+      $method->invoke($this->dbManager,$stmt,$params);
+    }
+    catch(\Exception $e) {
+      $exceptionMsg = $e->getMessage();
+    }
+    assertThat($exceptionMsg, is('$3 not found in prepared statement'));
   }
   
 } 
