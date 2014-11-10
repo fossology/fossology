b84fc848aca6cf5ff1522859462874b68aa5f858
Daniele Fognini <daniele.fognini@tngtech.com>
2014-11-10 19:19:12 +0100

    fix ninka tests and change insert new license

--- src/monk/agent_tests/Functional/scheduler_test.php
+++ src/monk/agent_tests/Functional/scheduler_test.php
@@ -111,7 +111,7 @@ class MonkScheduledTest extends \PHPUnit_Framework_TestCase
   private function setUpTables()
   {
     $this->testDb->createPlainTables(array('upload','uploadtree','uploadtree_a','license_ref','license_ref_bulk','clearing_event','license_file','highlight','highlight_bulk','agent','pfile','ars_master','group_user_member'),false);
-    $this->testDb->createSequences(array('agent_agent_pk_seq','pfile_pfile_pk_seq','upload_upload_pk_seq','nomos_ars_ars_pk_seq','license_file_fl_pk_seq','license_ref_bulk_lrb_pk_seq','clearing_event_clearing_event_pk_seq'),false);
+    $this->testDb->createSequences(array('agent_agent_pk_seq','pfile_pfile_pk_seq','upload_upload_pk_seq','nomos_ars_ars_pk_seq','license_file_fl_pk_seq','license_ref_rf_pk_seq','license_ref_bulk_lrb_pk_seq','clearing_event_clearing_event_pk_seq'),false);
     $this->testDb->createViews(array('license_file_ref'),false);
     $this->testDb->createConstraints(array('agent_pkey','pfile_pkey','upload_pkey_idx','FileLicense_pkey','clearing_event_pkey'),false);
     $this->testDb->alterTables(array('agent','pfile','upload','ars_master','license_ref_bulk','clearing_event','license_file','highlight'),false);
