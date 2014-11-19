edfec009296cfa14e2ed6f477e942ac73d235d7f
Steffen Weber <steffen.weber@tngtech.com>
2014-11-19 10:02:09 +0100

    names

--- src/www/ui/AboutPage.php
+++ src/www/ui/AboutPage.php
@@ -17,7 +17,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-namespace Fossology\UI;
+namespace Fossology\UI\Page;
 
 use Fossology\Lib\Dao\LicenseDao;
 use Fossology\Lib\Plugin\DefaultPlugin;
