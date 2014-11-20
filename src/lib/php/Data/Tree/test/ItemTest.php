087166cf0e1124f07b0e4d8ea7d6b9b1c8aec961
Steffen Weber <steffen.weber@tngtech.com>
2014-11-20 11:56:11 +0100

    copyright statement

--- src/lib/php/Data/Tree/test/ItemTest.php
+++ src/lib/php/Data/Tree/test/ItemTest.php
@@ -1,10 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andi
- * Date: 19.11.14
- * Time: 23:59
- */
+/*
+Copyright (C) 2014, Siemens AG
+Author: Andreas Würl
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 namespace Fossology\Lib\Data\Tree;
 
@@ -14,20 +25,13 @@ require_once(__DIR__ . '/../../../common-dir.php');
 
 class ItemTest extends \PHPUnit_Framework_TestCase
 {
-
   private $id = 234;
-
   private $parentId = 432;
-
   private $fileId = 123;
-
   private $fileMode = 21;
-
   private $fileName = "<fileName>";
-
   /** @var ItemTreeBounds|M\MockInterface */
   private $itemTreeBounds;
-
   /** @var Item */
   private $item;
 
