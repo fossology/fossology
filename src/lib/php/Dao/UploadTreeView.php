20f7ae93770155eff6254cb60839b92ab7934ae7
Steffen Weber <steffen.weber@tngtech.com>
2014-11-21 10:07:44 +0100

    php <5.4

--- src/lib/php/Dao/UploadTreeView.php
+++ src/lib/php/Dao/UploadTreeView.php
@@ -46,31 +46,23 @@ class UploadTreeView extends DbViewDao
   private static function getUploadTreeView(ItemTreeBounds $itemTreeBounds, $constraints)
   {
     $uploadTreeTableName = $itemTreeBounds->getUploadTreeTableName();
-
     $isDefaultTable = $uploadTreeTableName == 'uploadtree_a' || $uploadTreeTableName == 'uploadtree';
     if ($isDefaultTable)
     {
       $constraints[] = self::CONDITION_UPLOAD;
     }
-
     $baseQuery = "SELECT * FROM $uploadTreeTableName";
-
     $condition = self::getConstraintsCondition($itemTreeBounds, $constraints);
-
     return $baseQuery . $condition;
   }
 
   private static function getConstraintsCondition(ItemTreeBounds $itemTreeBounds, $constraints)
   {
-    $constraints = array_unique($constraints);
-
-    $mapper = function ($constraint) use ($itemTreeBounds)
-    {
-      return self::getConstraintCondition($itemTreeBounds, $constraint);
-    };
-
-    $condition = implode(" AND ", array_map($mapper, $constraints));
-
+    $conditions = array();
+    foreach(array_unique($constraints) as $constraint) {
+      $conditions[] = self::getConstraintCondition($itemTreeBounds, $constraint);
+    }
+    $condition = implode(' AND ', $conditions);
     return $condition ? ' WHERE ' . $condition : '';
   }
 
@@ -81,15 +73,12 @@ class UploadTreeView extends DbViewDao
       case self::CONDITION_UPLOAD:
         $uploadId = $itemTreeBounds->getUploadId();
         return "upload_fk = $uploadId";
-
       case self::CONDITION_RANGE:
         $left = $itemTreeBounds->getLeft();
         $right = $itemTreeBounds->getRight();
         return "lft BETWEEN $left AND $right";
-
       case self::CONDITION_PLAIN_FILES:
         return '((ufile_mode & (3<<28))=0) AND pfile_fk != 0';
-
       default:
         throw new \InvalidArgumentException("constraint $constraint is not defined");
     }
