a5cba1eba9b648e4e7d8ff6ded14e4df599a30c1
Steffen Weber <steffen.weber@tngtech.com>
2014-12-08 13:24:51 +0100

    desired by sw360 team

--- src/www/ui/ui-menus.php
+++ src/www/ui/ui-menus.php
@@ -274,6 +274,7 @@ class ui_menu extends FO_Plugin
     if(!$vars['isLoggedOut'])
     {
       $this->mergeUserLoginVars($vars);
+      $this->mergeVersionVars($vars);
     }
     
     $out = $this->renderTemplate('menu.html.twig', $vars);
@@ -298,6 +299,17 @@ class ui_menu extends FO_Plugin
     }
     $vars['logOutUrl'] = Traceback_uri() . '?mod=' . ((plugin_find_id('auth')>=0) ? 'auth' : 'smauth');
     $vars['userName'] = $_SESSION['User'];
+  }
+  
+  private function mergeVersionVars(&$vars)
+  {
+    global $SysConf;
+    $vars['versionInfo'] = array(
+        'version' => $SysConf['BUILD']['VERSION'],
+        'buildDate' => $SysConf['BUILD']['BUILD_DATE'],
+        'commitHash' => $SysConf['BUILD']['COMMIT_HASH'],
+        'commitDate' => $SysConf['BUILD']['COMMIT_DATE']
+    );
   }
 }
 
