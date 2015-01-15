4351ebc4ed6c0957da578c0b8147cbe6ca393109
Daniele Fognini <daniele.fognini@tngtech.com>
2015-01-15 17:23:34 +0100

    SOSS-131: change copyright regex to also match "(c) {%YEAR%} ..."

--- src/copyright/agent/regTypes.cc
+++ src/copyright/agent/regTypes.cc
@@ -35,7 +35,12 @@ const std::string regCopyright::getRegex() {
  return std::string(
   "("
   "("
-    "(" COPYR_SYM SPACESALL COPYR_TXT "|" COPYR_TXT ":?" SPACESALL COPYR_SYM "|" COPYR_TXT "|" COPYR_SYM_ALONE ")"
+    "("
+      COPYR_SYM "(" SPACESALL COPYR_TXT "|" SPACES "(19|20)[[:digit:]]{2,2}" ")"
+      "|" COPYR_TXT ":?" SPACESALL COPYR_SYM
+      "|" COPYR_TXT
+      "|" COPYR_SYM_ALONE
+    ")"
     "("
       SPACES
       "((and|hold|info|law|licen|message|notice|owner|state|string|tag|copy|permission|this|timestamp|@author)*)"
