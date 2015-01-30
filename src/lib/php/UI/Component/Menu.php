5810ffacb0b7f56cb942b4c0acfe77fbc92e1885
Andreas Würl <andreas.wuerl@tngtech.com>
2015-01-30 09:45:21 +0100

    fix version info in menu

--- src/lib/php/UI/Component/Menu.php
+++ src/lib/php/UI/Component/Menu.php
@@ -276,6 +276,14 @@ class Menu extends Object
     
     /* Handle login information */
     $vars['isLoggedOut'] = ((empty($_SESSION[Auth::USER_NAME])) or ($_SESSION[Auth::USER_NAME] == "Default User"));
+
+    global $SysConf;
+    $vars['versionInfo'] = array(
+        'version' => $SysConf['BUILD']['VERSION'],
+        'buildDate' => $SysConf['BUILD']['BUILD_DATE'],
+        'commitHash' => $SysConf['BUILD']['COMMIT_HASH'],
+        'commitDate' => $SysConf['BUILD']['COMMIT_DATE']
+    );
     
     if(!$vars['isLoggedOut'])
     {
