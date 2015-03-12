dfd30aa2a73235b85b04d114c41017f6326445e5
JNajjar <johannes.najjar@tngtech.com>
2015-03-12 18:28:22 +0100

    Refactored Permission globals, fixed fo_usergroup

--- src/lib/php/Dao/test/FolderDaoTest.php
+++ src/lib/php/Dao/test/FolderDaoTest.php
@@ -20,11 +20,11 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 namespace Fossology\Lib\Dao;
 
 use Fossology\Lib\Db\DbManager;
-use Fossology\Lib\Test\TestLiteDb;
+use Fossology\Lib\Test\TestPgDb;
 
 class FolderDaoTest extends \PHPUnit_Framework_TestCase
 {
-  /** @var TestLiteDb */
+  /** @var TestPgDb */
   private $testDb;
   /** @var DbManager */
   private $dbManager;
@@ -33,11 +33,11 @@ class FolderDaoTest extends \PHPUnit_Framework_TestCase
 
   public function setUp()
   {
-    $this->testDb = new TestLiteDb();
+    $this->testDb = new TestPgDb();
     $this->dbManager = $this->testDb->getDbManager();
     $this->folderDao = new FolderDao($this->dbManager);
   }
-  
+
   public function tearDown()
   {
     $this->testDb = null;
@@ -50,30 +50,60 @@ class FolderDaoTest extends \PHPUnit_Framework_TestCase
     $this->testDb->createPlainTables(array('folder'));
     $this->testDb->insertData(array('folder'));
     $htlf = $this->folderDao->hasTopLevelFolder();
-    assertThat($htlf, is(TRUE) );
+    assertThat($htlf, is(TRUE));
   }
-  
+
   public function testHasTopLevelFolder_no()
   {
     $this->testDb->createPlainTables(array('folder'));
     $htlf = $this->folderDao->hasTopLevelFolder();
-    assertThat($htlf, is(FALSE) );
-  }
-  
-  public function testInsertFolder() {
-    $this->testDb->createPlainTables(array('folder'));
-    $this->folderDao->insertFolder($folderId=3, $folderName='three', $folderDescription='floor(PI)');
-    $folderInfo = $this->dbManager->getSingleRow('SELECT folder_name,folder_desc FROM folder WHERE folder_pk=$1',
-            array($folderId), __METHOD__);
-    assertThat($folderInfo, is(array('folder_name'=>$folderName,'folder_desc'=>$folderDescription)));
+    assertThat($htlf, is(FALSE));
   }
 
-  public function testInsertFolderContents() {
-    $this->testDb->createPlainTables(array('foldercontents'));
-    $this->folderDao->insertFolderContents($parentId=7, $foldercontentsMode=2, $childId=22);
-    $contentsInfo = $this->dbManager->getSingleRow('SELECT foldercontents_mode, child_id FROM foldercontents WHERE parent_fk=$1',
-            array($parentId), __METHOD__);
-    assertThat($contentsInfo, is(equalTo(array('foldercontents_mode'=>$foldercontentsMode, 'child_id'=>$childId))));
+  public function testInsertFolder()
+  {
+    $this->testDb->createPlainTables(array('folder'));
+    $folderId = $this->folderDao->insertFolder($folderName = 'three', $folderDescription = 'floor(PI)');
+    $folderInfo = $this->dbManager->getSingleRow('SELECT folder_name,folder_desc FROM folder WHERE folder_pk=$1',
+      array($folderId), __METHOD__);
+    assertThat($folderInfo, is(array('folder_name' => $folderName, 'folder_desc' => $folderDescription)));
   }
-  
+
+  public function testInsertFolderContents()
+  {
+    $this->testDb->createPlainTables(array('foldercontents'));
+    $this->folderDao->insertFolderContents($parentId = 7, $foldercontentsMode = 2, $childId = 22);
+    $contentsInfo = $this->dbManager->getSingleRow('SELECT foldercontents_mode, child_id FROM foldercontents WHERE parent_fk=$1',
+      array($parentId), __METHOD__);
+    assertThat($contentsInfo, is(equalTo(array('foldercontents_mode' => $foldercontentsMode, 'child_id' => $childId))));
+  }
+
+
+  public function testGetFolderPK()
+  {
+    $this->testDb->createPlainTables(array('folder'));
+    $folderId = $this->folderDao->insertFolder($folderName = 'three', $folderDescription = 'floor(PI)');
+
+    assertThat($this->folderDao->getFolderId('three'), is($folderId));
+  }
+
+  public function testGetFolderPK_Null()
+  {
+    $this->testDb->createPlainTables(array('folder'));
+
+    assertThat($this->folderDao->getFolderId('three'), is(null));
+  }
+
+  /**
+   * @expectedException \Exception
+   */
+  public function testGetFolderPKAmbiguousName()
+  {
+    $this->testDb->createPlainTables(array('folder'));
+    $folderId = $this->folderDao->insertFolder($folderName = 'three', $folderDescription = 'floor(PI)');
+    $folderId2 =$this->folderDao->insertFolder($folderName = 'three', $folderDescription = 'floor(PI)+Epsilon',2);
+
+    $this->folderDao->getFolderId('three');
+  }
+
 }
