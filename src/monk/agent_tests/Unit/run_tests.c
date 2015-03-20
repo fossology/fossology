461c2541b5d922a9a9c3f80f37dc0922430b8256
Steffen Weber <steffen.weber@tngtech.com>
2015-03-20 15:47:07 +0100

    fix duplicated test suite name

--- src/monk/agent_tests/Unit/run_tests.c
+++ src/monk/agent_tests/Unit/run_tests.c
@@ -1,12 +1,19 @@
 /*
 Author: Daniele Fognini, Andreas Wuerl
-Copyright (C) 2013-2014, Siemens AG
+Copyright (C) 2013-2015, Siemens AG
 
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2 as published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 /* local includes */
@@ -47,7 +54,7 @@ CU_SuiteInfo suites[] = {
     {"Testing diff:", NULL, NULL, diff_testcases},
     {"Testing match:", NULL, NULL, match_testcases},
     {"Testing database:", NULL, NULL, database_testcases},
-    {"Testing database:", NULL, NULL, encoding_testcases},
+    {"Testing encoding:", NULL, NULL, encoding_testcases},
     CU_SUITE_INFO_NULL
 };
 
