48c6800173396c2edab7ff886ccce6eb93a0a659
Vincent Ma <dong.ma@hp.com>
2015-03-30 19:03:19 +0800

    Debug UnitTest issue.

--- src/testing/db/createTestDB.php
+++ src/testing/db/createTestDB.php
@@ -51,7 +51,7 @@ $usage = $argv[0] . ": [-h] -c path [-d name] [-s]\n" .
   "  Create test DB, start scheduler: 'createTestDb.php -s'\n" .
   "  Create empty DB, sysconf and repo: 'createTestDb.php -e'\n";
 
-$pathPrefix = '/srv/fossology';
+$pathPrefix = '/home/travis/build/larainema/';
 $dbPrefix = 'fosstest';
 
 // check if the user is in the fossy group
