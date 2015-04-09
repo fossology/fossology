267c559e87dd8069867f098ca03d895774b52409
Steffen Weber <steffen.weber@tngtech.com>
2015-04-09 18:01:05 +0200

    fix #415 fossinit detects if updating or fresh set-up

--- install/fossinit.php
+++ install/fossinit.php
@@ -46,7 +46,7 @@ function explainUsage()
  * @return 0 for success, 1 for failure.
  **/
 
-use Fossology\Lib\Db\DbManager;
+use Fossology\Lib\Db\Driver\Postgres;
 
 $groupInfo = posix_getgrnam("fossy");
 posix_setgid($groupInfo['gid']);
@@ -119,7 +119,9 @@ require_once("$LIBEXECDIR/dbmigrate_2.0-2.5-pre.php");
 Migrate_20_25($Verbose);
 */
 
-if (empty($SchemaFilePath)) $SchemaFilePath = "$MODDIR/www/ui/core-schema.dat";
+if (empty($SchemaFilePath)) {
+  $SchemaFilePath = "$MODDIR/www/ui/core-schema.dat";
+}
 
 if (!file_exists($SchemaFilePath))
 {
@@ -128,6 +130,10 @@ if (!file_exists($SchemaFilePath))
 }
 
 require_once("$MODDIR/lib/php/libschema.php");
+$libschema->setDriver(new Postgres($PG_CONN));
+$previousSchema = $libschema->getCurrSchema();
+$isUpdating = array_key_exists('TABLE', $previousSchema) && array_key_exists('users', $previousSchema['TABLE']);
+
 $migrateColumns = array('clearing_decision'=>array('reportinfo','clearing_pk','type_fk','comment'));
 $FailMsg = $libschema->applySchema($SchemaFilePath, $Verbose, $DatabaseName, $migrateColumns);
 if ($FailMsg)
@@ -197,7 +203,6 @@ if (array_key_exists('r', $Options))
 }
 
 /* migration */
-global $libschema;
 $currSchema = $libschema->getCurrSchema();
 $sysconfig = $dbManager->createMap('sysconfig','variablename','conf_value');
 global $LIBEXECDIR;
@@ -212,7 +217,7 @@ if(!array_key_exists('Release', $sysconfig)){
     require_once("$LIBEXECDIR/dbmigrate_2.5-2.6.php");
     migrate_25_26($Verbose);
   }
-  if(!array_key_exists('clearing_pk', $currSchema['TABLE']['clearing_decision']))
+  if(!array_key_exists('clearing_pk', $currSchema['TABLE']['clearing_decision']) && $isUpdating)
   {
     $timeoutSec = 20;
     echo "Missing column clearing_decision.clearing_pk, you should update to version 2.6.2 before migration\n";
