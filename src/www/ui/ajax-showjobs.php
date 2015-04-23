17adc35350f6d4ccc3d2353cdb4438adb6c298de
Shaheem Azmal <shaheem.azmal@siemens.com>
2015-04-23 14:02:30 +0530

    Added emails for project copyrights

--- src/www/ui/ajax-showjobs.php
+++ src/www/ui/ajax-showjobs.php
@@ -1,7 +1,8 @@
 <?php
 /*
  Copyright (C) 2015, Siemens AG
- Author: Shaheem azmal, Anupam Ghosh
+ Author: Shaheem Azmal<shaheem.azmal@siemens.com>, 
+         Anupam Ghosh <anupam.ghosh@siemens.com>
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
