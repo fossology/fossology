d817921e4e1dd01f32fe57bbcbddcbc4cc1b29fc
Florian Krügel <florian.kruegel@tngtech.com>
2015-05-27 16:38:44 +0200

    Upload from URL now works like Upload from File

---
+++ src/www/ui/page/UploadPageBase.php
@@ -0,0 +1,145 @@
+<?php
+/***********************************************************
+ * Copyright (C) 2015 Siemens AG
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+         
+namespace Fossology\UI\Page;
+
+use Fossology\Lib\Plugin\DefaultPlugin;
+use Symfony\Component\HttpFoundation\Request;
+use Fossology\Lib\Auth\Auth;
+use Fossology\Lib\Dao\FolderDao;
+use Fossology\Lib\Dao\UploadDao;
+use Fossology\Lib\UI\MenuHook;
+use Monolog\Logger;
+
+abstract class UploadPageBase extends DefaultPlugin
+{
+  const NAME = "upload_file";
+  const FOLDER_PARAMETER_NAME = 'folder';
+
+  const DESCRIPTION_INPUT_NAME = 'descriptionInputName';
+  const DESCRIPTION_VALUE = 'descriptionValue';
+  const UPLOAD_FORM_BUILD_PARAMETER_NAME = 'uploadformbuild';
+  const PUBLIC_ALL = 'public';
+  const PUBLIC_GROUPS = 'protected';
+
+  /** @var FolderDao */
+  private $folderDao;
+  /** @var UploadDao */
+  private $uploadDao;
+  /** @var Logger */
+  private $logger;
+  
+  public function __construct($name, $parameters = array())
+  {
+    parent::__construct($name, $parameters);
+
+    $this->folderDao = $this->getObject('dao.folder');
+    $this->uploadDao = $this->getObject('dao.upload');
+    $this->logger = $this->getObject('logger');
+  }
+  abstract protected function handleUpload(Request $request);
+  abstract protected function handleView(Request $request, $vars);
+  
+  protected function handle(Request $request)
+  {
+    // Handle request
+    $this->folderDao->ensureTopLevelFolder();
+    
+    $message = "";
+    $description = "";
+    if ($request->isMethod(Request::METHOD_POST))
+    {
+      list($success, $message, $description) = $this->handleUpload($request);
+    }
+    $vars['message'] = $message;
+    $vars['descriptionInputValue'] = $description ?: "";
+    $vars['descriptionInputName'] = self::DESCRIPTION_INPUT_NAME;
+    $vars['folderParameterName'] = self::FOLDER_PARAMETER_NAME;
+    $vars['upload_max_filesize'] = ini_get('upload_max_filesize');
+    $vars['agentCheckBoxMake'] = '';
+
+    $rootFolder = $this->folderDao->getRootFolder(Auth::getUserId());
+    $folderStructure = $this->folderDao->getFolderStructure($rootFolder->getId());
+
+    $vars['folderStructure'] = $folderStructure;
+    $vars['baseUrl'] = $request->getBaseUrl();
+    $vars['moduleName'] = $this->getName();
+    $vars[self::FOLDER_PARAMETER_NAME] = $request->get(self::FOLDER_PARAMETER_NAME);
+    
+    $parmAgentList = MenuHook::getAgentPluginNames("ParmAgents");
+    $vars['parmAgentContents'] = array();
+    $vars['parmAgentFoots'] = array();
+    foreach($parmAgentList as $parmAgent) {
+      $agent = plugin_find($parmAgent);
+      $vars['parmAgentContents'][] = $agent->renderContent($vars);
+      $vars['parmAgentFoots'][] = $agent->renderFoot($vars);
+    }
+    
+    $session = $request->getSession();
+    $session->set(self::UPLOAD_FORM_BUILD_PARAMETER_NAME, time().':'.$_SERVER['REMOTE_ADDR']);
+    $vars['uploadFormBuild'] = $session->get(self::UPLOAD_FORM_BUILD_PARAMETER_NAME);
+    $vars['uploadFormBuildParameterName'] = self::UPLOAD_FORM_BUILD_PARAMETER_NAME;
+
+    if (@$_SESSION[Auth::USER_LEVEL] >= PLUGIN_DB_WRITE)
+    {
+      $skip = array("agent_unpack", "agent_adj2nest", "wget_agent");
+      $vars['agentCheckBoxMake'] = AgentCheckBoxMake(-1, $skip);
+    }
+    return $this->handleView($request, $vars);
+  }
+  
+  protected function postUploadAddJobs(Request $request, $fileName, $uploadId, $jobId = null, $wgetDependency = false)
+  {
+    $userId = Auth::getUserId();
+    $groupId = Auth::getGroupId();
+
+    if ($jobId === null) {
+      $jobId = JobAddJob($userId, $groupId, $fileName, $uploadId);
+    }
+    $dummy = "";
+    if ($wgetDependency)
+    {
+      $unpackplugin = \plugin_find("agent_unpack");
+      $unpackplugin->AgentAdd($jobId, $uploadId, $dummy, array("wget_agent"));
+    }
+    $adj2nestplugin = \plugin_find('agent_adj2nest');
+    $adj2nestplugin->AgentAdd($jobId, $uploadId, $dummy);
+
+    $checkedAgents = checkedAgents();
+    AgentSchedule($jobId, $uploadId, $checkedAgents);
+
+    $errorMsg = '';
+    $parmAgentList = MenuHook::getAgentPluginNames("ParmAgents");
+    $plainAgentList = MenuHook::getAgentPluginNames("Agents");
+    $agentList = array_merge($plainAgentList, $parmAgentList);
+    foreach($parmAgentList as $parmAgent) {
+      $agent = plugin_find($parmAgent);
+      $agent->scheduleAgent($jobId, $uploadId, $errorMsg, $request, $agentList);
+    }
+    
+    $status = GetRunnableJobList();
+    $message = empty($status) ? _("Is the scheduler running? ") : "";
+    $jobUrl = Traceback_uri() . "?mod=showjobs&upload=$uploadId";
+    $message .= _("The file") . " " . $fileName . " " . _("has been uploaded. It is") . ' <a href=' . $jobUrl . '>upload #' . $uploadId . "</a>.\n";
+    if ($request->get('public')==self::PUBLIC_GROUPS)
+    {
+      $this->uploadDao->makeAccessibleToAllGroupsOf($uploadId, $userId);
+    }
+    return $message;
+  }
+}
---
+++ src/www/ui/page/UploadUrlPage.php
@@ -0,0 +1,125 @@
+<?php
+/***********************************************************
+ * Copyright (C) 2015 Siemens AG
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+
+namespace Fossology\UI\Page;
+
+use Symfony\Component\HttpFoundation\Request;
+use Fossology\Lib\Auth\Auth;
+
+class UploadUrlPage extends UploadPageBase
+{
+  const NAME = "upload_url22";
+  
+  const NAME_PARAM = 'name';
+  const ACCEPT_PARAM = 'accept';
+  const REJECT_PARAM = 'reject';
+  const GETURL_PARAM = 'geturl';
+  const LEVEL_PARAM = 'level';
+  
+  public function __construct()
+  {
+    parent::__construct(self::NAME, array(
+        self::TITLE => _("Upload from an URL"),
+        self::MENU_LIST => "Upload::From URL",
+        self::DEPENDENCIES => array("agent_unpack", "showjobs"),
+        self::PERMISSION => Auth::PERM_WRITE
+    ));
+  }
+  
+  protected function handleUpload(Request $request)
+  {
+    $folderId = intval($request->get(self::FOLDER_PARAMETER_NAME));
+    $description = stripslashes($request->get(self::DESCRIPTION_INPUT_NAME));
+    
+    $getUrlThatMightIncludeSpaces = trim($request->get(self::GETURL_PARAM));
+    $getURL = str_replace(" ", "%20", $getUrlThatMightIncludeSpaces);
+
+    if (empty($getURL)) 
+    {
+      return array(false, _("Invalid URL"), $description);
+    }
+    if (preg_match("@^((http)|(https)|(ftp))://([[:alnum:]]+)@i", $getURL) != 1) 
+    {
+      return array(false, _("Invalid URL"), $description);
+    }
+
+    $name = $request->get(self::NAME_PARAM);
+    if (empty($name)) {
+      $name = basename($getURL);
+    }
+    $shortName = basename($name);
+    if (empty($shortName)) {
+      $shortName = $name;
+    }
+
+    /* Create an upload record. */
+    $mode = (1 << 2); // code for "it came from wget"
+    $userId = Auth::getUserId();
+    $groupId = Auth::getGroupId();
+    $public = $request->get('public');
+    $publicPermission = ($public == self::PUBLIC_ALL) ? Auth::PERM_READ : Auth::PERM_NONE;
+
+    $uploadId = JobAddUpload($userId, $groupId, $shortName, $getURL, $description, $mode, $folderId, $publicPermission);
+    if (empty($uploadId)) {
+      $text = _("Failed to insert upload record");
+      return array(false, $text, $description);
+    }
+
+    $level = $request->get(self::LEVEL_PARAM);
+    if (empty($level) && !is_numeric($level) || $level < 0)
+    {
+      $level = 1;
+    }
+
+    /* first trim, then get rid of whitespaces before and after each comma letter */
+    $accept = preg_replace('/\s*,\s*/', ',', trim($request->get(self::ACCEPT_PARAM)));
+    $reject = preg_replace('/\s*,\s*/', ',', trim($request->get(self::REJECT_PARAM)));
+    
+    /* Create the job: job "wget" */
+    $jobId = JobAddJob($userId, $groupId, "wget", $uploadId);
+    if (empty($jobId) || ($jobId < 0)) {
+      return array(false, _("Failed to insert job record"), $description);
+    }
+
+    $jqArgs = "$uploadId - $getURL -l $level ";
+    if (!empty($accept)) {
+      $jqArgs .= "-A $accept ";
+    }
+    $jqArgs .= empty($reject) ? "-R index.html* " : "-R $reject,index.html* ";
+
+    $jobqueueId = JobQueueAdd($jobId, "wget_agent", $jqArgs, NULL, NULL);
+    if (empty($jobqueueId)) {
+      return array(false, "Failed to insert task 'wget_agent' into job queue", $description);
+    }
+
+    $message = $this->postUploadAddJobs($request, $shortName, $uploadId, $jobId, true);
+    return array(true, $message, $description);
+  }
+  
+  protected function handleView(Request $request, $vars)
+  {
+    $vars['geturlField'] = self::GETURL_PARAM;
+    $vars['nameField'] = self::NAME_PARAM;
+    $vars['acceptField'] = self::ACCEPT_PARAM;
+    $vars['rejectField'] = self::REJECT_PARAM;
+    $vars['levelField'] = self::LEVEL_PARAM;
+    return $this->render("upload_url.html.twig", $this->mergeWithDefault($vars));
+  }
+}
+
+register_plugin(new UploadUrlPage());
