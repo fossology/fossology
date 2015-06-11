3444b0e81f1266b1176037561bd4add71b90086d
Steffen Weber <steffen.weber@tngtech.com>
2015-06-11 12:15:35 +0200

    first throw of folder link from cli

---
+++ src/cli/fo_folder.php
@@ -0,0 +1,82 @@
+<?php
+/***********************************************************
+ Copyright (C) 2015 Siemens AG
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************/
+
+use Fossology\Lib\Auth\Auth;
+use Fossology\Lib\Dao\FolderDao;
+use Fossology\Lib\Data\Folder\Folder;
+
+require_once("$MODDIR/lib/php/common-cli.php");
+cli_Init();
+
+global $Plugins;
+error_reporting(E_NOTICE & E_STRICT);
+
+$Usage = "Usage: " . basename($argv[0]) . " [options]
+  --username  = user name
+  --password  = password
+  --groupname = a group the user belongs to (default active group)
+  --folderId  = id of folder (default root folder of user)
+  --linkFolder= create a link to this folder\n";
+
+$opts = getopt("hc:", array("username:", "groupname:", "folderId:", "password:", "linkFolder:"));
+if(array_key_exists('h', $opts))
+{
+  print $Usage;
+  exit(0);
+}
+
+$user = array_key_exists("username", $opts) ? $opts["username"] : '';
+$group = array_key_exists("groupname", $opts) ? $opts["groupname"] : '';
+$passwd = array_key_exists("password", $opts) ? $opts["password"] : null;
+account_check($user, $passwd, $group);
+
+global $SysConf;
+$userId = $_SESSION[Auth::USER_ID] = $SysConf['auth'][Auth::USER_ID];
+$groupId = $_SESSION[Auth::GROUP_ID] = $SysConf['auth'][Auth::GROUP_ID];
+
+
+/* @var $folderDao FolderDao */
+$folderDao = $GLOBALS['container']->get("dao.folder");
+
+if (!array_key_exists("folderId", $opts)) {
+  $folderId = $opts["folderId"];
+}
+else
+{
+  $folderId = $folderDao->getRootFolder($userId)->getId();
+}
+
+$childId = array_key_exists("linkFolder", $opts) ? $opts["linkFolder"] : null;
+if(!empty($childId))
+{
+  $folderDao->insertFolderContents($folderId,FolderDao::MODE_FOLDER,$childId);
+}
+else
+{
+  $structure = $folderDao->getFolderStructure($folderId);
+  foreach($structure as $folder)
+  {
+    for ($i = 0; $i < $folder[FolderDao::DEPTH_KEY]; $i++) {
+      echo '-';
+    }
+    /* @var $theFolder Folder */
+    $theFolder = $folder[FolderDao::FOLDER_KEY];
+    echo $theFolder->getName().' (id='.$theFolder->getId().")\n";
+  }
+  
+}
