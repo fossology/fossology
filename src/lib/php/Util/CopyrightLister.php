e973c5e1e8489ac1605c8be4309c0971dcea4e7e
Steffen Weber <steffen.weber@tngtech.com>
2015-06-24 14:38:19 +0200

    clean up CopyrightLister | removed cp2foss dependency in fo_copyright_list tests

--- src/lib/php/Util/CopyrightLister.php
+++ src/lib/php/Util/CopyrightLister.php
@@ -1,7 +1,7 @@
 <?php
 /***********************************************************
  Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
- Copyright (C) 2014 Siemens AG
+ Copyright (C) 2014-2015 Siemens AG
 
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
@@ -25,12 +25,22 @@
 namespace Fossology\Lib\Util;
 
 use Fossology\Lib\Dao\AgentDao;
+use Fossology\Lib\Dao\CopyrightDao;
+use Fossology\Lib\Dao\TreeDao;
+use Fossology\Lib\Dao\UploadDao;
 use Fossology\Lib\Db\DbManager;
 
 class CopyrightLister extends Object
 {
   /** @var DbManager */
   private $dbManager;
+  /** @var UploadDao */
+  private $uploadDao;
+  /** @var TreeDao */
+  private $treeDao;
+  /** @var CopyrightDao */
+  private $copyrightDao;
+  
   private $includeContainer = FALSE;
   private $excludingCopyright = -1;
   private $includingCopyright = -1;
@@ -40,8 +50,10 @@ class CopyrightLister extends Object
   
   function __construct(){
     global $container;
-    /** @var DbManager $dbManager */
     $this->dbManager = $container->get('db.manager');
+    $this->uploadDao = $container->get('dao.upload');
+    $this->treeDao = $container->get('dao.tree');
+    $this->copyrightDao = $container->get('dao.copyright');
   }
   
   public function setContainerInclusion($includeContainer)
@@ -65,41 +77,37 @@ class CopyrightLister extends Object
   }
 
   /**
-   * @param $uploadtree_pk - uploadtree id
-   * @param $upload_pk - upload id
+   * @param $itemId - uploadtree id
+   * @param $uploadId - upload id
    */
-  public function getCopyrightList($uploadtree_pk, $upload_pk)
+  public function getCopyrightList($itemId, $uploadId)
   {
-    if (empty($uploadtree_pk)) {
-      $uploadtree_pk = $this->getUploadtreeIdFromUploadId($upload_pk);
+    if (empty($itemId)) {
+      $itemId = $this->uploadDao->getUploadParent($uploadId);
     }    
-    if ($this->selectAgentId($upload_pk) === FALSE)
+    if (!$this->selectAgentId($uploadId))
     {
+      echo 'no valid copyright agent found';
       return;
     }
-    $sqlBounds = "SELECT upload_fk, lft, rgt from uploadtree where uploadtree_pk=$1";
-    $toprow = $this->dbManager->getSingleRow($sqlBounds, array($uploadtree_pk), __METHOD__.'.getBounds');
-    $uploadtree_tablename = GetUploadtreeTableName($toprow['upload_fk']);
+    $uploadtree_tablename = $this->uploadDao->getUploadtreeTableName($uploadId);
+    $toprow = $this->uploadDao->getItemTreeBounds($itemId,$uploadtree_tablename);
+
+    $extraWhere = 'agent_fk='.$this->agentId.' AND lft>'.$toprow->getLeft().' AND rgt<'.$toprow->getRight();
+    $allCopyrightEntries = $this->copyrightDao->getAllEntries('copyright', $uploadId, $uploadtree_tablename,
+            empty($this->type)||$this->type=='all' ? null : $this->type, false, null, $extraWhere);
+    
     $modeMask = empty($this->includeContainer) ? (3<<28) : (1<<28);
-    $sql = "select uploadtree_pk, ufile_name, lft, rgt from $uploadtree_tablename 
-              where upload_fk=$1 and lft>$2 and rgt<$3 and (ufile_mode & $4) = 0
-              order by uploadtree_pk";
+    $sql = "SELECT uploadtree_pk, ufile_name, lft, rgt FROM $uploadtree_tablename 
+              WHERE upload_fk=$1 AND lft>$2 AND rgt<$3 AND (ufile_mode & $4) = 0
+              ORDER BY uploadtree_pk";
     $this->dbManager->prepare($outerStmt=__METHOD__.'.loopThroughAllRecordsInTree',$sql);
-    $outerresult = $this->dbManager->execute($outerStmt,array($toprow['upload_fk'],$toprow['lft'],$toprow['rgt'],$modeMask));
+    $outerresult = $this->dbManager->execute($outerStmt,array($toprow->getUploadId(),$toprow->getLeft(),$toprow->getRight(),$modeMask));
     while ($row = $this->dbManager->fetchArray($outerresult))
     { 
-      $this->printRow($row,$uploadtree_tablename);
+      $this->printRow($row,$uploadtree_tablename, $allCopyrightEntries); //$this->uploadDao->getParentItemBounds($uploadId)->getItemId());
     } 
     $this->dbManager->freeResult($outerresult);
-  }
-  
-    
-  private function getUploadtreeIdFromUploadId($upload_pk){
-    $uploadtreeRec = $this->dbManager->getSingleRow('SELECT uploadtree_pk FROM uploadtree WHERE parent IS NULL AND upload_fk=$1',
-            array($upload_pk),
-            $sqlLog=__METHOD__);
-    $uploadtree_pk = $uploadtreeRec['uploadtree_pk'];
-    return $uploadtree_pk;
   }
   
   /**
@@ -109,9 +117,9 @@ class CopyrightLister extends Object
   private function selectAgentId($uploadId)
   {
     global $container;
-    /** @var AgentDao $agentDao */
+    /* @var $agentDao AgentDao */
     $agentDao = $container->get('dao.agent');
-    $agentRec = $agentDao->agentARSList($tableName="copyright_ars", $uploadId, $limit=1);
+    $agentRec = $agentDao->agentARSList($tableName="copyright_ars", $uploadId, 1);
 
     if ($agentRec === false)
     {
@@ -119,23 +127,25 @@ class CopyrightLister extends Object
       return false;
     }
     $this->agentId = $agentRec[0]["agent_fk"];
-    return TRUE;
+    return true;
   }
   
   /**
    *  @brief write out text in format 'filepath: copyright list'
    */  
-  private function printRow($row,$uploadtree_tablename)
+  private function printRow($row,$uploadtree_tablename, &$allCopyrightEntries, $parentId=0)
   {
-    $filepatharray = Dir2Path($row['uploadtree_pk'], $uploadtree_tablename);
-    $filepath = "";
-    foreach($filepatharray as $uploadtreeRow)
-    {
-      if (!empty($filepath)) $filepath .= "/";
-      $filepath .= $uploadtreeRow['ufile_name'];
-    }
+    $filepath = $this->treeDao->getFullPath($row['uploadtree_pk'], $uploadtree_tablename, $parentId);
 
-    $copyright = $this->getFileCopyright_string($row['uploadtree_pk']) ;
+    $copyrightArray = array();
+    foreach($allCopyrightEntries as $entry)
+    {
+      if ($entry['uploadtree_pk'] == $row['uploadtree_pk']) {
+        $copyrightArray[] = $entry['content'];
+      }
+    }
+    $copyright = implode(', ', $copyrightArray);
+    
     /** include and exclude together */
     if (-1 != $this->includingCopyright && -1 != $this->excludingCopyright && !empty($this->includingCopyright) && 
         !empty($this->excludingCopyright))
@@ -147,93 +157,20 @@ class CopyrightLister extends Object
     }
     else if (
             /** no value set for -x and -X, show all files */
-            (-1 == $this->includingCopyright && -1 == $this->excludingCopyright) ||
+          ! (-1 == $this->includingCopyright && -1 == $this->excludingCopyright) &&
             /** both value from -x and -X are empty, unmeaningful, show all files */
-            (empty($this->includingCopyright) && empty($this->excludingCopyright)) ||
+          ! (empty($this->includingCopyright) && empty($this->excludingCopyright)) &&
             /** just show files without copyright no matter if excluding_copyright */
-            (empty($this->includingCopyright) && empty($copyright)) ||
+          ! (empty($this->includingCopyright) && empty($copyright)) &&
             /** just show files with copyright */
-            (empty($this->excludingCopyright) && !empty($copyright)) ||
+          ! (empty($this->excludingCopyright) && !empty($copyright)) &&
             /** include  */
-            (-1 != $this->includingCopyright && !empty($this->includingCopyright) && !empty($copyright) && stristr($copyright, $this->includingCopyright)) ||
+          ! (-1 != $this->includingCopyright && !empty($this->includingCopyright) && !empty($copyright) && stristr($copyright, $this->includingCopyright)) &&
             /** exclude */
-            (-1 != $this->excludingCopyright && !empty($this->excludingCopyright) && !empty($copyright) && !stristr($copyright, $this->excludingCopyright)))
-      ;
-    else
+          ! (-1 != $this->excludingCopyright && !empty($this->excludingCopyright) && !empty($copyright) && !stristr($copyright, $this->excludingCopyright)))
     {
       return;
     }
     print ("$filepath: $copyright\n");
-}
-  
-
-  /**
-   * @brief get all the copyright for a single file or uploadtree
-   * @param $pfile_pk - pfile id, (if empty, $uploadtree_pk must be given)
-   * @param $uploadtree_pk - (used only if $pfile_pk is empty)
-   * @return Array of file copyright CopyrightArray[ct_pk] = copyright.content
-   */
-  private function getFileCopyrights($pfile_pk, $uploadtree_pk)
-  {
-    $agent_pk = $this->agentId;
-    // if $pfile_pk, then return the copyright for that one file
-    if ($pfile_pk)
-    {
-      $sql = "SELECT ct_pk, content FROM copyright WHERE pfile_fk=$1 AND agent_fk=$2";
-      $this->dbManager->prepare($stmt=__METHOD__.'.pfileGiven',$sql);
-      $result = $this->dbManager->execute($stmt,array($pfile_pk,$agent_pk));
-    }
-    else if ($uploadtree_pk)
-    {
-      $sqlUt = "SELECT lft, rgt, upload_fk FROM uploadtree WHERE uploadtree_pk = $1";
-      $row = $this->dbManager->getSingleRow($sqlUt, array($uploadtree_pk), __METHOD__.'.findLftAndRgtBounds');
-      $lft = $row["lft"];
-      $rgt = $row["rgt"];
-      $upload_pk = $row["upload_fk"];
-
-      $sql = "SELECT ct_pk, content from copyright,
-                  (SELECT distinct(pfile_fk) as PF from uploadtree
-                     where upload_fk=$1 and uploadtree.lft BETWEEN $2 and $3) as SS
-              where PF=pfile_fk and agent_fk=$4";
-      $param = array($upload_pk,$lft,$rgt,$agent_pk);
-      $type = $this->type;
-      if($type && "all" != $type)
-      {
-        $param[] = $type;
-        $sql .= " AND type=$".count($param);
-      }
-      $this->dbManager->prepare($stmt=__METHOD__.'.uploadtreeGiven',$sql);
-      $result = $this->dbManager->execute($stmt,$param);
-    }
-    else
-    {
-      throw new Exception("Missing function inputs in " . __FILE__ . ':' . __LINE__);
-    }
-
-    $copyrightArray = array();
-    while ($row = $this->dbManager->fetchArray($result))
-    {
-      $copyrightArray[$row['ct_pk']] = $row["content"];
-    }
-    $this->dbManager->freeResult($result);
-    return $copyrightArray;
-  }
-
-  /**
-   * @param int $uploadtree_pk - (used only if $pfile_pk is empty)
-   * @param int $pfile_pk - pfile id, (if empty, $uploadtree_pk must be given)
-   * @return string copyright list as a single string
-   */
-  private function getFileCopyright_string($uploadtree_pk, $pfile_pk=0)
-  {
-    $copyrightStr = '';
-    $copyrightArray = $this->getFileCopyrights($pfile_pk, $uploadtree_pk);
-    $glue = '';
-    foreach($copyrightArray as $ct)
-    {
-      $copyrightStr .= $glue.$ct;
-      $glue = ', ';
-    }
-    return $copyrightStr;
   }
 }
