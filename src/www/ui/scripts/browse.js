4afc69cd0e39341e83a4d34e8727ba407c62b1e3
Steffen Weber <steffen.weber@tngtech.com>
2015-06-29 14:19:34 +0200

    fix #469

--- src/www/ui/scripts/browse.js
+++ src/www/ui/scripts/browse.js
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2014, Siemens AG
+ Copyright (C) 2014-2015, Siemens AG
  Author: Steffen Weber, Johannes Najjar
 
  This program is free software; you can redistribute it and/or
@@ -41,6 +41,7 @@ $(document).ready(function () {
       } );
   });
   commentModal = $('#commentModal').plainModal();
+  $(document).tooltip();
 });
 
 function initPrioClick() {
@@ -68,9 +69,16 @@ function initPrioClick() {
 }
 function initPrioDraw() {
   $("td.priobucket").each(function () {
+    $('.ui-tooltip').remove();
     $(this).html(function () {
       return prioColumn(table.fnGetData(this), 'display');
     });
+  });
+  
+  $('.limit-mover').click(function(){
+    var uploadId = $(this).attr('data-source');
+    var dir = $(this).attr('data-dir');
+    move2limit(uploadId,dir);
   });
 
 }
@@ -87,34 +95,6 @@ function move2limit(uploadId, direction) {
     data: post_data,
     success: mysuccess4
   });
-}
-
-function prioColumn(source, type, val) {
-  if (type === 'set') {
-    source[1] = val;
-    // Store the computed display and filter values for efficiency?
-    return;
-  }
-  if (type === 'display') {
-    if (myKey === 0) {
-      return '<img alt="[to top]" src="images/icons/blue_arrow_up_32.png" class="icon-small" onclick="move2limit(' + source[0] + ',\'top\')"/>'
-              + ' <img alt="[select]" src="images/icons/arrow_updown_32.png" class="icon-small"/>'
-              + ' <img alt="[to bottom]" src="images/icons/blue_arrow_down_32.png" class="icon-small" onclick="move2limit(' + source[0] + ',\'-1\')"/>';
-    }
-    else if (myVal < source[1]) {
-      return '<img alt="[move here]" src="images/icons/arrow_up_32.png" class="icon-small"/>';
-    }
-    else if (myVal > source[1]) {
-      return '<img alt="[move here]" src="images/icons/arrow_down_32.png" class="icon-small"/>';
-    }
-    else
-      return 'cancel <img alt="[x]" src="images/icons/close_32.png" class="icon-small"/>';
-  }
-  if (type === 'sort') {
-    return -source[1];
-  }
-  // 'filter', 'sort', 'type' and undefined all just use the integer
-  return source[1];
 }
 
 function openCommentModal(upload, status, comment) {
@@ -205,88 +185,4 @@ function submitComment( ) {
     data: post_data,
     success: mysuccess3
   });
-}
-
-function ShowHide(name) {
-  if (name.length < 1) {
-    return;
-  }
-  var Element, State;
-  if (document.getElementById) // standard
-  {
-    Element = document.getElementById(name);
-  }
-  else if (document.all) // IE 4, 5, beta 6
-  {
-    Element = document.all[name];
-  }
-  else // if (document.layers) // Netscape 4 and older
-  {
-    Element = document.layers[name];
-  }
-  State = Element.style;
-  if (State.display == 'none') {
-    State.display = 'block';
-  }
-  else {
-    State.display = 'none';
-  }
-}
-
-function Expand() {
-  var E = document.getElementsByTagName('div');
-  for (var i = 0; i < E.length; i++)
-  {
-    if (E[i].id.substr(0, 8) == 'TreeDiv-')
-    {
-      var Element, State;
-      if (document.getElementById) // standard
-      {
-        Element = document.getElementById(E[i].id);
-      }
-      else if (document.all) // IE 4, 5, beta 6
-      {
-        Element = document.all[E[i].id];
-      }
-      else // if (document.layers) // Netscape 4 and older
-      {
-        Element = document.layers[E[i].id];
-      }
-      State = Element.style;
-      State.display = 'block';
-    }
-  }
-}
-
-function Collapse()
-{
-  var E = document.getElementsByTagName('div');
-  var First = 1;
-  for (var i = 0; i < E.length; i++)
-  {
-    if (E[i].id.substr(0, 8) == 'TreeDiv-')
-    {
-      var Element, State;
-      if (document.getElementById) // standard
-      {
-        Element = document.getElementById(E[i].id);
-      }
-      else if (document.all) // IE 4, 5, beta 6
-      {
-        Element = document.all[E[i].id];
-      }
-      else // if (document.layers) // Netscape 4 and older
-      {
-        Element = document.layers[E[i].id];
-      }
-      State = Element.style;
-      if (First) {
-        State.display = 'block';
-        First = 0;
-      }
-      else {
-        State.display = 'none';
-      }
-    }
-  }
 }
