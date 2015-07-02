8e9e774fecdf65d2e96b4687babb0d03b0a93fab
Steffen Weber <steffen.weber@tngtech.com>
2015-07-02 15:51:55 +0200

    bucket and nomos agent plugin extend AgentPlugin

--- src/nomos/ui/agent-nomos.php
+++ src/nomos/ui/agent-nomos.php
@@ -1,6 +1,7 @@
 <?php
 /***********************************************************
  Copyright (C) 2008-2013 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2015 Siemens AG
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -14,19 +15,11 @@
  You should have received a copy of the GNU General Public License along
  with this program; if not, write to the Free Software Foundation, Inc.,
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-***********************************************************/
-/*************************************************
- Restrict usage: Every PHP file should have this
- at the very beginning.
- This prevents hacking attempts.
-*************************************************/
+ ***********************************************************/
 
-/**
- * \file agent-nomos.php
- * \brief run the nomos license agent
- */
+use Fossology\Lib\Plugin\AgentPlugin;
 
-class agent_fonomos extends FO_Plugin
+class NomosAgentPlugin extends AgentPlugin
 {
   public function __construct() {
     $this->Name = "agent_nomos";
@@ -35,56 +28,11 @@ class agent_fonomos extends FO_Plugin
 
     parent::__construct();
   }
-  
-  /**
-   * \brief  Register additional menus.
-   */
-  function RegisterMenus() 
+
+  function AgentHasResults($uploadId=0)
   {
-    if ($this->State != PLUGIN_STATE_READY)  return (0); // don't run
-    menu_insert("Agents::" . $this->Title, 0, $this->Name);
+    return CheckARS($uploadId, $this->AgentName, "license scanner", "nomos_ars");
   }
-
-
-  /**
-   * \brief Check if the upload has already been successfully scanned.
-   *
-   * \param $upload_pk
-   *
-   * \returns:
-   * - 0 = no
-   * - 1 = yes, from latest agent version
-   * - 2 = yes, from older agent version 
-   **/
-  function AgentHasResults($upload_pk)
-  {
-    return CheckARS($upload_pk, $this->AgentName, "license scanner", "nomos_ars");
-  } // AgentHasResults()
-
-
-  /**
-   * \brief Queue the nomos agent.
-   *  Before queuing, check if agent needs to be queued.  It doesn't need to be queued if:
-   *  - It is already queued
-   *  - It has already been run by the latest agent version
-   *
-   * \param $job_pk
-   * \param $upload_pk
-   * \param $ErrorMsg - error message on failure
-   * \param $Dependencies - array of plugin names representing dependencies.
-   *        This is for dependencies that this plugin cannot know about ahead of time.
-   *
-   * \returns
-   * - jq_pk Successfully queued
-   * -   0   Not queued, latest version of agent has previously run successfully
-   * -  -1   Not queued, error, error string in $ErrorMsg
-   **/
-  function AgentAdd($job_pk, $upload_pk, &$ErrorMsg, $Dependencies)
-  {
-    $Dependencies[] = "agent_adj2nest";
-    return CommonAgentAdd($this, $job_pk, $upload_pk, $ErrorMsg, $Dependencies);
-  } // AgentAdd()
 }
 
-$NewPlugin = new agent_fonomos;
-?>
+register_plugin(new NomosAgentPlugin());
