b7bdc194a2c2724d52aee9b24eb8167845ff553d
Steffen Weber <steffen.weber@tngtech.com>
2015-07-21 18:03:32 +0200

    test nomos in travis

--- src/nomos/agent_tests/Functional/OneShot-lgpl2.1.php
+++ src/nomos/agent_tests/Functional/OneShot-lgpl2.1.php
@@ -15,15 +15,9 @@
  with this program; if not, write to the Free Software Foundation, Inc.,
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  ***********************************************************/
-/**
- * \brief Perform a one-shot license analysis on a glpv2.1 license
- *
- * License returned should be LGPL_v2.1
- *
- * @version "$Id$"
- *
- * Created on March 1, 2012
- */
+
+use Fossology\Lib\Test\TestInstaller;
+use Fossology\Lib\Test\TestPgDb;
 
 require_once (dirname(dirname(dirname(dirname(__FILE__)))).'/testing/lib/createRC.php');
 
@@ -33,14 +27,37 @@ class OneShotgplv21Test extends PHPUnit_Framework_TestCase
   public $nomos;
   public $gplv21;
 
+  
   function setUp()
   {
+    $this->testDb = new TestPgDb("nomosfun".time());
+    $this->agentDir = dirname(dirname(__DIR__))."/";
+    $this->testdir = dirname(dirname(__DIR__))."/agent_tests/testdata/NomosTestfiles/";
+
+    $sysConf = $this->testDb->getFossSysConf();
+    $this->testInstaller = new TestInstaller($sysConf);
+    $this->testInstaller->init();
+    $this->testInstaller->install($this->agentDir);
+
+    $this->testDb->createSequences(array(), true);
+    $this->testDb->createPlainTables(array(), true);
+    $this->testDb->alterTables(array(), true);
+    
     $this->gplv21 = dirname(dirname(dirname(dirname(__FILE__)))).'/testing/dataFiles/TestData/licenses/gplv2.1';
     $this->assertFileExists($this->gplv21,"OneShotgplv21Test FAILURE! $this->gplv21 not found\n");
-    createRC();
-    $sysconf = getenv('SYSCONFDIR');
-    $this->nomos = $sysconf . '/mods-enabled/nomos/agent/nomos';
+    // createRC();
+    // $sysconf = getenv('SYSCONFDIR');
+    $this->nomos = $sysConf . '/mods-enabled/nomos/agent/nomos';
   }
+
+  public function tearDown()
+  {
+    $this->testInstaller->uninstall($this->agentDir);
+    $this->testInstaller->clear();
+    $this->testInstaller->rmRepo();
+    $this->testDb = null;
+  }
+  
 
   function testOneShotgplv21()
   {
