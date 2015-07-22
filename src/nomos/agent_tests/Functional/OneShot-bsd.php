edb56e9f2a50eee10bbb44c9a7a9be140e116dfc
Steffen Weber <steffen.weber@tngtech.com>
2015-07-22 17:30:55 +0200

    changed nomos tests to use temp installation

--- src/nomos/agent_tests/Functional/OneShot-bsd.php
+++ src/nomos/agent_tests/Functional/OneShot-bsd.php
@@ -1,6 +1,7 @@
 <?php
 /***********************************************************
  Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2015 Siemens AG
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -18,44 +19,31 @@
 /**
  * \brief Perform a one-shot license analysis on a file (include bsd license)
  */
+require_once ('CommonCliTest.php');
 
-require_once (dirname(dirname(dirname(dirname(__FILE__)))).'/testing/lib/createRC.php');
-
-
-class OneShotbsdTest extends PHPUnit_Framework_TestCase
+class OneShotbsdTest extends CommonCliTest
 {
-  public $nomos;
   public $bsd;
-
-  function setUp()
-  {
-    createRC();
-    $sysconf = getenv('SYSCONFDIR');
-    $this->nomos = $sysconf . '/mods-enabled/nomos/agent/nomos';
-  }
 
   function testOneShotBsd()
   {
     $this->bsd = dirname(dirname(dirname(dirname(__FILE__)))).'/testing/dataFiles/TestData/licenses/BSD_style_d.txt';
     $this->assertFileExists($this->bsd,"OneShotbsdTest FAILURE! $this->bsd not found\n");
+    list($output,) = $this->runNomos("",array($this->bsd));
     $bsdlic = "BSD-3-Clause";
-    $last = exec("$this->nomos $this->bsd 2>&1", $out, $rtn);
-    list(,$fname,,,$license) = explode(' ', implode($out));
+    list(,$fname,,,$license) = explode(' ', $output);
     $this->assertEquals($fname, 'BSD_style_d.txt', "Error filename $fname does not equal BSD_style_d.txt");
-    $this->assertEquals($license, $bsdlic);
+    $this->assertEquals(trim($license), $bsdlic);
   }
 
   function testOneShotDnsdigest()
   {
-    $this->bsd = "";
-    $license = "";
-    $out = "";
     $this->bsd = dirname(dirname(dirname(dirname(__FILE__)))).'/testing/dataFiles/TestData/licenses/DNSDigest.c';
     $this->assertFileExists($this->bsd,"OneShotbsdTest FAILURE! $this->bsd not found\n");
+    list($output,) = $this->runNomos("",array($this->bsd));
     $bsdlic = "Apache-2.0,BSD-style,GPL";
-    $last = exec("$this->nomos $this->bsd 2>&1", $out, $rtn);
-    list(,$fname,,,$license) = explode(' ', implode($out));
+    list(,$fname,,,$license) = explode(' ', $output);
     $this->assertEquals($fname, 'DNSDigest.c', "Error filename $fname does not equal DNSDigest.c");
-    $this->assertEquals($license, $bsdlic);
+    $this->assertEquals(trim($license), $bsdlic);
   }
 }
--- src/nomos/agent_tests/Functional/OneShot-lgpl2.1.php
+++ src/nomos/agent_tests/Functional/OneShot-lgpl2.1.php
@@ -1,6 +1,7 @@
 <?php
 /***********************************************************
  Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2015 Siemens AG
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -16,55 +17,19 @@
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  ***********************************************************/
 
-use Fossology\Lib\Test\TestInstaller;
-use Fossology\Lib\Test\TestPgDb;
+require_once ('CommonCliTest.php');
 
-require_once (dirname(dirname(dirname(dirname(__FILE__)))).'/testing/lib/createRC.php');
-
-
-class OneShotgplv21Test extends PHPUnit_Framework_TestCase
+class OneShotgplv21Test extends CommonCliTest
 {
-  public $nomos;
-  public $gplv21;
-
-  
-  function setUp()
+  public function testOneShotgplv21()
   {
-    $this->testDb = new TestPgDb("nomosfun".time());
-    $this->agentDir = dirname(dirname(__DIR__))."/";
-    $this->testdir = dirname(dirname(__DIR__))."/agent_tests/testdata/NomosTestfiles/";
-
-    $sysConf = $this->testDb->getFossSysConf();
-    $this->testInstaller = new TestInstaller($sysConf);
-    $this->testInstaller->init();
-    $this->testInstaller->install($this->agentDir);
-
-    $this->testDb->createSequences(array(), true);
-    $this->testDb->createPlainTables(array(), true);
-    $this->testDb->alterTables(array(), true);
+    $gplv21 = dirname(dirname(dirname(dirname(__FILE__)))).'/testing/dataFiles/TestData/licenses/gplv2.1';
+    $this->assertFileExists($gplv21,"OneShotgplv21Test FAILURE! $gplv21 not found\n");
     
-    $this->gplv21 = dirname(dirname(dirname(dirname(__FILE__)))).'/testing/dataFiles/TestData/licenses/gplv2.1';
-    $this->assertFileExists($this->gplv21,"OneShotgplv21Test FAILURE! $this->gplv21 not found\n");
-    // createRC();
-    // $sysconf = getenv('SYSCONFDIR');
-    $this->nomos = $sysConf . '/mods-enabled/nomos/agent/nomos';
-  }
-
-  public function tearDown()
-  {
-    $this->testInstaller->uninstall($this->agentDir);
-    $this->testInstaller->clear();
-    $this->testInstaller->rmRepo();
-    $this->testDb = null;
-  }
-  
-
-  function testOneShotgplv21()
-  {
-    $last = exec("$this->nomos $this->gplv21 2>&1", $out, $rtn);
-    list(,$fname,,,$license) = explode(' ', implode($out));
+    list($output,) = $this->runNomos("",array($gplv21));
+    list(,$fname,,,$license) = explode(' ', $output);
     $this->assertEquals($fname, 'gplv2.1', "Error filename $fname does not equal gplv2.1");
-    $this->assertEquals($license, 'LGPL-2.1', "Error license does not equal LGPL_v2.1,
+    $this->assertEquals(trim($license), 'LGPL-2.1', "Error license does not equal LGPL_v2.1,
        $license was returned");
   }
 }
