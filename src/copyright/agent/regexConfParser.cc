c14a660494d770b24ca83967e3a42c829d348594
maximilianhuber <mail@maximilian-huber.de>
2015-09-24 15:27:35 +0200

    add regexConf-Parser and -Provider

---
+++ src/copyright/agent/regexConfParser.cc
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2015, Siemens AG
+ * Author: Maximilian Huber
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "regexConfParser.hpp"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+RegexDict readConfStreamToDict(std::istringstream& stream,
+                               const bool isVerbosityDebug)
+{
+  map<string, string> regexDict;
+  for (string line; getline(stream, line); )
+    addRegexToDict(regexDict, line, isVerbosityDebug);
+
+  return regexDict;
+}
+
+RegexDict readConfStreamToDict(std::ifstream& stream,
+                               const bool isVerbosityDebug)
+{
+  map<string, string> regexDict;
+  for (string line; getline(stream, line); )
+    addRegexToDict(regexDict, line, isVerbosityDebug);
+  stream.close();
+  return regexDict;
+}
+
+void addRegexToDict(/*in and out*/ RegexDict& regexDict,
+                    const std::string& regexDesc,
+                    const bool isVerbosityDebug)
+{
+  // rx::regex whitespaceLine = rx::regex("^[[:space:]]*$", rx::regex_constants::icase);
+
+  if (regexDesc[0] == '#')
+    return;
+
+  istringstream is_line(regexDesc);
+  string key, value;
+  if (getline(is_line, key, '=')) {
+    if(getline(is_line, value)) {
+      value=replaceTokens(regexDict, value);
+      regexDict[key]=value;
+      if (isVerbosityDebug)
+        cout << "loaded or updated regex definition: " << key << " -> \"" << value << "\"" << endl;
+    } else {
+      // if (! rx::regex_match(regexDesc,whitespaceLine))
+      cout << "empty regex definition in conf: \"" << regexDesc << "\"" << endl;
+    }
+  } else {
+    // if (! rx::regex_match(regexDesc,whitespaceLine))
+    cout << "bad regex definition in conf: \"" << regexDesc << "\"" << endl;
+  }
+}
+
+string replaceTokens(/*in*/ RegexDict& regexDict,
+                     const string& constInput)
+{
+#define RGX_SEPARATOR "@@"
+#define RGX_SEPARATOR_LEN 2
+
+  string input(constInput);
+  stringstream output;
+
+  size_t pos = 0;
+  string token;
+  while ((pos = input.find(RGX_SEPARATOR)) != string::npos) { // find start of token
+    output << input.substr(0, pos);
+    input.erase(0, pos + RGX_SEPARATOR_LEN);
+
+    if ((pos = input.find(RGX_SEPARATOR)) != string::npos) { // find end of token
+      output << regexDict[input.substr(0, pos)];
+      input.erase(0, pos + RGX_SEPARATOR_LEN);
+
+    }else{
+      cout << "uneven number of delimiters: " << constInput << endl;
+    }
+  }
+  output << input;
+  return output.str();
+}
---
+++ src/copyright/agent/regexConfProvider.hpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2015, Siemens AG
+ * Author: Maximilian Huber
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef REGEXCONFPROVIDER_HPP_
+#define REGEXCONFPROVIDER_HPP_
+
+#include "identity.hpp"
+
+#define AGENT_NAME IDENTITY ///< the name of the agent, used to get agent key
+#define AGENT_DESC IDENTITY " agent" ///< what program this is
+#define AGENT_ARS  IDENTITY "_ars"
+
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include "regexConfParser.hpp"
+
+// #include "regex.hpp"
+
+std::string getRegexConfFile(const std::string& identity);
+
+#define DEFAULT_DEBUG_VERBOSITY false
+class RegexConfProvider
+{
+public:
+  static RegexConfProvider* _instance;
+  static RegexConfProvider* instance (const bool isVerbosityDebug = DEFAULT_DEBUG_VERBOSITY)
+  {
+    static RegexConfProviderGuard g;
+    if (!_instance)
+      _instance = new RegexConfProvider (isVerbosityDebug);
+    return _instance;
+  }
+  static void resetInstance()
+  {
+    if(RegexConfProvider::_instance)
+      delete RegexConfProvider::_instance;
+    RegexConfProvider::_instance = 0;
+  }
+
+  std::string getRegexValue(const std::string name,
+                            const std::string key);
+
+  RegexDict getRegexDict(const std::string identity);
+
+  void maybeLoad(const char* identity);
+  void maybeLoad(const std::string identity,
+                 std::istringstream& stream);
+
+private:
+  RegexConfProvider(const bool isVerbosityDebug);
+  ~RegexConfProvider () { }
+  class RegexConfProviderGuard
+  {
+  public:
+    ~RegexConfProviderGuard() {
+      if ( NULL != RegexConfProvider::_instance ) {
+        delete RegexConfProvider::_instance;
+        RegexConfProvider::_instance = NULL;
+      }
+    }
+  };
+
+  std::map<std::string,RegexDict> glblRegexMap;
+
+  bool glblIsVerbosityDebug;
+
+  bool getRegexConfStream(const std::string& identity,
+                          /*out*/ std::ifstream& stream);
+};
+
+#endif /* REGEXCONFPROVIDER_HPP_ */
