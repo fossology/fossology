b49c64a894dc0362b11a888a5dca173bb5f13aef
maximilianhuber <mail@maximilian-huber.de>
2015-09-24 15:39:13 +0200

    Integrate regexConfProvider to regTypes

--- src/copyright/agent/regTypes.cc
+++ src/copyright/agent/regTypes.cc
@@ -18,45 +18,35 @@
 
 #include "regTypes.hpp"
 
+#include <iostream>
+
+#include <map>
+#include "regexConfProvider.hpp"
+
+// #include <string.h>
+
+#include <iostream>
+#include <cstring>
+#include <string>
 
 const char* regAuthor::getType(){
   return "author";
 };
 
-
-#define EMAILRGX  "[\\<\\(]?([\\w\\-\\.\\+]{1,100}@[\\w\\-\\.\\+]{1,100}\\.[a-z]{1,4})[\\>\\)]?"
-#define WEBSITE  "(?:http|https|ftp)\\://[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,4}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\\-\\._\\?\\,\'/\\\\+&amp;%\\$#\\=~])*[^\\.\\,\\)\\(\\s]"
-
-const char* regAuthor::getRegex() {
-// Alternative idea: use a heuristic similar to copyscan.cc
-#define SPACECLS          "[\\t ]"
-#define SPACES            SPACECLS "+"
-#define SPACESALL         "[[:space:]]*"
-#define PUNCT_OR_SPACE    "[[:punct:][:space:]]"
-//#define ABBR_AND_BRACED   "[A-Z]{2,7}\\([^)]+\\)"
-#define ALPHA             "[:alpha:]\u00c0-\u00d6\u00d9-\u00f6\u00f8-\u00ff"
-#define NAME_OR_COMPANY   "(?:[" ALPHA "]+|" EMAILRGX "|" WEBSITE ")"
-#define NAMESLIST         NAME_OR_COMPANY "(?:[\\-, &]+" NAME_OR_COMPANY ")*"
-#define DATE              "((19|20)[[:digit:]]{2,2}|[[:digit:]]{1,2})"
-#define DATESLIST         DATE "(([[:punct:][:space:]-]+)" DATE ")*"
-
- return 
-  "(?:"
-    "(?:(?:author|contributor|maintainer)s?)"
-    "|(?:(?:written|contribut(?:ed|ions?)|maintained|modifi(?:ed|cations?)|put" SPACES "together)" SPACES "by)"
-  ")"
-  "[:]?"
-  SPACESALL
-  NAMESLIST
-  "\\.?";
+const char* regAuthor::getRegex(const bool isVerbosityDebug) {
+  std::string key("AUTHOR");
+  RegexConfProvider::instance(isVerbosityDebug)->maybeLoad("copyright");
+  return RegexConfProvider::instance()->getRegexValue("copyright",key).c_str();
 };
 
 const char* regURL::getType(){
   return "url";
 };
 
-const char* regURL::getRegex() {
- return    "(?:(:?ht|f)tps?\\:\\/\\/[^\\s\\<]+[^\\<\\.\\,\\s])";
+const char* regURL::getRegex(const bool isVerbosityDebug) {
+  std::string key("URL");
+  RegexConfProvider::instance(isVerbosityDebug)->maybeLoad("copyright");
+  return RegexConfProvider::instance()->getRegexValue("copyright",key).c_str();
 };
 
 
@@ -64,8 +54,10 @@ const char* regEmail::getType(){
   return "email";
 };
 
-const char* regEmail::getRegex() {
- return EMAILRGX;
+const char* regEmail::getRegex(const bool isVerbosityDebug) {
+  std::string key("EMAILRGX");
+  RegexConfProvider::instance(isVerbosityDebug)->maybeLoad("copyright");
+  return RegexConfProvider::instance()->getRegexValue("copyright",key).c_str();
 };
 
 
@@ -76,23 +68,9 @@ const  char* regEcc::getType(){
   return "ecc";
 };
 
-const char* regEcc::getRegex() {
- return "("
-    "eccn|tsu|ecc|ccl|wco"
-    "|(export" SPACES "control)"
-    "|(customs)"
-    "|(foreign" SPACES "trade(" SPACES "regulations)?)"
-    "|(commerce" SPACES "control)"
-    "|(country" SPACES "of" SPACES "origin)"
-    "|(export" SPACES "administration)"
-    "|((k|c)rypt)"
-    "|(information" SPACES "security)"
-    "|(encryption)"
-    "|(nuclear|surveillance|military|defense|marine|avionics|laser)"
-    "|(propulsion" SPACES "systems)"
-    "|(space" SPACES "vehicle(s)?)"
-    "|(dual" SPACES "use)"
-   ")"
-   "[^)\n]{0,60}";     // \TODO
+const char* regEcc::getRegex(const bool isVerbosityDebug) {
+  std::string key("ECC");
+  RegexConfProvider::instance(isVerbosityDebug)->maybeLoad("ecc");
+  return RegexConfProvider::instance()->getRegexValue("ecc",key).c_str();
 };
 
