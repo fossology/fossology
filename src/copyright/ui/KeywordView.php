efb144810318597ba5f9dea0c0ed2b664031bdff
Shaheem Azmal M MD <shaheem.azmal@siemens.com>
2015-11-23 12:59:03 +0530

    Changes for micromenu for IP and Keyword agents

--- src/copyright/ui/KeywordView.php
+++ src/copyright/ui/KeywordView.php
@@ -15,9 +15,11 @@
  with this program; if not, write to the Free Software Foundation, Inc.,
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-use Fossology\Lib\Data\Highlight;
 
-require_once('Xpview.php');
+namespace Fossology\Agent\Copyright\UI;
+
+use Fossology\Lib\Data\Highlight;
+use Fossology\Lib\UI\Component\MicroMenu;
 
 class KeywordView extends Xpview
 {
@@ -37,6 +39,31 @@ class KeywordView extends Xpview
     parent::__construct(self::NAME, array(
         self::TITLE => _("Keyword Analysis")
     ));
+  }
+
+  /**
+   * \brief Customize submenus.
+   */
+  function RegisterMenus()
+  {
+    $itemId = GetParm("item", PARM_INTEGER);
+    $textFormat = $this->microMenu->getFormatParameter($itemId);
+    $pageNumber = GetParm("page", PARM_INTEGER);
+    $this->microMenu->addFormatMenuEntries($textFormat, $pageNumber);
+
+    // For all other menus, permit coming back here.
+    $uploadId = GetParm("upload", PARM_INTEGER);
+    if (!empty($itemId) && !empty($uploadId)) {
+      $menuText = "keyword";
+      $tooltipText = "keyword Analysis";
+      $menuPosition = 56;
+      $URI = EccView::NAME . Traceback_parm_keep(array("show", "format", "page", "upload", "item"));
+      $this->microMenu->insert(MicroMenu::TARGET_DEFAULT, $menuText, $menuPosition, $this->getName(), $URI, $tooltipText);
+    }
+    $licId = GetParm("lic", PARM_INTEGER);
+    if (!empty($licId)) {
+      $this->NoMenu = 1;
+    }
   }
 }
 
