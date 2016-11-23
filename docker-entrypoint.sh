d16d497184f3271a0fc27e4b8f19bf1a48bc162f
maximilianhuber <maximilian.huber@tngtech.com>
2016-11-23 14:37:30 +0100

    chore: refactor dockerfiles
    - the dockerfile commands were reordered to prevent unnecessary rebuilds
    - the repository is now a volume
    - the containers run now unpriviledged
    - some commands were moved from the entrypoint to the Dockerfile
    - the docker-compose file is now `version: 2`

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # FOSSology docker-entrypoint script
 # Copyright Siemens AG 2016, fabio.huser@siemens.com
+# Copyright TNG Technology Consulting GmbH 2016, maximilian.huber@tngtech.com
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -9,22 +10,24 @@
 #
 # Description: startup helper script for the FOSSology Docker container
 
+set -e
+
 db_host="localhost"
 db_name="fossology"
 db_user="fossy"
 db_password="fossy"
 
 if [ "$FOSSOLOGY_DB_HOST" ]; then
-	db_host="$FOSSOLOGY_DB_HOST"
+  db_host="$FOSSOLOGY_DB_HOST"
 fi
 if [ "$FOSSOLOGY_DB_NAME" ]; then
-	db_name="$FOSSOLOGY_DB_NAME"
+  db_name="$FOSSOLOGY_DB_NAME"
 fi
 if [ "$FOSSOLOGY_DB_USER" ]; then
-	db_user="$FOSSOLOGY_DB_USER"
+  db_user="$FOSSOLOGY_DB_USER"
 fi
 if [ "$FOSSOLOGY_DB_PASSWORD" ]; then
-	db_password="$FOSSOLOGY_DB_PASSWORD"
+  db_password="$FOSSOLOGY_DB_PASSWORD"
 fi
 
 # Write configuration
@@ -35,6 +38,9 @@ user=$db_user;
 password=$db_password;
 EOM
 
+sed -i 's/address = .*/address = '"${FOSSOLOGY_SCHEDULER_HOST:-localhost}"'/' \
+    /usr/local/etc/fossology/fossology.conf
+
 # Startup DB if needed or wait for external DB
 if [ "$db_host" = 'localhost' ]; then
   echo '*****************************************************'
@@ -42,6 +48,7 @@ if [ "$db_host" = 'localhost' ]; then
   echo 'internal database without persistency will be used.'
   echo 'THIS IS NOT RECOMENDED FOR PRODUCTIVE USE!'
   echo '*****************************************************'
+  sleep 5
   /etc/init.d/postgresql start
 else
   testForPostgres(){
@@ -55,11 +62,28 @@ else
 fi
 
 # Setup environment
-/usr/local/lib/fossology/fo-postinstall
+if [[ $# = 0 || ( $# = 1 && "$1" == "scheduler" ) ]]; then
+  /usr/local/lib/fossology/fo-postinstall --database --licenseref
+fi
 
 # Start Fossology
 echo
 echo 'Fossology initialisation complete; Starting up...'
 echo
-/etc/init.d/fossology start
-/usr/sbin/apache2ctl -D FOREGROUND
+if [ $# -eq 0 ]; then
+  /etc/init.d/fossology start
+  /usr/local/share/fossology/scheduler/agent/fo_scheduler \
+    --log /dev/stdout \
+    --verbose=3 \
+    --reset &
+  /usr/sbin/apache2ctl -D FOREGROUND
+elif [[ $# = 1 && "$1" == "scheduler" ]]; then
+  exec /usr/local/share/fossology/scheduler/agent/fo_scheduler \
+    --log /dev/stdout \
+    --verbose=3 \
+    --reset
+elif [[ $# = 1 && "$1" == "web" ]]; then
+  exec /usr/sbin/apache2ctl -D FOREGROUND
+else
+  exec "$@"
+fi
