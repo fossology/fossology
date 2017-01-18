c7c97c158aa8b3eb2c6e7ca0b0e0ace1c79899f7
maximilianhuber <maximilian.huber@tngtech.com>
2017-01-18 15:20:16 +0100

    fix(test): phpunit-bootstrap doesn't find Hamcrest
    As a quickfix, this adds a fallback in `phpunit-bootstrap.php` which
    will work in all cases.

--- src/phpunit-bootstrap.php
+++ src/phpunit-bootstrap.php
@@ -17,5 +17,14 @@ namespace TestSupport;
 $classLoader = require __DIR__ . '/vendor/autoload.php';
 
 $hamcrestUtilPath = $classLoader->findFile('\Hamcrest\Util');
-require dirname($hamcrestUtilPath) . '/../Hamcrest.php';
-
+if($hamcrestUtilPath != null && $hamcrestUtilPath != "") {
+    $hamcrestPath = dirname($hamcrestUtilPath) . '/..';
+}else{
+    // failed to find folder with classLoader, try fallback
+    $hamcrestPath = __DIR__ . "/vendor/hamcrest/hamcrest-php/hamcrest";
+    if(! is_dir($hamcrestPath) || ! is_file($hamcrestPath . '/Hamcrest.php')){
+        // fallback folder or file does not exist
+        exit(1);
+    }
+}
+require $hamcrestPath . '/Hamcrest.php';
