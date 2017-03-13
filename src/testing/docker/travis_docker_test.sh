9373e5b76e514fc7c694fbc9238f7185912766d8
Max Wittig <max.wittig@siemens.com>
2017-03-13 16:31:50 +0100

    Test Travis build

--- src/testing/docker/travis_docker_test.sh
+++ src/testing/docker/travis_docker_test.sh
@@ -3,4 +3,4 @@ docker build -t fossology/test .
 container_id=`docker run -d fossology/test`
 ip=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $container_id | awk "NR==1{print;exit}"`
 docker ps | grep -q fossology/test
-curl -L -I http://$ip/repo | grep -q "200 OK"
+curl -L -I http://$ip:8081/repo | grep -q "200 OK"
