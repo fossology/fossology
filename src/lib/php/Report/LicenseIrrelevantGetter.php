99254a5e69949e18d120876e99aa224b459523c8
Shaheem Azmal M MD <shaheem.azmal@siemens.com>
2017-11-28 13:33:32 +0530

    feat(unifiedreport): update phpword from v0.12.0 to v0.13.*

--- src/lib/php/Report/LicenseIrrelevantGetter.php
+++ src/lib/php/Report/LicenseIrrelevantGetter.php
@@ -51,7 +51,7 @@ class LicenseIrrelevantGetter extends ClearedGetterCommon
    * @param type $ungrupedStatements
    * @return type
    */
-  protected function groupStatements($ungrupedStatements)
+  protected function groupStatements($ungrupedStatements, $extended, $agentcall)
   {
     $statements = array();
     foreach($ungrupedStatements as $statement){
