aef00705ce1446a87269baa04abed9b912ea685f
Gaurav Mishra <gmishx@gmail.com>
2018-04-27 14:38:44 +0530

    feat(copyright): New JSON hpp version
    feat(install-json-hpp): Updated the version of nlohmann/json from 2.1.1
    to 3.1.2 because of several fixes in the library.
    
    Signed-off-by: Gaurav Mishra <gmishx@gmail.com>

--- install/scripts/install-json-hpp.sh
+++ install/scripts/install-json-hpp.sh
@@ -2,11 +2,11 @@
 
 cd "$(dirname "$0")/../.."
 FINAL_FILE=./src/copyright/agent/json.hpp
-SHASUM=faa2321beb1aa7416d035e7417fcfa59692ac3d8c202728f9bcc302e2d558f57
+SHASUM=fbdfec4b4cf63b3b565d09f87e6c3c183bdd45c5be1864d3fcb338f6f02c1733
 TMP=$(mktemp)
 CHECKSUMFILE=$(mktemp)
 
-curl -s -o "$TMP" -L https://github.com/nlohmann/json/releases/download/v2.1.1/json.hpp
+curl -s -o "$TMP" -L https://github.com/nlohmann/json/releases/download/v3.1.2/json.hpp
 echo "$SHASUM $TMP" > "$CHECKSUMFILE"
 sha256sum --quiet -c "$CHECKSUMFILE"
 SUCCESS=$?
