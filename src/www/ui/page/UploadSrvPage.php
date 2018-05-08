196731fb004cffcb1ff50332b50b0d5c725e0b81
Gaurav Mishra <gmishx@gmail.com>
2018-05-08 11:04:52 +0530

    fix(uploadSrv): Copyright statement fix
    fix(UploadSrvPage): Fix copyright year from 2018 to 2015, 2018
    fix(upload_srv): Fix copyright year from 2014-2018 to 2014-2015,
    2018
    
    Signed-off-by: Gaurav Mishra <gmishx@gmail.com>

--- src/www/ui/page/UploadSrvPage.php
+++ src/www/ui/page/UploadSrvPage.php
@@ -1,7 +1,7 @@
 <?php
 /***********************************************************
  Copyright (C) 2008-2014 Hewlett-Packard Development Company, L.P.
- Copyright (C) 2018 Siemens AG
+ Copyright (C) 2015, 2018 Siemens AG
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
