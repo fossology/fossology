ee154ea99b51fac5f7eb4a097af7870dcd6481f8
Maximilian Huber <mail@maximilian-huber.de>
2018-08-27 14:01:39 +0200

    test(monk): add more unit and functional tests for monk
    especially for serialization and CLI_OFFLINE

--- src/monk/agent_tests/Functional/cliTest.php
+++ src/monk/agent_tests/Functional/cliTest.php
@@ -29,6 +29,14 @@ class MonkCliTest extends \PHPUnit_Framework_TestCase
   /** @var string */
   private $testDataDir;
 
+  public static function providerWhetherToUseStandalone()
+  {
+    return array(
+      array(FALSE), // not standalone
+      array(TRUE)   // standalone
+    );
+  }
+
   protected function setUp()
   {
     $this->testDataDir = dirname(__DIR__)."/testlicenses";
@@ -43,8 +51,16 @@ class MonkCliTest extends \PHPUnit_Framework_TestCase
     $this->dbManager = null;
   }
 
-  private function runMonk($args="", $files=array())
+  private function runMonk($args="", $files=array(), $standalone=FALSE)
   {
+    if($standalone) {
+      $temporaryKB = tempnam("/tmp", "monk.knowledgebase");
+      list($output,$retCode) = $this->runMonk("-s $temporaryKB");
+      $this->assertEquals(0, $retCode, "monk failed to save the knowledgebase to $temporaryKB: ".$output);
+      $result = $this->runMonk("-k $temporaryKB $args", $files);
+      unlink($temporaryKB);
+      return $result;
+    }
     $sysConf = $this->testDb->getFossSysConf();
 
     $confFile = $sysConf."/fossology.conf";
@@ -62,7 +78,9 @@ class MonkCliTest extends \PHPUnit_Framework_TestCase
       $args .= " ".escapeshellarg($file);
     }
 
-    $pipeFd = popen("$execDir/$agentName -c $sysConf $args", "r");
+    $cmd = "$execDir/$agentName -c $sysConf $args";
+    echo "run: $cmd\n";
+    $pipeFd = popen($cmd, "r");
     $this->assertTrue($pipeFd !== false, 'running monk failed');
 
     $output = "";
@@ -88,11 +106,14 @@ class MonkCliTest extends \PHPUnit_Framework_TestCase
     $this->testDb->insertData_license_ref(1<<10);
   }
 
-  public function testRunMonkScan()
+  /**
+   * @dataProvider providerWhetherToUseStandalone
+   */
+  public function testRunMonkScan($standalone)
   {
     $this->setUpTables();
 
-    list($output,$retCode) = $this->runMonk("", array($this->testDataDir."/expectedFull/Apache-2.0"));
+    list($output,$retCode) = $this->runMonk("", array($this->testDataDir."/expectedFull/Apache-2.0"), $standalone);
 
     $this->assertEquals(0, $retCode, 'monk failed: '.$output);
 
@@ -130,13 +151,16 @@ class MonkCliTest extends \PHPUnit_Framework_TestCase
     }
   }
 
-  public function testRunMultipleMonkScansFulls()
+  /**
+   * @dataProvider providerWhetherToUseStandalone
+   */
+  public function testRunMultipleMonkScansFulls($standalone)
   {
     $this->setUpTables();
 
     $testFiles = glob($this->testDataDir."/expectedFull/*");
 
-    list($output,$retCode) = $this->runMonk("", $testFiles);
+    list($output,$retCode) = $this->runMonk("", $testFiles, $standalone);
 
     $this->assertEquals(0, $retCode, 'monk failed: '.$output);
 
@@ -151,13 +175,16 @@ class MonkCliTest extends \PHPUnit_Framework_TestCase
     $lines, $testFiles);
   }
 
-  public function testRunMultipleMonkScansDiff()
+  /**
+   * @dataProvider providerWhetherToUseStandalone
+   */
+  public function testRunMultipleMonkScansDiff($standalone)
   {
     $this->setUpTables();
 
     $testFiles = glob($this->testDataDir."/expectedDiff/*");
 
-    list($output,$retCode) = $this->runMonk("", $testFiles);
+    list($output,$retCode) = $this->runMonk("", $testFiles, $standalone);
 
     $this->assertEquals(0, $retCode, 'monk failed: '.$output);
 
@@ -171,33 +198,32 @@ class MonkCliTest extends \PHPUnit_Framework_TestCase
     $lines, $testFiles);
   }
 
-  public function testRunMonkHelpMode()
+  /**
+   * @dataProvider providerWhetherToUseStandalone
+   */
+  public function testRunMonkHelpMode($standalone)
   {
     $this->setUpTables();
 
-    list($output,$retCode) = $this->runMonk("-h", array());
+    list($output,$retCode) = $this->runMonk("-h", array(), $standalone);
 
-    $this->assertEquals(3, $retCode, 'monk failed: '.$output);
+    $this->assertEquals(0, $retCode, 'monk failed: '.$output);
 
-    $expectedOutputRgx = "/Usage: .*\/monk \[options\] -- \[file \[file \[\.\.\.\]\]
-  -h   :: help \(print this message\), then exit\.
-  -c   :: specify the directory for the system configuration\.
-  -v   :: verbose output\.
-  -J   :: JSON output.
-  file :: scan file and print licenses detected within it\.
-  no file :: process data from the scheduler\.
-  -V   :: print the version info, then exit\./";
+    $expectedOutputRgx = '/Usage:.*/';
 
     $this->assertRegExp($expectedOutputRgx,$output);
   }
 
-  public function testRunMonkScansWithNegativeMatch()
+  /**
+   * @dataProvider providerWhetherToUseStandalone
+   */
+  public function testRunMonkScansWithNegativeMatch($standalone)
   {
     $this->setUpTables();
 
     $fileName = tempnam(".", "monkCli");
 
-    list($output,$retCode) = $this->runMonk("", array($fileName));
+    list($output,$retCode) = $this->runMonk("", array($fileName), $standalone);
 
     unlink($fileName);
 
@@ -206,14 +232,17 @@ class MonkCliTest extends \PHPUnit_Framework_TestCase
     $this->assertEquals("",$output);
   }
 
-  public function testRunMonkScansWithNegativeMatchVerbose()
+  /**
+   * @dataProvider providerWhetherToUseStandalone
+   */
+  public function testRunMonkScansWithNegativeMatchVerbose($standalone)
   {
     $this->setUpTables();
 
     $fileName = tempnam(".", "monkCli");
     $testFiles = array($fileName);
 
-    list($output,$retCode) = $this->runMonk("-v", $testFiles);
+    list($output,$retCode) = $this->runMonk("-v", $testFiles, $standalone);
 
     unlink($fileName);
 
---
+++ src/monk/agent_tests/Unit/test_serialize.c
@@ -0,0 +1,116 @@
+/*
+Author: Maximilian Huber
+Copyright (C) 2018, TNG Technology Consulting GmbH
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <libfocunit.h>
+
+#include "serialize.h"
+#include "monk.h"
+#include "match.h"
+#include "license.h"
+#include "libfocunit.h"
+
+Licenses* getNLicensesWithText2(int count, ...) {
+  GArray* licenseArray = g_array_new(TRUE, FALSE, sizeof(License));
+  va_list texts;
+  va_start(texts, count);
+  for (int i = 0; i < count; i++) {
+    char* text = g_strdup(va_arg(texts, char*));
+    License license;
+    license.refId = i;
+    license.shortname = g_strdup_printf("%d-testLic", i);
+    license.tokens = tokenize(text, "^" );
+
+    g_array_append_val(licenseArray, license);
+    g_free(text);
+  }
+  va_end(texts);
+
+  return buildLicenseIndexes(licenseArray, 1, 0);
+}
+
+Licenses* roundtrip(Licenses* licenses) {
+  FILE *out, *in;
+  size_t size;
+  char *ptr;
+  out = open_memstream(&ptr, &size);
+  if (out == NULL){
+    return NULL;
+  }
+
+  serialize(licenses, out);
+  fclose(out);
+
+  in = fmemopen(ptr, size, "r");
+  Licenses* lics = deserialize(in, MIN_ADJACENT_MATCHES, MAX_LEADING_DIFF);
+
+  free(ptr);
+
+  return lics;
+}
+
+void assert_Token(Token* token1, Token* token2) {
+  CU_ASSERT_EQUAL(token1->length, token2->length);
+  CU_ASSERT_EQUAL(token1->removedBefore, token2->removedBefore);
+  CU_ASSERT_EQUAL(token1->hashedContent, token2->hashedContent);
+}
+
+void assert_License(License* lic1, License* lic2) {
+  CU_ASSERT_EQUAL(lic1->refId, lic2->refId);
+  CU_ASSERT_STRING_EQUAL(lic1->shortname, lic2->shortname);
+  CU_ASSERT_EQUAL(lic1->tokens->len, lic2->tokens->len);
+
+  for (guint i = 0; i < lic1->tokens->len; i++) {
+    Token* tokenFrom1 = tokens_index(lic1->tokens, i);
+    Token* tokenFrom2 = tokens_index(lic2->tokens, i);
+
+    assert_Token(tokenFrom1, tokenFrom2);
+  }
+}
+
+void assert_Licenses(Licenses* lics1, Licenses* lics2) {
+  CU_ASSERT_EQUAL(lics1->licenses->len, lics2->licenses->len);
+
+  for (guint i = 0; i < lics1->licenses->len; i++) {
+    License* licFrom1 = license_index(lics1->licenses, i);
+    License* licFrom2 = license_index(lics2->licenses, i);
+
+    assert_License(licFrom1, licFrom2);
+  }
+}
+
+void test_roundtrip_one() {
+  Licenses* licenses = getNLicensesWithText2(1,"a b cde f");
+  Licenses* returnedLicenses = roundtrip(licenses);
+
+  assert_Licenses(licenses, returnedLicenses);
+}
+
+void test_roundtrip() {
+  Licenses* licenses = getNLicensesWithText2(6, "a^b", "a^b^c^d", "d", "e", "f", "e^f^g");
+  Licenses* returnedLicenses = roundtrip(licenses);
+
+  assert_Licenses(licenses, returnedLicenses);
+}
+
+CU_TestInfo serialize_testcases[] = {
+  {"Test roundtrip with empty:", test_roundtrip_one},
+  {"Test roundtrip with some licenses with tokens:", test_roundtrip},
+  CU_TEST_INFO_NULL
+};
