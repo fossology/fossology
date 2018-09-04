cc232789a24aa08fd2fd8f244f77e9a079d003d0
Toussaint Nicolas <nicolas1.toussaint@orange.com>
2018-09-04 15:33:54 +0200

    fix(install-json-hpp.sh) make json-hpp download more robust to network errors: - do not download file if already available - use curl retry options - show proper error if download failed
    Signed-off-by: Toussaint Nicolas <nicolas1.toussaint@orange.com>

--- install/scripts/install-json-hpp.sh
+++ install/scripts/install-json-hpp.sh
@@ -2,20 +2,38 @@
 
 cd "$(dirname "$0")/../.."
 FINAL_FILE=./src/copyright/agent/json.hpp
-SHASUM=fbdfec4b4cf63b3b565d09f87e6c3c183bdd45c5be1864d3fcb338f6f02c1733
-TMP=$(mktemp)
-CHECKSUMFILE=$(mktemp)
+FILE_SHASUM="faa2321beb1aa7416d035e7417fcfa59692ac3d8c202728f9bcc302e2d558f57"
+FILE_URL="https://github.com/nlohmann/json/releases/download/v2.1.1/json.hpp"
+CURL_OPTIONS="--max-time 10 --retry 4"
 
-curl -s -o "$TMP" -L https://github.com/nlohmann/json/releases/download/v3.1.2/json.hpp
-echo "$SHASUM $TMP" > "$CHECKSUMFILE"
-sha256sum --quiet -c "$CHECKSUMFILE"
+_check_file() {
+  [[ -r "$2" ]] || return 1
+  local checksum_file=$(mktemp)
+  echo "$1 $2" > "$checksum_file"
+  sha256sum --quiet -c "$checksum_file"
+  local res=$?
+  rm "$checksum_file"
+  return $res
+}
+
+# Check if file is already available
+_check_file "$SHASUM" "$FINAL_FILE" && exit 0
+
+TMP=$(mktemp)
+curl -s $CURL_OPION -o "$TMP" -L $FILe_URL
 SUCCESS=$?
+
+if [[ $SUCCESS -ne 0 ]]
+then
+  echo "Failed to download file $FINAL_FILE"
+else
+  _check_file "$SHASUM" "$TMP"
+  SUCCESS=$?
+fi
 
 if [[ $SUCCESS -eq 0 ]]
 then
   mv "$TMP" "$FINAL_FILE"
 fi
-
-rm "$CHECKSUMFILE"
 
 exit $SUCCESS
