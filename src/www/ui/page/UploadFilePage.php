5bb92d51eeb5497928ff6bb38af2a56edea50cc2
Christophe Requillart <christophe.requillart@atos.net>
2019-01-04 14:40:22 +0100

    feat(scm): ignore scm data when scanning

--- src/www/ui/page/UploadFilePage.php
+++ src/www/ui/page/UploadFilePage.php
@@ -116,11 +116,13 @@ class UploadFilePage extends UploadPageBase
     $public = $request->get('public');
     $publicPermission = ($public == self::PUBLIC_ALL) ? Auth::PERM_READ : Auth::PERM_NONE;
 
+    $scm = intval($request->get('scm') == 1) ? 't' : 'f';
+
     /* Create an upload record. */
     $uploadMode = (1 << 3); // code for "it came from web upload"
     $userId = Auth::getUserId();
     $groupId = Auth::getGroupId();
-    $uploadId = JobAddUpload($userId, $groupId, $originalFileName, $originalFileName, $description, $uploadMode, $folderId, $publicPermission);
+    $uploadId = JobAddUpload($userId, $groupId, $originalFileName, $originalFileName, $description, $uploadMode, $folderId, $publicPermission, $scm);
 
     if (empty($uploadId))
     {
