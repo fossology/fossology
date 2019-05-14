af3f5738db262a6b2a088a78eef3424301c63c1d
Gaurav Mishra <mishra.gaurav@siemens.com>
2019-05-14 10:02:42 +0530

    feat(license): Provide predefined license comments
    Provide a drop-down to user to pick a license comment from a predefined
    set of standard license comments.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

---
+++ src/lib/php/Dao/LicenseStdCommentDao.php
@@ -0,0 +1,244 @@
+<?php
+/***********************************************************
+ * Copyright (C) 2019 Siemens AG
+ * Author: Gaurav Mishra <mishra.gaurav@siemens.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+/**
+ * @file
+ * DAO layer for license_std_comment table.
+ */
+namespace Fossology\Lib\Dao;
+
+use Fossology\Lib\Auth\Auth;
+use Fossology\Lib\Db\DbManager;
+
+/**
+ * @class LicenseStdCommentDao
+ * DAO layer for license_std_comment table.
+ */
+class LicenseStdCommentDao
+{
+  /** @var DbManager $dbManager
+   * DB manager in use */
+  private $dbManager;
+
+  function __construct(DbManager $dbManager)
+  {
+    $this->dbManager = $dbManager;
+  }
+
+  /**
+   * Get all the comments and their name stored in the DB, sorted by their ID
+   * in ascending order.
+   * @param boolean $skipNotSet Skip the entries where the name is not changed
+   *        or are disabled.
+   * @return array All comments from the DB
+   */
+  public function getAllComments($skipNotSet = false)
+  {
+    $where = "";
+    if ($skipNotSet) {
+      $where = "WHERE name <> 'not-set' AND is_enabled = TRUE";
+    }
+    $sql = "SELECT lsc_pk, name, comment, is_enabled " .
+      "FROM license_std_comment $where " .
+      "ORDER BY lsc_pk ASC;";
+    return $this->dbManager->getRows($sql);
+  }
+
+  /**
+   * Update single comment
+   * @param int $commentPk     The comment id
+   * @param string $newName    New name of the comment
+   * @param string $newComment Updated comment
+   * @return boolean True if the comments are updated, false otherwise.
+   */
+  function updateComment($commentPk, $newName, $newComment)
+  {
+    if (!Auth::isAdmin()) {
+      // Only admins can update the comments.
+      return false;
+    }
+    $this->isCommentIdValid($commentPk);
+
+    $userFk = Auth::getUserId();
+
+    $sql = "UPDATE license_std_comment " .
+      "SET name = $2, comment = $3, updated = NOW(), user_fk = $4 " .
+      "WHERE lsc_pk = $1 " .
+      "RETURNING 1 AS updated;";
+    $row = $this->dbManager->getSingleRow($sql,
+      [$commentPk, $newName, $newComment, $userFk]);
+    return $row['updated'] == 1;
+  }
+
+  /**
+   * Insert a new comment
+   *
+   * @param string $name Name of the comment
+   * @param string $comment Comment
+   * @return int New comment ID if inserted successfully, -1 otherwise or -2 in
+   *         case of exception.
+   */
+  function insertComment($name, $comment)
+  {
+    if (! Auth::isAdmin()) {
+      // Only admins can add comments.
+      return -1;
+    }
+
+    $name = trim($name);
+    $comment = trim($comment);
+
+    if (empty($name) || empty($comment)) {
+      // Cannot insert empty fields.
+      return -1;
+    }
+
+    $userFk = Auth::getUserId();
+
+    $params = [
+      'name' => $name,
+      'comment' => $comment,
+      'user_fk' => $userFk
+    ];
+    $statement = __METHOD__ . ".insertNewLicStdComment";
+    $returning = "lsc_pk";
+    $returnVal = -1;
+    try {
+      $returnVal = $this->dbManager->insertTableRow("license_std_comment",
+        $params, $statement, $returning);
+    } catch (\Exception $e) {
+      $returnVal = -2;
+    }
+    return $returnVal;
+  }
+
+  /**
+   * @brief Update the comments based only on the values provided.
+   *
+   * Takes an array as input and update only the fields passed.
+   * @param array $commentArray Associative array with comment id as the index,
+   *        name and comment as child index with corresponding values.
+   * @return int Count of values updated
+   * @throws \UnexpectedValueException If an entry does not contain any field,
+   *         throws unexpected value exception.
+   */
+  function updateCommentFromArray($commentArray)
+  {
+    if (!Auth::isAdmin()) {
+      // Only admins can update the comments.
+      return false;
+    }
+
+    $userFk = Auth::getUserId();
+    $updated = 0;
+
+    foreach ($commentArray as $commentPk => $comment) {
+      if (count($comment) < 1 ||
+        (! array_key_exists("name", $comment) &&
+        ! array_key_exists("comment", $comment))) {
+        throw new \UnexpectedValueException(
+          "At least name or comment is " . "required for entry " . $commentPk);
+      }
+      $this->isCommentIdValid($commentPk);
+      $statement = __METHOD__;
+      $params = [$commentPk, $userFk];
+      $updateStatement = [];
+      if (array_key_exists("name", $comment)) {
+        $params[] = $comment["name"];
+        $updateStatement[] = "name = $" . count($params);
+        $statement .= ".name";
+      }
+      if (array_key_exists("comment", $comment)) {
+        $params[] = $comment["comment"];
+        $updateStatement[] = "comment = $" . count($params);
+        $statement .= ".comment";
+      }
+      $sql = "UPDATE license_std_comment " .
+        "SET updated = NOW(), user_fk = $2, " . join(",", $updateStatement) .
+        "WHERE lsc_pk = $1 " .
+        "RETURNING 1 AS updated;";
+      $retVal = $this->dbManager->getSingleRow($sql, $params, $statement);
+      $updated += intval($retVal);
+    }
+    return $updated;
+  }
+
+  /**
+   * Get the comment for the given comment id
+   * @param int $commentPk The comment id
+   * @return string|null Comment from the DB (if set, null otherwise)
+   */
+  function getComment($commentPk)
+  {
+    $this->isCommentIdValid($commentPk);
+    $sql = "SELECT comment FROM license_std_comment " . "WHERE lsc_pk = $1;";
+    $statement = __METHOD__ . ".getComment";
+
+    $comment = $this->dbManager->getSingleRow($sql, [$commentPk], $statement);
+    $comment = $comment['comment'];
+    if (strcasecmp($comment, "null") === 0) {
+      return null;
+    }
+    return $comment;
+  }
+
+  /**
+   * Toggle comment status.
+   *
+   * @param int $commentPk The comment id
+   * @return boolean True if the comment is toggled, false otherwise.
+   */
+  function toggleComment($commentPk)
+  {
+    if (! Auth::isAdmin()) {
+      // Only admins can update the comments.
+      return false;
+    }
+    $this->isCommentIdValid($commentPk);
+
+    $userFk = Auth::getUserId();
+
+    $sql = "UPDATE license_std_comment " .
+      "SET is_enabled = NOT is_enabled, user_fk = $2 " .
+      "WHERE lsc_pk = $1;";
+
+    $this->dbManager->getSingleRow($sql, [$commentPk, $userFk]);
+    return true;
+  }
+
+  /**
+   * Check if the given comment id is an integer and exists in DB.
+   * @param int $commentPk Comment id to check for
+   * @throws \UnexpectedValueException Throw exception in comment id is not int
+   *         or does not exists in DB.
+   */
+  private function isCommentIdValid($commentPk)
+  {
+    if (! is_int($commentPk)) {
+      throw new \UnexpectedValueException("Inavlid comment id");
+    }
+    $sql = "SELECT count(*) AS cnt FROM license_std_comment " .
+      "WHERE lsc_pk = $1;";
+
+    $commentCount = $this->dbManager->getSingleRow($sql, [$commentPk]);
+    if ($commentCount['cnt'] < 1) {
+      // Invalid comment id
+      throw new \UnexpectedValueException("Inavlid comment id");
+    }
+  }
+}
---
+++ src/www/ui/scripts/admin-license-std-comments.js
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2019, Siemens AG
+ *
+ * Author: Gaurav Mishra, <mishra.gaurav@siemens.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+function showTheMessage(message) {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  $("#messageSpace").html(message + "<hr />").fadeIn(500).delay(5000).fadeOut(500);
+}
+
+$(document).ready(function() {
+
+  var form = $('form#adminLicenseCommentForm');
+
+  var t = $("#adminLicenseCommentTable").DataTable({
+    "processing": true,
+    "paginationType": "listbox",
+    "order": [[ 1, 'asc' ]],
+    "autoWidth": false,
+    "columnDefs": [{
+      "createdCell": function (cell) {
+        $(cell).attr("style", "text-align:center");
+      },
+      "searchable": false,
+      "targets": [0]
+    },{
+      "orderable": false,
+      "targets": [0,2,3]
+    },{
+      "orderable": true,
+      "targets": [1]
+    }],
+  });
+
+  t.on('order.dt search.dt', function () {
+    t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+      cell.innerHTML = i+1;
+    });
+  }).draw();
+
+  form.find("input[type=text],textarea").on("change", function(){
+    $(this).addClass("inputChanged");
+  });
+
+  form.submit(function(event) {
+    var updatedFields = form.find(".inputChanged").serializeArray();
+    var insertedFields = form.find(".newCommentInputs").serializeArray();
+    if (updatedFields.length > 0 || insertedFields.length > 0) {
+      var itemsToSend = $.merge(updatedFields, insertedFields);
+      itemsToSend.push({"name": "formUpdated", "value": 1});
+      $.ajax({
+        url : '?mod=admin_license_std_comments',
+        type : 'post',
+        dataType : 'json',
+        data : itemsToSend,
+        success : function(data) {
+          var message = "";
+          if (data.updated == -1) {
+            message = "No comments updated";
+          } else if (data.updated > 0) {
+            form.find(".inputChanged").removeClass("inputChanged");
+            message = "Comments updated succesfully";
+          } else {
+            message = data.updated;
+          }
+          var messageIns = [];
+          if (data.inserted.status != 0) {
+            if (data.inserted.status & 1) {
+              form.find(".newCommentInputs").each(function(){
+                if ($(this).val().trim()) {
+                  $(this).removeClass("newCommentInputs");
+                }
+              });
+              messageIns.push("Comments inserted successfully");
+            }
+            if (data.inserted.status & 1<<1) {
+              messageIns.push("errors during insertion");
+            }
+            if (data.inserted.status & 1<<2) {
+              messageIns.push("exceptions during insertion");
+            }
+          }
+          showTheMessage(message + ".<br />" + messageIns.join(" with some ") + ".");
+        },
+        error : function(data) {
+          showTheMessage(data);
+        }
+      });
+    }
+    event.preventDefault();
+  });
+
+  $("#addStdLicComment").on('click', function(){
+    t.row.add([
+      null,
+      '<input type="text" name="insertStdLicNames[]" ' +
+        'placeholder="Please enter a name for the comment" ' +
+        'class="newCommentInputs" />',
+      '<textarea rows="7" cols="80" name="insertStdLicComments[]" ' +
+        'placeholder="Please enter a comment statement" ' +
+        'class="newCommentInputs"></textarea>',
+      '<input type="checkbox" checked disabled />'
+    ]).draw(false);
+  });
+
+  $(".licStdCommentToggle").change(function(){
+    var changedBox = $(this);
+    var boxName = changedBox.attr("name");
+    var idRegex = /stdLicCommentEnabled\[(\d+)\]/g;
+    var commId = idRegex.exec(boxName);
+    commId = commId[1];
+    $.ajax({
+      url : '?mod=ajax_license_std_comments',
+      type : 'post',
+      dataType : 'json',
+      data : {"toggle": commId},
+      success : function(data) {
+        if (data.status != true) {
+          // Not updated, revert the UI
+          var current = changedBox.prop("checked");
+          changedBox.prop("checked", !current);
+        }
+      }
+    });
+  });
+});
