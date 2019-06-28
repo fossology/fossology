1a961298fb67dfa59213c2610878edeb7ff283aa
Gaurav Mishra <mishra.gaurav@siemens.com>
2019-06-28 11:03:09 +0530

    fix(migration): Make pfile sha calculation separate script
    1. Make the script to calculate SHA256 of pfile as a separate script
    which can run without fo-postinstall
    2. Make PostgreSQL transactions more atomic.
    3. Optimize update query.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

---
+++ install/fossinit-common.php
@@ -0,0 +1,137 @@
+<?php
+/***********************************************************
+ Copyright (C) 2019 Siemens AG
+ Author: Gaurav Mishra <mishra.gaurav@siemens.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************/
+
+/**
+ * @file
+ * @brief Common functions required by init scripts
+ */
+
+
+function guessSysconfdir()
+{
+  $rcfile = "fossology.rc";
+  $varfile = dirname(__DIR__).'/variable.list';
+  $sysconfdir = getenv('SYSCONFDIR');
+  if ((false===$sysconfdir) && file_exists($rcfile))
+  {
+    $sysconfdir = file_get_contents($rcfile);
+  }
+  if ((false===$sysconfdir) && file_exists($varfile))
+  {
+    $ini_array = parse_ini_file($varfile);
+    if($ini_array!==false && array_key_exists('SYSCONFDIR', $ini_array))
+    {
+      $sysconfdir = $ini_array['SYSCONFDIR'];
+    }
+  }
+  if (false===$sysconfdir)
+  {
+    $text = _("FATAL! System Configuration Error, no SYSCONFDIR.");
+    echo "$text\n";
+    exit(1);
+  }
+  return $sysconfdir;
+}
+
+
+/**
+ * \brief Determine SYSCONFDIR, parse fossology.conf
+ *
+ * \param $sysconfdir Typically from the caller's -c command line parameter
+ *
+ * \return the $SysConf array of values.  The first array dimension
+ * is the group, the second is the variable name.
+ * For example:
+ *  -  $SysConf[DIRECTORIES][MODDIR] => "/mymoduledir/
+ *
+ * The global $SYSCONFDIR is also set for backward compatibility.
+ *
+ * \Note Since so many files expect directory paths that used to be in pathinclude.php
+ * to be global, this function will define the same globals (everything in the
+ * DIRECTORIES section of fossology.conf).
+ */
+function bootstrap($sysconfdir="")
+{
+  if (empty($sysconfdir))
+  {
+    $sysconfdir = guessSysconfdir();
+    echo "assuming SYSCONFDIR=$sysconfdir\n";
+  }
+
+  $sysconfdir = trim($sysconfdir);
+  $GLOBALS['SYSCONFDIR'] = $sysconfdir;
+
+  /*************  Parse fossology.conf *******************/
+  $ConfFile = "{$sysconfdir}/fossology.conf";
+  if (!file_exists($ConfFile))
+  {
+    $text = _("FATAL! Missing configuration file: $ConfFile");
+    echo "$text\n";
+    exit(1);
+  }
+  $SysConf = parse_ini_file($ConfFile, true);
+  if ($SysConf === false)
+  {
+    $text = _("FATAL! Invalid configuration file: $ConfFile");
+    echo "$text\n";
+    exit(1);
+  }
+
+  /* evaluate all the DIRECTORIES group for variable substitutions.
+   * For example, if PREFIX=/usr/local and BINDIR=$PREFIX/bin, we
+   * want BINDIR=/usr/local/bin
+   */
+  foreach($SysConf['DIRECTORIES'] as $var=>$assign)
+  {
+    $toeval = "\$$var = \"$assign\";";
+    eval($toeval);
+
+    /* now reassign the array value with the evaluated result */
+    $SysConf['DIRECTORIES'][$var] = ${$var};
+    $GLOBALS[$var] = ${$var};
+  }
+
+  if (empty($MODDIR))
+  {
+    $text = _("FATAL! System initialization failure: MODDIR not defined in $SysConf");
+    echo "$text\n";
+    exit(1);
+  }
+
+  //require("i18n.php"); DISABLED until i18n infrastructure is set-up.
+  require_once("$MODDIR/lib/php/common.php");
+  require_once("$MODDIR/lib/php/Plugin/FO_Plugin.php");
+  return $SysConf;
+}
+
+/**
+ * @brief Using bash's read command, read input from STDIN.
+ *
+ * This function runs a new bash shell and execute read command on it with a
+ * timeout set.
+ * @param integer $seconds Timeout in seconds
+ * @param string  $default Default value to return (in case of timeout)
+ * @return string The input read from STDIN or default value.
+ */
+function readlineTimeout($seconds, $default)
+{
+  return trim(shell_exec('bash -c ' .
+    escapeshellarg('fossstdin=' . escapeshellarg($default) .
+    ';read -t ' . ((int)$seconds) . ' fossstdin;echo "$fossstdin"')));
+}
