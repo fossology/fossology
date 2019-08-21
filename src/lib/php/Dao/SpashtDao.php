1609030ceb3cfe66be515b8f71d0d6633603f3ca
vivek kumar <vvksindia@gmail.com>
2019-08-21 23:19:58 +0530

    feat(spasht): Updated UI and Fixed spasht db table
    Signed-off-by: vivek kumar <vvksindia@gmail.com>

---
+++ src/lib/php/Dao/SpashtDao.php
@@ -0,0 +1,114 @@
+<?php
+/*
+Copyright (C) 2008-2012 Hewlett-Packard Development Company, L.P.
+Copyright (C) 2014, Siemens AG
+Author: Johannes Najjar
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+namespace Fossology\Lib\Dao;
+
+use Fossology\Lib\Db\DbManager;
+use Monolog\Logger;
+
+/**
+ * Class AgentDao
+ * @package Fossology\Lib\Dao
+ */
+class SpashtDao
+{
+  /** @var DbManager */
+  private $dbManager;
+
+  /**
+   * @param DbManager $dbManager
+   * @param Logger $logger
+   */
+  function __construct(DbManager $dbManager, Logger $logger)
+  {
+    $this->dbManager = $dbManager;
+    $this->logger = $logger;
+  }
+
+  /**
+   * Add new entry into spasht Agent
+   */
+
+  public function addComponentRevision($revisionBody, $uploadID){
+
+    $statement = __METHOD__.".AddingNewRevision";
+
+    $params = [
+      $revisionBody['body_revision'],
+      $revisionBody['body_namespace'],
+      $revisionBody['body_name'],
+      $revisionBody['body_type'],
+      $revisionBody['body_provider'],
+      $uploadID
+    ];
+
+    $sql = "INSERT INTO spasht ".
+    "(spasht_revision, spasht_namespace, spasht_name, spasht_type, spasht_provider, upload_fk)".
+    " VALUES($1,$2,$3,$4,$5,$6)";
+
+    $returningValue = "spasht_pk";
+
+    return ($this->dbManager->insertPreparedAndReturn($statement, $sql, $params, $returningValue));
+  }
+
+  public function alterComponentRevision($revisionBody, $uploadID){
+    
+    $assocParams = array('spasht_namespace' => $revisionBody['body_namespace'], 'spasht_name' => $revisionBody['body_name'],
+    'spasht_type' => $revisionBody['body_type'], 'spasht_provider' => $revisionBody['body_provider'],
+    'spasht_revision' => $revisionBody['body_revision']);
+
+    $tableName = "spasht";
+    $primaryColumn =  'upload_fk';
+
+    $this->dbManager->updateTableRow($tableName, $assocParams, $primaryColumn, $uploadID);
+    return $uploadID;
+  }
+
+  /**
+   * Get available row in spasht.
+   * Where uploadId is found.
+   */
+
+  public function getComponent($uploadID){
+    $statement = __METHOD__.".CheckUpload";
+
+    $params = [
+      $uploadID
+    ];
+
+    $sql = "SELECT * FROM spasht ".
+    "WHERE upload_fk = $1";
+
+    $row = $this->dbManager->getSingleRow($sql, $params, $statement);
+
+    /**
+     * $row["spasht_revision"] = "spasht_revision";
+     * $row["spasht_namespace"] = "spasht_namespace";
+     * $row["spasht_name"] = "spasht_name";
+     * $row["spasht_type"] = "spasht_type";
+     * $row["spasht_provider"] = "spasht_provider";
+     * $row["upload_fk"] = "upload_fk";
+     */
+
+    return ($row);
+  }
+
+  
+}
--- src/spasht/ui/agent-spasht.php
+++ src/spasht/ui/agent-spasht.php
@@ -1,21 +1,12 @@
 <?php
-######################################################################
-# Copyright (C)
+# Copyright 2019
 # Author: Vivek Kumar<vvksindia@gmail.com>
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# version 2 as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-######################################################################
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
 
 use Fossology\Lib\Auth\Auth;
 use Fossology\Lib\Dao\AgentDao;
@@ -40,6 +31,16 @@ class SpashtAgentPlugin extends AgentPlugin
   function preInstall()
   {
       menu_insert("Agents::" . $this->Title, 0, $this->Name);
+  }
+
+
+   /**
+   * @copydoc Fossology::Lib::Plugin::AgentPlugin::AgentHasResults()
+   * @see Fossology::Lib::Plugin::AgentPlugin::AgentHasResults()
+   */
+  function AgentHasResults($uploadId=0)
+  {
+    return CheckARS($uploadId, $this->AgentName, "spasht scanner", "spasht_ars");
   }
 }
 
