8f4c630102cdf42ad554033ba572eb9f501dd73d
Gaurav Mishra <mishra.gaurav@siemens.com>
2020-02-17 13:55:27 +0530

    feat(ojo): Remove upper limit from license name
    1. Remove upper limit for license name (lower bound remains same).
    2. Add ojoregex.hpp as dependency to EXE to rebuild whenever regex
       changes.
    3. Added Dual-License check in Functional test.
    4. Added bad license name check in Unit test.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- src/ojo/agent_tests/Unit/test_regex.cc
+++ src/ojo/agent_tests/Unit/test_regex.cc
@@ -1,5 +1,5 @@
 /*********************************************************************
-Copyright (C) 2014, Siemens AG
+Copyright (C) 2019, Siemens AG
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -33,6 +33,7 @@ using namespace std;
 class regexTest : public CPPUNIT_NS :: TestFixture {
   CPPUNIT_TEST_SUITE (regexTest);
   CPPUNIT_TEST (regTest);
+  CPPUNIT_TEST (badNameTest);
 
   CPPUNIT_TEST_SUITE_END ();
 
@@ -51,9 +52,10 @@ protected:
 
     const std::string gplLicense = "GPL-2.0";
     const std::string lgplLicense = "LGPL-2.1+";
-    std::string content = "SPDX-License-Identifier: " + gplLicense + " AND " + lgplLicense;
-    boost::regex listRegex (SPDX_LICENSE_LIST, boost::regex_constants::icase);
-    boost::regex nameRegex (SPDX_LICENSE_NAMES, boost::regex_constants::icase);
+    std::string content = "SPDX-License-Identifier: " + gplLicense + " AND "
+        + lgplLicense;
+    boost::regex listRegex(SPDX_LICENSE_LIST, boost::regex_constants::icase);
+    boost::regex nameRegex(SPDX_LICENSE_NAMES, boost::regex_constants::icase);
 
     std::string::const_iterator begin = content.begin();
     std::string::const_iterator end = content.end();
@@ -88,10 +90,77 @@ protected:
     size_t expectedNos = 2;
     size_t actualNos = licensesFound.size();
     // Check if 2 licenses are found
-    CPPUNIT_ASSERT_EQUAL (expectedNos, actualNos);
+    CPPUNIT_ASSERT_EQUAL(expectedNos, actualNos);
     // Check if the result contains the expected string
-    CPPUNIT_ASSERT(std::find(licensesFound.begin(), licensesFound.end(), gplLicense) != licensesFound.end());
-    CPPUNIT_ASSERT(std::find(licensesFound.begin(), licensesFound.end(), lgplLicense) != licensesFound.end());
+    CPPUNIT_ASSERT(
+      std::find(licensesFound.begin(), licensesFound.end(), gplLicense)
+        != licensesFound.end());
+    CPPUNIT_ASSERT(
+      std::find(licensesFound.begin(), licensesFound.end(), lgplLicense)
+        != licensesFound.end());
+  };
+
+  /**
+   * \brief Test regex on a string with bad identifier
+   *
+   * \test
+   * -# Create a test SPDX identifier string with bad license identifier
+   * -# Load the regex patterns
+   * -# Run the regex on the string
+   * -# Check the actual number of matches against expected result
+   * -# Check the actual findings matches the expected licenses
+   */
+  void badNameTest (void) {
+
+    const std::string gplLicense = "GPL-2.0";
+    const std::string badLicense = "AB";
+    std::string content = "SPDX-License-Identifier: " + gplLicense + " AND "
+        + badLicense;
+    boost::regex listRegex(SPDX_LICENSE_LIST, boost::regex_constants::icase);
+    boost::regex nameRegex(SPDX_LICENSE_NAMES, boost::regex_constants::icase);
+
+    std::string::const_iterator begin = content.begin();
+    std::string::const_iterator end = content.end();
+    boost::match_results<std::string::const_iterator> what;
+
+    string licenseList;
+    boost::regex_search(begin, end, what, listRegex);
+    licenseList = what[1].str();
+
+    // Check if only correct license is found
+    CPPUNIT_ASSERT_EQUAL(gplLicense, licenseList);
+
+    // Find the actual license in the list
+    begin = licenseList.begin();
+    end = licenseList.end();
+    list<string> licensesFound;
+
+    while (begin != end)
+    {
+      boost::smatch res;
+      if (boost::regex_search(begin, end, res, nameRegex))
+      {
+        licensesFound.push_back(res[1].str());
+        begin = res[0].second;
+      }
+      else
+      {
+        break;
+      }
+    }
+
+    size_t expectedNos = 1;
+    size_t actualNos = licensesFound.size();
+    // Check if only 1 license is found
+    CPPUNIT_ASSERT_EQUAL(expectedNos, actualNos);
+    // Check if the result contains the expected string
+    CPPUNIT_ASSERT(
+      std::find(licensesFound.begin(), licensesFound.end(), gplLicense)
+        != licensesFound.end());
+    // Check if the result does not contain the bad string
+    CPPUNIT_ASSERT(
+      std::find(licensesFound.begin(), licensesFound.end(), badLicense)
+        == licensesFound.end());
   };
 
 };
