c07cf2ee009774317c29fd10d09e0173105d7576
Shaheem Azmal M MD <shaheem.azmal@siemens.com>
2020-05-15 16:17:55 +0530

    fix(SCM): fix warnings in apache log if SCM is not selected
    Signed-off-by: Shaheem Azmal M MD <shaheem.azmal@siemens.com>

--- src/copyright/ui/agent-ecc.php
+++ src/copyright/ui/agent-ecc.php
@@ -47,7 +47,7 @@ class EccAgentPlugin extends AgentPlugin
    */
   public function AgentAdd($jobId, $uploadId, &$errorMsg, $dependencies=array(), $arguments=null)
   {
-    $unpackArgs = intval($_POST['scm']) == 1 ? '-I' : '';
+    $unpackArgs = intval(@$_POST['scm']) == 1 ? '-I' : '';
     if ($this->AgentHasResults($uploadId) == 1) {
       return 0;
     }
