657a40fd87680be94a4823ef1ed421316fdc88b1
GMishx <gmishx@gmail.com>
2021-06-03 12:24:39 +0000

    chore(gitpod): Update gitpod config
    Update gitpod config to install fossology under gitpod user and inside
    /workspace folders.

--- utils/gitpod-install.sh
+++ utils/gitpod-install.sh
@@ -16,23 +16,34 @@
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-# Make sure apache conf is accessible
-mkdir -p /workspace/apache
-chmod 777 /workspace/apache
+sudo a2dismod php7.4 mpm_prefork
+
+# Install apache and PHP packages inside GitPod as gp is not available in Docker
+sudo apt-get update && sudo apt-get install -y \
+ --option Dpkg::Options::="--force-confold" \
+ apache2 libapache2-mod-php php php-cli php-curl php-mbstring php-pear \
+ php-pgsql php-sqlite3 php-xdebug php-xml php-zip
+sudo ./utils/fo-installdeps -ey
+
+sudo a2dismod mpm_event
+sudo a2enmod php7.4
 
 # Install FOSSology in Gitpod's workspace
-sudo make install PREFIX="/workspace/fossy/code" INITDIR="/workspace/fossy/etc" \
-  REPODIR="/workspace/fossy/srv" LOCALSTATEDIR="/workspace/fossy/var" \
-  APACHE2_SITE_DIR="/workspace/apache" SYSCONFDIR="/workspace/fossy/etc/fossology"
+make install \
+ PREFIX="/workspace/fossy/code" \
+ INITDIR="/workspace/fossy/etc" \
+ REPODIR="/workspace/fossy/srv" \
+ LOCALSTATEDIR="/workspace/fossy/var" \
+ APACHE2_SITE_DIR="/workspace/apache" \
+ SYSCONFDIR="/workspace/fossy/etc/fossology" \
+ PROJECTUSER='gitpod' PROJECTGROUP='gitpod'
+
+# Setup DB for gitpod
+sudo su postgres -c "psql" < install/db/gitpod-fossologyinit.sql
 
 # Run postinstall script
-sudo /workspace/fossy/code/lib/fossology/fo-postinstall || echo "Done"
+sudo -HE /workspace/fossy/code/lib/fossology/fo-postinstall || echo "Done with fo-postinstall"
 
 # Fix the FOSSology path for Apache
-sudo sed -i "s/\/usr\/local\/share/\/workspace\/fossy\/code\/share/" "/workspace/apache/fossology.conf"
-
-# Fix permissions
-sudo mkdir -p /workspace/fossy/srv
-sudo chown gitpod:fossy -R /workspace/fossy/var /workspace/fossy/srv
-sudo chmod 775 -R /workspace/fossy/srv
-sudo chmod 775 -R /workspace/fossy/var
+echo "Fixing path in Apache"
+sed -i "s/\/usr\/local\/share/\/workspace\/fossy\/code\/share/" "/workspace/apache/fossology.conf"
