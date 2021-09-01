b3d8b4789b1a74856900f21256675c96e541b4c6
Shaheem Azmal M MD <shaheem.azmal@siemens.com>
2021-09-01 12:54:09 +0530

    feat(unifiedreport): include assigned to in component clearing section
    Signed-off-by: Shaheem Azmal M MD <shaheem.azmal@siemens.com>

--- src/unifiedreport/agent/unifiedreport.php
+++ src/unifiedreport/agent/unifiedreport.php
@@ -704,9 +704,15 @@ class UnifiedReport extends Agent
     list($contents['licensesMain']['statements'], $contents['licenses']['statements']) = $this->licenseClearedGetter->updateIdentifiedGlobalLicenses($contents['licensesMain']['statements'], $contents['licenses']['statements']);
 
     /* Summery table */
+    $assignedToUserId = $this->uploadDao->getAssignee($uploadId, $groupId);
+    if ($assignedToUserId != 1) {
+      $assignedToUserName = $this->userDao->getUserName($assignedToUserId);
+    } else {
+      $assignedToUserName = "";
+    }
     $reportSummarySection->summaryTable($section, $uploadId, $userName,
       $contents['licensesMain']['statements'], $contents['licenses']['statements'],
-      $contents['licensesHist']['statements'], $contents['otherStatement'], $timestamp, $groupName, $packageUri);
+      $contents['licensesHist']['statements'], $contents['otherStatement'], $timestamp, $groupName, $packageUri, $assignedToUserName);
 
     if (!empty($contents['otherStatement']['ri_unifiedcolumns'])) {
       $unifiedColumns = (array) json_decode($contents['otherStatement']['ri_unifiedcolumns'], true);
