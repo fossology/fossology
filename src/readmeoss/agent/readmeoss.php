11a98d1fd3150bb69bb1e6a3642ccb724266ffc5
rohitpandey49 <rohit.pandey4900@gmail.com>
2022-06-23 17:10:36 +0530

    feat(reuse): implemented REUSE standard
    Signed-off-by: rohitpandey49 <rohit.pandey4900@gmail.com>

--- src/readmeoss/agent/readmeoss.php
+++ src/readmeoss/agent/readmeoss.php
@@ -1,21 +1,10 @@
 <?php
 /*
- * Author: Daniele Fognini, Shaheem Azmal M MD
- * Copyright (C) 2016-2018, Siemens AG
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+ Author: Daniele Fognini, Shaheem Azmal M MD
+ SPDX-FileCopyrightText: © 2016-2018 Siemens AG
+
+ SPDX-License-Identifier: GPL-2.0-only
+*/
 /**
  * @dir readmeoss
  * @brief Readme_OSS agent
