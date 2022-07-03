40dfd5833ff72783143f8c54b49f0f0786aab076
rohitpandey49 <rohit.pandey4900@gmail.com>
2022-07-03 11:43:57 +0530

    feat(reuse): implemented REUSE standard
    Signed-off-by: rohitpandey49 <rohit.pandey4900@gmail.com>

--- src/scheduler/agent_tests/Functional/Functional.py
+++ src/scheduler/agent_tests/Functional/Functional.py
@@ -10,22 +10,9 @@
 # testing that piece of software.
 
 """
-  ==============================================================================
-  Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
- 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  version 2 as published by the Free Software Foundation.
- 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+ SPDX-FileCopyrightText: © 2012 Hewlett-Packard Development Company, L.P.
 
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-  ==============================================================================
+ SPDX-License-Identifier: GPL-2.0-only
 """
 
 from xml.dom.minidom import getDOMImplementation
--- src/scheduler/agent_tests/Unit/utils.h
+++ src/scheduler/agent_tests/Unit/utils.h
@@ -1,2 +1,7 @@
+/*
+ SPDX-FileCopyrightText: © Fossology contributors
+
+ SPDX-License-Identifier: GPL-2.0-only
+*/
 #include <scheduler.h>
 int Prepare_Testing_Data(scheduler_t * scheduler);
