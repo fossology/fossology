b5cf6a9891d92356b0e0e164a47ccb0ff0200a9e
Thanvi pendyala <thanvipendyala194@gmail.com>
2022-07-14 13:05:31 +0530

    fix: required changes

--- src/spasht/ui/ui-spasht.php
+++ src/spasht/ui/ui-spasht.php
@@ -387,7 +387,7 @@ class ui_spasht extends FO_Plugin
     * @param string  $contributionRevision  Revision of selected definition
     * @return array
     */
-  protected function handleContributionToClearlyDefined($coordinatesInfo, $contributionInfo, $contributionRevision) 
+  protected function handleContributionToClearlyDefined($coordinatesInfo, $contributionInfo, $contributionRevision)
   {
     $contributionPacket = array();
 
@@ -416,7 +416,7 @@ class ui_spasht extends FO_Plugin
 
     if (!empty($contributionPacket)) {
       $jsonPacket = json_encode($contributionPacket);
-      return $contributionPacket;
+      return $jsonPacket;
     }
   }
 
