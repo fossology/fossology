9374cf5b55f3c2b99967f01b4da21ae8268cb87f
dushimsam <dushsam@gmail.com>
2023-06-26 07:51:34 +0200

    feat(api): get a list of scanned licenses for an upload
    Signed-off-by: dushimsam <dushsam@gmail.com>

---
+++ src/www/ui/api/Models/ScannedLicense.php
@@ -0,0 +1,159 @@
+<?php
+/*
+ SPDX-FileCopyrightText: © 2023 Samuel Dushimimana <dushsam100@gmail.com>
+ SPDX-License-Identifier: GPL-2.0-only
+*/
+/**
+ * @file
+ * @brief ScannedLicense model
+ */
+namespace Fossology\UI\Api\Models;
+
+class ScannedLicense
+{
+  /**
+   * @var integer $id
+   * License id
+   */
+  private $id;
+  /**
+   * @var string $shortname
+   * License's short name
+   */
+  private $shortname;
+  /**
+   * @var integer $occurence
+   * License's occurrence count
+   */
+  private $occurence;
+  /**
+   * @var integer $unique
+   * License's unique count
+   */
+  private $unique;
+  /**
+   * @var string $spdxName
+   * License's spdx name
+   */
+  private $spdxName;
+
+  /**
+   * @param integer $id
+   * @param string $shortname
+   * @param integer $occurence
+   * @param integer $unique
+   * @param string $spdxName
+   */
+  public function __construct($id, $shortname, $occurence, $unique, $spdxName)
+  {
+    $this->id = $id;
+    $this->shortname = $shortname;
+    $this->occurence = $occurence;
+    $this->unique = $unique;
+    $this->spdxName = $spdxName;
+  }
+
+  /**
+   * @return integer
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * @return string
+   */
+  public function getShortname()
+  {
+    return $this->shortname;
+  }
+
+  /**
+   * @return integer
+   */
+  public function getOccurence()
+  {
+    return $this->occurence;
+  }
+
+  /**
+   * @return integer
+   */
+  public function getUnique()
+  {
+    return $this->unique;
+  }
+
+  /**
+   * @return string
+   */
+  public function getSpdxName()
+  {
+    return $this->spdxName;
+  }
+
+  /**
+   * JSON representation of current scannedLicense
+   * @return string
+   */
+  public function getJSON()
+  {
+    return json_encode($this->getArray());
+  }
+
+  /**
+   * Get ScannedLicense element as associative array
+   * @return array
+   */
+  public function getArray()
+  {
+    return [
+      'id' => $this->getId(),
+      'shortname' => $this->getShortname(),
+      'occurence' => $this->getOccurence(),
+      'unique' => $this->getUnique(),
+      'spdxName' => $this->getSpdxName()
+    ];
+  }
+
+  /**
+   * @param integer $id
+   */
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  /**
+   * @param string $shortname
+   */
+  public function setShortname($shortname)
+  {
+    $this->shortname = $shortname;
+  }
+
+  /**
+   * @param integer $occurence
+   */
+  public function setOccurence($occurence)
+  {
+    $this->occurence = $occurence;
+  }
+
+  /**
+   * @param integer $unique
+   */
+  public function setUnique($unique)
+  {
+    $this->unique = $unique;
+  }
+
+  /**
+   * @param string $spdxName
+   */
+  public function setSpdxName($spdxName)
+  {
+    $this->spdxName = $spdxName;
+  }
+}
--- src/www/ui/api/index.php
+++ src/www/ui/api/index.php
@@ -154,6 +154,7 @@ $app->group('/uploads',
     $app->get('/{id:\\d+}/agents', UploadController::class . ':getAllAgents');
     $app->get('/{id:\\d+}/licenses/edited', UploadController::class . ':getEditedLicenses');
     $app->get('/{id:\\d+}/licenses/reuse', UploadController::class . ':getReuseReportSummary');
+    $app->get('/{id:\\d+}/licenses/scanned', UploadController::class . ':getScannedLicenses');
     $app->get('/{id:\\d+}/download', UploadController::class . ':uploadDownload');
     $app->get('/{id:\\d+}/copyrights', UploadController::class . ':getUploadCopyrights');
     $app->get('/{id:\\d+}/clearing-progress', UploadController::class . ':getClearingProgressInfo');
