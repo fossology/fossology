f55fed9ac6d0b89694f2b342f28421bf17d727f7
dushimsam <dushsam@gmail.com>
2023-08-22 16:36:38 +0200

    feat(SETUP-V2): Support Multiple Versions (V1 & V2)
    Signed-off-by: dushimsam <dushsam@gmail.com>

---
+++ src/www/ui/api/Models/ApiVersion.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ SPDX-FileCopyrightText: © 2023 Samuel Dushimimana <dushsam100@gmail.com>
+
+ SPDX-License-Identifier: GPL-2.0-only
+*/
+/**
+ * @file
+ * @brief ApiVersion enum
+ */
+namespace Fossology\UI\Api\Models;
+
+/**
+ * @class ApiVersion
+ * @brief ApiVersion enum
+ */
+class ApiVersion
+{
+  const V1 = 1;
+  const V2 = 2;
+}
