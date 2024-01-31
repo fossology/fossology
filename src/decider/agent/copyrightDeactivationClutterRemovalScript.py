68ef0202108a8e986f12d7787e51c697f0426e4a
Gaurav Mishra <mishra.gaurav@siemens.com>
2024-01-31 11:17:36 +0530

    chore(decider): fix indent
    Fix some indent issue in decider agent and change how clutter flag works
    in python script from int to a bool.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- src/decider/agent/copyrightDeactivationClutterRemovalScript.py
+++ src/decider/agent/copyrightDeactivationClutterRemovalScript.py
@@ -3,7 +3,7 @@
  Author: Kaushlendra Pratap Singh
  SPDX-FileCopyrightText: © 2021 Kaushlendra Pratap <kaushlendrapratap.9837@gmail.com>
  SPDX-FileCopyrightText: © 2023 Abdelrahman Jamal <abdelrahmanjamal5565@gmail.com>
- 
+
  SPDX-License-Identifier: GPL-2.0-only
 '''
 import pandas as pd
@@ -13,20 +13,20 @@ from safaa.Safaa import *
 def CopyrightFalsePositiveDetection(file, clutter_flag):
   with open(file, 'r') as f:
     df = pd.read_json(f, orient='records')
-    agent = SafaaAgent()
-  df['is_copyright'] = agent.predict(df['content'])
-  if clutter_flag == 1:
-    df['decluttered_content'] = SafaaAgent.declutter(df['content'], df['is_copyright'])
+  agent = SafaaAgent()
+  df['is_copyright'] = agent.predict(df['content'], 0.5)
+  if clutter_flag:
+    df['decluttered_content'] = agent.declutter(df['content'],
+                                                df['is_copyright'])
   print(df.to_json(orient='records'))
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument(
-      "-f", "--file", help="File to be processed", required=True)
+  parser.add_argument("-f", "--file", help="File to be processed",
+                      required=True)
   parser.add_argument("-c", "--clutter",
-                      help="Integer Flag for clutter removal", required=True)
+                      help="Integer Flag for clutter removal", required=False,
+                      action="store_true")
   args = parser.parse_args()
-  file = args.file
-  clutter_flag = int(args.clutter)
-  CopyrightFalsePositiveDetection(file, clutter_flag)
+  CopyrightFalsePositiveDetection(args.file, args.clutter)
