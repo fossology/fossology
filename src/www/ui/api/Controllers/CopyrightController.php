fddd68322141d303b9efa87aa127de17cfc6266d
Divij Sharma <divijs75@gmail.com>
2024-06-27 03:05:50 +0530

    feat(api): New endpoints to get/delete/restore/update scancode email, author, url findings
    Signed-off-by: Divij Sharma <divijs75@gmail.com>

--- src/www/ui/api/Controllers/CopyrightController.php
+++ src/www/ui/api/Controllers/CopyrightController.php
@@ -63,6 +63,9 @@ class CopyrightController extends RestController
   const TYPE_IPRA = 64;
   const TYPE_COPYRIGHT_USERFINDINGS = 128;
   const TYPE_COPYRIGHT_SCANCODE = 256;
+  const TYPE_EMAIL_SCANCODE = 512;
+  const TYPE_URL_SCANCODE = 1024;
+  const TYPE_AUTHOR_SCANCODE = 2048;
 
   public function __construct($container)
   {
@@ -124,6 +127,19 @@ class CopyrightController extends RestController
   }
 
   /**
+   * Get all scancode email findings for a particular upload-tree
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function getFileScanCodeEmail($request, $response, $args)
+  {
+    return $this->getFileCX($request, $response, $args, self::TYPE_EMAIL_SCANCODE);
+  }
+
+  /**
    * Get all urls for a particular upload-tree
    *
    * @param  ServerRequestInterface $request
@@ -137,6 +153,19 @@ class CopyrightController extends RestController
   }
 
   /**
+   * Get all scancode url findings for a particular upload-tree
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function getFileScanCodeUrl($request, $response, $args)
+  {
+    return $this->getFileCX($request, $response, $args, self::TYPE_URL_SCANCODE);
+  }
+
+  /**
    * Get all authors for a particular upload-tree
    *
    * @param  ServerRequestInterface $request
@@ -147,6 +176,19 @@ class CopyrightController extends RestController
   public function getFileAuthor($request, $response, $args)
   {
     return $this->getFileCX($request, $response, $args, self::TYPE_AUTHOR);
+  }
+
+  /**
+   * Get all scancode author findings for a particular upload-tree
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function getFileScanCodeAuthor($request, $response, $args)
+  {
+    return $this->getFileCX($request, $response, $args, self::TYPE_AUTHOR_SCANCODE);
   }
 
   /**
@@ -241,6 +283,18 @@ class CopyrightController extends RestController
   }
 
   /**
+   * Delete scancode email findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function deleteFileScanCodeEmail($request, $response, $args)
+  {
+    return $this->deleteFileCX($args, $response, self::TYPE_EMAIL_SCANCODE);
+  }
+  /**
    * Delete URL for a particular file
    *
    * @param  ServerRequestInterface $request
@@ -254,6 +308,19 @@ class CopyrightController extends RestController
   }
 
   /**
+   * Delete scancode URL findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function deleteFileScanCodeUrl($request, $response, $args)
+  {
+    return $this->deleteFileCX($args, $response, self::TYPE_URL_SCANCODE);
+  }
+
+  /**
    * Delete author for a particular file
    *
    * @param  ServerRequestInterface $request
@@ -264,6 +331,19 @@ class CopyrightController extends RestController
   public function deleteFileAuthor($request, $response, $args)
   {
     return $this->deleteFileCX($args, $response, self::TYPE_AUTHOR);
+  }
+
+  /**
+   * Delete scancode author findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function deleteFileScanCodeAuthor($request, $response, $args)
+  {
+    return $this->deleteFileCX($args, $response, self::TYPE_AUTHOR_SCANCODE);
   }
 
   /**
@@ -358,6 +438,19 @@ class CopyrightController extends RestController
   }
 
   /**
+   * Update scancode email findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function updateFileScanCodeEmail($request, $response, $args)
+  {
+    return $this->updateFileCx($request, $response, $args, self::TYPE_EMAIL_SCANCODE);
+  }
+
+  /**
    * Update URL for a particular file
    *
    * @param  ServerRequestInterface $request
@@ -371,6 +464,19 @@ class CopyrightController extends RestController
   }
 
   /**
+   * Update scancode URL findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function updateFileScanCodeUrl($request, $response, $args)
+  {
+    return $this->updateFileCx($request, $response, $args, self::TYPE_URL_SCANCODE);
+  }
+
+  /**
    * Update author for a particular file
    *
    * @param  ServerRequestInterface $request
@@ -381,6 +487,19 @@ class CopyrightController extends RestController
   public function updateFileAuthor($request, $response, $args)
   {
     return $this->updateFileCx($request, $response, $args, self::TYPE_AUTHOR);
+  }
+
+  /**
+   * Update scancode author findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function updateFileScanCodeAuthor($request, $response, $args)
+  {
+    return $this->updateFileCx($request, $response, $args, self::TYPE_AUTHOR_SCANCODE);
   }
 
   /**
@@ -475,6 +594,19 @@ class CopyrightController extends RestController
   }
 
   /**
+   * Restore scancode email findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function restoreFileScanCodeEmail($request, $response, $args)
+  {
+    return $this->restoreFileCx($args, $response, self::TYPE_EMAIL_SCANCODE);
+  }
+
+  /**
    * Restore URL for a particular file
    *
    * @param  ServerRequestInterface $request
@@ -488,6 +620,19 @@ class CopyrightController extends RestController
   }
 
   /**
+   * Restore scancode URL findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function restoreFileScanCodeUrl($request, $response, $args)
+  {
+    return $this->restoreFileCx($args, $response, self::TYPE_URL_SCANCODE);
+  }
+
+  /**
    * Restore author for a particular file
    *
    * @param  ServerRequestInterface $request
@@ -498,6 +643,19 @@ class CopyrightController extends RestController
   public function restoreFileAuthor($request, $response, $args)
   {
     return $this->restoreFileCx($args, $response, self::TYPE_AUTHOR);
+  }
+
+  /**
+   * Restore scancode author findings for a particular file
+   *
+   * @param  ServerRequestInterface $request
+   * @param  ResponseHelper         $response
+   * @param  array                  $args
+   * @return ResponseHelper
+   */
+  public function restoreFileScanCodeAuthor($request, $response, $args)
+  {
+    return $this->restoreFileCx($args, $response, self::TYPE_AUTHOR_SCANCODE);
   }
 
   /**
@@ -640,13 +798,25 @@ class CopyrightController extends RestController
         $dataType = 'email';
         $agentArs = 'copyright_ars';
         break;
+      case self::TYPE_EMAIL_SCANCODE:
+        $dataType = 'scancode_email';
+        $agentArs = 'scancode_ars';
+        break;
       case self::TYPE_URL:
         $dataType = 'url';
         $agentArs = 'copyright_ars';
         break;
+      case self::TYPE_URL_SCANCODE:
+        $dataType = 'scancode_url';
+        $agentArs = 'scancode_ars';
+        break;
       case self::TYPE_AUTHOR:
         $dataType = 'author';
         $agentArs = 'copyright_ars';
+        break;
+      case self::TYPE_AUTHOR_SCANCODE:
+        $dataType = 'scancode_author';
+        $agentArs = 'scancode_ars';
         break;
       case self::TYPE_ECC:
         $dataType = 'ecc';
@@ -875,11 +1045,23 @@ class CopyrightController extends RestController
       case self::TYPE_EMAIL:
         $dispName = $dataType = 'email';
         break;
+      case self::TYPE_EMAIL_SCANCODE:
+        $dataType = 'scancode_email';
+        $dispName = 'scancode-email';
+        break;
       case self::TYPE_URL:
         $dispName = $dataType = 'url';
         break;
+      case self::TYPE_URL_SCANCODE:
+        $dataType = 'scancode_url';
+        $dispName = 'scancode-url';
+        break;
       case self::TYPE_AUTHOR:
         $dispName = $dataType = 'author';
+        break;
+      case self::TYPE_AUTHOR_SCANCODE:
+        $dataType = 'scancode_author';
+        $dispName = 'scancode-author';
         break;
       case self::TYPE_ECC:
         $dispName = $dataType = 'ecc';
