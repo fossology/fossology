95a9ca8f0afada0b764a58ccc4a1418b5fe87403
Rajul Jha <rajuljha49@gmail.com>
2024-07-20 18:55:10 +0530

    feat(line_numbers) : Add line numbers to CI Scanners output
    Add line numnber for different scans: simple \n count for repo
    and unified diff format for diff and differential scans
    
    Signed-off-by: Rajul Jha <rajuljha49@gmail.com>

--- utils/automation/FoScanner/FormatResults.py
+++ utils/automation/FoScanner/FormatResults.py
@@ -15,6 +15,7 @@ class FormatResult:
 
     :ivar cli_options: CliOptions object
     """
+    cli_options : CliOptions = None
 
     def __init__(self,cli_options:CliOptions):
         self.cli_options = cli_options
@@ -29,31 +30,31 @@ class FormatResult:
       formatted_diff = []
       diff_lines = diff_content.splitlines()
       left = right = 0
-      ll = rl = 0
+      left_num_len = right_num_len = 0
       for line in diff_lines:
         match = re.match(r'^@@ -([0-9]+),([0-9]+) [+]([0-9]+),([0-9]+) @@', line)
         if match:
           left = int(match.group(1))
-          ll = len(match.group(2))
+          left_num_len = len(match.group(2))
           right = int(match.group(3))
-          rl = len(match.group(4))
+          right_num_len = len(match.group(4))
           formatted_diff.append(line)
           continue
-          
+
         if re.match(r'^(---|\+\+\+|[^-+ ])', line):
           formatted_diff.append(line)
           continue
         line_content = line[1:]
         if line.startswith('-'):
-          padding = ' ' * rl
-          formatted_diff.append(f"-{left:<{ll}} {padding}:{line_content}")
+          padding = ' ' * right_num_len
+          formatted_diff.append(f"-{left:<{left_num_len}} {padding}:{line_content}")
           left += 1
         elif line.startswith('+'):
-          padding = ' ' * ll
-          formatted_diff.append(f"+{padding} {right:<{rl}}:{line_content}")
+          padding = ' ' * left_num_len
+          formatted_diff.append(f"+{padding} {right:<{right_num_len}}:{line_content}")
           right += 1
         else:
-          formatted_diff.append(f" {left:<{ll}} {right:<{rl}}:{line_content}")
+          formatted_diff.append(f" {left:<{left_num_len}} {right:<{right_num_len}}:{line_content}")
           left += 1
           right += 1
 
@@ -69,29 +70,48 @@ class FormatResult:
       :return: List of line_numbers found for a given word
       """
       escaped_word = re.escape(diff_string[word_start_byte:word_end_byte])
-      # pattern = re.compile(r'(\d+):.*\b' + escaped_word + r'\b')
       pattern = re.compile(r'(\d+):.*?' + escaped_word)
       matches = pattern.findall(diff_string)
       return matches
 
-    def find_word_line_numbers(self, file_path, words:list) -> dict:
+    def find_word_line_numbers(self, file_path, words:list, key:str) -> dict:
       """
       Find the line number of each word found for a given file path
 
       :param: file_path : str Path of the file to scan
       :param: words: list List of words(ScanResult Objects) to be scanned for
+      :param: Key to scan: 'contents' for copyright and keyword and 'license' for nomos and ojo
       :return: found_words_with_line_number : dict Dictionary of scanned results
                 with key as scanned word and value as list of line_numbers where 
                 it is found.
       """
-      if self.cli_options.repo is True:
-          return {}
       found_words_with_line_number = {}
-      with open(file_path, 'r') as file:
+      if self.cli_options.repo is True:
+        try:
+          with open(file_path, 'rb') as file:
+            binary_data = file.read()
+          string_data = binary_data.decode('utf-8', errors='ignore')
+          for i in range(0,len(words)):
+            line_numbers_list = []
+            line_number = string_data[0:words[i]['start']].count("\n") + 1
+            line_numbers_list.append(str(line_number))
+            found_words_with_line_number[words[i][f'{key}']] = line_numbers_list
+          return found_words_with_line_number
+        except FileNotFoundError:
+          print(f"The file {file_path} does not exist.")
+          return None
+        except IOError as e:
+          print(f"An I/O error occurred: {e}")
+          return None
+        except Exception as e:
+          print(f"An error occurred: {e}")
+          return None
+      else:    
+        with open(file_path, 'r') as file:
           content = file.read()
           for i in range(0,len(words)):
-              line_numbers = self.find_line_numbers(content, words[i]['start'], words[i]['end'])
-              found_words_with_line_number[words[i]['content']] = line_numbers
+            line_numbers = self.find_line_numbers(content, words[i]['start'], words[i]['end'])
+            found_words_with_line_number[words[i][f'{key}']] = line_numbers
       return found_words_with_line_number
 
     def process_files(self, root_dir):
