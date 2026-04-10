bb14412de031a892f3e0c2cd9cfe0194cf59a25f
Shivangisriva <02shivangisrivastava@gmail.com>
2026-04-10 18:04:23 +0000

    test(docker): wait for web and scheduler readiness in cluster test
    Signed-off-by: Shivangisriva <02shivangisrivastava@gmail.com>

--- src/testing/docker/test-cluster.sh
+++ src/testing/docker/test-cluster.sh
@@ -8,8 +8,21 @@ set -o errexit -o nounset -o xtrace
 docker compose build
 docker compose up -d
 
-#### fossology needs up to 15 seconds to startup
-sleep 15
+#### Wait for web and scheduler to be ready instead of fixed sleep
+readonly MAX_WAIT_SECONDS=120
+readonly POLL_SECONDS=3
+
+waited=0
+until curl --silent --location "http://$(docker compose port web 80)/repo/" | grep -q "<title>Getting Started with FOSSology</title>"; do
+	if (( waited >= MAX_WAIT_SECONDS )); then
+		docker compose ps
+		docker compose logs --no-color --tail=100 web scheduler db
+		echo "Timed out waiting for web UI to become ready"
+		exit 1
+	fi
+	sleep "${POLL_SECONDS}"
+	waited=$((waited + POLL_SECONDS))
+done
 
 readonly HOST="$(docker compose port web 80)"
 
@@ -20,6 +33,16 @@ curl --silent --location "http://${HOST}/repo/" | grep -q "<title>Getting Starte
 docker compose exec -T web /usr/local/share/fossology/copyright/agent/copyright -h
 
 #### test whether the scheduler is running
-docker compose exec -T scheduler /usr/local/share/fossology/scheduler/agent/fo_cli -S
+waited=0
+until docker compose exec -T scheduler /usr/local/share/fossology/scheduler/agent/fo_cli -S; do
+	if (( waited >= MAX_WAIT_SECONDS )); then
+		docker compose ps
+		docker compose logs --no-color --tail=100 scheduler db
+		echo "Timed out waiting for scheduler RPC endpoint on port 24693"
+		exit 1
+	fi
+	sleep "${POLL_SECONDS}"
+	waited=$((waited + POLL_SECONDS))
+done
 
 docker compose down
