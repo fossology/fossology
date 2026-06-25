1569174c5a91d7e39f801f1aa168d2e23163ed69
Dietmar Helmut Leher <helmut.leher.ext@vaillant-group.com>
2026-06-25 16:42:48 +0200

    fix(security): remove eval and escape URI output
    Signed-off-by: Dietmar Helmut Leher <helmut.leher.ext@vaillant-group.com>

--- src/demomod/ui/demomod.php
+++ src/demomod/ui/demomod.php
@@ -216,7 +216,15 @@ class ui_demomod extends FO_Plugin
     {
       $text = _("cached");
       $text1 = _("Update");
-      echo " <i>$text</i>   <a href=\"$_SERVER[REQUEST_URI]&updcache=1\"> $text1 </a>";
+      $separator = strpos($_SERVER['REQUEST_URI'], '?') === false ? '?' : '&';
+      $updateHref = htmlspecialchars(
+        $_SERVER['REQUEST_URI'] . $separator . 'updcache=1',
+        ENT_QUOTES | ENT_SUBSTITUTE,
+        'UTF-8'
+      );
+      echo ' <i>' . htmlspecialchars($text, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') .
+        '</i>   <a href="' . $updateHref . '"> ' .
+        htmlspecialchars($text1, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . ' </a>';
     }
     else
     {
